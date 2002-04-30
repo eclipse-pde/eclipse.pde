dafa5d7c1ef6e1168c65a0676568faec27acf99b
Dejan Glozic <dejan>
2002-04-30 22:24:14 +0000

    *** empty log message ***

--- ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/editor/schema/DocSection.java
+++ ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/editor/schema/DocSection.java
@@ -8,12 +8,12 @@ import org.eclipse.jface.resource.JFaceResources;
 import org.eclipse.jface.text.*;
 import org.eclipse.jface.text.rules.RuleBasedPartitioner;
 import org.eclipse.jface.text.source.*;
-import org.eclipse.pde.core.IEditable;
+import org.eclipse.pde.core.*;
+import org.eclipse.pde.internal.core.ischema.*;
 import org.eclipse.pde.internal.core.schema.*;
 import org.eclipse.pde.internal.ui.PDEPlugin;
 import org.eclipse.pde.internal.ui.editor.*;
 import org.eclipse.pde.internal.ui.editor.text.*;
-import org.eclipse.pde.internal.core.ischema.*;
 import org.eclipse.pde.internal.ui.util.PDEProblemFinder;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.*;
@@ -47,134 +47,140 @@ public class DocSection extends PDEFormSection {
 	//private TableTreeViewer topicTree;
 	private FormWidgetFactory factory;
 	private boolean ignoreChange;
+	private boolean updateNeeded;
 
+	public DocSection(PDEFormPage page, IColorManager colorManager) {
+		super(page);
+		setHeaderPainted(false);
+		setAddSeparator(false);
+		String description = PDEPlugin.getResourceString(SECTION_DESC);
+		setDescription(TextUtil.createMultiLine(description, 80));
+		this.colorManager = colorManager;
+		sourceConfiguration = new XMLConfiguration(colorManager);
+		document = new Document();
+		partitioner =
+			new RuleBasedPartitioner(
+				new PDEPartitionScanner(),
+				new String[] { PDEPartitionScanner.XML_TAG, PDEPartitionScanner.XML_COMMENT });
+		partitioner.connect(document);
+		document.setDocumentPartitioner(partitioner);
+	}
+	public void commitChanges(boolean onSave) {
+		handleApply();
+		if (onSave) {
+			setDirty(false);
+			resetButton.setEnabled(false);
+		}
+	}
+	public Composite createClient(Composite parent, FormWidgetFactory factory) {
+		this.factory = factory;
+		Composite container = factory.createComposite(parent);
+		GridLayout layout = new GridLayout();
+		layout.numColumns = 2;
+		layout.marginWidth = factory.BORDER_STYLE == SWT.NULL ? 2 : 0;
+		layout.marginHeight = factory.BORDER_STYLE == SWT.NULL ? 2 : 0;
+		layout.verticalSpacing = 6;
+		container.setLayout(layout);
+		GridData gd;
 
-public DocSection(PDEFormPage page, IColorManager colorManager) {
-	super(page);
-	setHeaderPainted(false);
-	setAddSeparator(false);
-	String description = PDEPlugin.getResourceString(SECTION_DESC);
-	setDescription(TextUtil.createMultiLine(description, 80));
-	this.colorManager = colorManager;
-	sourceConfiguration = new XMLConfiguration(colorManager);
-	document = new Document();
-	partitioner =
-		new RuleBasedPartitioner(
-			new PDEPartitionScanner(),
-			new String[] { PDEPartitionScanner.XML_TAG, PDEPartitionScanner.XML_COMMENT });
-	partitioner.connect(document);
-	document.setDocumentPartitioner(partitioner);
-}
-public void commitChanges(boolean onSave) {
-	handleApply();
-	if (onSave) {
-		setDirty(false);
+		Label label = factory.createLabel(container, null);
+		gd = new GridData();
+		gd.horizontalSpan = 2;
+		label.setLayoutData(gd);
+
+		int comboStyle = SWT.READ_ONLY;
+		if (SWT.getPlatform().equals("motif") == false)
+			comboStyle |= SWT.FLAT;
+		else
+			comboStyle |= SWT.BORDER;
+		sectionCombo = new CCombo(container, comboStyle);
+		sectionCombo.setBackground(factory.getBackgroundColor());
+		gd = new GridData(GridData.FILL_HORIZONTAL);
+		sectionCombo.setLayoutData(gd);
+		factory.createLabel(container, null);
+
+		int styles = SWT.MULTI | SWT.V_SCROLL | SWT.H_SCROLL /*| SWT.WRAP */
+		| factory.BORDER_STYLE;
+		sourceViewer = new SourceViewer(container, null, styles);
+		sourceViewer.configure(sourceConfiguration);
+		sourceViewer.setDocument(document);
+		StyledText styledText = sourceViewer.getTextWidget();
+		styledText.setFont(JFaceResources.getTextFont());
+		if (SWT.getPlatform().equals("motif") == false)
+			factory.paintBordersFor(container);
+		Control[] children = container.getChildren();
+		Control control = children[children.length - 1];
+		gd =
+			new GridData(
+				GridData.FILL_BOTH | GridData.GRAB_HORIZONTAL | GridData.GRAB_VERTICAL);
+		//gd.widthHint = 600;
+		//gd.heightHint = 600;
+		control.setLayoutData(gd);
+		Composite buttonContainer = factory.createComposite(container);
+		layout = new GridLayout();
+		layout.marginHeight = 0;
+		layout.marginWidth = 0;
+		buttonContainer.setLayout(layout);
+		gd = new GridData(GridData.FILL_VERTICAL);
+		buttonContainer.setLayoutData(gd);
+
+		applyButton =
+			factory.createButton(
+				buttonContainer,
+				PDEPlugin.getResourceString(KEY_APPLY),
+				SWT.PUSH);
+		applyButton.setEnabled(false);
+		gd = new GridData(GridData.FILL_HORIZONTAL | GridData.VERTICAL_ALIGN_BEGINNING);
+		applyButton.setLayoutData(gd);
+		applyButton.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {
+				handleApply();
+			}
+		});
+
+		resetButton =
+			factory.createButton(
+				buttonContainer,
+				PDEPlugin.getResourceString(KEY_RESET),
+				SWT.PUSH);
 		resetButton.setEnabled(false);
+		gd = new GridData(GridData.FILL_HORIZONTAL | GridData.VERTICAL_ALIGN_BEGINNING);
+		resetButton.setLayoutData(gd);
+		resetButton.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {
+				handleReset();
+			}
+		});
+		return container;
 	}
-}
-public Composite createClient(Composite parent, FormWidgetFactory factory) {
-	this.factory =factory;
-	Composite container = factory.createComposite(parent);
-	GridLayout layout = new GridLayout();
-	layout.numColumns = 2;
-	layout.marginWidth = factory.BORDER_STYLE==SWT.NULL? 2 : 0;
-	layout.marginHeight = factory.BORDER_STYLE==SWT.NULL? 2 : 0;
-	layout.verticalSpacing = 6;
-	container.setLayout(layout);
-	GridData gd;
-	
-	Label label = factory.createLabel(container, null);
-	gd = new GridData();
-	gd.horizontalSpan = 2;
-	label.setLayoutData(gd);
-
-	int comboStyle = SWT.READ_ONLY;
-	if (SWT.getPlatform().equals("motif")==false)
-	   comboStyle |= SWT.FLAT;
-	else
-	   comboStyle |= SWT.BORDER;
-	sectionCombo = new CCombo(container, comboStyle);
-	sectionCombo.setBackground(factory.getBackgroundColor());
-	gd = new GridData(GridData.FILL_HORIZONTAL);
-	sectionCombo.setLayoutData(gd);
-	factory.createLabel(container, null);
-	
-	int styles = SWT.MULTI | SWT.V_SCROLL | SWT.H_SCROLL /*| SWT.WRAP */ | factory.BORDER_STYLE;
-	sourceViewer = new SourceViewer(container, null, styles);
-	sourceViewer.configure(sourceConfiguration);
-	sourceViewer.setDocument(document);
-	StyledText styledText= sourceViewer.getTextWidget();
-	styledText.setFont(JFaceResources.getTextFont());
-	if (SWT.getPlatform().equals("motif")==false)
-	   factory.paintBordersFor(container);
-	Control [] children = container.getChildren();
-	Control control = children[children.length-1];
-	gd = new GridData(GridData.FILL_BOTH | GridData.GRAB_HORIZONTAL | GridData.GRAB_VERTICAL);
-	//gd.widthHint = 600;
-	//gd.heightHint = 600;
-	control.setLayoutData(gd);
-	Composite buttonContainer = factory.createComposite(container);
-	layout = new GridLayout();
-	layout.marginHeight=0;
-	buttonContainer.setLayout(layout);
-	gd = new GridData(GridData.FILL_VERTICAL);
-	buttonContainer.setLayoutData(gd);
-	
-	applyButton = factory.createButton(buttonContainer, PDEPlugin.getResourceString(KEY_APPLY), SWT.PUSH);
-	applyButton.setEnabled(false);
-	gd = new GridData(GridData.FILL_HORIZONTAL | GridData.VERTICAL_ALIGN_BEGINNING);
-	applyButton.setLayoutData(gd);
-	applyButton.addSelectionListener(new SelectionAdapter() {
-		public void widgetSelected(SelectionEvent e) {
-			handleApply();
+	public boolean doGlobalAction(String actionId) {
+		PDEProblemFinder.fixMe("Global operation mapping must be done better");
+		if (actionId.equals(org.eclipse.ui.IWorkbenchActionConstants.CUT)) {
+			sourceViewer.doOperation(sourceViewer.CUT);
+			return true;
+		} else if (actionId.equals(org.eclipse.ui.IWorkbenchActionConstants.COPY)) {
+			sourceViewer.doOperation(sourceViewer.COPY);
+			return true;
+		} else if (actionId.equals(org.eclipse.ui.IWorkbenchActionConstants.PASTE)) {
+			sourceViewer.doOperation(sourceViewer.PASTE);
+			return true;
+		} else if (actionId.equals(org.eclipse.ui.IWorkbenchActionConstants.DELETE)) {
+			sourceViewer.doOperation(sourceViewer.DELETE);
+			return true;
+		} else if (actionId.equals(org.eclipse.ui.IWorkbenchActionConstants.UNDO)) {
+			sourceViewer.doOperation(sourceViewer.UNDO);
+			return true;
+		} else if (actionId.equals(org.eclipse.ui.IWorkbenchActionConstants.REDO)) {
+			sourceViewer.doOperation(sourceViewer.REDO);
+			return true;
 		}
-	});
-
-	resetButton = factory.createButton(buttonContainer, PDEPlugin.getResourceString(KEY_RESET), SWT.PUSH);
-	resetButton.setEnabled(false);
-	gd = new GridData(GridData.FILL_HORIZONTAL | GridData.VERTICAL_ALIGN_BEGINNING);
-	resetButton.setLayoutData(gd);
-	resetButton.addSelectionListener(new SelectionAdapter() {
-		public void widgetSelected(SelectionEvent e) {
-			handleReset();
-		}
-	});
-	return container;
-}
-public boolean doGlobalAction(String actionId) {
-	PDEProblemFinder.fixMe("Global operation mapping must be done better");
-	if (actionId.equals(org.eclipse.ui.IWorkbenchActionConstants.CUT)) {
-		sourceViewer.doOperation(sourceViewer.CUT);
-		return true;
+		return false;
 	}
-	else if (actionId.equals(org.eclipse.ui.IWorkbenchActionConstants.COPY)) {
-		sourceViewer.doOperation(sourceViewer.COPY);
-		return true;
-	}
-	else if (actionId.equals(org.eclipse.ui.IWorkbenchActionConstants.PASTE)) {
-		sourceViewer.doOperation(sourceViewer.PASTE);
-		return true;
-	}
-	else if (actionId.equals(org.eclipse.ui.IWorkbenchActionConstants.DELETE)) {
-		sourceViewer.doOperation(sourceViewer.DELETE);
-		return true;
-	}
-	else if (actionId.equals(org.eclipse.ui.IWorkbenchActionConstants.UNDO)) {
-		sourceViewer.doOperation(sourceViewer.UNDO);
-		return true;
-	}
-	else if (actionId.equals(org.eclipse.ui.IWorkbenchActionConstants.REDO)) {
-		sourceViewer.doOperation(sourceViewer.REDO);
-		return true;
-	}
-	return false;
-}
-public void expandTo(Object input) {
-	int index = -1;
-	if (input instanceof ISchema) {
-		index = 0;
-	} else
-		if (input instanceof IDocumentSection) {
+	public void expandTo(Object input) {
+		int index = -1;
+		if (input instanceof ISchema) {
+			index = 0;
+		} else if (input instanceof IDocumentSection) {
 			IDocumentSection[] sections = schema.getDocumentSections();
 			for (int i = 0; i < sections.length; i++) {
 				IDocumentSection section = sections[i];
@@ -184,15 +190,14 @@ public void expandTo(Object input) {
 				}
 			}
 		}
-	if (index != -1)
-		sectionCombo.select(index);
-	updateEditorInput(input);
-}
-private String getTopicName(Object object) {
-	if (object instanceof ISchema) {
-		return PDEPlugin.getResourceString(KEY_TOPIC_OVERVIEW);
-	} else
-		if (object instanceof IDocumentSection) {
+		if (index != -1)
+			sectionCombo.select(index);
+		updateEditorInput(input);
+	}
+	private String getTopicName(Object object) {
+		if (object instanceof ISchema) {
+			return PDEPlugin.getResourceString(KEY_TOPIC_OVERVIEW);
+		} else if (object instanceof IDocumentSection) {
 			IDocumentSection section = (IDocumentSection) object;
 			String sectionId = section.getSectionId();
 			if (sectionId.equals(IDocumentSection.EXAMPLES))
@@ -204,88 +209,113 @@ private String getTopicName(Object object) {
 			if (sectionId.equals(IDocumentSection.COPYRIGHT))
 				return PDEPlugin.getResourceString(KEY_TOPIC_COPYRIGHT);
 		}
-	return "?";
-}
-private void handleApply() {
-	if (element != null) {
-		if (element instanceof ISchema)
-			 ((Schema) element).setDescription(document.get());
+		return "?";
+	}
+	private void handleApply() {
+		if (element != null) {
+			if (element instanceof ISchema)
+				 ((Schema) element).setDescription(document.get());
+			else
+				 ((SchemaObject) element).setDescription(document.get());
+		}
+		applyButton.setEnabled(false);
+		resetButton.setEnabled(false);
+	}
+	private void handleReset() {
+		updateEditorInput(element);
+	}
+	public void initialize(Object model) {
+		schema = (ISchema) model;
+		sourceViewer.setEditable(schema.isEditable());
+		initializeSectionCombo();
+		document.addDocumentListener(new IDocumentListener() {
+			public void documentChanged(DocumentEvent e) {
+				if (!ignoreChange && schema.isEditable()) {
+					setDirty(true);
+					((IEditable) schema).setDirty(true);
+					getFormPage().getEditor().fireSaveNeeded();
+				}
+				applyButton.setEnabled(true);
+				resetButton.setEnabled(true);
+			}
+			public void documentAboutToBeChanged(DocumentEvent e) {
+			}
+		});
+		updateEditorInput(schema);
+		schema.addModelChangedListener(this);
+	}
+
+	public void dispose() {
+		schema.removeModelChangedListener(this);
+		super.dispose();
+	}
+
+	private void initializeSectionCombo() {
+		IDocumentSection[] sections = schema.getDocumentSections();
+		sectionCombo.add(getTopicName(schema));
+		for (int i = 0; i < sections.length; i++) {
+			IDocumentSection section = sections[i];
+			sectionCombo.add(getTopicName(section));
+		}
+		sectionCombo.select(0);
+		sectionCombo.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {
+				int index = sectionCombo.getSelectionIndex();
+				if (schema.isEditable() && isDirty()) {
+					handleApply();
+				}
+				if (index == 0)
+					updateEditorInput(schema);
+				else {
+					IDocumentSection[] sections = schema.getDocumentSections();
+					updateEditorInput(sections[index - 1]);
+				}
+			}
+		});
+	}
+
+	private String resolveObjectName(Object object) {
+		if (object instanceof ISchemaObject) {
+			return ((ISchemaObject) object).getName();
+		}
+		return object.toString();
+	}
+	public void setFocus() {
+		sourceViewer.getTextWidget().setFocus();
+	}
+	public void updateEditorInput(Object input) {
+		ignoreChange = true;
+		String text = "";
+		if (input instanceof ISchemaObject) {
+			text = ((ISchemaObject) input).getDescription();
+		}
+		if (text == null)
+			text = "";
 		else
-			 ((SchemaObject) element).setDescription(document.get());
+			text = TextUtil.createMultiLine(text, 60, false);
+		document.set(text);
+		applyButton.setEnabled(false);
+		resetButton.setEnabled(false);
+		element = input;
+		ignoreChange = false;
 	}
-	applyButton.setEnabled(false);
-	resetButton.setEnabled(false);
-}
-private void handleReset() {
-	updateEditorInput(element);
-}
-public void initialize(Object model) {
-	schema = (ISchema) model;
-	sourceViewer.setEditable(schema.isEditable());
-	initializeSectionCombo();
-	document.addDocumentListener(new IDocumentListener() {
-		public void documentChanged(DocumentEvent e) {
-			if (!ignoreChange && schema.isEditable()) {
-				setDirty(true);
-				((IEditable) schema).setDirty(true);
-				getFormPage().getEditor().fireSaveNeeded();
-			}
-			applyButton.setEnabled(true);
-			resetButton.setEnabled(true);
-		}
-		public void documentAboutToBeChanged(DocumentEvent e) {
-		}
-	});
-	updateEditorInput(schema);
-}
 
-private void initializeSectionCombo() {
-	IDocumentSection[] sections = schema.getDocumentSections();
-	sectionCombo.add(getTopicName(schema));
-	for (int i = 0; i < sections.length; i++) {
-		IDocumentSection section = sections[i];
-		sectionCombo.add(getTopicName(section));
-	}
-	sectionCombo.select(0);
-	sectionCombo.addSelectionListener(new SelectionAdapter() {
-		public void widgetSelected(SelectionEvent e) {
-			int index = sectionCombo.getSelectionIndex();
-			if (schema.isEditable() && isDirty()) {
-				handleApply();
-			}
-			if (index == 0)
-				updateEditorInput(schema);
-			else {
-				IDocumentSection[] sections = schema.getDocumentSections();
-				updateEditorInput(sections[index - 1]);
+	public void modelChanged(IModelChangedEvent e) {
+		if (e.getChangeType() == IModelChangedEvent.WORLD_CHANGED) {
+			updateNeeded = true;
+			if (getFormPage().isVisible()) {
+				update();
 			}
 		}
-	});
-}
+	}
 
-private String resolveObjectName(Object object) {
-	if (object instanceof ISchemaObject) {
-		return ((ISchemaObject)object).getName();
+	public void update() {
+		int index = sectionCombo.getSelectionIndex();
+		if (index == 0)
+			updateEditorInput(schema);
+		else {
+			IDocumentSection[] sections = schema.getDocumentSections();
+			updateEditorInput(sections[index - 1]);
+		}
 	}
-	return object.toString();
-}
-public void setFocus() {
-	sourceViewer.getTextWidget().setFocus();
-}
-public void updateEditorInput(Object input) {
-	ignoreChange=true;
-	String text = "";
-	if (input instanceof ISchemaObject) {
-		text = ((ISchemaObject) input).getDescription();
-	}
-	if (text == null)
-		text = "";
-	else
-		text = TextUtil.createMultiLine(text, 60, false);
-	document.set(text);
-	applyButton.setEnabled(false);
-	resetButton.setEnabled(false);
-	element = input;
-	ignoreChange=false;
-}
 }
--- ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/editor/schema/SchemaDocPage.java
+++ ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/editor/schema/SchemaDocPage.java
@@ -4,61 +4,64 @@ package org.eclipse.pde.internal.ui.editor.schema;
  * All Rights Reserved.
  */
 
+import org.eclipse.pde.internal.core.ischema.*;
 import org.eclipse.pde.internal.ui.PDEPlugin;
 import org.eclipse.pde.internal.ui.editor.PDEChildFormPage;
-import org.eclipse.pde.internal.core.ischema.*;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.ui.views.contentoutline.IContentOutlinePage;
 import org.eclipse.update.ui.forms.internal.AbstractSectionForm;
-
 
 public class SchemaDocPage extends PDEChildFormPage {
 	private IContentOutlinePage outlinePage;
 	private Image topicImage;
 	public static final String PAGE_TITLE = "SchemaEditor.DocPage.title";
 
-
-public SchemaDocPage(SchemaFormPage mainPage) {
-	super(mainPage, PDEPlugin.getResourceString(PAGE_TITLE));
-}
-protected AbstractSectionForm createForm() {
-	return new DocForm(this);
-}
-public IContentOutlinePage getContentOutlinePage() {
-	return null;
-}
-private boolean isElementComplete(ISchemaElement element) {
-	if (isObjectComplete(element)) return false;
-	ISchemaType type = element.getType();
-	if (!(type instanceof ISchemaComplexType)) return true;
-	ISchemaComplexType complexType = (ISchemaComplexType)type;
-
-	ISchemaAttribute [] attributes = complexType.getAttributes();
-	for (int i=0; i<attributes.length; i++) {
-		ISchemaAttribute att = attributes[i];
-		if (isObjectComplete(att)==false) return false;
+	public SchemaDocPage(SchemaFormPage mainPage) {
+		super(mainPage, PDEPlugin.getResourceString(PAGE_TITLE));
 	}
-	return true;
-}
-private boolean isMarkupComplete() {
-	ISchema schema = (ISchema)getModel();
-	ISchemaElement [] elements = schema.getElements();
-	for (int i=0; i<elements.length; i++) {
-		ISchemaElement element = elements[i];
-		if (isElementComplete(element)==false) return false;
+	protected AbstractSectionForm createForm() {
+		return new DocForm(this);
 	}
-	return true;
-}
-private boolean isObjectComplete(ISchemaObject object) {
-	String text = object.getDescription();
-	if (text != null)
-		text.trim();
-	return (text != null && text.length() > 0);
-}
-public void update() {
-	super.update();
-}
-public void updateEditorInput(Object object) {
-	((DocForm)getForm()).updateEditorInput(object);
-}
+	public IContentOutlinePage createContentOutlinePage() {
+		return null;
+	}
+
+	private boolean isElementComplete(ISchemaElement element) {
+		if (isObjectComplete(element))
+			return false;
+		ISchemaType type = element.getType();
+		if (!(type instanceof ISchemaComplexType))
+			return true;
+		ISchemaComplexType complexType = (ISchemaComplexType) type;
+
+		ISchemaAttribute[] attributes = complexType.getAttributes();
+		for (int i = 0; i < attributes.length; i++) {
+			ISchemaAttribute att = attributes[i];
+			if (isObjectComplete(att) == false)
+				return false;
+		}
+		return true;
+	}
+	private boolean isMarkupComplete() {
+		ISchema schema = (ISchema) getModel();
+		ISchemaElement[] elements = schema.getElements();
+		for (int i = 0; i < elements.length; i++) {
+			ISchemaElement element = elements[i];
+			if (isElementComplete(element) == false)
+				return false;
+		}
+		return true;
+	}
+	private boolean isObjectComplete(ISchemaObject object) {
+		String text = object.getDescription();
+		if (text != null)
+			text.trim();
+		return (text != null && text.length() > 0);
+	}
+	public void update() {
+		super.update();
+	}
+	public void updateEditorInput(Object object) {
+		((DocForm) getForm()).updateEditorInput(object);
+	}
 }
