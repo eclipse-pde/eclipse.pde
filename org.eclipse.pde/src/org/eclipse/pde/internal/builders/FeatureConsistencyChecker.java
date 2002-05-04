8a3c3760771f5aabf8776e90b24a001f384ab2c2
Dejan Glozic <dejan>
2002-05-04 02:41:14 +0000

    *** empty log message ***

--- org.eclipse.pde/src/org/eclipse/pde/internal/builders/FeatureConsistencyChecker.java
+++ org.eclipse.pde/src/org/eclipse/pde/internal/builders/FeatureConsistencyChecker.java
@@ -17,12 +17,11 @@ import org.eclipse.pde.internal.core.feature.WorkspaceFeatureModel;
 import org.eclipse.pde.internal.core.ifeature.*;
 import org.xml.sax.*;
 
-
 public class FeatureConsistencyChecker extends IncrementalProjectBuilder {
 	public static final String BUILDERS_VERIFYING = "Builders.verifying";
-	public static final String BUILDERS_FEATURE_REFERENCE = "Builders.Component.reference";
+	public static final String BUILDERS_FEATURE_REFERENCE =
+		"Builders.Feature.reference";
 	public static final String BUILDERS_UPDATING = "Builders.updating";
-	public static final String BUILDERS_FEATURE_FOLDER_SYNC = "Builders.Component.folderSync";
 
 	class DeltaVisitor implements IResourceDeltaVisitor {
 		private IProgressMonitor monitor;
@@ -33,7 +32,7 @@ public class FeatureConsistencyChecker extends IncrementalProjectBuilder {
 			IResource resource = delta.getResource();
 
 			if (resource instanceof IProject) {
-				// Only check projects with plugin nature
+				// Only check projects with feature nature
 				IProject project = (IProject) resource;
 				try {
 					return (project.hasNature(PDE.FEATURE_NATURE));
@@ -57,112 +56,91 @@ public class FeatureConsistencyChecker extends IncrementalProjectBuilder {
 		}
 	}
 
-public FeatureConsistencyChecker() {
-	super();
-}
-protected IProject[] build(int kind, Map args, IProgressMonitor monitor)
-	throws CoreException {
-
-	IResourceDelta delta = null;
-	if (kind != FULL_BUILD)
-		delta = getDelta(getProject());
-
-	if (delta == null || kind == FULL_BUILD) {
-		// Full build
-		IProject project = getProject();
-		IPath path = project.getFullPath().append("install.xml");
-		IWorkspace workspace = project.getWorkspace();
-		IFile file = workspace.getRoot().getFile(path);
-		if (file.exists()) {
-			checkFile(file, monitor);
-		}
-	} else {
-		delta.accept(new DeltaVisitor(monitor));
+	public FeatureConsistencyChecker() {
+		super();
 	}
-	return null;
-}
-private void checkFile(IFile file, IProgressMonitor monitor) {
-	String message =
-		PDE.getFormattedMessage(
-			BUILDERS_VERIFYING,
-			file.getFullPath().toString());
-	monitor.subTask(message);
-	PluginErrorReporter reporter = new PluginErrorReporter(file);
-	SAXParser parser = new SAXParser();
-	parser.setErrorHandler(reporter);
-	InputStream source = null;
-	try {
-		source = file.getContents();
-		InputSource inputSource = new InputSource(source);
-		parser.parse(inputSource);
-		if (reporter.getErrorCount() == 0) {
-			testPluginReferences(file, reporter);
+	protected IProject[] build(int kind, Map args, IProgressMonitor monitor)
+		throws CoreException {
+
+		IResourceDelta delta = null;
+		if (kind != FULL_BUILD)
+			delta = getDelta(getProject());
+
+		if (delta == null || kind == FULL_BUILD) {
+			// Full build
+			IProject project = getProject();
+			IFile file = project.getFile("feature.xml");
+			if (file.exists()) {
+				checkFile(file, monitor);
+			}
+		} else {
+			delta.accept(new DeltaVisitor(monitor));
 		}
-	} catch (CoreException e) {
-		PDE.logException(e);
-	} catch (SAXException e) {
-	} catch (IOException e) {
-		PDE.logException(e);
-	} finally {
-		if (source != null) {
-			try {
-				source.close();
-			} catch (IOException e) {
+		return null;
+	}
+	private void checkFile(IFile file, IProgressMonitor monitor) {
+		String message =
+			PDE.getFormattedMessage(BUILDERS_VERIFYING, file.getFullPath().toString());
+		monitor.subTask(message);
+		PluginErrorReporter reporter = new PluginErrorReporter(file);
+		SAXParser parser = new SAXParser();
+		parser.setErrorHandler(reporter);
+		InputStream source = null;
+		try {
+			source = file.getContents();
+			InputSource inputSource = new InputSource(source);
+			parser.parse(inputSource);
+			if (reporter.getErrorCount() == 0) {
+				testPluginReferences(file, reporter);
+			}
+		} catch (CoreException e) {
+			PDE.logException(e);
+		} catch (SAXException e) {
+		} catch (IOException e) {
+			PDE.logException(e);
+		} finally {
+			if (source != null) {
+				try {
+					source.close();
+				} catch (IOException e) {
+				}
+			}
+		}
+		monitor.subTask(PDE.getResourceString(BUILDERS_UPDATING));
+		monitor.done();
+	}
+	private boolean isManifestFile(IFile file) {
+		return file.getName().toLowerCase().equals("feature.xml");
+	}
+	private boolean isValidReference(IFeaturePlugin plugin) {
+		WorkspaceModelManager manager = PDECore.getDefault().getWorkspaceModelManager();
+		IPluginModelBase[] models =
+			plugin.isFragment()
+				? (IPluginModelBase[]) manager.getWorkspaceFragmentModels()
+				: (IPluginModelBase[]) manager.getWorkspacePluginModels();
+		for (int i = 0; i < models.length; i++) {
+			IPluginModelBase model = models[i];
+			if (model.getPluginBase().getId().equals(plugin.getId())) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private void testPluginReferences(IFile file, PluginErrorReporter reporter) {
+		WorkspaceFeatureModel model = new WorkspaceFeatureModel(file);
+		model.load();
+		if (model.isLoaded()) {
+			IFeature feature = model.getFeature();
+			IFeaturePlugin[] plugins = feature.getPlugins();
+			for (int i = 0; i < plugins.length; i++) {
+				IFeaturePlugin plugin = plugins[i];
+				if (isValidReference(plugin) == false) {
+					String message =
+						PDE.getFormattedMessage(BUILDERS_FEATURE_REFERENCE, plugin.getLabel());
+					reporter.reportError(message);
+				}
 			}
 		}
 	}
-	monitor.subTask(PDE.getResourceString(BUILDERS_UPDATING));
-	monitor.done();
-}
-private boolean isManifestFile(IFile file) {
-   return file.getName().toLowerCase().equals("feature.xml");
-}
-private boolean isValidReference(IFeaturePlugin plugin) {
-	WorkspaceModelManager manager =
-		PDECore.getDefault().getWorkspaceModelManager();
-	IPluginModelBase [] models = plugin.isFragment() ? 
-		(IPluginModelBase[])manager.getWorkspaceFragmentModels() :
-		(IPluginModelBase[])manager.getWorkspacePluginModels();
-	for (int i = 0; i < models.length; i++) {
-		IPluginModelBase model = models[i];
-		if (model.getPluginBase().getId().equals(plugin.getId())) {
-			return true;
-		}
-	}
-	return false;
-}
-protected void startupOnInitialize() {
-	super.startupOnInitialize();
-}
-
-private void testPluginReferences(IFile file, PluginErrorReporter reporter) {
-	WorkspaceFeatureModel model = new WorkspaceFeatureModel(file);
-	model.load();
-	if (model.isLoaded()) {
-		IFeature feature = model.getFeature();
-		IFeaturePlugin[] plugins = feature.getPlugins();
-		for (int i = 0; i < plugins.length; i++) {
-			IFeaturePlugin plugin = plugins[i];
-			if (isValidReference(plugin) == false) {
-				String message =
-					PDE.getFormattedMessage(
-						BUILDERS_FEATURE_REFERENCE,
-						plugin.getLabel());
-				reporter.reportError(message);
-			}
-		}
-		String version = feature.getVersion();
-		String id = feature.getId();
-		String expectedFolderName = id+"_"+version;
-		IFolder folder = (IFolder)file.getParent();
-		String realName = folder.getName();
-		if (realName.equals(expectedFolderName)==false) {
-			String message =
-				PDE.getFormattedMessage(
-					BUILDERS_FEATURE_FOLDER_SYNC,
-					realName);
-			reporter.reportError(message);
-		}
-	}
-}
 }
