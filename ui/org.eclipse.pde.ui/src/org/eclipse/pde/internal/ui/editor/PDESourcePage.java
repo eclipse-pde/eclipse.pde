88bc7130e8c4cd44146df171ff683c1d3c151e4a
Dejan Glozic <dejan>
2003-01-20 01:50:38 +0000

    *** empty log message ***

--- ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/editor/PDESourcePage.java
+++ ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/editor/PDESourcePage.java
@@ -22,14 +22,16 @@ import org.eclipse.ui.views.contentoutline.IContentOutlinePage;
 import org.eclipse.ui.views.properties.IPropertySheetPage;
 import org.eclipse.update.ui.forms.internal.IFormPage;
 
-public abstract class PDESourcePage extends AbstractTextEditor implements IPDEEditorPage {
+public abstract class PDESourcePage
+	extends AbstractTextEditor
+	implements IPDEEditorPage {
 	public static final String PAGE_TITLE = "SourcePage.title";
 	public static final String ERROR_MESSAGE = "SourcePage.errorMessage";
 	public static final String ERROR_TITLE = "SourcePage.errorTitle";
 	private IContentOutlinePage outlinePage;
 	private boolean errorMode;
 	private PDEMultiPageEditor editor;
-	private boolean modelNeedsUpdating=false;
+	private boolean modelNeedsUpdating = false;
 	private Control control;
 	private DocumentListener documentListener = new DocumentListener();
 
@@ -43,177 +45,189 @@ public abstract class PDESourcePage extends AbstractTextEditor implements IPDEEd
 		}
 	}
 
-public PDESourcePage(PDEMultiPageEditor editor) {
-	this.editor = editor;
-}
-public boolean becomesInvisible(IFormPage newPage) {
-	if (errorMode || modelNeedsUpdating) {
-		boolean cleanModel = getEditor().updateModel();
-		if (cleanModel==false) {
-			warnErrorsInSource();
-			errorMode=true;
-			return false;
+	public PDESourcePage(PDEMultiPageEditor editor) {
+		this.editor = editor;
+		setEditorContextMenuId("#PDESourcePageEditorContext"); //$NON-NLS-1$
+		setRulerContextMenuId("#PDESourcePageRulerContext"); //$NON-NLS-1$
+	}
+	public boolean becomesInvisible(IFormPage newPage) {
+		if (errorMode || modelNeedsUpdating) {
+			boolean cleanModel = getEditor().updateModel();
+			if (cleanModel == false) {
+				warnErrorsInSource();
+				errorMode = true;
+				return false;
+			}
+			modelNeedsUpdating = false;
+			errorMode = false;
 		}
+		getSite().setSelectionProvider(getEditor());
+		return true;
+	}
+
+	public void becomesVisible(IFormPage oldPage) {
 		modelNeedsUpdating = false;
-		errorMode = false;
-	}
-	getSite().setSelectionProvider(getEditor());
-	return true;
-}
-
-public void becomesVisible(IFormPage oldPage) {
-	modelNeedsUpdating=false;
-	if (oldPage instanceof PDEFormPage) {
-    	selectObjectRange(((PDEFormPage)oldPage).getSelection());
-	}
-	getSite().setSelectionProvider(getSelectionProvider());
-}
-
-public boolean contextMenuAboutToShow(IMenuManager manager) {
-	return false;
-}
-public abstract IContentOutlinePage createContentOutlinePage();
-public void createControl(Composite parent) {
-	createPartControl(parent);
-}
-public void createPartControl(Composite parent) {
-	super.createPartControl(parent);
-	Control[] children = parent.getChildren();
-	control = children[children.length - 1];
-
-	IDocument document = getDocumentProvider().getDocument(getEditorInput());
-	document.addDocumentListener(documentListener);
-	errorMode = !getEditor().isModelCorrect(getEditor().getModel());
-}
-public void dispose() {
-	IDocument document = getDocumentProvider().getDocument(getEditorInput());
-	if (document != null)
-		document.removeDocumentListener(documentListener);
-	super.dispose();
-}
-protected void firePropertyChange(int type) {
-	if (type == PROP_DIRTY) {
-		getEditor().fireSaveNeeded();
-	} else
-		super.firePropertyChange(type);
-}
-public IContentOutlinePage getContentOutlinePage() {
-	if (outlinePage == null) {
-		outlinePage = createContentOutlinePage();
-	}
-	return outlinePage;
-}
-public Control getControl() {
-	return control;
-}
-public PDEMultiPageEditor getEditor() {
-	if (editor.getEditorInput() != getEditorInput())
-		editor.setInput(getEditorInput());
-	return editor;
-}
-public String getLabel() {
-	return getTitle();
-}
-public IPropertySheetPage getPropertySheetPage() {
-	return null;
-}
-public String getTitle() {
-	return PDEPlugin.getResourceString(PAGE_TITLE);
-}
-public void init(IEditorSite site, IEditorInput input)
-	throws PartInitException {
-	setDocumentProvider(getEditor().getDocumentProvider());
-	super.init(site, input);
-}
-public boolean isEditable() {
-	return getEditor().isEditable();
-}
-public boolean isSource() {
-	return true;
-}
-public boolean isVisible() {
-	return editor.getCurrentPage()==this;
-}
-
-public void openTo(Object object) {
-	if (object instanceof IMarker) {
-		gotoMarker((IMarker)object);
-	}
-}
-
-private void selectObjectRange(ISelection selection) {
-	if (selection instanceof IStructuredSelection) {
-		IStructuredSelection ssel = (IStructuredSelection)selection;
-		int start = 0;
-		int stop = 0;
-		// Compute the entire range
-		for (Iterator iter=ssel.iterator(); iter.hasNext();) {
-			Object obj = iter.next();
-			ISourceObject sobj = null;
-			
-			if (obj instanceof ISourceObject)
-			   	sobj = (ISourceObject)obj;
-			if (obj instanceof IAdaptable) {
-				IAdaptable adaptable = (IAdaptable)obj;
-				sobj = (ISourceObject)adaptable.getAdapter(ISourceObject.class);
-			}
-			if (sobj != null) {
-				if (start==0) {
-					start = sobj.getStartLine()-1;
-				}
-				else {
-				   start = Math.min(start, sobj.getStartLine()-1);
-				}
-				stop = Math.max(stop, sobj.getStopLine()-1);
-			}
+		if (oldPage instanceof PDEFormPage) {
+			selectObjectRange(((PDEFormPage) oldPage).getSelection());
 		}
-		if (start>0) {
-			IDocument document = getDocumentProvider().getDocument(getEditorInput());
-			if (document==null) return;
-			try {
-				//int offset = editor.getRealStartOffset(document.getLineOffset(start-1));
-				int startOffset = document.getLineOffset(start);
-				int stopOffset = document.getLineOffset(stop) + document.getLineLength(stop);
-				selectAndReveal(startOffset, stopOffset-startOffset);
-			}
-			catch (BadLocationException e) {
-			}
+		getSite().setSelectionProvider(getSelectionProvider());
+	}
+
+	public boolean contextMenuAboutToShow(IMenuManager manager) {
+		return false;
+	}
+
+	public abstract IContentOutlinePage createContentOutlinePage();
+	public void createControl(Composite parent) {
+		createPartControl(parent);
+	}
+	public void createPartControl(Composite parent) {
+		super.createPartControl(parent);
+		Control[] children = parent.getChildren();
+		control = children[children.length - 1];
+
+		IDocument document =
+			getDocumentProvider().getDocument(getEditorInput());
+		document.addDocumentListener(documentListener);
+		errorMode = !getEditor().isModelCorrect(getEditor().getModel());
+	}
+	public void dispose() {
+		IDocument document =
+			getDocumentProvider().getDocument(getEditorInput());
+		if (document != null)
+			document.removeDocumentListener(documentListener);
+		super.dispose();
+	}
+	protected void firePropertyChange(int type) {
+		if (type == PROP_DIRTY) {
+			getEditor().fireSaveNeeded();
+		} else
+			super.firePropertyChange(type);
+	}
+	public IContentOutlinePage getContentOutlinePage() {
+		if (outlinePage == null) {
+			outlinePage = createContentOutlinePage();
+		}
+		return outlinePage;
+	}
+	public Control getControl() {
+		return control;
+	}
+	public PDEMultiPageEditor getEditor() {
+		if (editor.getEditorInput() != getEditorInput())
+			editor.setInput(getEditorInput());
+		return editor;
+	}
+	public String getLabel() {
+		return getTitle();
+	}
+	public IPropertySheetPage getPropertySheetPage() {
+		return null;
+	}
+	public String getTitle() {
+		return PDEPlugin.getResourceString(PAGE_TITLE);
+	}
+	public void init(IEditorSite site, IEditorInput input)
+		throws PartInitException {
+		setDocumentProvider(getEditor().getDocumentProvider());
+		super.init(site, input);
+	}
+	public boolean isEditable() {
+		return getEditor().isEditable();
+	}
+	public boolean isSource() {
+		return true;
+	}
+	public boolean isVisible() {
+		return editor.getCurrentPage() == this;
+	}
+
+	public void openTo(Object object) {
+		if (object instanceof IMarker) {
+			gotoMarker((IMarker) object);
 		}
 	}
-}
 
-public boolean performGlobalAction(String id) { return true; }
-public String toString() {
-	return getTitle();
-}
-public void update() {}
-private void warnErrorsInSource() {
-	Display.getCurrent().beep();
-	String title = editor.getSite().getRegisteredName();
-	MessageDialog.openError(
-		PDEPlugin.getActiveWorkbenchShell(),
-		title,
-		PDEPlugin.getResourceString(ERROR_MESSAGE));
-}
+	private void selectObjectRange(ISelection selection) {
+		if (selection instanceof IStructuredSelection) {
+			IStructuredSelection ssel = (IStructuredSelection) selection;
+			int start = 0;
+			int stop = 0;
+			// Compute the entire range
+			for (Iterator iter = ssel.iterator(); iter.hasNext();) {
+				Object obj = iter.next();
+				ISourceObject sobj = null;
 
-protected void createActions () {
-	PDEEditorContributor contributor = getEditor().getContributor();
-	super.createActions();
-	setAction(ITextEditorActionConstants.SAVE, contributor.getSaveAction());
-}
+				if (obj instanceof ISourceObject)
+					sobj = (ISourceObject) obj;
+				if (obj instanceof IAdaptable) {
+					IAdaptable adaptable = (IAdaptable) obj;
+					sobj =
+						(ISourceObject) adaptable.getAdapter(
+							ISourceObject.class);
+				}
+				if (sobj != null) {
+					if (start == 0) {
+						start = sobj.getStartLine() - 1;
+					} else {
+						start = Math.min(start, sobj.getStartLine() - 1);
+					}
+					stop = Math.max(stop, sobj.getStopLine() - 1);
+				}
+			}
+			if (start > 0) {
+				IDocument document =
+					getDocumentProvider().getDocument(getEditorInput());
+				if (document == null)
+					return;
+				try {
+					//int offset = editor.getRealStartOffset(document.getLineOffset(start-1));
+					int startOffset = document.getLineOffset(start);
+					int stopOffset =
+						document.getLineOffset(stop)
+							+ document.getLineLength(stop);
+					selectAndReveal(startOffset, stopOffset - startOffset);
+				} catch (BadLocationException e) {
+				}
+			}
+		}
+	}
 
-public void close(boolean save) {
-	editor.close(save);
-}
+	public boolean performGlobalAction(String id) {
+		return true;
+	}
+	public String toString() {
+		return getTitle();
+	}
+	public void update() {
+	}
+	private void warnErrorsInSource() {
+		Display.getCurrent().beep();
+		String title = editor.getSite().getRegisteredName();
+		MessageDialog.openError(
+			PDEPlugin.getActiveWorkbenchShell(),
+			title,
+			PDEPlugin.getResourceString(ERROR_MESSAGE));
+	}
 
-public boolean canPaste(Clipboard clipboard) {
-	return true;
-}
-public void setFocus() {
-}
+	protected void createActions() {
+		PDEEditorContributor contributor = getEditor().getContributor();
+		super.createActions();
+		setAction(ITextEditorActionConstants.SAVE, contributor.getSaveAction());
+	}
 
-public boolean containsError() {
-	return errorMode;
-}
+	public void close(boolean save) {
+		editor.close(save);
+	}
+
+	public boolean canPaste(Clipboard clipboard) {
+		return true;
+	}
+	public void setFocus() {
+	}
+
+	public boolean containsError() {
+		return errorMode;
+	}
 
 }
