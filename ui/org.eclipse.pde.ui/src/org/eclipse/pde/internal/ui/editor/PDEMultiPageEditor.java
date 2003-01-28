b3205eb92a968ceeb11121deae2a841d382d66e1
Wassim Melhem <wmelhem>
2003-01-28 23:08:39 +0000

    defect 21074

--- ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/editor/PDEMultiPageEditor.java
+++ ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/editor/PDEMultiPageEditor.java
@@ -69,6 +69,7 @@ public abstract class PDEMultiPageEditor
 		public void elementDirtyStateChanged(Object element, boolean isDirty) {
 		}
 		public void elementMoved(Object originalElement, Object movedElement) {
+			close(true);
 		}
 	}
 
