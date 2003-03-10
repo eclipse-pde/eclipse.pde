2343332c40e7bd6e6b586773bc6295886a828781
Dean Roberts <droberts>
2003-03-10 21:56:14 +0000

    Copyright fix - automated changes

--- org.eclipse.pde/src/org/eclipse/pde/internal/PDE.java
+++ org.eclipse.pde/src/org/eclipse/pde/internal/PDE.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2000, 2003 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.pde.internal;
 
 import java.lang.reflect.InvocationTargetException;
@@ -7,10 +17,6 @@ import org.eclipse.core.resources.*;
 import org.eclipse.core.runtime.*;
 import org.eclipse.pde.internal.builders.CompilerFlags;
 
-/*
- * (c) Copyright IBM Corp. 2000, 2001.
- * All Rights Reserved.
- */
 
 public class PDE extends Plugin {
 	public static final String PLUGIN_ID = "org.eclipse.pde";
