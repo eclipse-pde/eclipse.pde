0c95ca77c4e81c803b6be2392cb869ce01469711
Wassim Melhem <wmelhem>
2003-06-17 21:34:51 +0000

    defect 38537

--- ui/org.eclipse.pde.ui/templates/newWizard/java/$wizardPageClassName$.java
+++ ui/org.eclipse.pde.ui/templates/newWizard/java/$wizardPageClassName$.java
@@ -135,7 +135,7 @@ public class $wizardPageClassName$ extends WizardPage {
 			updateStatus("File name must be specified");
 			return;
 		}
-		int dotLoc = fileName.indexOf('.');
+		int dotLoc = fileName.lastIndexOf('.');
 		if (dotLoc != -1) {
 			String ext = fileName.substring(dotLoc + 1);
 			if (ext.equalsIgnoreCase("$extension$") == false) {
