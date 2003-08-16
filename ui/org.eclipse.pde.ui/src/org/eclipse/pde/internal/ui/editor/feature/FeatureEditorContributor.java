423ebf5c4a8333a0df2eadd7dfab7a79a5ab0377
Wassim Melhem <wmelhem>
2003-08-16 07:54:01 +0000

    removed the previiew action

--- ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/editor/feature/FeatureEditorContributor.java
+++ ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/editor/feature/FeatureEditorContributor.java
@@ -19,15 +19,12 @@ import org.eclipse.pde.internal.ui.editor.*;
 public class FeatureEditorContributor extends PDEEditorContributor {
 	private EditorBuildFeatureAction buildAction;
 	private SynchronizeVersionsAction synchronizeAction;
-	private PreviewAction previewAction;
 
 public FeatureEditorContributor() {
 	super("&Component");
 }
 public void contextMenuAboutToShow(IMenuManager mng) {
 	super.contextMenuAboutToShow(mng);
-	mng.add(new Separator());
-	mng.add(previewAction);
 	mng.add(new Separator());
 	mng.add(synchronizeAction);
 	mng.add(buildAction);
@@ -42,13 +39,11 @@ protected void makeActions() {
 	super.makeActions();
 	buildAction = new EditorBuildFeatureAction();
 	synchronizeAction = new SynchronizeVersionsAction();
-	previewAction = new PreviewAction();
 }
 public void setActiveEditor(IEditorPart targetEditor) {
 	super.setActiveEditor(targetEditor);
 	buildAction.setActiveEditor((FeatureEditor) targetEditor);
 	synchronizeAction.setActiveEditor((FeatureEditor) targetEditor);
-	previewAction.setActiveEditor((FeatureEditor)targetEditor);
 }
 
 	protected boolean hasKnownTypes(Clipboard clipboard) {
