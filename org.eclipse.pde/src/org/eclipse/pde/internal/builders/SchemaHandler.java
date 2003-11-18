79727cdb83c74e74813662c5a7d75c280c0787b1
Wassim Melhem <wmelhem>
2003-11-18 23:55:37 +0000

    *** empty log message ***

--- org.eclipse.pde/src/org/eclipse/pde/internal/builders/SchemaHandler.java
+++ org.eclipse.pde/src/org/eclipse/pde/internal/builders/SchemaHandler.java
@@ -102,6 +102,7 @@ public class SchemaHandler extends PluginErrorReporter {
 	}
 	
 	public Node getDocumentElement() {
+		fDocument.getDocumentElement().normalize();
 		return fDocument.getDocumentElement();
 	}
 	
