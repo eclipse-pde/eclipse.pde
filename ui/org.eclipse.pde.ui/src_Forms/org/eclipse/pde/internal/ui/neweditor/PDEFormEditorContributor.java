f7e04024a15c6148de81195d0f80030b965bda84
Dejan Glozic <dejan>
2004-03-11 21:04:34 +0000

    *** empty log message ***

--- ui/org.eclipse.pde.ui/src_Forms/org/eclipse/pde/internal/ui/neweditor/PDEFormEditorContributor.java
+++ ui/org.eclipse.pde.ui/src_Forms/org/eclipse/pde/internal/ui/neweditor/PDEFormEditorContributor.java
@@ -1,17 +1,15 @@
 /*
  * Created on Jan 29, 2004
- *
- * To change the template for this generated file go to
- * Window - Preferences - Java - Code Generation - Code and Comments
+ * 
+ * To change the template for this generated file go to Window - Preferences -
+ * Java - Code Generation - Code and Comments
  */
 package org.eclipse.pde.internal.ui.neweditor;
-
+import java.util.*;
 import java.util.Hashtable;
-
 import org.eclipse.jface.action.*;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.pde.core.*;
-import org.eclipse.pde.core.IEditable;
 import org.eclipse.pde.internal.ui.PDEPlugin;
 import org.eclipse.ui.*;
 import org.eclipse.ui.actions.ActionFactory;
@@ -20,12 +18,11 @@ import org.eclipse.ui.forms.editor.IFormPage;
 import org.eclipse.ui.ide.IDEActionFactory;
 import org.eclipse.ui.part.MultiPageEditorActionBarContributor;
 import org.eclipse.ui.texteditor.IUpdate;
-
 /**
  * @author dejan
- *
- * To change the template for this generated type comment go to
- * Window - Preferences - Java - Code Generation - Code and Comments
+ * 
+ * To change the template for this generated type comment go to Window -
+ * Preferences - Java - Code Generation - Code and Comments
  */
 public class PDEFormEditorContributor
 		extends
@@ -35,11 +32,8 @@ public class PDEFormEditorContributor
 	public static final String ACTIONS_COPY = "EditorActions.copy";
 	public static final String ACTIONS_PASTE = "EditorActions.paste";
 	public static final String ACTIONS_REVERT = "EditorActions.revert";
-	private SubMenuManager subMenuManager;
-	private SubStatusLineManager subStatusManager;
-	private SubToolBarManager subToolbarManager;
-
-	private PDEFormEditor editor;	
+	private SubActionBars sourceActionBars;
+	private PDEFormEditor editor;
 	private IFormPage page;
 	private SaveAction saveAction;
 	private RevertAction revertAction;
@@ -48,7 +42,7 @@ public class PDEFormEditorContributor
 	private ClipboardAction pasteAction;
 	private Hashtable globalActions = new Hashtable();
 	private TextEditorActionContributor sourceContributor;
-
+	
 	class GlobalAction extends Action implements IUpdate {
 		private String id;
 		public GlobalAction(String id) {
@@ -62,7 +56,6 @@ public class PDEFormEditorContributor
 			getActionBars().updateActionBars();
 		}
 	}
-
 	class ClipboardAction extends GlobalAction {
 		public ClipboardAction(String id) {
 			super(id);
@@ -71,14 +64,14 @@ public class PDEFormEditorContributor
 		public void selectionChanged(ISelection selection) {
 		}
 		public boolean isEditable() {
-			if (editor==null) return false;
+			if (editor == null)
+				return false;
 			IModel model = editor.getAggregateModel();
 			if (model instanceof IEditable)
-				return ((IEditable)model).isEditable();
+				return ((IEditable) model).isEditable();
 			return false;
 		}
 	}
-
 	class CutAction extends ClipboardAction {
 		public CutAction() {
 			super(ActionFactory.CUT.getId());
@@ -88,7 +81,6 @@ public class PDEFormEditorContributor
 			//setEnabled(isEditable() && editor.canCopy(selection));
 		}
 	}
-
 	class CopyAction extends ClipboardAction {
 		public CopyAction() {
 			super(ActionFactory.COPY.getId());
@@ -98,7 +90,6 @@ public class PDEFormEditorContributor
 			//setEnabled(editor.canCopy(selection));
 		}
 	}
-
 	class PasteAction extends ClipboardAction {
 		public PasteAction() {
 			super(ActionFactory.PASTE.getId());
@@ -109,7 +100,6 @@ public class PDEFormEditorContributor
 			//setEnabled(isEditable()&& editor.canPasteFromClipboard());
 		}
 	}
-
 	class SaveAction extends Action implements IUpdate {
 		public SaveAction() {
 		}
@@ -124,23 +114,20 @@ public class PDEFormEditorContributor
 				setEnabled(false);
 		}
 	}
-	
 	class RevertAction extends Action implements IUpdate {
 		public RevertAction() {
 		}
 		public void run() {
 			if (editor != null)
-			editor.doRevert();
+				editor.doRevert();
 		}
 		public void update() {
 			if (editor != null) {
 				setEnabled(editor.isDirty());
-			}
-			else 
+			} else
 				setEnabled(false);
 		}
 	}
-
 	public PDEFormEditorContributor(String menuName) {
 		sourceContributor = new TextEditorActionContributor();
 		makeActions();
@@ -158,52 +145,52 @@ public class PDEFormEditorContributor
 		mng.add(pasteAction);
 		mng.add(new Separator());
 		mng.add(revertAction);
-
 	}
-	
 	public void contextMenuAboutToShow(IMenuManager mng) {
 		contextMenuAboutToShow(mng, true);
 	}
 	public void contextMenuAboutToShow(IMenuManager mng, boolean addClipboard) {
-		if (editor != null)	
+		if (editor != null)
 			updateSelectableActions(editor.getSelection());
 		if (addClipboard) {
 			addClipboardActions(mng);
-			//mng.add(new Separator());
 		}
 		mng.add(saveAction);
 	}
 	public void contributeToMenu(IMenuManager mm) {
-		subMenuManager = new SubMenuManager(mm);
-		sourceContributor.contributeToMenu(subMenuManager);
 	}
 	public void contributeToStatusLine(IStatusLineManager slm) {
-		subStatusManager = new SubStatusLineManager(slm);
-		sourceContributor.contributeToStatusLine(subStatusManager);
 	}
 	public void contributeToToolBar(IToolBarManager tbm) {
-		subToolbarManager = new SubToolBarManager(tbm);
-		sourceContributor.contributeToToolBar(subToolbarManager);
 	}
+	public void contributeToCoolBar(ICoolBarManager cbm) {
+	}
+	public void dispose() {
+		sourceContributor.dispose();
+		sourceActionBars.dispose();
+		super.dispose();
+	}
+	public void init(IActionBars bars) {
+		super.init(bars);
+		sourceActionBars = new SubActionBars(bars);
+		sourceContributor.init(sourceActionBars);
+	}
+	
 	public PDEFormEditor getEditor() {
 		return editor;
 	}
 	public IAction getGlobalAction(String id) {
 		return (IAction) globalActions.get(id);
 	}
-
 	public IAction getSaveAction() {
 		return saveAction;
 	}
-	
 	public IAction getRevertAction() {
 		return revertAction;
 	}
-	
 	public IStatusLineManager getStatusLineManager() {
 		return getActionBars().getStatusLineManager();
 	}
-
 	protected void makeActions() {
 		// clipboard actions
 		cutAction = new CutAction();
@@ -212,20 +199,20 @@ public class PDEFormEditorContributor
 		addGlobalAction(ActionFactory.CUT.getId(), cutAction);
 		addGlobalAction(ActionFactory.COPY.getId(), copyAction);
 		addGlobalAction(ActionFactory.PASTE.getId(), pasteAction);
-
 		addGlobalAction(ActionFactory.DELETE.getId());
+		// undo/redo
 		addGlobalAction(ActionFactory.UNDO.getId());
 		addGlobalAction(ActionFactory.REDO.getId());
+		// select/find
 		addGlobalAction(ActionFactory.SELECT_ALL.getId());
 		addGlobalAction(ActionFactory.FIND.getId());
+		// bookmark
 		addGlobalAction(IDEActionFactory.BOOKMARK.getId());
-
+		// save/revert
 		saveAction = new SaveAction();
 		saveAction.setText(PDEPlugin.getResourceString(ACTIONS_SAVE));
-		
 		revertAction = new RevertAction();
 		revertAction.setText(PDEPlugin.getResourceString(ACTIONS_REVERT));
-
 	}
 	public void setActiveEditor(IEditorPart targetEditor) {
 		//if (editor != null)
@@ -235,87 +222,85 @@ public class PDEFormEditorContributor
 			// the action is thinking that source page
 			// is a standalone editor and tries to activate it
 			// #19361
-			PDESourcePage page = (PDESourcePage)targetEditor;
+			PDESourcePage page = (PDESourcePage) targetEditor;
 			PDEPlugin.getActivePage().activate(page.getEditor());
 			return;
 		}
-	    if (targetEditor instanceof PDEFormEditor)
-	    	this.editor = (PDEFormEditor) targetEditor;
-	    else
-	    	return;
-	    /*
-		editor.updateUndo(
-			getGlobalAction(ActionFactory.UNDO.getId()),
-			getGlobalAction(ActionFactory.REDO.getId()));
-		*/
+		if (targetEditor instanceof PDEFormEditor)
+			this.editor = (PDEFormEditor) targetEditor;
+		else
+			return;
+		/*
+		 * editor.updateUndo( getGlobalAction(ActionFactory.UNDO.getId()),
+		 * getGlobalAction(ActionFactory.REDO.getId()));
+		 */
 		IEditorPart page = editor.getActiveEditor();
 		setActivePage(page);
 		updateSelectableActions(editor.getSelection());
 	}
 	public void setActivePage(IEditorPart newEditor) {
-		if (editor==null) return;
+		if (editor == null)
+			return;
 		IFormPage oldPage = page;
 		IFormPage newPage = editor.getActivePageInstance();
 		this.page = newPage;
-		if (newPage==null) return;
-		updateActions();
-		if (oldPage != null
-			&& oldPage.isEditor() == false
-			&& newPage.isEditor() == false)
+		if (newPage == null)
 			return;
-
-		IActionBars bars = getActionBars();
+		updateActions();
+		if (oldPage != null && oldPage.isEditor() == false
+				&& newPage.isEditor() == false) {
+			getActionBars().updateActionBars();
+			return;
+		}
 		PDESourcePage sourcePage = null;
-		
 		if (newPage instanceof PDESourcePage)
-			sourcePage = (PDESourcePage)newPage;
-		
-		subMenuManager.setVisible(sourcePage!=null);
-		subStatusManager.setVisible(sourcePage!=null);
-		subToolbarManager.setVisible(sourcePage!=null);
-
+			sourcePage = (PDESourcePage) newPage;
+		if (sourcePage!=null && sourcePage.equals(oldPage))
+			return;
 		sourceContributor.setActiveEditor(sourcePage);
-		// update global actions
-		bars.setGlobalActionHandler(
-			ActionFactory.DELETE.getId(),
-			getPageAction(page, ActionFactory.DELETE.getId()));
-		bars.setGlobalActionHandler(
-			ActionFactory.UNDO.getId(),
-			getPageAction(page, ActionFactory.UNDO.getId()));
-		bars.setGlobalActionHandler(
-			ActionFactory.REDO.getId(),
-			getPageAction(page, ActionFactory.REDO.getId()));
-		bars.setGlobalActionHandler(
-			ActionFactory.CUT.getId(),
-			getPageAction(page, ActionFactory.CUT.getId()));
-		bars.setGlobalActionHandler(
-			ActionFactory.COPY.getId(),
-			getPageAction(page, ActionFactory.COPY.getId()));
-		bars.setGlobalActionHandler(
-			ActionFactory.PASTE.getId(),
-			getPageAction(page, ActionFactory.PASTE.getId()));
-		bars.setGlobalActionHandler(
-			ActionFactory.SELECT_ALL.getId(),
-			getPageAction(page, ActionFactory.SELECT_ALL.getId()));
-		bars.setGlobalActionHandler(
-			ActionFactory.FIND.getId(),
-			getPageAction(page, ActionFactory.FIND.getId()));
-		bars.setGlobalActionHandler(
-			IDEActionFactory.BOOKMARK.getId(),
-			getPageAction(page, IDEActionFactory.BOOKMARK.getId()));
-		bars.updateActionBars();
-	}
-	private IAction getPageAction(IFormPage page, String id) {
-		if (page instanceof PDESourcePage)
-			return ((PDESourcePage)page).getAction(id);
-		return getGlobalAction(id);
+		setSourceActionBarsActive(sourcePage != null);
 	}
 	
+	private void setSourceActionBarsActive(boolean active) {
+		IActionBars rootBars = getActionBars();
+		rootBars.clearGlobalActionHandlers();
+		rootBars.updateActionBars();
+		if (active) {
+			sourceActionBars.activate();
+			Map handlers = sourceActionBars.getGlobalActionHandlers();
+			if (handlers != null) {
+				Set keys = handlers.keySet();
+				for (Iterator iter = keys.iterator(); iter.hasNext();) {
+					String id = (String) iter.next();
+					rootBars.setGlobalActionHandler(id, (IAction) handlers
+							.get(id));
+				}
+			}
+		} else {
+			sourceActionBars.deactivate();
+			registerGlobalActionHandlers();
+		}
+		rootBars.updateActionBars();
+	}
+
+	private void registerGlobalActionHandlers() {
+		registerGlobalAction(ActionFactory.DELETE.getId());
+		registerGlobalAction(ActionFactory.UNDO.getId());
+		registerGlobalAction(ActionFactory.REDO.getId());
+		registerGlobalAction(ActionFactory.CUT.getId());
+		registerGlobalAction(ActionFactory.COPY.getId());
+		registerGlobalAction(ActionFactory.PASTE.getId());
+		registerGlobalAction(ActionFactory.SELECT_ALL.getId());
+		registerGlobalAction(ActionFactory.FIND.getId());
+	}
+	private void registerGlobalAction(String id) {
+		IAction action = getGlobalAction(id);
+		getActionBars().setGlobalActionHandler(id, action);
+	}
 	public void updateActions() {
 		saveAction.update();
 		revertAction.update();
 	}
-
 	public void updateSelectableActions(ISelection selection) {
 		if (editor != null) {
 			cutAction.selectionChanged(selection);
