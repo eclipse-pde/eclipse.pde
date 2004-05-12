9ed0dcbf8f036b3ae6a58abb7da6fecfacd31b53
Dejan Glozic <dejan>
2004-05-12 17:09:37 +0000

    *** empty log message ***

--- ui/org.eclipse.pde.core/src/org/eclipse/pde/internal/core/bundle/BundlePlugin.java
+++ ui/org.eclipse.pde.core/src/org/eclipse/pde/internal/core/bundle/BundlePlugin.java
@@ -1,33 +1,51 @@
 /*
  * Created on Oct 19, 2003
- *
- * To change the template for this generated file go to
- * Window - Preferences - Java - Code Generation - Code and Comments
+ * 
+ * To change the template for this generated file go to Window - Preferences -
+ * Java - Code Generation - Code and Comments
  */
 package org.eclipse.pde.internal.core.bundle;
 
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.pde.core.plugin.IPluginImport;
 import org.eclipse.pde.internal.core.ibundle.*;
 import org.osgi.framework.*;
 
 public class BundlePlugin extends BundlePluginBase implements IBundlePlugin {
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.eclipse.pde.core.plugin.IPlugin#getClassName()
 	 */
 	public String getClassName() {
-		return parseSingleValuedHeader(Constants.BUNDLE_ACTIVATOR);
+		return parseSingleValuedHeader(getClassHeader());
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.eclipse.pde.core.plugin.IPlugin#setClassName(java.lang.String)
 	 */
 	public void setClassName(String className) throws CoreException {
 		IBundle bundle = getBundle();
 		if (bundle != null) {
 			String old = getClassName();
-			bundle.setHeader(Constants.BUNDLE_ACTIVATOR, className);
+			bundle.setHeader(getClassHeader(), className);
 			model.fireModelObjectChanged(this, P_CLASS_NAME, old, className);
 		}
+	}
+	private String getClassHeader() {
+		boolean compatibilityMode = false;
+		IPluginImport[] imports = getImports();
+		for (int i = 0; i < imports.length; i++) {
+			IPluginImport iimport = imports[i];
+			String id = iimport.getId();
+			if (id.equals("org.eclipse.core.runtime.compatibility")) {
+				compatibilityMode = true;
+				break;
+			}
+		}
+		return compatibilityMode ? "Plugin-Class" : Constants.BUNDLE_ACTIVATOR;
 	}
 }
