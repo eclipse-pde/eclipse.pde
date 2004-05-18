014cca290406e736bf0a619f6ea87cb3d6b8318f
Dejan Glozic <dejan>
2004-05-18 20:02:19 +0000

    *** empty log message ***

---
+++ ui/org.eclipse.pde.core/src/org/eclipse/pde/internal/core/ExternalJavaSearchInitializer.java
@@ -0,0 +1,38 @@
+/*******************************************************************************
+ * Copyright (c) 2000, 2003 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.pde.internal.core;
+
+import org.eclipse.core.resources.*;
+import org.eclipse.core.runtime.*;
+import org.eclipse.jdt.core.*;
+
+/**
+ * 
+ */
+public class ExternalJavaSearchInitializer extends ClasspathContainerInitializer {
+
+	/**
+	 * Constructor for RequiredPluginsInitializer.
+	 */
+	public ExternalJavaSearchInitializer() {
+		super();
+	}
+
+	/**
+	 * @see org.eclipse.jdt.core.ClasspathContainerInitializer#initialize(IPath, IJavaProject)
+	 */
+	public void initialize(IPath containerPath, IJavaProject javaProject)
+		throws CoreException {
+		PluginModelManager manager = PDECore.getDefault().getModelManager();
+		SearchablePluginsManager searchManager = manager.getSearchablePluginsManager();
+		searchManager.updateClasspathContainer(javaProject);
+	}
+}
