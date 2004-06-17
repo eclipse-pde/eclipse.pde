57c337b1a042bc1505b4dc168b6be7a513d70722
Wassim Melhem <wmelhem>
2004-06-17 16:01:05 +0000

    *** empty log message ***

--- ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/editor/plugin/rows/ClassAttributeRow.java
+++ ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/editor/plugin/rows/ClassAttributeRow.java
@@ -130,7 +130,7 @@ public class ClassAttributeRow extends ReferenceAttributeRow {
 						PlatformUI.getWorkbench().getProgressService(),
 						SearchEngine.createWorkspaceScope(),
 						IJavaElementSearchConstants.CONSIDER_CLASSES, false,
-						"*"); //$NON-NLS-1$
+						""); //$NON-NLS-1$
 				dialog.setTitle(PDEPlugin.getResourceString("ClassAttributeRow.dialogTitle")); //$NON-NLS-1$
 				if (dialog.open() == SelectionDialog.OK) {
 					IType type = (IType) dialog.getResult()[0];
