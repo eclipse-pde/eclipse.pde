784e02f6b64fc17ffaebbaaf6aacefeccc6cbc99
Wassim Melhem <wmelhem>
2004-10-08 02:22:39 +0000

    *** empty log message ***

--- ui/org.eclipse.pde.core/src/org/eclipse/pde/core/plugin/IFragment.java
+++ ui/org.eclipse.pde.core/src/org/eclipse/pde/core/plugin/IFragment.java
@@ -46,7 +46,6 @@ public interface IFragment extends IPluginBase {
 	/**
 	 * Returns an optional version match rule as defined in
 	 * IMatchRule interface.
-	 * @see IMatchRule#getRule
 	 */
 	int getRule();
 	/**
