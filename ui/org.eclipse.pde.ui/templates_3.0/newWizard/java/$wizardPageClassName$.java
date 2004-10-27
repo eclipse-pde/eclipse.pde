ff3e318e701fd664140f5617d5afa0a7ee2c5d9a
Wassim Melhem <wmelhem>
2004-10-27 00:29:28 +0000

    *** empty log message ***

--- ui/org.eclipse.pde.ui/templates_3.0/newWizard/java/$wizardPageClassName$.java
+++ ui/org.eclipse.pde.ui/templates_3.0/newWizard/java/$wizardPageClassName$.java
@@ -1,29 +1,42 @@
 package $packageName$;
 
-import org.eclipse.jface.wizard.WizardPage;
-import org.eclipse.swt.widgets.*;
-import org.eclipse.swt.layout.*;
-import org.eclipse.swt.SWT;
-import org.eclipse.core.resources.*;
+import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.Path;
-import org.eclipse.swt.events.*;
+import org.eclipse.jface.dialogs.IDialogPage;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.WizardPage;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.dialogs.ContainerSelectionDialog;
-import org.eclipse.jface.viewers.*;
 
 /**
- * The "New" wizard page allows setting the container for
- * the new file as well as the file name. The page
- * will only accept file name without the extension OR
- * with the extension that matches the expected one ($extension$).
+ * The "New" wizard page allows setting the container for the new file as well
+ * as the file name. The page will only accept file name without the extension
+ * OR with the extension that matches the expected one ($extension$).
  */
 
 public class $wizardPageClassName$ extends WizardPage {
 	private Text containerText;
+
 	private Text fileText;
+
 	private ISelection selection;
 
 	/**
 	 * Constructor for SampleNewWizardPage.
+	 * 
 	 * @param pageName
 	 */
 	public $wizardPageClassName$(ISelection selection) {
@@ -76,63 +89,75 @@ public class $wizardPageClassName$ extends WizardPage {
 		dialogChanged();
 		setControl(container);
 	}
-	
+
 	/**
-	 * Tests if the current workbench selection is a suitable
-	 * container to use.
+	 * Tests if the current workbench selection is a suitable container to use.
 	 */
-	
+
 	private void initialize() {
-		if (selection!=null && selection.isEmpty()==false && selection instanceof IStructuredSelection) {
-			IStructuredSelection ssel = (IStructuredSelection)selection;
-			if (ssel.size()>1) return;
+		if (selection != null && selection.isEmpty() == false
+				&& selection instanceof IStructuredSelection) {
+			IStructuredSelection ssel = (IStructuredSelection) selection;
+			if (ssel.size() > 1)
+				return;
 			Object obj = ssel.getFirstElement();
 			if (obj instanceof IResource) {
 				IContainer container;
 				if (obj instanceof IContainer)
-					container = (IContainer)obj;
+					container = (IContainer) obj;
 				else
-					container = ((IResource)obj).getParent();
+					container = ((IResource) obj).getParent();
 				containerText.setText(container.getFullPath().toString());
 			}
 		}
 		fileText.setText("$initialFileName$");
 	}
-	
+
 	/**
-	 * Uses the standard container selection dialog to
-	 * choose the new value for the container field.
+	 * Uses the standard container selection dialog to choose the new value for
+	 * the container field.
 	 */
 
 	private void handleBrowse() {
-		ContainerSelectionDialog dialog =
-			new ContainerSelectionDialog(
-				getShell(),
-				ResourcesPlugin.getWorkspace().getRoot(),
-				false,
+		ContainerSelectionDialog dialog = new ContainerSelectionDialog(
+				getShell(), ResourcesPlugin.getWorkspace().getRoot(), false,
 				"Select new file container");
 		if (dialog.open() == ContainerSelectionDialog.OK) {
 			Object[] result = dialog.getResult();
 			if (result.length == 1) {
-				containerText.setText(((Path)result[0]).toOSString());
+				containerText.setText(((Path) result[0]).toString());
 			}
 		}
 	}
-	
+
 	/**
 	 * Ensures that both text fields are set.
 	 */
 
 	private void dialogChanged() {
-		String container = getContainerName();
+		IResource container = ResourcesPlugin.getWorkspace().getRoot()
+				.findMember(new Path(getContainerName()));
 		String fileName = getFileName();
 
-		if (container.length() == 0) {
+		if (getContainerName().length() == 0) {
 			updateStatus("File container must be specified");
+			return;
+		}
+		if (container == null
+				|| (container.getType() & (IResource.PROJECT | IResource.FOLDER)) == 0) {
+			updateStatus("File container must exist");
+			return;
+		}
+		if (!container.isAccessible()) {
+			updateStatus("Project must be writable");
 			return;
 		}
 		if (fileName.length() == 0) {
 			updateStatus("File name must be specified");
+			return;
+		}
+		if (fileName.replace('\\', '/').indexOf('/', 1) > 0) {
+			updateStatus("File name must be valid");
 			return;
 		}
 		int dotLoc = fileName.lastIndexOf('.');
@@ -154,6 +179,7 @@ public class $wizardPageClassName$ extends WizardPage {
 	public String getContainerName() {
 		return containerText.getText();
 	}
+
 	public String getFileName() {
 		return fileText.getText();
 	}
