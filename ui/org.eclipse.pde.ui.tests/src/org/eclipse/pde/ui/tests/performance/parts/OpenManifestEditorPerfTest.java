23e6aebcb2988817558dc1c023c957987ee5faa8
kkolosow <>
2004-11-11 20:47:39 +0000

    *** empty log message ***

--- ui/org.eclipse.pde.ui.tests/src/org/eclipse/pde/ui/tests/performance/parts/OpenManifestEditorPerfTest.java
+++ ui/org.eclipse.pde.ui.tests/src/org/eclipse/pde/ui/tests/performance/parts/OpenManifestEditorPerfTest.java
@@ -2,6 +2,9 @@ package org.eclipse.pde.ui.tests.performance.parts;
 
 import junit.framework.*;
 
+import org.eclipse.pde.core.plugin.IMatchRules;
+import org.eclipse.pde.core.plugin.IPluginModelBase;
+import org.eclipse.pde.internal.core.PDECore;
 import org.eclipse.pde.internal.ui.editor.manifest.ManifestEditor;
 import org.eclipse.test.performance.*;
 import org.eclipse.ui.*;
@@ -15,6 +18,9 @@ public class OpenManifestEditorPerfTest extends PerformanceTestCase {
 	public void testOpen() throws Exception {
 		tagAsGlobalSummary("Open Plug-in Editor", Dimension.CPU_TIME); //$NON-NLS-1$
 		IWorkbenchPage page= PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+		IPluginModelBase model = PDECore.getDefault().getModelManager().findPlugin("org.eclipse.jdt.ui", null, IMatchRules.NONE);
+		assertFalse("org.eclipse.jdt.ui model does not exist", model == null);
+		model.setEnabled(true);
 
 		for (int i = 0; i < 20; i++) {
 			startMeasuring();
