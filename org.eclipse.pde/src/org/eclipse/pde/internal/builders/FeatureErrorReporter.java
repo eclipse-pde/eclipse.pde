551c68127235040790e87ef308617892f5c9919e
Wassim Melhem <wmelhem>
2004-11-30 08:24:10 +0000

    *** empty log message ***

---
+++ org.eclipse.pde/src/org/eclipse/pde/internal/builders/FeatureErrorReporter.java
@@ -0,0 +1,12 @@
+package org.eclipse.pde.internal.builders;
+
+import org.eclipse.core.resources.*;
+
+
+public class FeatureErrorReporter extends XMLErrorReporter {
+
+	public FeatureErrorReporter(IFile file) {
+		super(file);
+	}
+
+}
---
+++ org.eclipse.pde/src/org/eclipse/pde/internal/builders/UpdateSiteErrorReporter.java
@@ -0,0 +1,12 @@
+package org.eclipse.pde.internal.builders;
+
+import org.eclipse.core.resources.*;
+
+
+public class UpdateSiteErrorReporter extends XMLErrorReporter {
+
+	public UpdateSiteErrorReporter(IFile file) {
+		super(file);
+	}
+
+}
