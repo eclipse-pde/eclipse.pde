e8e25a4a2cd00fb4fdf44682849a035720976fa0
kkolosow <>
2005-01-31 22:57:36 +0000

    83448 Features/plugins from target are not exposed

--- ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/wizards/exports/FeatureExportJob.java
+++ ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/wizards/exports/FeatureExportJob.java
@@ -357,7 +357,7 @@ public class FeatureExportJob extends Job implements IPreferenceConstants {
 
 	protected String[] getPaths() throws CoreException {
 		ArrayList paths = new ArrayList();
-		IFeatureModel[] models = PDECore.getDefault().getWorkspaceModelManager().getFeatureModels();
+		IFeatureModel[] models = PDECore.getDefault().getFeatureModelManager().getAllFeatures();
 		for (int i = 0; i < models.length; i++) {
 			paths.add(models[i].getInstallLocation() + IPath.SEPARATOR + "feature.xml"); //$NON-NLS-1$
 		}
