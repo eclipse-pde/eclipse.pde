6b9bd96c92189f7df2e0e9732dca74dd19058276
Wassim Melhem <wmelhem>
2005-01-31 02:44:31 +0000

    *** empty log message ***

--- ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/editor/product/LauncherSection.java
+++ ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/editor/product/LauncherSection.java
@@ -1,5 +1,8 @@
 package org.eclipse.pde.internal.ui.editor.product;
 
+import java.util.*;
+
+import org.eclipse.pde.internal.core.iproduct.*;
 import org.eclipse.pde.internal.ui.editor.*;
 import org.eclipse.pde.internal.ui.parts.*;
 import org.eclipse.swt.*;
@@ -13,19 +16,25 @@ import org.eclipse.ui.forms.widgets.*;
 public class LauncherSection extends PDESection {
 
 	private FormEntry fNameEntry;
-	private FormEntry fLinuxIcon;
-	private FormEntry fMacIcon;
-	private FormEntry fSmallIcon;
-	private FormEntry fLargeIcon;
-	private FormEntry fMediumIcon;
-	private FormEntry fTinyIcon;
-	private FormEntry fIco;
-	private FormEntry f16Low;
-	private FormEntry f16High;
-	private FormEntry f32Low;
-	private FormEntry f32High;
-	private FormEntry f48Low;
-	private FormEntry f48High;
+
+	private Map fIconMap = new HashMap();
+	
+	class IconEntry extends FormEntry {
+		String fIconId;
+		public IconEntry(Composite parent, FormToolkit toolkit, String labelText, String iconId) {
+			super(parent, toolkit, labelText, "Browse...", isEditable(), 20);
+			fIconId = iconId;
+			addEntryFormListener();
+			setEditable(isEditable());
+		}		
+		private void addEntryFormListener() {
+			IActionBars actionBars = getPage().getPDEEditor().getEditorSite().getActionBars();
+			setFormEntryListener(new FormEntryAdapter(LauncherSection.this, actionBars) {
+				
+			});
+		}
+		
+	}
 
 	public LauncherSection(PDEFormPage page, Composite parent) {
 		super(page, parent, Section.DESCRIPTION);
@@ -52,197 +61,82 @@ public class LauncherSection extends PDESection {
 		});
 		fNameEntry.setEditable(isEditable());
 		
-		Label label = toolkit.createLabel(client, "");
-		GridData gd = new GridData();
-		gd.horizontalSpan = 2;
-		label.setLayoutData(gd);
+		createLabel(client, toolkit, "", 2);	
+		createLabel(client, toolkit, "Customizing the launcher icons varies per platform:", 2);
 		
-		label = toolkit.createLabel(client, "Customizing the launcher icons varies per platform:");
-		gd = new GridData();
-		gd.horizontalSpan = 2;
-		label.setLayoutData(gd);
-		
-		addLinuxSection(client, toolkit, actionBars);
-		addMacSection(client, toolkit, actionBars);
-		addSolarisSection(client, toolkit, actionBars);
-		addWin32Section(client, toolkit, actionBars);
+		addLinuxSection(client, toolkit);
+		addMacSection(client, toolkit);
+		addSolarisSection(client, toolkit);
+		addWin32Section(client, toolkit);
 		
 		toolkit.paintBordersFor(client);
 		section.setClient(client);
-		gd = new GridData(GridData.FILL_HORIZONTAL|GridData.VERTICAL_ALIGN_BEGINNING);
+		GridData gd = new GridData(GridData.FILL_HORIZONTAL|GridData.VERTICAL_ALIGN_BEGINNING);
 		gd.verticalSpan = 3;
 		section.setLayoutData(gd);
 	}
 	
-	private void addWin32Section(Composite parent, FormToolkit toolkit, IActionBars actionBars) {
-		ExpandableComposite ec = toolkit.createExpandableComposite(parent, ExpandableComposite.TWISTIE|ExpandableComposite.CLIENT_INDENT);
-		ec.setText("win32");
-		
-		GridData gd = new GridData(GridData.FILL_BOTH);
-		gd.horizontalSpan = 2;
-		ec.setLayoutData(gd);
-		ec.addExpansionListener(new ExpansionAdapter() {
-			public void expansionStateChanged(ExpansionEvent e) {
-				getPage().getManagedForm().reflow(true);
-			}
-		});	
-		
-		Composite comp = toolkit.createComposite(ec);
-		comp.setLayout(new GridLayout(3, false));
-		comp.setLayoutData(new GridData(GridData.FILL_BOTH));
+	private void addWin32Section(Composite parent, FormToolkit toolkit) {
+		Composite comp = createComposite(parent, toolkit, "win32");
 		
 		Button button = toolkit.createButton(comp, "Use a single .ICO file", SWT.RADIO);
-		gd = new GridData();
+		GridData gd = new GridData();
 		gd.horizontalSpan = 3;
 		button.setLayoutData(gd);
 		
-		fIco = new FormEntry(comp, toolkit, "File:", "Browse...", isEditable(), 20); //$NON-NLS-1$
-		fIco.setFormEntryListener(new FormEntryAdapter(this, actionBars) {
-			public void textValueChanged(FormEntry entry) {
-			}
-		});
-		fIco.setEditable(isEditable());
+		fIconMap.put(ILauncherInfo.P_ICO_PATH, new IconEntry(comp, toolkit, "File:", ILauncherInfo.P_ICO_PATH));
 		
-		button = toolkit.createButton(comp, "Specify six '.bmp' images for both low (16-color) and high (256-color) graphics", SWT.RADIO);
+		button = toolkit.createButton(comp, "Specify six BMP images for both low (16-color) and high (256-color) graphics", SWT.RADIO);
 		gd = new GridData();
 		gd.horizontalSpan = 3;
 		button.setLayoutData(gd);
-		
-		f16Low = new FormEntry(comp, toolkit, "16x16 (Low):", "Browse...", isEditable(), 20); //$NON-NLS-1$
-		f16Low.setFormEntryListener(new FormEntryAdapter(this, actionBars) {
-			public void textValueChanged(FormEntry entry) {
-			}
-		});
-		f16Low.setEditable(isEditable());
 
-		f16High = new FormEntry(comp, toolkit, "16x16 (High):", "Browse...", isEditable(), 20); //$NON-NLS-1$
-		f16High.setFormEntryListener(new FormEntryAdapter(this, actionBars) {
-			public void textValueChanged(FormEntry entry) {
-			}
-		});
-		f16High.setEditable(isEditable());
+		fIconMap.put(ILauncherInfo.WIN32_16_LOW, new IconEntry(comp, toolkit, "16x16 (Low):", ILauncherInfo.WIN32_16_LOW));
+		fIconMap.put(ILauncherInfo.WIN32_16_HIGH, new IconEntry(comp, toolkit, "16x16 (High):", ILauncherInfo.WIN32_16_HIGH));
+		fIconMap.put(ILauncherInfo.WIN32_32_LOW, new IconEntry(comp, toolkit, "32x32 (Low):", ILauncherInfo.WIN32_32_LOW));
+		fIconMap.put(ILauncherInfo.WIN32_32_HIGH, new IconEntry(comp, toolkit, "32x32 (High):", ILauncherInfo.WIN32_32_HIGH));
+		fIconMap.put(ILauncherInfo.WIN32_48_LOW, new IconEntry(comp, toolkit, "48x48 (Low):", ILauncherInfo.WIN32_48_LOW));
+		fIconMap.put(ILauncherInfo.WIN32_48_HIGH, new IconEntry(comp, toolkit, "48x48 (High):", ILauncherInfo.WIN32_48_HIGH));
 
-		f32Low = new FormEntry(comp, toolkit, "32x32 (Low):", "Browse...", isEditable(), 20); //$NON-NLS-1$
-		f32Low.setFormEntryListener(new FormEntryAdapter(this, actionBars) {
-			public void textValueChanged(FormEntry entry) {
-			}
-		});
-		f32Low.setEditable(isEditable());
-
-		f32High = new FormEntry(comp, toolkit, "32x32 (High):", "Browse...", isEditable(), 20); //$NON-NLS-1$
-		f32High.setFormEntryListener(new FormEntryAdapter(this, actionBars) {
-			public void textValueChanged(FormEntry entry) {
-			}
-		});
-		f32High.setEditable(isEditable());
-
-		f48Low = new FormEntry(comp, toolkit, "48x48 (Low):", "Browse...", isEditable(), 20); //$NON-NLS-1$
-		f48Low.setFormEntryListener(new FormEntryAdapter(this, actionBars) {
-			public void textValueChanged(FormEntry entry) {
-			}
-		});
-		f48Low.setEditable(isEditable());
-
-		f48High = new FormEntry(comp, toolkit, "48x48 (High):", "Browse...", isEditable(), 20); //$NON-NLS-1$
-		f48High.setFormEntryListener(new FormEntryAdapter(this, actionBars) {
-			public void textValueChanged(FormEntry entry) {
-			}
-		});
-		f48High.setEditable(isEditable());
 		toolkit.paintBordersFor(comp);
-		ec.setClient(comp);
 	}
 	
-	private void addLinuxSection(Composite parent, FormToolkit toolkit, IActionBars actionBars) {
-		ExpandableComposite ec = toolkit.createExpandableComposite(parent, ExpandableComposite.TWISTIE|ExpandableComposite.CLIENT_INDENT);
-		ec.setText("linux");
-			
-		GridData gd = new GridData(GridData.FILL_BOTH);
-		gd.horizontalSpan = 2;
-		ec.setLayoutData(gd);
-		ec.addExpansionListener(new ExpansionAdapter() {
-			public void expansionStateChanged(ExpansionEvent e) {
-				getPage().getManagedForm().reflow(true);
-			}
-		});	
-		
-		Composite comp = toolkit.createComposite(ec);
-		comp.setLayout(new GridLayout(3, false));
-		comp.setLayoutData(new GridData(GridData.FILL_BOTH));
-		
-		Label label = toolkit.createLabel(comp, "A single XPM icon is required:");
-		gd = new GridData();
-		gd.horizontalSpan = 3;
+	private void createLabel(Composite parent, FormToolkit toolkit, String text, int span) {
+		Label label = toolkit.createLabel(parent, text);
+		GridData gd = new GridData();
+		gd.horizontalSpan = span;
 		label.setLayoutData(gd);
-		
-		fLinuxIcon = new FormEntry(comp, toolkit, "Icon:", "Browse...", isEditable()); //$NON-NLS-1$
-		fLinuxIcon.setFormEntryListener(new FormEntryAdapter(this, actionBars) {
-			public void textValueChanged(FormEntry entry) {
-			}
-		});
-		fLinuxIcon.setEditable(isEditable());
-		toolkit.paintBordersFor(comp);
-		ec.setClient(comp);
-		
-	}
-
-	private void addSolarisSection(Composite parent, FormToolkit toolkit, IActionBars actionBars) {
-		ExpandableComposite ec = toolkit.createExpandableComposite(parent, ExpandableComposite.TWISTIE|ExpandableComposite.CLIENT_INDENT);
-		ec.setText("solaris");
-		
-		GridData gd = new GridData(GridData.FILL_BOTH);
-		gd.horizontalSpan = 2;
-		ec.setLayoutData(gd);
-		ec.addExpansionListener(new ExpansionAdapter() {
-			public void expansionStateChanged(ExpansionEvent e) {
-				getPage().getManagedForm().reflow(true);
-			}
-		});	
-		
-		Composite comp = toolkit.createComposite(ec);
-		comp.setLayout(new GridLayout(3, false));
-		comp.setLayoutData(new GridData(GridData.FILL_BOTH));
-		
-		Label label = toolkit.createLabel(comp, "Four PM icons are required:");
-		gd = new GridData();
-		gd.horizontalSpan = 3;
-		label.setLayoutData(gd);
-		
-		fLargeIcon = new FormEntry(comp, toolkit, "Large:", "Browse...", isEditable()); //$NON-NLS-1$
-		fLargeIcon.setFormEntryListener(new FormEntryAdapter(this, actionBars) {
-			public void textValueChanged(FormEntry entry) {
-			}
-		});
-		fLargeIcon.setEditable(isEditable());
-
-		fMediumIcon = new FormEntry(comp, toolkit, "Medium:", "Browse...", isEditable()); //$NON-NLS-1$
-		fMediumIcon.setFormEntryListener(new FormEntryAdapter(this, actionBars) {
-			public void textValueChanged(FormEntry entry) {
-			}
-		});
-		fMediumIcon.setEditable(isEditable());
-
-		fSmallIcon = new FormEntry(comp, toolkit, "Small:", "Browse...", isEditable()); //$NON-NLS-1$
-		fSmallIcon.setFormEntryListener(new FormEntryAdapter(this, actionBars) {
-			public void textValueChanged(FormEntry entry) {
-			}
-		});
-		fSmallIcon.setEditable(isEditable());
-
-		fTinyIcon = new FormEntry(comp, toolkit, "Tiny:", "Browse...", isEditable()); //$NON-NLS-1$
-		fTinyIcon.setFormEntryListener(new FormEntryAdapter(this, actionBars) {
-			public void textValueChanged(FormEntry entry) {
-			}
-		});
-		fTinyIcon.setEditable(isEditable());
-
-		toolkit.paintBordersFor(comp);
-		ec.setClient(comp);
 	}
 	
-	private void addMacSection(Composite parent, FormToolkit toolkit, IActionBars actionBars) {
+	private void addLinuxSection(Composite parent, FormToolkit toolkit) {
+		Composite comp = createComposite(parent, toolkit, "linux");
+		createLabel(comp, toolkit, "A single XPM icon is required:", 3);	
+		fIconMap.put(ILauncherInfo.LINUX_ICON, new IconEntry(comp, toolkit, "Icon:", ILauncherInfo.LINUX_ICON));
+		toolkit.paintBordersFor(comp);
+	}
+
+	private void addSolarisSection(Composite parent, FormToolkit toolkit) {
+		Composite comp = createComposite(parent, toolkit, "solaris");
+		createLabel(comp, toolkit, "Four PM icons are required:", 3);
+
+		fIconMap.put(ILauncherInfo.SOLARIS_LARGE, new IconEntry(comp, toolkit, "Large:", ILauncherInfo.SOLARIS_LARGE));
+		fIconMap.put(ILauncherInfo.SOLARIS_MEDIUM, new IconEntry(comp, toolkit, "Medium:", ILauncherInfo.SOLARIS_MEDIUM));
+		fIconMap.put(ILauncherInfo.SOLARIS_SMALL, new IconEntry(comp, toolkit, "Small:", ILauncherInfo.SOLARIS_SMALL));
+		fIconMap.put(ILauncherInfo.SOLARIS_TINY, new IconEntry(comp, toolkit, "Tiny:", ILauncherInfo.SOLARIS_TINY));
+		
+		toolkit.paintBordersFor(comp);
+	}
+	
+	private void addMacSection(Composite parent, FormToolkit toolkit) {
+		Composite comp = createComposite(parent, toolkit, "macosx");		
+		createLabel(comp, toolkit, "A single ICNS file must be specified:", 3);		
+		fIconMap.put(ILauncherInfo.MACOSX_ICON, new IconEntry(comp, toolkit, "File:", ILauncherInfo.MACOSX_ICON));
+		toolkit.paintBordersFor(comp);
+	}
+	
+	private Composite createComposite(Composite parent, FormToolkit toolkit, String text) {
 		ExpandableComposite ec = toolkit.createExpandableComposite(parent, ExpandableComposite.TWISTIE|ExpandableComposite.CLIENT_INDENT);
-		ec.setText("macosx");
+		ec.setText(text);
 		
 		GridData gd = new GridData(GridData.FILL_BOTH);
 		gd.horizontalSpan = 2;
@@ -252,28 +146,11 @@ public class LauncherSection extends PDESection {
 				getPage().getManagedForm().reflow(true);
 			}
 		});	
-		
 		Composite comp = toolkit.createComposite(ec);
 		comp.setLayout(new GridLayout(3, false));
 		comp.setLayoutData(new GridData(GridData.FILL_BOTH));
-		
-		Label label = toolkit.createLabel(comp, "A single '*.icns' file must be specified:");
-		gd = new GridData();
-		gd.horizontalSpan = 3;
-		label.setLayoutData(gd);
-		
-		fMacIcon = new FormEntry(comp, toolkit, "Icon:", "Browse...", isEditable()); //$NON-NLS-1$
-		fMacIcon.setFormEntryListener(new FormEntryAdapter(this, actionBars) {
-			public void textValueChanged(FormEntry entry) {
-			}
-		});
-		fMacIcon.setEditable(isEditable());
-		
-		toolkit.paintBordersFor(comp);
 		ec.setClient(comp);
+		return comp;
 	}
-
-
-
 
 }
