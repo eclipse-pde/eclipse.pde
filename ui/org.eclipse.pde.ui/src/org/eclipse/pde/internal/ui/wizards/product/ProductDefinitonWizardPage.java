219775c352422df8132fe705b69ff27bd678b142
Wassim Melhem <wmelhem>
2005-02-03 15:40:38 +0000

    *** empty log message ***

--- ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/wizards/product/ProductDefinitonWizardPage.java
+++ ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/wizards/product/ProductDefinitonWizardPage.java
@@ -1,27 +1,36 @@
 package org.eclipse.pde.internal.ui.wizards.product;
 
+import java.util.*;
+
+import org.eclipse.core.runtime.*;
 import org.eclipse.jface.wizard.*;
 import org.eclipse.pde.core.plugin.*;
 import org.eclipse.pde.internal.core.*;
 import org.eclipse.pde.internal.ui.*;
+import org.eclipse.pde.internal.ui.search.*;
 import org.eclipse.pde.internal.ui.util.*;
+import org.eclipse.pde.internal.ui.wizards.*;
 import org.eclipse.swt.*;
 import org.eclipse.swt.events.*;
 import org.eclipse.swt.layout.*;
 import org.eclipse.swt.widgets.*;
+import org.eclipse.ui.forms.events.*;
 import org.eclipse.ui.forms.widgets.*;
 
-public class ProductDefinitonWizardPage extends WizardPage {
+public class ProductDefinitonWizardPage extends WizardPage implements IHyperlinkListener {
 
 	private Text fPluginText;
 	private Text fProductText;
+	private Set fProductSet;
+	private Combo fApplicationCombo;
 
 	private ModifyListener fListener = new ModifyListener() {
 		public void modifyText(ModifyEvent e) {
 			validatePage();
 		}
 	};
-
+	
+	
 	public ProductDefinitonWizardPage(String pageName) {
 		super(pageName);
 		setTitle(PDEPlugin.getResourceString("ProductDefinitonWizardPage.title")); //$NON-NLS-1$
@@ -49,6 +58,7 @@ public class ProductDefinitonWizardPage extends WizardPage {
 		gd.horizontalSpan = span;
 		text.setLayoutData(gd);
 		text.setBackground(null);
+		text.addHyperlinkListener(this);
 	}
 
 	private void createProductGroup(FormToolkit toolkit, Composite comp) {
@@ -68,6 +78,11 @@ public class ProductDefinitonWizardPage extends WizardPage {
 		Button button = new Button(group, SWT.PUSH);
 		button.setText(PDEPlugin.getResourceString("ProductDefinitonWizardPage.browse")); //$NON-NLS-1$
 		SWTUtil.setButtonDimensionHint(button);
+		button.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {
+				handleBrowse();
+			}
+		});
 		
 		label = new Label(group, SWT.NONE);
 		label.setText(PDEPlugin.getResourceString("ProductDefinitonWizardPage.productId")); //$NON-NLS-1$
@@ -91,11 +106,11 @@ public class ProductDefinitonWizardPage extends WizardPage {
 		Label label = new Label(group, SWT.NONE);
 		label.setText(PDEPlugin.getResourceString("ProductDefinitonWizardPage.application")); //$NON-NLS-1$
 		
-		Combo combo = new Combo(group, SWT.SINGLE|SWT.READ_ONLY);
-		combo.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
-		combo.setItems(TargetPlatform.getApplicationNames());
-		if (combo.getItemCount() > 0)
-			combo.setText(combo.getItem(0));	
+		fApplicationCombo = new Combo(group, SWT.SINGLE|SWT.READ_ONLY);
+		fApplicationCombo.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		fApplicationCombo.setItems(TargetPlatform.getApplicationNames());
+		if (fApplicationCombo.getItemCount() > 0)
+			fApplicationCombo.setText(fApplicationCombo.getItem(0));	
 	}
 	
 	public void setVisible(boolean visible) {
@@ -117,8 +132,10 @@ public class ProductDefinitonWizardPage extends WizardPage {
 			else if (model.getUnderlyingResource() == null)
 				error = PDEPlugin.getResourceString("ProductDefinitonWizardPage.notInWorkspace"); //$NON-NLS-1$
 		}
-		if (error == null) {
+		if (error == null)
 			error = validateId();
+		if (error == null && getProductNameSet().contains(pluginId + "." + fProductText.getText().trim())) { //$NON-NLS-1$
+			error = PDEPlugin.getResourceString("ProductDefinitonWizardPage.productExists"); //$NON-NLS-1$
 		}
 		setErrorMessage(error);
 		setPageComplete(error == null);
@@ -136,7 +153,43 @@ public class ProductDefinitonWizardPage extends WizardPage {
 		return null;
 	}
 
+	public void linkEntered(HyperlinkEvent e) {
+	}
 
+	public void linkExited(HyperlinkEvent e) {
+	}
+
+	public void linkActivated(HyperlinkEvent e) {
+		String extPoint = Platform.PI_RUNTIME + "." + e.getHref().toString(); //$NON-NLS-1$
+		IPluginExtensionPoint point = PDECore.getDefault().findExtensionPoint(extPoint);
+		if (point != null)
+			new ShowDescriptionAction(point).run();
+	}
+
+	private void handleBrowse() {
+		PluginSelectionDialog dialog = new PluginSelectionDialog(getShell(), PDECore.getDefault().getWorkspaceModelManager().getAllModels(), false);
+		if (dialog.open() == PluginSelectionDialog.OK) {
+			IPluginModelBase model = (IPluginModelBase)dialog.getFirstResult();
+			fPluginText.setText(model.getPluginBase().getId());
+		}
+	}
 	
-
+	private Set getProductNameSet() {
+		if (fProductSet == null)
+			fProductSet = TargetPlatform.getProductNameSet();
+		return fProductSet;
+	}
+	
+	public String getDefiningPlugin() {
+		return fPluginText.getText().trim();
+	}
+	
+	public String getProductId() {
+		return fProductText.getText().trim();
+	}
+	
+	public String getApplication() {
+		return fApplicationCombo.getText();
+	}
+	
 }
