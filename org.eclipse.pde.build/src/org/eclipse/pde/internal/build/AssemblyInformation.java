9ddbd7324f745cdaa6a94ceec535e7bc39228136
Pascal Rapicault <prapicau>
2005-03-23 19:18:40 +0000

    Fix an NPE

--- org.eclipse.pde.build/src/org/eclipse/pde/internal/build/AssemblyInformation.java
+++ org.eclipse.pde.build/src/org/eclipse/pde/internal/build/AssemblyInformation.java
@@ -53,7 +53,7 @@ public class AssemblyInformation implements IPDEBuildConstants {
 		for (Iterator iter = allPlugins.iterator(); iter.hasNext();) {
 			BundleDescription bundle = (BundleDescription) iter.next();
 			Properties bundleProperties = ((Properties) bundle.getUserObject());
-			if (bundleProperties == null || bundleProperties.get(IS_COMPILED).equals(Boolean.FALSE))
+			if (bundleProperties == null || Boolean.FALSE == bundleProperties.get(IS_COMPILED))
 				result.add(bundle);
 		}
 		return result;
@@ -65,7 +65,7 @@ public class AssemblyInformation implements IPDEBuildConstants {
 		for (Iterator iter = allPlugins.iterator(); iter.hasNext();) {
 			BundleDescription bundle = (BundleDescription) iter.next();
 			Properties bundleProperties = ((Properties) bundle.getUserObject());
-			if (bundleProperties != null && bundleProperties.get(IS_COMPILED).equals(Boolean.TRUE))
+			if (bundleProperties != null && Boolean.TRUE == bundleProperties.get(IS_COMPILED))
 				result.add(bundle);
 		}
 		return result;
