9e73378a61caca896f85f880de59eefcc92a37af
kkolosow <>
2005-04-08 13:38:30 +0000

    *** empty log message ***

--- ui/org.eclipse.pde.core/src/org/eclipse/pde/internal/core/PDECoreMessages.java
+++ ui/org.eclipse.pde.core/src/org/eclipse/pde/internal/core/PDECoreMessages.java
@@ -13,17 +13,7 @@ import org.eclipse.osgi.util.NLS;
 
 public class PDECoreMessages extends NLS {
 	private static final String BUNDLE_NAME = "org.eclipse.pde.internal.core.pderesources";//$NON-NLS-1$
-	//
-	// Copyright (c) 2000, 2005 IBM Corporation and others.
-	// All rights reserved. This program and the accompanying materials
-	// are made available under the terms of the Eclipse Public License v1.0
-	// which accompanies this distribution, and is available at
-	// http://www.eclipse.org/legal/epl-v10.html
-	//
-	// Contributors:
-	//     IBM Corporation - initial API and implementation
-	//
-	//
+
 	public static String PluginModelManager_outOfSync;
 
 	// Status text #####################################
