c0dd399bb3c501a9f4c0c4007ee2aafc93b75fa4
kkolosow <>
2005-04-09 03:00:42 +0000

    3.1 way of NLSing

--- ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/PDEUIMessages.java
+++ ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/PDEUIMessages.java
@@ -13,21 +13,6 @@ import org.eclipse.osgi.util.NLS;
 
 public class PDEUIMessages extends NLS {
 	private static final String BUNDLE_NAME = "org.eclipse.pde.internal.ui.pderesources";//$NON-NLS-1$
-	//
-	// Copyright (c) 2000, 2005 IBM Corporation and others.
-	// All rights reserved. This program and the accompanying materials
-	// are made available under the terms of the Eclipse Public License v1.0
-	// which accompanies this distribution, and is available at
-	// http://www.eclipse.org/legal/epl-v10.html
-	//
-	// Contributors:
-	//     IBM Corporation - initial API and implementation
-	//
-	//
-	// PDE resource strings
-	// Part 1.    (DO NOT TRANSLATE Part 1)
-	// These are the non-translable properties.
-	//
 
 	public static String ProjectStructurePage_sourceName;
 	public static String ProjectNamesPage_duplicateNames;
