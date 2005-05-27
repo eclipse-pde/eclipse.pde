c3d34cc02ded5a25dbb4742075e3462f934d06ac
Wassim Melhem <wmelhem>
2005-05-27 03:01:35 +0000

    bug 96887

--- ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/editor/plugin/ManifestEditorMatchingStrategy.java
+++ ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/editor/plugin/ManifestEditorMatchingStrategy.java
@@ -12,20 +12,18 @@
 package org.eclipse.pde.internal.ui.editor.plugin;
 
 import org.eclipse.core.resources.IFile;
+import org.eclipse.pde.internal.ui.editor.JarEntryEditorInput;
+import org.eclipse.pde.internal.ui.editor.SystemFileEditorInput;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorMatchingStrategy;
 import org.eclipse.ui.IEditorReference;
-import org.eclipse.ui.IFileEditorInput;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.ide.ResourceUtil;
 
 
 public class ManifestEditorMatchingStrategy implements IEditorMatchingStrategy {
 
-    public boolean matches(IEditorReference editorRef, IEditorInput input) {
-    	if (!(input instanceof IFileEditorInput))
-    		return false;
-    	
+    public boolean matches(IEditorReference editorRef, IEditorInput input) {    	
         IFile inputFile = ResourceUtil.getFile(input);
         if (inputFile != null) {
             String path = inputFile.getProjectRelativePath().toString();
@@ -38,6 +36,12 @@ public class ManifestEditorMatchingStrategy implements IEditorMatchingStrategy {
                     return false;
                 }
             }
+        } else if (input instanceof SystemFileEditorInput || input instanceof JarEntryEditorInput) {
+        	try {
+    			IEditorInput existing = editorRef.getEditorInput();
+    			return input.equals(existing);
+    		} catch (PartInitException e1) {
+    		}      	
         }
         return false;
     }
