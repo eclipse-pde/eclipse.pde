7a64df09a3b0ae740fc1d7bb8bb96d861911be1e
Wassim Melhem <wmelhem>
2005-06-07 18:07:17 +0000

    *** empty log message ***

--- ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/wizards/exports/ProductExportWizard.java
+++ ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/wizards/exports/ProductExportWizard.java
@@ -89,7 +89,10 @@ public class ProductExportWizard extends BaseExportWizard {
 		else
 			info.items = getPluginModels();
 		
-		ProductExportJob job = new ProductExportJob(info, fProductModel, page.getRootDirectory());
+		String rootDirectory = page.getRootDirectory();
+		if ("".equals(rootDirectory.trim())) //$NON-NLS-1 //$NON-NLS-1$
+			rootDirectory = "."; //$NON-NLS-1 //$NON-NLS-1$
+		ProductExportJob job = new ProductExportJob(info, fProductModel, rootDirectory);
 		job.setUser(true);
 		job.schedule();
 		job.setProperty(IProgressConstants.ICON_PROPERTY, PDEPluginImages.DESC_FEATURE_OBJ);
