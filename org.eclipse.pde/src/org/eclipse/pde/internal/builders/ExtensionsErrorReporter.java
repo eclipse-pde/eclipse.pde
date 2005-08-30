7b7ef137f4add1107cd4a3d8df2e6eab1829c470
Wassim Melhem <wmelhem>
2005-08-30 04:40:24 +0000

    *** empty log message ***

--- org.eclipse.pde/src/org/eclipse/pde/internal/builders/ExtensionsErrorReporter.java
+++ org.eclipse.pde/src/org/eclipse/pde/internal/builders/ExtensionsErrorReporter.java
@@ -436,10 +436,10 @@ public class ExtensionsErrorReporter extends ManifestErrorReporter {
 			if (index != -1)
 				value = value.substring(0, index);
 
+			if (value.indexOf('$') != -1)
+				value = value.replace('$', '.');
 			IType javaType = javaProject.findType(value);
-			if (javaType == null && value.indexOf('$') != -1)
-				javaType = javaProject.findType(value.replace('$', '.'));
-		
+
 			if (javaType == null) {
 				report(NLS.bind(PDEMessages.Builders_Manifest_class, (new String[] { value, attr.getName() })), getLine(
 						element, attr.getName()), severity);
