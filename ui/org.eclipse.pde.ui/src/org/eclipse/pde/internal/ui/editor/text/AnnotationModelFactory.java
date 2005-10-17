dbd1f7255a62acc7f7de85fd52d2f1f8b8b76e3a
Wassim Melhem <wmelhem>
2005-10-17 07:42:07 +0000

    *** empty log message ***

---
+++ ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/editor/text/AnnotationModelFactory.java
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ * Copyright (c) 2005 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.pde.internal.ui.editor.text;
+
+import org.eclipse.core.filebuffers.FileBuffers;
+import org.eclipse.core.filebuffers.IAnnotationModelFactory;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.jface.text.source.IAnnotationModel;
+import org.eclipse.pde.internal.ui.editor.SystemFileMarkerAnnotationModel;
+import org.eclipse.ui.texteditor.ResourceMarkerAnnotationModel;
+
+public class AnnotationModelFactory implements IAnnotationModelFactory {
+
+	public IAnnotationModel createAnnotationModel(IPath location) {
+		IFile file = FileBuffers.getWorkspaceFileAtLocation(location);
+		if (file == null) 
+			return new SystemFileMarkerAnnotationModel();
+		
+		return new ResourceMarkerAnnotationModel(file);
+	}
+
+}
