52994b359813eaa6cb700d8bd3b30c8b32b05717
Wassim Melhem <wmelhem>
2005-11-23 07:07:08 +0000

    *** empty log message ***

--- ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/editor/schema/DescriptionSection.java
+++ ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/editor/schema/DescriptionSection.java
@@ -16,26 +16,20 @@ import org.eclipse.jface.text.Document;
 import org.eclipse.jface.text.DocumentEvent;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.IDocumentListener;
-import org.eclipse.jface.text.IDocumentPartitioner;
-import org.eclipse.jface.text.rules.FastPartitioner;
 import org.eclipse.jface.text.source.SourceViewer;
-import org.eclipse.jface.text.source.SourceViewerConfiguration;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.ISelectionChangedListener;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.SelectionChangedEvent;
-import org.eclipse.pde.core.IEditable;
-import org.eclipse.pde.internal.core.ischema.ISchema;
 import org.eclipse.pde.internal.core.ischema.ISchemaObject;
 import org.eclipse.pde.internal.core.ischema.ISchemaObjectReference;
-import org.eclipse.pde.internal.core.schema.Schema;
 import org.eclipse.pde.internal.core.schema.SchemaObject;
 import org.eclipse.pde.internal.ui.PDEUIMessages;
 import org.eclipse.pde.internal.ui.editor.PDEFormPage;
 import org.eclipse.pde.internal.ui.editor.PDESection;
+import org.eclipse.pde.internal.ui.editor.context.XMLDocumentSetupParticpant;
 import org.eclipse.pde.internal.ui.editor.text.IColorManager;
 import org.eclipse.pde.internal.ui.editor.text.XMLConfiguration;
-import org.eclipse.pde.internal.ui.editor.text.XMLPartitionScanner;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.dnd.Clipboard;
 import org.eclipse.swt.events.FocusAdapter;
@@ -51,35 +45,25 @@ import org.eclipse.ui.forms.widgets.FormToolkit;
 import org.eclipse.ui.forms.widgets.Section;
 
 public class DescriptionSection extends PDESection implements IPartSelectionListener {
-	private IDocument document;
-	private boolean editable = true;
-	private SourceViewerConfiguration sourceConfiguration;
-	private ISchemaObject element;
-	private SourceViewer sourceViewer;
-	private IDocumentPartitioner partitioner;
-	private ISchema schema;
-	private boolean ignoreChange = false;
+	private SourceViewer fSourceViewer;
+	private IDocument fDocument;
+	private ISchemaObject fElement;
+	private boolean fIgnoreChange;
+	private IColorManager fColorManager;
 
 	public DescriptionSection(PDEFormPage page, Composite parent, IColorManager colorManager) {
 		super(page, parent, Section.DESCRIPTION);
+		fColorManager = colorManager;
 		getSection().setText(PDEUIMessages.SchemaEditor_DescriptionSection_title);
 		getSection().setDescription(PDEUIMessages.SchemaEditor_DescriptionSection_desc);
-		sourceConfiguration = new XMLConfiguration(colorManager);
-		document = new Document();
-		partitioner =
-			new FastPartitioner(
-				new XMLPartitionScanner(),
-				new String[] {
-					XMLPartitionScanner.XML_TAG,
-					XMLPartitionScanner.XML_COMMENT });
-		partitioner.connect(document);
-		document.setDocumentPartitioner(partitioner);
 		createClient(getSection(), page.getManagedForm().getToolkit());
 	}
+	
 	public void commit(boolean onSave) {
 		updateDescription();
 		super.commit(onSave);
 	}
+	
 	public void createClient(
 		Section section,
 		FormToolkit toolkit) {
@@ -89,21 +73,19 @@ public class DescriptionSection extends PDESection implements IPartSelectionList
 		layout.marginWidth = 2;
 		container.setLayout(layout);
 		GridData gd;
-		int styles =
-			SWT.MULTI
-				| SWT.WRAP
-				| SWT.V_SCROLL
-				| SWT.H_SCROLL;
-		sourceViewer = new SourceViewer(container, null, styles);
-		sourceViewer.configure(sourceConfiguration);
-		sourceViewer.setDocument(document);
-		sourceViewer.setEditable(isEditable());
-		sourceViewer.addSelectionChangedListener(new ISelectionChangedListener() {
+		fSourceViewer = new SourceViewer(container, null, SWT.MULTI|SWT.WRAP|SWT.V_SCROLL| SWT.H_SCROLL);
+		fSourceViewer.configure(new XMLConfiguration(fColorManager));
+		fDocument = new Document();
+		new XMLDocumentSetupParticpant().setup(fDocument);
+		fSourceViewer.setDocument(fDocument);
+		fSourceViewer.setEditable(isEditable());
+		fSourceViewer.addSelectionChangedListener(new ISelectionChangedListener() {
 			public void selectionChanged(SelectionChangedEvent event) {
 				updateSelection(event.getSelection());
 			}
 		});
-		Control styledText = sourceViewer.getTextWidget();
+		
+		Control styledText = fSourceViewer.getTextWidget();
 		styledText.setFont(JFaceResources.getFontRegistry().get(JFaceResources.TEXT_FONT));
 		if (SWT.getPlatform().equals("motif") == false) //$NON-NLS-1$
 			toolkit.paintBordersFor(container);
@@ -112,12 +94,12 @@ public class DescriptionSection extends PDESection implements IPartSelectionList
 		Control control = children[children.length - 1];
 		gd = new GridData(GridData.FILL_BOTH);
 		gd.widthHint = 200;
-		gd.heightHint = 90;
+		gd.heightHint = 120;
 		control.setLayoutData(gd);
 		styledText.setMenu(getPage().getPDEEditor().getContextMenu());
 		styledText.addFocusListener(new FocusAdapter() {
 			public void focusGained(FocusEvent e) {
-				updateSelection(sourceViewer.getSelection());
+				updateSelection(fSourceViewer.getSelection());
 			}
 		});
 		section.setClient(container);
@@ -130,53 +112,51 @@ public class DescriptionSection extends PDESection implements IPartSelectionList
 
 	public boolean doGlobalAction(String actionId) {
 		if (actionId.equals(ActionFactory.CUT.getId())) {
-			sourceViewer.doOperation(SourceViewer.CUT);
+			fSourceViewer.doOperation(SourceViewer.CUT);
 			return true;
 		} else if (
 			actionId.equals(ActionFactory.COPY.getId())) {
-			sourceViewer.doOperation(SourceViewer.COPY);
+			fSourceViewer.doOperation(SourceViewer.COPY);
 			return true;
 		} else if (
 			actionId.equals(ActionFactory.PASTE.getId())) {
-			sourceViewer.doOperation(SourceViewer.PASTE);
+			fSourceViewer.doOperation(SourceViewer.PASTE);
 			return true;
 		} else if (
 			actionId.equals(ActionFactory.SELECT_ALL.getId())) {
-			sourceViewer.doOperation(SourceViewer.SELECT_ALL);
+			fSourceViewer.doOperation(SourceViewer.SELECT_ALL);
 			return true;
 		} else if (
 			actionId.equals(ActionFactory.DELETE.getId())) {
-			sourceViewer.doOperation(SourceViewer.DELETE);
+			fSourceViewer.doOperation(SourceViewer.DELETE);
 			return true;
 		} else if (
 			actionId.equals(ActionFactory.UNDO.getId())) {
-			sourceViewer.doOperation(SourceViewer.UNDO);
+			fSourceViewer.doOperation(SourceViewer.UNDO);
 			return true;
 		} else if (
 			actionId.equals(ActionFactory.REDO.getId())) {
-			sourceViewer.doOperation(SourceViewer.REDO);
+			fSourceViewer.doOperation(SourceViewer.REDO);
 			return true;
 		}
 		return false;
 	}
+	
 	protected void fillContextMenu(IMenuManager manager) {
 		getPage().getPDEEditor().getContributor().contextMenuAboutToShow(manager);
 	}
+	
 	private void updateDescription() {
-		if (element != null) {
-			if (element == schema)
-				((Schema)schema).setDescription(document.get());
-			else
-				((SchemaObject) element).setDescription(document.get());
+		if (fElement instanceof SchemaObject) {
+			((SchemaObject)fElement).setDescription(fDocument.get());
 		}
 	}
+	
 	public void initialize() {
-		schema = (ISchema) getPage().getModel();
-		element = schema;
 		updateDocument();
-		document.addDocumentListener(new IDocumentListener() {
+		fDocument.addDocumentListener(new IDocumentListener() {
 			public void documentChanged(DocumentEvent e) {
-				if (!ignoreChange && schema instanceof IEditable) {
+				if (!fIgnoreChange && getPage().getPDEEditor().getAggregateModel().isEditable()) {
 					markDirty();
 				}
 			}
@@ -184,38 +164,33 @@ public class DescriptionSection extends PDESection implements IPartSelectionList
 			}
 		});
 	}
-	public boolean isEditable() {
-		return editable;
-	}
+
 	public void selectionChanged(IFormPart part, ISelection selection) {
 		if (!(part instanceof ElementSection))
 			return;
 		Object changeObject = ((IStructuredSelection)selection).getFirstElement();
-		if (changeObject != element && isDirty())
+		if (changeObject != fElement && isDirty())
 			updateDescription();
-		element = (ISchemaObject) changeObject;
-		if (element instanceof ISchemaObjectReference)
-			element = ((ISchemaObjectReference)element).getReferencedObject();
-		if (element == null)
-			element = schema;
+		fElement = (ISchemaObject) changeObject;
+		if (fElement instanceof ISchemaObjectReference)
+			fElement = ((ISchemaObjectReference)fElement).getReferencedObject();
 		updateDocument();
 	}
+	
 	public void setFocus() {
-		sourceViewer.getTextWidget().setFocus();
+		fSourceViewer.getTextWidget().setFocus();
 	}
-	public void setEditable(boolean newEditable) {
-		editable = newEditable;
-	}
+
 	public void updateDocument() {
-		ignoreChange = true;
-		String text = element.getDescription();
-		if (text == null)
-			text = ""; //$NON-NLS-1$
-		document.set(text);
-		ignoreChange = false;
+		if (fElement != null) {
+			fIgnoreChange = true;
+			String text = fElement.getDescription();
+			fDocument.set(text == null ? "" : text); //$NON-NLS-1$
+			fIgnoreChange = false;
+		}
 	}
 
 	public boolean canPaste(Clipboard clipboard) {
-		return sourceViewer.canDoOperation(SourceViewer.PASTE);
+		return fSourceViewer.canDoOperation(SourceViewer.PASTE);
 	}
 }
