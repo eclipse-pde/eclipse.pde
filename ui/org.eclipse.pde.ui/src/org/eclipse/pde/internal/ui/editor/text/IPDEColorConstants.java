9b701afd75b6035c1ca4d00a1fc70c8c62c4761c
Wassim Melhem <wmelhem>
2005-11-25 08:09:15 +0000

    *** empty log message ***

--- ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/editor/text/IPDEColorConstants.java
+++ ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/editor/text/IPDEColorConstants.java
@@ -19,10 +19,10 @@ public interface IPDEColorConstants {
 	RGB DEFAULT =           new RGB(  0,   0,   0);
 	RGB TAG =               new RGB(  0,   0, 128);
 	
-	RGB HEADER_KEY = 		new RGB(128,  64,  64);
+	RGB HEADER_KEY = 		new RGB(128,   0,   0);
 	RGB HEADER_VALUE = 		new RGB(  0,   0,   0);
 	RGB HEADER_ASSIGNMENT = new RGB(  0,   0,   0);
-	RGB HEADER_OSGI		  = new RGB(128,  64,  64);
+	RGB HEADER_OSGI		  = new RGB(128,   0,   0);
 	RGB HEADER_ATTRIBUTES = new RGB(128, 128,   0);
 	
 	String P_BOLD_SUFFIX = "_bold"; //$NON-NLS-1$
