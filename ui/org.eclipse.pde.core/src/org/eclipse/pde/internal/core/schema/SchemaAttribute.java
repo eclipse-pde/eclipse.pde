cc60e18bfed9f198000e1c1018c9c1e91731939a
Wassim Melhem <wmelhem>
2005-12-12 23:19:02 +0000

    *** empty log message ***

--- ui/org.eclipse.pde.core/src/org/eclipse/pde/internal/core/schema/SchemaAttribute.java
+++ ui/org.eclipse.pde.core/src/org/eclipse/pde/internal/core/schema/SchemaAttribute.java
@@ -99,7 +99,7 @@ public class SchemaAttribute extends SchemaObject implements ISchemaAttribute {
 	}
 	
 	public void setTranslatableProperty(boolean translatable) {
-		if (getKind() == STRING && type.getName().equals("string")) { //$NON-NLS-1$
+		if (getKind() == STRING && "string".equals(type.getName())) { //$NON-NLS-1$
 			boolean oldValue = fTranslatable;
 			fTranslatable = translatable;
 			getSchema().fireModelObjectChanged(this, P_TRANSLATABLE,
