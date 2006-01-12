31121ef65edc89bf909fb1590778da0fdc1c9538
Wassim Melhem <wmelhem>
2006-01-12 09:04:41 +0000

    *** empty log message ***

---
+++ ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/launcher/PDESourceLookupDirector.java
@@ -0,0 +1,49 @@
+/*******************************************************************************
+ * Copyright (c) 2005 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.pde.internal.ui.launcher;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.eclipse.debug.core.sourcelookup.AbstractSourceLookupDirector;
+import org.eclipse.debug.core.sourcelookup.ISourceContainerType;
+import org.eclipse.debug.core.sourcelookup.ISourceLookupParticipant;
+import org.eclipse.debug.core.sourcelookup.containers.ProjectSourceContainer;
+import org.eclipse.debug.core.sourcelookup.containers.WorkspaceSourceContainer;
+import org.eclipse.debug.ui.sourcelookup.WorkingSetSourceContainer;
+import org.eclipse.jdt.launching.sourcelookup.containers.JavaSourceLookupParticipant;
+
+public class PDESourceLookupDirector extends AbstractSourceLookupDirector {
+
+	private static Set fFilteredTypes;
+	
+	static {
+		fFilteredTypes = new HashSet(3);
+		fFilteredTypes.add(ProjectSourceContainer.TYPE_ID);
+		fFilteredTypes.add(WorkspaceSourceContainer.TYPE_ID);
+		fFilteredTypes.add(WorkingSetSourceContainer.TYPE_ID); //$NON-NLS-1$
+	}
+	
+	/* (non-Javadoc)
+	 * @see org.eclipse.debug.internal.core.sourcelookup.ISourceLookupDirector#initializeParticipants()
+	 */
+	public void initializeParticipants() {
+		addParticipants(new ISourceLookupParticipant[] {new JavaSourceLookupParticipant()});
+	}
+	
+	/* (non-Javadoc)
+	 * @see org.eclipse.debug.internal.core.sourcelookup.ISourceLookupDirector#supportsSourceContainerType(org.eclipse.debug.internal.core.sourcelookup.ISourceContainerType)
+	 */
+	public boolean supportsSourceContainerType(ISourceContainerType type) {
+		return !fFilteredTypes.contains(type.getId());
+	}
+
+}
