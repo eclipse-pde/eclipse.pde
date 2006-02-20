49bdee3dc887be7098637df02f98ab883061569a
janeklb <janeklb>
2006-02-20 19:09:05 +0000

    Bug 128518 should "mark internal" default filter be *.internal*

--- ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/wizards/tools/IOrganizeManifestsSettings.java
+++ ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/wizards/tools/IOrganizeManifestsSettings.java
@@ -5,7 +5,7 @@ public interface IOrganizeManifestsSettings {
 	public static final String PROP_ADD_MISSING = "OrganizeManifests.ExportedPackages.addMissing"; //$NON-NLS-1$
 	public static final String PROP_MARK_INTERNAL = "OrganizeManifests.ExportedPackages.makeInternal"; //$NON-NLS-1$
 	public static final String PROP_INTERAL_PACKAGE_FILTER = "OrganizeManifests.ExportedPackages.packageFilter"; //$NON-NLS-1$
-	public static final String VALUE_DEFAULT_FILTER = "*.internal.*"; //$NON-NLS-1$
+	public static final String VALUE_DEFAULT_FILTER = "*.internal*"; //$NON-NLS-1$
 	public static final String PROP_REMOVE_UNRESOLVED_EX = "OrganizeManifests.ExportedPackages.removeUnresolved"; //$NON-NLS-1$
 	public static final String PROP_MODIFY_DEP = "OrganizeManifests.RequireImport.modifyDep"; //$NON-NLS-1$
 	public static final String PROP_RESOLVE_IMP_MARK_OPT = "OrganizeManifests.RequireImport.resolve:markOptional"; //$NON-NLS-1$
