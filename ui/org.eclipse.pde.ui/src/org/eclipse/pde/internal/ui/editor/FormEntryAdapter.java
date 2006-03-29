6057965011be5e3e8c455ce409a0516a8d570860
Wassim Melhem <wmelhem>
2006-03-29 08:47:22 +0000

    removed extraneous comments

--- ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/editor/FormEntryAdapter.java
+++ ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/editor/FormEntryAdapter.java
@@ -8,12 +8,6 @@
  * Contributors:
  *     IBM Corporation - initial API and implementation
  *******************************************************************************/
-/*
- * Created on Feb 27, 2004
- *
- * To change the template for this generated file go to
- * Window - Preferences - Java - Code Generation - Code and Comments
- */
 package org.eclipse.pde.internal.ui.editor;
 
 import org.eclipse.jface.action.IStatusLineManager;
