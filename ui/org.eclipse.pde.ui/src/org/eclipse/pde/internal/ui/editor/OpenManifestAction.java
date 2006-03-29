29eb238e8f54bb74abca97edfa569254df44a073
Wassim Melhem <wmelhem>
2006-03-29 08:46:22 +0000

    added copyright

--- ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/editor/OpenManifestAction.java
+++ ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/editor/OpenManifestAction.java
@@ -1,4 +1,18 @@
+/*******************************************************************************
+ * Copyright (c) 2006 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *     Alex Blewitt (alex_blewitt@yahoo.com) - contributed a patch for:
+ *       o Add an 'Open Manifest' to projects to open the manifest editor
+ *         (see https://bugs.eclipse.org/bugs/show_bug.cgi?id=133692)
+ *******************************************************************************/
 package org.eclipse.pde.internal.ui.editor;
+
 import java.util.ArrayList;
 import java.util.Iterator;
 import org.eclipse.core.resources.IFile;
