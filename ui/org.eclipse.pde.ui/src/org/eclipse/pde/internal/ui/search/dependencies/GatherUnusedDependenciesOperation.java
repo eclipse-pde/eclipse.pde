77bc17b1aa81b546952ff2452e998dae0d231de8
Wassim Melhem <wmelhem>
2006-04-07 00:00:38 +0000

    bug 128683 - Organize Manifest does not detect and remove all re-exported bundles

--- ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/search/dependencies/GatherUnusedDependenciesOperation.java
+++ ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/search/dependencies/GatherUnusedDependenciesOperation.java
@@ -12,6 +12,10 @@ package org.eclipse.pde.internal.ui.search.dependencies;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.ListIterator;
+import java.util.Stack;
 
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
@@ -37,6 +41,7 @@ import org.eclipse.pde.core.plugin.IPlugin;
 import org.eclipse.pde.core.plugin.IPluginImport;
 import org.eclipse.pde.core.plugin.IPluginModelBase;
 import org.eclipse.pde.internal.core.ClasspathUtilCore;
+import org.eclipse.pde.internal.core.PDECore;
 import org.eclipse.pde.internal.core.ibundle.IBundle;
 import org.eclipse.pde.internal.core.ibundle.IBundlePluginModelBase;
 import org.eclipse.pde.internal.core.ibundle.IManifestHeader;
@@ -81,19 +86,22 @@ public class GatherUnusedDependenciesOperation implements IRunnableWithProgress 
 		}
 		IPluginImport[] imports = fModel.getPluginBase().getImports();
 	
-		int totalWork = imports.length * 3 + (packages != null ? packages.length : 0);
+		int totalWork = imports.length * 3 + (packages != null ? packages.length : 0) + 1;
 		monitor.beginTask("", totalWork); //$NON-NLS-1$
 
+		HashMap usedPlugins = new HashMap();
 		fList = new ArrayList();
 		for (int i = 0; i < imports.length; i++) {
 			if (monitor.isCanceled())
 				break;
 			if (isUnused(imports[i], new SubProgressMonitor(monitor, 3))) {
 				fList.add(imports[i]);
-			}
+			} else 
+				usedPlugins.put(imports[i].getId(), imports[i]);
 			updateMonitor(monitor, fList.size());
 		}
 		
+		ArrayList usedPackages = new ArrayList();
 		if (packages != null && !monitor.isCanceled()) {
 			for (int i = 0; i < packages.length; i++) {
 				if (monitor.isCanceled())
@@ -101,9 +109,13 @@ public class GatherUnusedDependenciesOperation implements IRunnableWithProgress 
 				if (isUnused(packages[i], new SubProgressMonitor(monitor, 1))) {
 					fList.add(packages[i]);
 					updateMonitor(monitor, fList.size());
-				}
+				} else 
+					usedPackages.add(packages[i]);
 			}
 		}
+		if (!monitor.isCanceled())
+			minimizeDependencies(usedPlugins, usedPackages, monitor);
+		monitor.done();
 	}
 
 	private void updateMonitor(IProgressMonitor monitor, int size) {
@@ -246,6 +258,38 @@ public class GatherUnusedDependenciesOperation implements IRunnableWithProgress 
 					pkgHeader = (ImportPackageHeader)((ImportPackageObject)elements[i]).getHeader();
 				pkgHeader.removePackage((ImportPackageObject)elements[i]);			
 			}
+		}
+	}
+	
+	private void minimizeDependencies(HashMap usedPlugins, ArrayList usedPackages, IProgressMonitor monitor) {
+		ListIterator li = usedPackages.listIterator();
+		while (li.hasNext()) {
+			ImportPackageObject ipo = (ImportPackageObject) li.next();
+			String bundle = ipo.getAttribute(Constants.BUNDLE_SYMBOLICNAME_ATTRIBUTE);
+			if (usedPlugins.containsKey(bundle))
+				fList.add(ipo);
+		}
+		
+		Iterator it = usedPlugins.keySet().iterator();
+		Stack plugins = new Stack();
+		while (it.hasNext())
+			plugins.push(it.next().toString());
+		
+		while (!(plugins.isEmpty())) {
+			String pluginId = (String)plugins.pop();
+			IPluginModelBase base = PDECore.getDefault().getModelManager().findModel(pluginId);
+			IPluginImport[] imports = base.getPluginBase().getImports();
+			
+			for (int j = 0; j < imports.length; j++) 
+				if (imports[j].isReexported()) {
+					String reExportedId = imports[j].getId();
+					Object pluginImport = usedPlugins.remove(imports[j].getId());
+					if (pluginImport != null) {
+						fList.add(pluginImport);
+						updateMonitor(monitor, fList.size());
+					}
+					plugins.push(reExportedId);
+				}
 		}
 	}
 }
