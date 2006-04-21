6933a5f1648eccc63ad12f398b3c95292c1e55f3
Andrew Niefer <aniefer>
2006-04-21 16:54:15 +0000

    bug 136503 - cache PDEPropeties for performance

--- org.eclipse.pde.build/src/org/eclipse/pde/internal/build/properties/PDEProperties.java
+++ org.eclipse.pde.build/src/org/eclipse/pde/internal/build/properties/PDEProperties.java
@@ -11,7 +11,10 @@
 package org.eclipse.pde.internal.build.properties;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import org.eclipse.ant.core.IAntPropertyValueProvider;
+import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.Platform;
 
 public class PDEProperties implements IAntPropertyValueProvider {
@@ -19,27 +22,34 @@ public class PDEProperties implements IAntPropertyValueProvider {
 	static private final String HOME = PREFIX + ".home"; //$NON-NLS-1$
 	static private final String SCRIPTS = PREFIX + ".scripts"; //$NON-NLS-1$
 	static private final String TEMPLATES = PREFIX + ".templates"; //$NON-NLS-1$
-	
+	static private final Map cache = new HashMap();
+
 	public String getAntPropertyValue(String antPropertyName) {
 		String searchedEntry = null;
 		if (HOME.equals(antPropertyName))
 			searchedEntry = "."; //$NON-NLS-1$
-		
+
 		if (SCRIPTS.equals(antPropertyName))
 			searchedEntry = "scripts"; //$NON-NLS-1$
-		
+
 		if (TEMPLATES.equals(antPropertyName))
 			searchedEntry = "templates"; //$NON-NLS-1$
-		
+
 		if (searchedEntry == null)
-			return null; //TODO Throw an expcetion or log an error
-		
+			return null; //TODO Throw an exception or log an error
+
 		try {
-			return Platform.asLocalURL(Platform.getBundle("org.eclipse.pde.build").getEntry(searchedEntry)).getPath(); //$NON-NLS-1$
+			String result = (String) cache.get(searchedEntry);
+			if (result == null) {
+				result = FileLocator.toFileURL(Platform.getBundle("org.eclipse.pde.build").getEntry(searchedEntry)).getPath(); //$NON-NLS-1$
+				if (result != null)
+					cache.put(searchedEntry, result);
+			}
+			return result;
 		} catch (IOException e) {
 			return null;
 		}
-		
+
 	}
 
 }
