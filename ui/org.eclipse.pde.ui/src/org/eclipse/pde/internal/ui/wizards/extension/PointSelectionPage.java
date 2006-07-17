93505c8c805171ab34d243747214d657d3f3f1f8
Wassim Melhem <wmelhem>
2006-07-17 03:09:49 +0000

    bug 148687 - Extension Point enhancement

--- ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/wizards/extension/PointSelectionPage.java
+++ ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/wizards/extension/PointSelectionPage.java
@@ -128,6 +128,9 @@ public class PointSelectionPage
 	class WildcardFilter extends ViewerFilter {
 		private String wMatch = "*"; //$NON-NLS-1$
 		protected void setMatchText(String match) {
+			if (match.indexOf("*") < 0 & match.indexOf("?") < 0 & match.indexOf(".") < 0) { //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+				match = "*." + match; //$NON-NLS-1$
+			}
 			wMatch = match + "*"; //$NON-NLS-1$
 		}
 		public boolean select(Viewer viewer, Object parentElement, Object element) {
