13cf0e783e0a116ee14bc8ce214fd9c3ed471ede
Wassim Melhem <wmelhem>
2006-07-24 14:31:10 +0000

    disabled tests until we write reliable ones

--- ui/org.eclipse.pde.ui.tests/src/org/eclipse/pde/ui/tests/performance/PDEPerformanceTests.java
+++ ui/org.eclipse.pde.ui.tests/src/org/eclipse/pde/ui/tests/performance/PDEPerformanceTests.java
@@ -13,19 +13,16 @@ package org.eclipse.pde.ui.tests.performance;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
-import org.eclipse.pde.ui.tests.performance.parts.InitializeModelsPerfTest;
-import org.eclipse.pde.ui.tests.performance.parts.OpenManifestEditorPerfTest;
-
 public class PDEPerformanceTests {
 
 	public static Test suite() {
 		TestSuite suite = new TestSuite("Performance Test Suite for org.eclipse.pde.ui"); //$NON-NLS-1$
-		suite.addTest(OpenManifestEditorPerfTest.suite());
+		//suite.addTest(OpenManifestEditorPerfTest.suite());
 		// removing the schema test for now, as it is bogus
 		// the set of schemas being read are not the same between 3.1 and 3.2
 		// will revise and re-enable post-3.2 when we have a constant set of schemas to parse
 		//suite.addTest(SchemaPerfTest.suite());
-		suite.addTest(InitializeModelsPerfTest.suite());
+		//suite.addTest(InitializeModelsPerfTest.suite());
 		return suite;
 	}
 
