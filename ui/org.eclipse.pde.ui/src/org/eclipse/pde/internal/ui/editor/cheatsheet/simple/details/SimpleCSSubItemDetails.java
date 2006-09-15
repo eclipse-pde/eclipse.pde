dfa522b1826d219383749141e42b90a536fd52ed
Mike Pawlowski <>
2006-09-15 01:25:42 +0000

    157132 - [Cheat Sheet Authoring] Glitches in Cheatsheet editor

--- ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/editor/cheatsheet/simple/details/SimpleCSSubItemDetails.java
+++ ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/editor/cheatsheet/simple/details/SimpleCSSubItemDetails.java
@@ -12,7 +12,6 @@
 package org.eclipse.pde.internal.ui.editor.cheatsheet.simple.details;
 
 import org.eclipse.pde.internal.core.icheatsheet.simple.ISimpleCSSubItem;
-import org.eclipse.pde.internal.core.util.PDEHTMLHelper;
 import org.eclipse.pde.internal.ui.PDEUIMessages;
 import org.eclipse.pde.internal.ui.editor.FormEntryAdapter;
 import org.eclipse.pde.internal.ui.editor.PDESection;
@@ -136,10 +135,7 @@ public class SimpleCSSubItemDetails extends SimpleCSAbstractDetails {
 		fLabel.setFormEntryListener(new FormEntryAdapter(this) {
 			public void textValueChanged(FormEntry entry) {
 				// TODO: MP: Can when ever be null?
-				// TODO: MP: Check all text fields and see what we can strip
-				// i.e. trailing and leading space, duplicate spaces, newlines, tabs, carriage returns
-//				 TODO: MP: HIGH: Replace with internal utility stripper
-				fSubItem.setLabel(PDEHTMLHelper.stripTags(fLabel.getValue()));
+				fSubItem.setLabel(fLabel.getValue());
 			}
 		});	
 		// Attribute: skip
