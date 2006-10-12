c04cbd6bfb1b31319b90db6a70daa5126e54d806
Brian Bauman <bbauman>
2006-10-12 17:49:38 +0000

    Bug 155062 - <pde.exportFeatures> task's "destination" attribute does not accept relative a relative path

--- ui/org.eclipse.pde.core/src_ant/org/eclipse/pde/internal/core/ant/PluginExportTask.java
+++ ui/org.eclipse.pde.core/src_ant/org/eclipse/pde/internal/core/ant/PluginExportTask.java
@@ -10,6 +10,7 @@
  *******************************************************************************/
 package org.eclipse.pde.internal.core.ant;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
@@ -28,10 +29,16 @@ public class PluginExportTask extends BaseExportTask {
 		info.toDirectory = fToDirectory;
 		info.useJarFormat = fUseJarFormat;
 		info.exportSource = fExportSource;
-		info.destinationDirectory = fDestination;
 		info.zipFileName = fZipFilename;
 		info.items = fPlugins;
 		info.qualifier = fQualifier;
+		// if destination is relative, then make it absolute
+		if (!new File(fDestination).isAbsolute()) {
+			File home = new File(getLocation().getFileName()).getParentFile();
+			info.destinationDirectory = new File(home, fDestination).toString();
+		}
+		else
+			info.destinationDirectory = fDestination;
 		return new PluginExportOperation(info);
 	}
 	
