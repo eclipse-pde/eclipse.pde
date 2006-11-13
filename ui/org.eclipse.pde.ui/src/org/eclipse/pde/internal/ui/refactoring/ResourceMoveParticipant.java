6238bd2912165e16ad468b3b18d1288e7a00174b
Brian Bauman <bbauman>
2006-11-13 21:10:51 +0000

    copyrights

--- ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/refactoring/ResourceMoveParticipant.java
+++ ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/refactoring/ResourceMoveParticipant.java
@@ -10,6 +10,16 @@ import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.ltk.core.refactoring.Change;
 import org.eclipse.ltk.core.refactoring.CompositeChange;
+/*******************************************************************************
+ * Copyright (c) 2006 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
 import org.eclipse.pde.internal.core.WorkspaceModelManager;
 
 public abstract class ResourceMoveParticipant extends PDEMoveParticipant {
