310ba3b77382826f8da4bc8d9e77ba47eb5fe245
Brian Bauman <bbauman>
2006-12-20 17:55:24 +0000

    Bug 167529 - Plug-ins Search option should be available when working with PDE perspective

---
+++ ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/search/OpenPluginSearchPageAction.java
@@ -0,0 +1,43 @@
+package org.eclipse.pde.internal.ui.search;
+
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.pde.internal.ui.PDEPlugin;
+import org.eclipse.search.ui.NewSearchUI;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.IWorkbenchWindowActionDelegate;
+
+public class OpenPluginSearchPageAction implements
+		IWorkbenchWindowActionDelegate {
+	
+	private static final String PLUGIN_SEARCH_PAGE_ID = "org.eclipse.pde.internal.ui.search.SearchPage"; //$NON-NLS-1$
+	private IWorkbenchWindow fWindow;
+
+	public void dispose() {
+		fWindow = null;
+	}
+
+	public void init(IWorkbenchWindow window) {
+		fWindow = window;
+	}
+
+	public void run(IAction action) {
+		if (fWindow == null || fWindow.getActivePage() == null) {
+			beep();
+			return;
+		}
+		NewSearchUI.openSearchDialog(fWindow, PLUGIN_SEARCH_PAGE_ID);
+	}
+
+	public void selectionChanged(IAction action, ISelection selection) {
+		// do nothing since the action isn't selection dependent.
+	}
+	
+	protected void beep() {
+		Shell shell= PDEPlugin.getActiveWorkbenchShell();
+		if (shell != null && shell.getDisplay() != null)
+			shell.getDisplay().beep();
+	}	
+
+}
