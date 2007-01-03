fdb91e462d559231df5aa4cda8fb3b1f28e6cf55
Chris Aniszczyk <caniszczyk>
2007-01-03 21:42:19 +0000

    more code additions

--- ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/wizards/provisioner/update/UpdateSitePage.java
+++ ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/wizards/provisioner/update/UpdateSitePage.java
@@ -11,12 +11,28 @@
 package org.eclipse.pde.internal.ui.wizards.provisioner.update;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.pde.internal.ui.PDEUIMessages;
+import org.eclipse.pde.internal.ui.util.SWTUtil;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
 
 public class UpdateSitePage extends WizardPage {
+
+	private List fElements = new ArrayList();
+	private Button fAddButton;
+	private Button fRemoveButton;
+	private TreeViewer fTreeViewer;
 
 	protected UpdateSitePage(String pageName) {
 		super(pageName);
@@ -26,15 +42,69 @@ public class UpdateSitePage extends WizardPage {
 	}
 
 	public void createControl(Composite parent) {
+		Composite client = new Composite(parent, SWT.NONE);
+		GridLayout layout = new GridLayout();
+		layout.marginWidth = 2;
+		layout.numColumns = 2;
+		client.setLayout(layout);
+		client.setLayoutData(new GridData(GridData.FILL_BOTH));
+
+		Label label = new Label(client, SWT.None);
+		label.setText(PDEUIMessages.DirectorySelectionPage_label);
+		GridData gd = new GridData();
+		gd.horizontalSpan = 2;
+		label.setLayoutData(gd);
+
+		fTreeViewer = new TreeViewer(client, SWT.VIRTUAL);
+		fTreeViewer.setInput(fElements);
+
+
 		// TODO Auto-generated method stub
 
 	}
-	
+
+	protected void createButtons(Composite parent) {
+		fAddButton = new Button(parent, SWT.PUSH);
+		fAddButton.setText(PDEUIMessages.DirectorySelectionPage_add);
+		fAddButton.setLayoutData(new GridData(GridData.VERTICAL_ALIGN_BEGINNING));
+		SWTUtil.setButtonDimensionHint(fAddButton);
+		fAddButton.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {
+				handleAdd();
+			}
+		});
+
+		fRemoveButton = new Button(parent, SWT.PUSH);
+		fRemoveButton.setText(PDEUIMessages.DirectorySelectionPage_remove);
+		fRemoveButton.setLayoutData(new GridData(GridData.VERTICAL_ALIGN_BEGINNING));
+		SWTUtil.setButtonDimensionHint(fRemoveButton);
+		fRemoveButton.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {
+				handleRemove();
+			}
+		});
+		updateButtons();		
+	}
+
+	protected void handleRemove() {
+		// TODO Auto-generated method stub
+
+	}
+
+	protected void handleAdd() {
+		// TODO Auto-generated method stub
+
+	}
+
+	protected void updateButtons() {
+		int num = fTreeViewer.getTree().getSelectionCount();
+		fRemoveButton.setEnabled(num > 0);
+	}
+
 	public File[] getLocations() {
 //		Preferences pref = PDECore.getDefault().getPluginPreferences();
 //		pref.setValue(LAST_LOCATION, fLastLocation);
-//		return (File[]) fElements.toArray(new File[fElements.size()]);
-		return null;
+		return (File[]) fElements.toArray(new File[fElements.size()]);
 	}
 
 }
