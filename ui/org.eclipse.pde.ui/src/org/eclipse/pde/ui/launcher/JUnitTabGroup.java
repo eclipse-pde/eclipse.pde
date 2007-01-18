5424fd4d89e93cf973ee0ecc6305d407923ece4b
Wassim Melhem <wmelhem>
2007-01-18 21:04:44 +0000

    removed useless VMAssertionArg

--- ui/org.eclipse.pde.ui/src/org/eclipse/pde/ui/launcher/JUnitTabGroup.java
+++ ui/org.eclipse.pde.ui/src/org/eclipse/pde/ui/launcher/JUnitTabGroup.java
@@ -10,15 +10,12 @@
  *******************************************************************************/
 package org.eclipse.pde.ui.launcher;
 
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
 import org.eclipse.debug.ui.CommonTab;
 import org.eclipse.debug.ui.EnvironmentTab;
 import org.eclipse.debug.ui.ILaunchConfigurationDialog;
 import org.eclipse.debug.ui.ILaunchConfigurationTab;
 import org.eclipse.jdt.debug.ui.launchConfigurations.JavaArgumentsTab;
 import org.eclipse.jdt.junit.launcher.JUnitLaunchConfigurationTab;
-import org.eclipse.jdt.launching.IJavaLaunchConfigurationConstants;
 
 /**
  * Creates and initializes the tabs for the Plug-in JUnit test launch configuration.
@@ -47,21 +44,4 @@ public class JUnitTabGroup extends AbstractPDELaunchConfigurationTabGroup {
 		setTabs(tabs);
 	}
 	
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.pde.internal.ui.launcher.AbstractPDELaunchConfigurationTabGroup#setDefaults(org.eclipse.debug.core.ILaunchConfigurationWorkingCopy)
-	 */
-	public void setDefaults(ILaunchConfigurationWorkingCopy configuration) {
-		super.setDefaults(configuration);
-		
-		String vmArgs;
-		try {
-			vmArgs = configuration.getAttribute(IJavaLaunchConfigurationConstants.ATTR_VM_ARGUMENTS, ""); //$NON-NLS-1$
-		} catch (CoreException e) {
-			vmArgs= ""; //$NON-NLS-1$
-		}
-		if (vmArgs.length() > 0)
-			configuration.setAttribute(IJavaLaunchConfigurationConstants.ATTR_VM_ARGUMENTS, vmArgs);
-	}
-
 }
