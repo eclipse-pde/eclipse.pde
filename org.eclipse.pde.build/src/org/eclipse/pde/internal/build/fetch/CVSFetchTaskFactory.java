6e1ade30e5df35fdc1ef69ab03410fb1c4af900b
DJ Houghton <dj>
2007-02-05 17:09:03 +0000

    Bug 172683 - Add new arg to CVS fetch factory

--- org.eclipse.pde.build/src/org/eclipse/pde/internal/build/fetch/CVSFetchTaskFactory.java
+++ org.eclipse.pde.build/src/org/eclipse/pde/internal/build/fetch/CVSFetchTaskFactory.java
@@ -21,8 +21,18 @@ import org.eclipse.pde.internal.build.*;
  * An <code>FetchTaskFactory</code> for building fetch scripts that will
  * fetch content from a CVS repository (id: <code>CVS</code>).
  * <p>
- * Map file arguments:
- * <code>&lt;TAG&gt;,&lt;CVSROOT&gt;[,&lt;PASSWORD&gt;[,&lt;PATH&gt;[,&lt;CVSPASSFILE&gt;]]]</code>
+ * <code><pre>
+ * Map file format:
+ * 	type@id,[version]=CVS,args
+ * args is a comma-separated list of key/value pairs
+ * Accepted args include:
+ * 	cvsPassFile - optional password file
+ * 	cvsRoot - mandatory repo location
+ * 	password - optional password
+ * 	path - optional path relative to the cvsRoot
+ * 	prebuilt - optional boolean value indicating that the entry points to a pre-built bundle in the repository
+ * 	tag - mandatory CVS tag
+ * </pre></code>
  * </p>
  */
 public class CVSFetchTaskFactory implements IFetchFactory {
@@ -37,6 +47,7 @@ public class CVSFetchTaskFactory implements IFetchFactory {
 	private static final String KEY_CVSPASSFILE = "cvsPassFile"; //$NON-NLS-1$
 	private static final String KEY_PASSWORD = "password"; //$NON-NLS-1$
 	private static final String KEY_PATH = "path"; //$NON-NLS-1$
+	private static final String KEY_PREBUILT = "prebuilt"; //$NON-NLS-1$
 
 	//Properties used in the CVS part of the scripts
 	private static final String PROP_DESTINATIONFOLDER = "destinationFolder"; //$NON-NLS-1$
@@ -57,30 +68,50 @@ public class CVSFetchTaskFactory implements IFetchFactory {
 	public void generateRetrieveElementCall(Map entryInfos, IPath destination, IAntScript script) {
 		String type = (String) entryInfos.get(KEY_ELEMENT_TYPE);
 		String element = (String) entryInfos.get(KEY_ELEMENT_NAME);
+		boolean prebuilt = "true".equalsIgnoreCase((String) entryInfos.get(KEY_PREBUILT));
 
 		Map params = new HashMap(5);
-		// we directly export the CVS content into the destination
-		params.put(PROP_DESTINATIONFOLDER, destination.uptoSegment(destination.segmentCount() - 1).toString());
+		// we directly export the CVS content into the destination. if we have a pre-built JAR then
+		// we want to put it right in the /plugins directory and not a sub-directory so strip off 2 segments
+		// to leave us with the build directory (/plugins will be added by the "element" attribute)
+		int remove = prebuilt ? 2 : 1;
+		params.put(PROP_DESTINATIONFOLDER, destination.removeLastSegments(remove).toString());
 		params.put(PROP_TAG, entryInfos.get(IFetchFactory.KEY_ELEMENT_TAG));
 		params.put(PROP_CVSROOT, entryInfos.get(KEY_CVSROOT));
 		params.put(PROP_QUIET, "${cvs.quiet}"); //$NON-NLS-1$
+		// the call to CVS requires us to pass a destination directory for the files that we are
+		// retrieving, so give it the /plugins dir here
+		if (prebuilt) {
+			if (type.equals(ELEMENT_TYPE_PLUGIN))
+				element = IPDEBuildConstants.DEFAULT_PLUGIN_LOCATION;
+			else if (type.equals(ELEMENT_TYPE_FEATURE))
+				element = IPDEBuildConstants.DEFAULT_FEATURE_LOCATION;
+		}
 		params.put(PROP_ELEMENTNAME, element);
-		params.put(PROP_MODULE, (entryInfos.get(KEY_PATH) == null ? element : (String) entryInfos.get(KEY_PATH)));
+		String module = entryInfos.get(KEY_PATH) == null ? element : (String) entryInfos.get(KEY_PATH);
+		params.put(PROP_MODULE, module);
 
 		IPath locationToCheck = (IPath) destination.clone();
-		if (type.equals(ELEMENT_TYPE_FEATURE)) {
-			locationToCheck = locationToCheck.append(Constants.FEATURE_FILENAME_DESCRIPTOR);
-		} else if (type.equals(ELEMENT_TYPE_PLUGIN)) {
-			locationToCheck = locationToCheck.append(Constants.PLUGIN_FILENAME_DESCRIPTOR);
-		} else if (type.equals(ELEMENT_TYPE_FRAGMENT)) {
-			locationToCheck = locationToCheck.append(Constants.FRAGMENT_FILENAME_DESCRIPTOR);
-		} else if (type.equals(ELEMENT_TYPE_BUNDLE)) {
-			locationToCheck = locationToCheck.append(Constants.BUNDLE_FILENAME_DESCRIPTOR);
+		// if we have a pre-built plug-in then we want to check the existence of the JAR file
+		// rather than the plug-in manifest.
+		if (prebuilt) {
+			locationToCheck = locationToCheck.removeLastSegments(1);
+			locationToCheck = locationToCheck.append(new Path(module).lastSegment());
+		} else {
+			if (type.equals(ELEMENT_TYPE_FEATURE)) {
+				locationToCheck = locationToCheck.append(Constants.FEATURE_FILENAME_DESCRIPTOR);
+			} else if (type.equals(ELEMENT_TYPE_PLUGIN)) {
+				locationToCheck = locationToCheck.append(Constants.PLUGIN_FILENAME_DESCRIPTOR);
+			} else if (type.equals(ELEMENT_TYPE_FRAGMENT)) {
+				locationToCheck = locationToCheck.append(Constants.FRAGMENT_FILENAME_DESCRIPTOR);
+			} else if (type.equals(ELEMENT_TYPE_BUNDLE)) {
+				locationToCheck = locationToCheck.append(Constants.BUNDLE_FILENAME_DESCRIPTOR);
+			}
 		}
 		params.put(PROP_FILETOCHECK, locationToCheck.toString());
 
 		printAvailableTask(locationToCheck.toString(), locationToCheck.toString(), script);
-		if (type.equals(IFetchFactory.ELEMENT_TYPE_PLUGIN) || type.equals(IFetchFactory.ELEMENT_TYPE_FRAGMENT)) {
+		if (!prebuilt&& (type.equals(IFetchFactory.ELEMENT_TYPE_PLUGIN) || type.equals(IFetchFactory.ELEMENT_TYPE_FRAGMENT))) {
 			printAvailableTask(locationToCheck.toString(), locationToCheck.removeLastSegments(1).append(Constants.BUNDLE_FILENAME_DESCRIPTOR).toString(), script);
 		}
 
@@ -99,7 +130,10 @@ public class CVSFetchTaskFactory implements IFetchFactory {
 			} else {
 				filePath = new Path((String) entryInfos.get(KEY_ELEMENT_NAME)).append(file);
 			}
-			printCVSTask("export -r " + (String) entryInfos.get(IFetchFactory.KEY_ELEMENT_TAG) + ' ' + filePath.toString(), (String) entryInfos.get(KEY_CVSROOT), destination.toString(), null, null, "true", null, null, script);  //$NON-NLS-1$//$NON-NLS-2$
+			String tag = (String) entryInfos.get(IFetchFactory.KEY_ELEMENT_TAG);
+			String cvsRoot = (String) entryInfos.get(KEY_CVSROOT);
+			String dest = "true".equalsIgnoreCase((String) entryInfos.get(KEY_PREBUILT)) ? destination.toString() : destination.removeLastSegments(1).toString();
+			printCVSTask("export -r " + tag + ' ' + filePath.toString(), cvsRoot, dest, null, null, "true", null, null, script);  //$NON-NLS-1$//$NON-NLS-2$
 			script.println("<move file=\"" + destination + '/' + filePath + "\"" + " tofile=\"" + destination.append(file) + "\" failonerror=\"false\" />");  //$NON-NLS-1$//$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
 		}
 	}
@@ -110,6 +144,20 @@ public class CVSFetchTaskFactory implements IFetchFactory {
 		script.printTargetEnd();
 	}
 
+	/*
+	 * Handle the old file format:
+	 * Map file arguments:
+ 	 * <code>&lt;TAG&gt;,&lt;CVSROOT&gt;[,&lt;PASSWORD&gt;[,&lt;PATH&gt;[,&lt;CVSPASSFILE&gt;]]]</code>
+	 */
+	private void legacyParseMapFileEntry(String[] arguments, Properties overrideTags, Map entryInfos) {
+		String overrideTag = overrideTags.getProperty(OVERRIDE_TAG);
+		entryInfos.put(KEY_CVSPASSFILE, (arguments.length > 4 && !arguments[4].equals("")) ? arguments[4] : null); //$NON-NLS-1$
+		entryInfos.put(IFetchFactory.KEY_ELEMENT_TAG, (overrideTag != null && overrideTag.trim().length() != 0 ? overrideTag : arguments[0]));
+		entryInfos.put(KEY_CVSROOT, arguments[1]);
+		entryInfos.put(KEY_PASSWORD, (arguments.length > 2 && !arguments[2].equals("")) ? arguments[2] : null); //$NON-NLS-1$
+		entryInfos.put(KEY_PATH, (arguments.length > 3 && !arguments[3].equals("")) ? arguments[3] : null); //$NON-NLS-1$ 
+	}
+
 	public void parseMapFileEntry(String repoSpecificentry, Properties overrideTags, Map entryInfos) throws CoreException {
 		String[] arguments = Utils.getArrayFromStringWithBlank(repoSpecificentry, SEPARATOR);
 		if (arguments.length < 2) {
@@ -117,12 +165,32 @@ public class CVSFetchTaskFactory implements IFetchFactory {
 			throw new CoreException(new Status(IStatus.ERROR, IPDEBuildConstants.PI_PDEBUILD, 1, message, null)); //TODO Need to fix this
 		}
 
+		// build up the table of arguments in the map file entry
+		Map table = new HashMap();
+		for (int i=0; i<arguments.length; i++) {
+			String arg = arguments[i];
+			// if we have at least one arg without an equals sign, then
+			// revert back to the legacy parsing
+			int index = arg.indexOf('=');
+			if (index == -1) {
+				legacyParseMapFileEntry(arguments, overrideTags, entryInfos);
+				return;
+			}
+			String key = arg.substring(0, index);
+			String value = arg.substring(index+1);
+			table.put(key, value);
+		}
+
+		// add entries to the entryInfo map here instead of inside the loop
+		// in case we revert to legacy parsing in the middle of the loop (we
+		// don't want to contaminate entryInfos
+		entryInfos.put(KEY_CVSPASSFILE, table.get(KEY_CVSPASSFILE));
 		String overrideTag = overrideTags.getProperty(OVERRIDE_TAG);
-		entryInfos.put(KEY_CVSPASSFILE, (arguments.length > 4 && !arguments[4].equals("")) ? arguments[4] : null); //$NON-NLS-1$
-		entryInfos.put(IFetchFactory.KEY_ELEMENT_TAG, (overrideTag != null && overrideTag.trim().length() != 0 ? overrideTag : arguments[0]));
-		entryInfos.put(KEY_CVSROOT, arguments[1]);
-		entryInfos.put(KEY_PASSWORD, (arguments.length > 2 && !arguments[2].equals("")) ? arguments[2] : null); //$NON-NLS-1$
-		entryInfos.put(KEY_PATH, (arguments.length > 3 && !arguments[3].equals("")) ? arguments[3] : null); //$NON-NLS-1$ 
+		entryInfos.put(IFetchFactory.KEY_ELEMENT_TAG, (overrideTag != null && overrideTag.trim().length() != 0 ? overrideTag : table.get(IFetchFactory.KEY_ELEMENT_TAG)));
+		entryInfos.put(KEY_CVSROOT, table.get(KEY_CVSROOT));
+		entryInfos.put(KEY_PASSWORD, table.get(KEY_PASSWORD));
+		entryInfos.put(KEY_PATH, table.get(KEY_PATH));
+		entryInfos.put(KEY_PREBUILT, table.get(KEY_PREBUILT));
 	}
 
 	/**
