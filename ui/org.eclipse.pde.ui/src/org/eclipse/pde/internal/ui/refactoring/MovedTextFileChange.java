57c3d4018b6ce397f4a68c9bfe843c86fd759700
Brian Bauman <bbauman>
2007-02-27 21:25:13 +0000

    Bug 166819 - Renaming project's name won't be renamed Bundle-SymbolicName
    - added new change because if you used TextFileChange, you would get errors when previewing.  This was because the file didn't exist until the rename operation took place.

---
+++ ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/refactoring/MovedTextFileChange.java
@@ -0,0 +1,55 @@
+package org.eclipse.pde.internal.ui.refactoring;
+
+import org.eclipse.core.filebuffers.FileBuffers;
+import org.eclipse.core.filebuffers.ITextFileBuffer;
+import org.eclipse.core.filebuffers.ITextFileBufferManager;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.ltk.core.refactoring.TextFileChange;
+
+/*
+ * Class is meant to be used to perform a TextFileChange on a file which will be moved during the refactoring execution.  This
+ * is useful for editing text files when a project is renamed, since the resource will be moved during the project refactoring.
+ */
+
+public class MovedTextFileChange extends TextFileChange {
+	
+	private IFile fCurrentFile;
+
+	public MovedTextFileChange(String name, IFile newFile, IFile currentFile) {
+		super(name, newFile);
+		fCurrentFile = currentFile;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ltk.core.refactoring.TextChange#getCurrentDocument(org.eclipse.core.runtime.IProgressMonitor)
+	 * 
+	 * Override getCurrentDocument to return the document of the fCurrentFile instead of the fFile.  Since fFile
+	 * does not exist, it creates problems when displaying preview information
+	 */
+	public IDocument getCurrentDocument(IProgressMonitor pm)
+			throws CoreException {
+		if (pm == null)
+			pm= new NullProgressMonitor();
+		IDocument result= null;
+		pm.beginTask("", 2); //$NON-NLS-1$
+		ITextFileBufferManager manager= FileBuffers.getTextFileBufferManager();
+		try{
+			IPath path= fCurrentFile.getFullPath();
+			manager.connect(path, pm);
+			ITextFileBuffer buffer = manager.getTextFileBuffer(path);
+			result= buffer.getDocument();
+		} finally {
+			if (result != null)
+				manager.disconnect(fCurrentFile.getFullPath(), pm);
+		}
+		pm.done();
+		return result;
+	}
+
+}
