2b3c3809779aa6799edadeb65892649efdba333c
Wassim Melhem <wmelhem>
2007-03-19 03:08:08 +0000

    Launcher Plugins tab facelift - part II

--- ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/launcher/PluginStatusDialog.java
+++ ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/launcher/PluginStatusDialog.java
@@ -108,20 +108,4 @@ public class PluginStatusDialog extends TrayDialog {
 		return super.close();
 	}
 
-	public Object[] getChildren(Object parentElement) {
-		return null;
-	}
-
-	public Object getParent(Object element) {
-		return null;
-	}
-
-	public boolean hasChildren(Object element) {
-		return false;
-	}
-
-	public Object[] getElements(Object inputElement) {
-		return null;
-	}
-
 }
