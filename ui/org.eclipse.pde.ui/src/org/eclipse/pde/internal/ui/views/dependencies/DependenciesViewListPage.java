d6ed09bc14be36aed2e5484d1697621b537a42bc
Brian Bauman <bbauman>
2007-04-10 03:59:34 +0000

    Bug 181523 - [Plug-in Dependencies View] Sorting is not correct
    Created custom sorter for dependencies view list

--- ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/views/dependencies/DependenciesViewListPage.java
+++ ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/views/dependencies/DependenciesViewListPage.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2006 IBM Corporation and others.
+ * Copyright (c) 2007 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -13,7 +13,12 @@ package org.eclipse.pde.internal.ui.views.dependencies;
 import org.eclipse.jface.viewers.IContentProvider;
 import org.eclipse.jface.viewers.StructuredViewer;
 import org.eclipse.jface.viewers.TableViewer;
-import org.eclipse.pde.internal.ui.wizards.ListUtil;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.jface.viewers.ViewerComparator;
+import org.eclipse.osgi.service.resolver.BundleDescription;
+import org.eclipse.osgi.service.resolver.BundleSpecification;
+import org.eclipse.osgi.service.resolver.ExportPackageDescription;
+import org.eclipse.osgi.service.resolver.ImportPackageSpecification;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.DisposeEvent;
 import org.eclipse.swt.events.DisposeListener;
@@ -21,6 +26,8 @@ import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Table;
 
 public class DependenciesViewListPage extends DependenciesViewPage {
+	
+	private static ViewerComparator fComparator = null;
 
 	/**
 	 * 
@@ -28,6 +35,27 @@ public class DependenciesViewListPage extends DependenciesViewPage {
 	public DependenciesViewListPage(DependenciesView view,
 			IContentProvider contentProvider) {
 		super(view, contentProvider);
+	}
+	
+	static class DependenciesComparator extends ViewerComparator {
+
+		public int compare(Viewer viewer, Object e1, Object e2) {
+			return getId(e1).compareTo(getId(e2));
+		}
+		
+		private String getId(Object obj) {
+			BundleDescription desc = null;
+			if (obj instanceof ImportPackageSpecification) {
+				desc = ((ExportPackageDescription)((ImportPackageSpecification)obj).getSupplier()).getSupplier();
+			} else if (obj instanceof BundleSpecification) {
+				desc = (BundleDescription)((BundleSpecification)obj).getSupplier();
+			} else if (obj instanceof BundleDescription)
+				desc = (BundleDescription)obj;
+			if (desc != null)
+				return desc.getSymbolicName();
+			return ""; //$NON-NLS-1$
+		}
+		
 	}
 
 	/*
@@ -48,8 +76,14 @@ public class DependenciesViewListPage extends DependenciesViewPage {
 				labelProvider.dispose();
 			}
 		});
-		fViewer.setComparator(ListUtil.PLUGIN_COMPARATOR);
+		fViewer.setComparator(getComparator());
 
 		return fViewer;
+	}
+	
+	private ViewerComparator getComparator() {
+		if (fComparator == null)
+			fComparator = new DependenciesComparator();
+		return fComparator;
 	}
 }
