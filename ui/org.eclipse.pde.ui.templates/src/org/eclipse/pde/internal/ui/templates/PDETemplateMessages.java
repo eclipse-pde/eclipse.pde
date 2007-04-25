c4cbedc5cd54adfdc5cccb2a14306e2d1e634c00
Brian Bauman <bbauman>
2007-04-25 19:36:17 +0000

    Bug 184033 - [Contributions] Add a Command Contribution template
    Submitting Paul's patch with an update to the copyrights and included the icons/sampe.gif file.

--- ui/org.eclipse.pde.ui.templates/src/org/eclipse/pde/internal/ui/templates/PDETemplateMessages.java
+++ ui/org.eclipse.pde.ui.templates/src/org/eclipse/pde/internal/ui/templates/PDETemplateMessages.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006 IBM Corporation and others.
+ * Copyright (c) 2006, 2007 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -54,6 +54,20 @@ public class PDETemplateMessages extends NLS {
 	public static String HelloWorldTemplate_sampleActionSet;
 	public static String HelloWorldTemplate_sampleMenu;
 	public static String HelloWorldTemplate_sampleAction;
+
+	public static String HelloWorldCmdNewWizard_wtitle;
+	public static String HelloWorldCmdTemplate_title;
+	public static String HelloWorldCmdTemplate_desc;
+	public static String HelloWorldCmdTemplate_packageName;
+	public static String HelloWorldCmdTemplate_className;
+	public static String HelloWorldCmdTemplate_messageText;
+	public static String HelloWorldCmdTemplate_defaultMessage;
+	public static String HelloWorldCmdTemplate_sampleCategory;
+	public static String HelloWorldCmdTemplate_sampleMenu_name;
+	public static String HelloWorldCmdTemplate_sampleMenu_mnemonic;
+	public static String HelloWorldCmdTemplate_sampleAction_name;
+	public static String HelloWorldCmdTemplate_sampleAction_mnemonic;
+	public static String HelloWorldCmdTemplate_sampleAction_tooltip;
 
 	public static String IntroNewWizard_wtitle;
 	public static String IntroTemplate_title;
---
+++ ui/org.eclipse.pde.ui.templates/src/org/eclipse/pde/internal/ui/templates/ide/HelloWorldCmdNewWizard.java
@@ -0,0 +1,38 @@
+/*******************************************************************************
+ * Copyright (c) 2007 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.pde.internal.ui.templates.ide;
+
+import org.eclipse.pde.internal.ui.templates.PDETemplateMessages;
+import org.eclipse.pde.ui.IFieldData;
+import org.eclipse.pde.ui.templates.ITemplateSection;
+import org.eclipse.pde.ui.templates.NewPluginTemplateWizard;
+
+public class HelloWorldCmdNewWizard extends NewPluginTemplateWizard {
+	/**
+	 * Constructor for HelloWorldNewWizard.
+	 */
+	public HelloWorldCmdNewWizard() {
+		super();
+	}
+
+	public void init(IFieldData data) {
+		super.init(data);
+		setWindowTitle(PDETemplateMessages.HelloWorldCmdNewWizard_wtitle);
+	}
+	/*
+	 * @see NewExtensionTemplateWizard#createTemplateSections()
+	 */
+	public ITemplateSection[] createTemplateSections() {
+		return new ITemplateSection [] {
+				new HelloWorldCmdTemplate() };
+	}
+}
