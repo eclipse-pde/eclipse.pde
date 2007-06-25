b531e884af2b083292df56cbf4feda144a405aea
Andrew Niefer <aniefer>
2007-06-25 15:02:12 +0000

    base the build configuration on the pde.build template

--- org.eclipse.pde.build.tests/src/org/eclipse/pde/build/tests/BuildConfiguration.java
+++ org.eclipse.pde.build.tests/src/org/eclipse/pde/build/tests/BuildConfiguration.java
@@ -1,11 +1,12 @@
 package org.eclipse.pde.build.tests;
 
-import java.util.*;
+import java.io.*;
+import java.net.URL;
+import java.util.Enumeration;
+import java.util.Properties;
 
-import org.eclipse.ant.core.AntCorePlugin;
-import org.eclipse.ant.core.Property;
 import org.eclipse.core.resources.IFolder;
-import org.eclipse.core.runtime.Platform;
+import org.eclipse.core.runtime.*;
 
 public class BuildConfiguration {
 	public static final String TRUE = "true";
@@ -14,71 +15,58 @@ public class BuildConfiguration {
 	private static Properties defaultBuildConfig = new Properties();
 	private static Properties defaultScriptConfig = new Properties();
 	static {
-		defaultBuildConfig.put("runPackager", TRUE);
+		// use the template build.properties from pde.build
+		URL resource = FileLocator.find(Platform.getBundle("org.eclipse.pde.build"), new Path("/templates/headless-build/build.properties"), null);
+		try {
+			String buildPropertiesPath = FileLocator.toFileURL(resource).getPath();
+			defaultBuildConfig.load(new BufferedInputStream(new FileInputStream(buildPropertiesPath)));
+		} catch (IOException e) {
+		}
+
+		// just a few changes from the template
 		defaultBuildConfig.put("archiveNamePrefix", "eclipse");
-		defaultBuildConfig.put("archivePrefix", "eclipse");
-		defaultBuildConfig.put("collectingFolder", "eclipse");
-		defaultBuildConfig.put("configs", "*,*,*");
-		defaultBuildConfig.put("buildType", "I");
-		defaultBuildConfig.put("buildId", "TestBuild");
-		defaultBuildConfig.put("buildLabel", "I.TestBuild");
-		defaultBuildConfig.put("base", Platform.getInstallLocation().getURL().getPath());
-		defaultBuildConfig.put("baseLocation", Platform.getInstallLocation().getURL().getPath());
+		String baseLocation = Platform.getInstallLocation().getURL().getPath();
+		defaultBuildConfig.put("base", new File(baseLocation).getParent());
+		defaultBuildConfig.put("baseLocation", baseLocation);
 		defaultBuildConfig.put("baseos", Platform.getOS());
 		defaultBuildConfig.put("basews", Platform.getWS());
 		defaultBuildConfig.put("basearch", Platform.getOSArch());
-		defaultBuildConfig.put("zipargs", "");
-		defaultBuildConfig.put("tarargs", "");
-		defaultBuildConfig.put("timestamp", "007");
-		defaultBuildConfig.put("filteredDependencyCheck", FALSE);
-		defaultBuildConfig.put("resolution.devMode", FALSE);
-		defaultBuildConfig.put("skipBase", TRUE);
-		defaultBuildConfig.put("skipMaps", TRUE);
-		defaultBuildConfig.put("skipFetch", TRUE);
-		defaultBuildConfig.put("logExtension", ".log");
-		defaultBuildConfig.put("javacDebugInfo", FALSE);
-		defaultBuildConfig.put("javacFailOnError", TRUE);
-		defaultBuildConfig.put("javacVerbose", FALSE);
-		
-		defaultScriptConfig.put("configs", "*,*,*");
-		defaultScriptConfig.put("baseLocation", Platform.getInstallLocation().getURL().getPath());
-		defaultScriptConfig.put("buildingOSGi", TRUE);
-		defaultScriptConfig.put("outputUpdateJars", FALSE);
-		defaultScriptConfig.put("archivesFormat", "");
-		defaultScriptConfig.put("product", "");
-		defaultScriptConfig.put("forceContextQualifier", "");
-		defaultScriptConfig.put("generateJnlp", FALSE);
-		defaultScriptConfig.put("signJars", FALSE);
-		defaultScriptConfig.put("generateFeatureVersionSuffix", FALSE);
-		defaultScriptConfig.put("significantVersionDigits", "");
-		defaultScriptConfig.put("generateVersionsLists", TRUE);
-		defaultScriptConfig.put("groupConfigurations", FALSE);
-		defaultScriptConfig.put("pluginPath", "");
-		defaultScriptConfig.put("filteredDepenedencyCheck", FALSE);
-		defaultScriptConfig.put("platformProperties", "");
+
+		defaultScriptConfig = (Properties) defaultBuildConfig.clone();
+		if (!defaultBuildConfig.containsKey("configs"))
+			defaultScriptConfig.put("configs", "*,*,*");
+		if (!defaultBuildConfig.containsKey("buildingOSGi"))
+			defaultScriptConfig.put("buildingOSGi", TRUE);
+		if (!defaultBuildConfig.containsKey("signJars"))
+			defaultScriptConfig.put("signJars", FALSE);
+		if (!defaultBuildConfig.containsKey("generateFeatureVersionSuffix"))
+			defaultScriptConfig.put("generateFeatureVersionSuffix", FALSE);
+		if (!defaultBuildConfig.containsKey("generateVersionsLists"))
+			defaultScriptConfig.put("generateVersionsLists", TRUE);
+		if (!defaultBuildConfig.containsKey("groupConfigurations"))
+			defaultScriptConfig.put("groupConfigurations", FALSE);
+		if (!defaultBuildConfig.containsKey("pluginPath"))
+			defaultScriptConfig.put("pluginPath", "");
+		if (!defaultBuildConfig.containsKey("filteredDepenedencyCheck"))
+			defaultScriptConfig.put("filteredDepenedencyCheck", FALSE);
 	}
 
-	
 	private static Properties getBuildConfig() {
 		Properties properties = new Properties();
-		
-		List antProperties = AntCorePlugin.getPlugin().getPreferences().getDefaultProperties();
-		ListIterator li = antProperties.listIterator();
-		while (li.hasNext()) {
-			Property prop = (Property)li.next();
-			properties.put(prop.getName(), prop.getValue());
-		}
-		
 		Enumeration e = defaultBuildConfig.keys();
 		while (e.hasMoreElements()) {
 			String key = (String) e.nextElement();
 			properties.put(key, defaultBuildConfig.get(key));
 		}
-		
-		
-		return properties;	
+
+		return properties;
 	}
-	
+
+	/**
+	 * Get a default build configuration build.properties based on the template from pde.build
+	 * @param buildFolder
+	 * @return
+	 */
 	public static Properties getBuilderProperties(IFolder buildFolder) {
 		String builder = buildFolder.getLocation().toOSString();
 		Properties builderProperties = getBuildConfig();
@@ -86,8 +74,16 @@ public class BuildConfiguration {
 		builderProperties.put("builder", builder);
 		return builderProperties;
 	}
-	
-	public static Properties getScriptGenerationProperties(IFolder buildFolder, String type, String id ) {
+
+	/**
+	 * Get a default set of properties used for invoking the genericTargets/generateScript task 
+	 * (which invokes the eclipse.buildScript target)
+	 * @param buildFolder
+	 * @param type
+	 * @param id
+	 * @return
+	 */
+	public static Properties getScriptGenerationProperties(IFolder buildFolder, String type, String id) {
 		Properties properties = (Properties) defaultScriptConfig.clone();
 		properties.put("type", type);
 		properties.put("id", id);
