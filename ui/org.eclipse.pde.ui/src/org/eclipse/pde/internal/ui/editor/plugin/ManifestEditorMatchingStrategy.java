6162f24203649dbf65904ceb57789a4df92002d8
Wassim Melhem <wmelhem>
2007-07-23 06:02:38 +0000

    bug 128236 - [Manifest Editor] Opening plugin.xml then Manifest.mf no longer shares same editor

--- ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/editor/plugin/ManifestEditorMatchingStrategy.java
+++ ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/editor/plugin/ManifestEditorMatchingStrategy.java
@@ -11,7 +11,10 @@
 
 package org.eclipse.pde.internal.ui.editor.plugin;
 
+import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.pde.internal.core.ICoreConstants;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorMatchingStrategy;
 import org.eclipse.ui.IEditorReference;
@@ -27,12 +30,42 @@ public class ManifestEditorMatchingStrategy implements IEditorMatchingStrategy {
         IFile inputFile = ResourceUtil.getFile(input);
         if (input instanceof IFileEditorInput) {
             try {
+            	// a positive match if there is an editor already open on the same file
 	        	if (input.equals(editorRef.getEditorInput()))
-	        		return true;       	
-	            String path = inputFile.getProjectRelativePath().toString();
-	            if (path.equals("build.properties")) { //$NON-NLS-1$ 
-                    IFile editorFile = ResourceUtil.getFile(editorRef.getEditorInput());
-                    return editorFile != null && inputFile.getProject().equals(editorFile.getProject());
+	        		return true;  
+	        	
+	        	// a quick no-match if the project of the file being opened is not the
+	        	// same as the project of the file associated with the open editor
+	        	IFile editorFile = ResourceUtil.getFile(editorRef.getEditorInput());
+	        	if (editorFile == null || !inputFile.getProject().equals(editorFile.getProject()))
+	        		return false;
+	        	
+	        	// If a MANIFEST.MF file is being opened, make sure we return a positive match
+	        	// only if it is colocated with the plugin.xml/fragment.xml file already open
+	        	if (inputFile.getName().equals("MANIFEST.MF")) { //$NON-NLS-1$
+	        		IContainer parent = inputFile.getParent();
+	        		return parent instanceof IFolder 
+	        				&& parent.getName().equals("META-INF")  //$NON-NLS-1$
+	        				&& parent.getParent().equals(editorFile.getParent());
+	        	}
+	        	
+	        	// if a plugin.xml/fragment.xml is being opened, make sure we return a positive match
+	        	// only if the editor that is open is associated with a colocated MANIFEST.MF
+	        	if (inputFile.getName().equals("plugin.xml") || inputFile.getName().equals("fragment.xml")) { //$NON-NLS-1$ //$NON-NLS-2$
+	        		IContainer parent = inputFile.getParent();
+	        		IFile file = parent.getFile(ICoreConstants.MANIFEST_PATH);
+	        		return file.exists() && editorFile.equals(file);
+	        	}
+	        	
+	        	// if a build.properties is being opened, only return a positive match
+	        	// if an editor is already open on a sibling plugin.xml/fragment.xml or a META-INF/MANIFEST.MF
+	            if (inputFile.getName().equals("build.properties")) { //$NON-NLS-1$ 
+                    IContainer parent = inputFile.getParent();
+                    if (parent.equals(editorFile.getParent())) {
+                    	return editorFile.getName().equals("plugin.xml") || editorFile.getName().equals("fragment.xml"); //$NON-NLS-1$ //$NON-NLS-2$
+                    }
+                    IFile file = parent.getFile(ICoreConstants.MANIFEST_PATH);
+                    return file.exists() && editorFile.equals(file);
 	            }
             } catch (PartInitException e) {
                 return false;
