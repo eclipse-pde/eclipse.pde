ca33dec4f2913881c72cea387157f860bf8e5f15
Chris Aniszczyk <caniszczyk>
2007-08-05 19:06:40 +0000

    RESOLVED - bug 198881: Hyperlink detector not working on Build source page https://bugs.eclipse.org/bugs/show_bug.cgi?id=198881

--- ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/editor/build/BuildSourcePage.java
+++ ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/editor/build/BuildSourcePage.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2003, 2006 IBM Corporation and others.
+ * Copyright (c) 2003, 2007 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
+ *     Brock Janiczak <brockj@tpg.com.au> - bug 198881
  *******************************************************************************/
 package org.eclipse.pde.internal.ui.editor.build;
 import org.eclipse.jface.preference.IPreferenceStore;
@@ -126,8 +127,8 @@ public class BuildSourcePage extends KeyValueSourcePage {
 	}
 	
 	public Object getAdapter(Class adapter) {
-		if (IHyperlinkDetector[].class.equals(adapter))
-			return new IHyperlinkDetector[] { new BuildHyperlinkDetector(this) };
+		if (IHyperlinkDetector.class.equals(adapter))
+			return new BuildHyperlinkDetector(this);
 		return super.getAdapter(adapter);
 	}
 	
