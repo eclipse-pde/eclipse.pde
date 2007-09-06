1061415863824546ba2c204f333fd04e528b7e45
Brian Bauman <bbauman>
2007-09-06 17:35:45 +0000

    Bug 197817 - Plug-in should be removed from Java Search when imported as source

--- ui/org.eclipse.pde.core/src/org/eclipse/pde/internal/core/SearchablePluginsManager.java
+++ ui/org.eclipse.pde.core/src/org/eclipse/pde/internal/core/SearchablePluginsManager.java
@@ -172,7 +172,7 @@ public class SearchablePluginsManager
 						addModel = false;
 				}
 				if (!addModel)
-					break;
+					continue;
 				IPluginModelBase[] models = entry.getExternalModels();
 				for (int i = 0; i < models.length; i++) {
 					if (models[i].isEnabled())
