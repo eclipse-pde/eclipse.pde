4ba012e44ddd6688b8b2d03487ed8a603bf3b4db
Brian Bauman <bbauman>
2007-09-07 19:07:18 +0000

    Bug 201980 - [Wizards] Update Site Project wizard causes other wizards to be displayed incorrectly

--- ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/wizards/site/NewSiteProjectCreationPage.java
+++ ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/wizards/site/NewSiteProjectCreationPage.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2006 IBM Corporation and others.
+ * Copyright (c) 2000, 2007 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -92,8 +92,8 @@ public class NewSiteProjectCreationPage extends WizardNewProjectCreationPage {
 		});
 
 		setPageComplete(validatePage());
-		setControl(webGroup);
-		Dialog.applyDialogFont(webGroup);
+		setControl(control);
+		Dialog.applyDialogFont(control);
 		PlatformUI.getWorkbench().getHelpSystem().setHelp(control, IHelpContextIds.NEW_SITE_MAIN);
 	}
 
