bc805854705ab1af4c04d33440eb1de878d03ad8
Chris Aniszczyk <caniszczyk>
2007-10-03 15:13:20 +0000

    clear out images due to log view move

--- ui/org.eclipse.pde.runtime/src/org/eclipse/pde/internal/runtime/PDERuntimePluginImages.java
+++ ui/org.eclipse.pde.runtime/src/org/eclipse/pde/internal/runtime/PDERuntimePluginImages.java
@@ -25,10 +25,9 @@ public class PDERuntimePluginImages {
 	public final static String ICONS_PATH = "icons/"; //$NON-NLS-1$
 
 	private static final String PATH_OBJ = ICONS_PATH + "obj16/"; //$NON-NLS-1$
+	private static final String PATH_DCL = ICONS_PATH + "dlcl16/"; //$NON-NLS-1$
 	private static final String PATH_LCL = ICONS_PATH + "elcl16/"; //$NON-NLS-1$
-	private static final String PATH_LCL_DISABLED = ICONS_PATH + "dlcl16/"; //$NON-NLS-1$
 	private static final String PATH_OVR = ICONS_PATH + "ovr16/"; //$NON-NLS-1$
-	private static final String PATH_EVENTS = ICONS_PATH + "eview16/"; //$NON-NLS-1$
 
 	// Plug-in Spy related images
 	public static final String IMG_CLASS_OBJ = "class_obj.gif"; //$NON-NLS-1$
@@ -54,6 +53,12 @@ public class PDERuntimePluginImages {
 	public static final ImageDescriptor MENUSPY_OBJ =
 		create(PATH_OBJ, IMG_MENUSPY_OBJ);
 
+	public static final ImageDescriptor DESC_REFRESH_DISABLED =
+		create(PATH_DCL, "refresh.gif"); //$NON-NLS-1$
+	public static final ImageDescriptor DESC_REFRESH =
+		create(PATH_LCL, "refresh.gif"); //$NON-NLS-1$
+	public static final ImageDescriptor DESC_COLLAPSE_ALL =
+		create(PATH_LCL, "collapseall.gif"); //$NON-NLS-1$
 	public static final ImageDescriptor DESC_ERROR_ST_OBJ =
 		create(PATH_OBJ, "error_st_obj.gif"); //$NON-NLS-1$
 	public static final ImageDescriptor DESC_ERROR_STACK_OBJ =
@@ -91,62 +96,6 @@ public class PDERuntimePluginImages {
 	public static final ImageDescriptor DESC_LOCATION =
 		create(PATH_OBJ, "location_obj.gif"); //$NON-NLS-1$
 	
-	/*
-	 * Local tool bar image descriptors
-	 */
-
-	public static final ImageDescriptor DESC_PROPERTIES =
-		create(PATH_LCL, "properties.gif"); //$NON-NLS-1$
-	public static final ImageDescriptor DESC_OPEN_LOG =
-		create(PATH_LCL, "open_log.gif"); //$NON-NLS-1$
-	public static final ImageDescriptor DESC_OPEN_LOG_DISABLED =
-		create(PATH_LCL_DISABLED, "open_log.gif"); //$NON-NLS-1$
-
-	public static final ImageDescriptor DESC_PROPERTIES_DISABLED =
-		create(PATH_LCL_DISABLED, "properties.gif"); //$NON-NLS-1$
-	public static final ImageDescriptor DESC_REFRESH =
-		create(PATH_LCL, "refresh.gif"); //$NON-NLS-1$
-	public static final ImageDescriptor DESC_REFRESH_DISABLED =
-		create(PATH_LCL_DISABLED, "refresh.gif"); //$NON-NLS-1$
-	public static final ImageDescriptor DESC_CLEAR = create(PATH_LCL, "clear.gif"); //$NON-NLS-1$
-	public static final ImageDescriptor DESC_CLEAR_DISABLED =
-		create(PATH_LCL_DISABLED, "clear.gif"); //$NON-NLS-1$
-
-	public static final ImageDescriptor DESC_READ_LOG =
-		create(PATH_LCL, "restore_log.gif"); //$NON-NLS-1$
-	public static final ImageDescriptor DESC_READ_LOG_DISABLED =
-		create(PATH_LCL_DISABLED, "restore_log.gif"); //$NON-NLS-1$
-		
-	public static final ImageDescriptor DESC_REMOVE_LOG =
-		create(PATH_LCL, "remove.gif"); //$NON-NLS-1$
-	public static final ImageDescriptor DESC_REMOVE_LOG_DISABLED =
-		create(PATH_LCL_DISABLED, "remove.gif"); //$NON-NLS-1$
-		
-	public static final ImageDescriptor DESC_FILTER =
-		create(PATH_LCL, "filter_ps.gif"); //$NON-NLS-1$
-	public static final ImageDescriptor DESC_FILTER_DISABLED =
-		create(PATH_LCL_DISABLED, "filter_ps.gif"); //$NON-NLS-1$
-
-	public static final ImageDescriptor DESC_EXPORT =
-		create(PATH_LCL, "export_log.gif"); //$NON-NLS-1$
-	public static final ImageDescriptor DESC_EXPORT_DISABLED =
-		create(PATH_LCL_DISABLED, "export_log.gif"); //$NON-NLS-1$
-		
-	public static final ImageDescriptor DESC_IMPORT =
-		create(PATH_LCL, "import_log.gif"); //$NON-NLS-1$
-	public static final ImageDescriptor DESC_IMPORT_DISABLED =
-		create(PATH_LCL_DISABLED, "import_log.gif"); //$NON-NLS-1$
-	
-	public static final ImageDescriptor DESC_COLLAPSE_ALL =
-		create(PATH_LCL, "collapseall.gif"); //$NON-NLS-1$
-	
-	/*
-	 * Event Details
-	 */
-	public static final ImageDescriptor DESC_PREV_EVENT =
-		create(PATH_EVENTS, "event_prev.gif"); //$NON-NLS-1$
-	public static final ImageDescriptor DESC_NEXT_EVENT = 
-		create(PATH_EVENTS, "event_next.gif"); //$NON-NLS-1$
 	/*
 	 * Overlays
 	 */
