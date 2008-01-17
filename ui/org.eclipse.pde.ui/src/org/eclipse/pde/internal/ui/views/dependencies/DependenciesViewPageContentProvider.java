40a44a51d98c127da68cf35dc2dd06fe21ed2eb7
Brian Bauman <bbauman>
2008-01-17 03:25:37 +0000

    removing unnecessary code

--- ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/views/dependencies/DependenciesViewPageContentProvider.java
+++ ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/views/dependencies/DependenciesViewPageContentProvider.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2007 IBM Corporation and others.
+ * Copyright (c) 2000, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -19,7 +19,6 @@ import org.eclipse.pde.internal.core.*;
 import org.eclipse.pde.internal.ui.elements.DefaultContentProvider;
 
 public class DependenciesViewPageContentProvider extends DefaultContentProvider implements IPluginModelListener {
-	private PluginModelManager fPluginManager;
 
 	private DependenciesView fView;
 
@@ -30,16 +29,15 @@ public class DependenciesViewPageContentProvider extends DefaultContentProvider 
 	 */
 	public DependenciesViewPageContentProvider(DependenciesView view) {
 		this.fView = view;
-		fPluginManager = PDECore.getDefault().getModelManager();
 		attachModelListener();
 	}
 
 	public void attachModelListener() {
-		fPluginManager.addPluginModelListener(this);
+		PDECore.getDefault().getModelManager().addPluginModelListener(this);
 	}
 
 	public void removeModelListener() {
-		fPluginManager.removePluginModelListener(this);
+		PDECore.getDefault().getModelManager().removePluginModelListener(this);
 	}
 
 	public void dispose() {
@@ -115,10 +113,4 @@ public class DependenciesViewPageContentProvider extends DefaultContentProvider 
 		});
 	}
 
-	/**
-	 * @return Returns the fPluginManager.
-	 */
-	protected PluginModelManager getPluginManager() {
-		return fPluginManager;
-	}
 }
