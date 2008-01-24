80ff0f2ab88aa4f9a83d3d99d9a77722d3662f30
Brian Bauman <bbauman>
2008-01-24 21:16:44 +0000

    Bug 216240 - Quick fix to convert "Eclipse-LazyStart:" mangles my file

--- ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/correction/UpdateActivationResolution.java
+++ ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/correction/UpdateActivationResolution.java
@@ -10,17 +10,17 @@
  *******************************************************************************/
 package org.eclipse.pde.internal.ui.correction;
 
+import java.util.Map;
 import org.eclipse.osgi.util.NLS;
 import org.eclipse.pde.internal.core.ICoreConstants;
 import org.eclipse.pde.internal.core.TargetPlatformHelper;
-import org.eclipse.pde.internal.core.text.bundle.BundleModel;
-import org.eclipse.pde.internal.core.text.bundle.LazyStartHeader;
+import org.eclipse.pde.internal.core.text.bundle.*;
 import org.eclipse.pde.internal.ui.PDEUIMessages;
 import org.osgi.framework.Constants;
 
 public class UpdateActivationResolution extends AbstractManifestMarkerResolution {
 
-	String fHeader = null;
+	private String fHeader = null;
 
 	public UpdateActivationResolution(int type, String currentHeader) {
 		super(type);
@@ -29,28 +29,34 @@ public class UpdateActivationResolution extends AbstractManifestMarkerResolution
 
 	protected void createChange(BundleModel model) {
 		if (TargetPlatformHelper.getTargetVersion() >= 3.4) {
-			boolean lazy = false;
-			// store the packages we need to exclude as defined by Eclipse-AutoStart and Eclipse-LazyStart
-			String excludes = null;
-
-			LazyStartHeader header = (LazyStartHeader) model.getBundle().getManifestHeader(ICoreConstants.ECLIPSE_AUTOSTART);
+			// get the header we wish to replace
+			LazyStartHeader header = (LazyStartHeader) model.getBundle().getManifestHeader(fHeader);
 			if (header != null) {
-				lazy = header.isLazyStart();
-				excludes = header.getAttribute("exceptions"); //$NON-NLS-1$
-				header.setValue(null);
-			}
-			header = (LazyStartHeader) model.getBundle().getManifestHeader(ICoreConstants.ECLIPSE_LAZYSTART);
-			if (header != null) {
-				lazy = header.isLazyStart();
-				excludes = header.getAttribute("exceptions"); //$NON-NLS-1$
-				model.getBundle().setHeader(ICoreConstants.ECLIPSE_LAZYSTART, null);
-			}
-			if (lazy) {
-				model.getBundle().setHeader(Constants.BUNDLE_ACTIVATIONPOLICY, Constants.ACTIVATION_LAZY);
+				// create a new header and copy over information
+				LazyStartHeader newHeader = (LazyStartHeader) model.getFactory().createHeader(Constants.BUNDLE_ACTIVATIONPOLICY, Constants.ACTIVATION_LAZY);
+				newHeader.setLazyStart(header.isLazyStart());
+				String excludes = header.getAttribute("exceptions"); //$NON-NLS-1$
+				// handle 'exceptions' attribute.
 				if (excludes != null) {
-					header = (LazyStartHeader) model.getBundle().getManifestHeader(Constants.BUNDLE_ACTIVATIONPOLICY);
-					header.setDirective(Constants.EXCLUDE_DIRECTIVE, excludes);
+					// if lazystart = true, then use the exclude directive.
+					// if lazystart = false, then use the include directive making sure to set the Bundle-ActivationPolicy header to lazy (or true).
+					String directive = (header.isLazyStart()) ? Constants.EXCLUDE_DIRECTIVE : Constants.INCLUDE_DIRECTIVE;
+					newHeader.setDirective(directive, excludes);
+					if (!header.isLazyStart())
+						newHeader.setLazyStart(true);
 				}
+
+				// This is a HACK to overwrite the existing header with the new header.  Since newHeader has the same length/offset as the old header, the
+				// BundleTextChangeListener will create a ReplaceTextEdit and will keep the same ordering.  NOTE: NOT recommended usage for modifying a header!!
+				newHeader.setOffset(header.getOffset());
+				newHeader.setLength(header.getLength());
+				// remove old header from Bundle object.  Add new header to Bundle object
+				Bundle bundle = (Bundle) model.getBundle();
+				Map map = bundle.getHeaders();
+				map.remove(fHeader);
+				map.put(Constants.BUNDLE_ACTIVATIONPOLICY, newHeader);
+				// fire ModelChanged so that way the BundleTextChangeListener will make proper ReplaceTextEdits
+				model.fireModelObjectChanged(newHeader, Constants.BUNDLE_ACTIVATIONPOLICY, null, header.getValue());
 			}
 		} else {
 			// if we should not use the Bundle-ActivationPolicy header, then we know we are renaming the Eclipse-AutoStart header to Eclipse-LazyStart
