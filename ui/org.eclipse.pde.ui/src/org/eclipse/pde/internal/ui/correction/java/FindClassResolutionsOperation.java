ca14e57f7c1b44f47534f594199eb1d577759aa9
Brian Bauman <bbauman>
2008-01-28 23:29:19 +0000

    Bug 208107 - Add import package suggestions back to classpathfix processor

---
+++ ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/correction/java/FindClassResolutionsOperation.java
@@ -0,0 +1,154 @@
+/*******************************************************************************
+ * Copyright (c) 2008 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.pde.internal.ui.correction.java;
+
+import java.util.*;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.jface.operation.IRunnableWithProgress;
+import org.eclipse.osgi.service.resolver.*;
+import org.eclipse.pde.core.plugin.IPluginModelBase;
+import org.eclipse.pde.core.plugin.PluginRegistry;
+import org.eclipse.pde.internal.core.PDECore;
+
+/**
+ * This Operation is used to find possible resolutions to an unresolved class reference in a plug-in project.
+ * When it is run, it will pass any ExportPackageDescriptions which provide the package to the AbstractClassResolutionCollector.
+ * The AbstractClassResolutionCollector is responsible for creating the appropriate resolutions.
+ *
+ * @since 3.4
+ */
+public class FindClassResolutionsOperation implements IRunnableWithProgress {
+
+	String fClassName = null;
+	IProject fProject = null;
+	AbstractClassResolutionCollector fCollector = null;
+
+	/**
+	 * This class is meant to be sub-classed for use with FindClassResolutionsOperation.  The subclass is responsible for creating 
+	 * corresponding proposals with the help of JavaResolutionFactory.
+	 * 
+	 * @since 3.4
+	 * @see JavaResolutionFactory
+	 */
+	public static abstract class AbstractClassResolutionCollector {
+
+		/**
+		 * This method is meant to be sub-classed.  The subclass should decide if it wishes to create a proposals for either
+		 * Require-Bundle and/or Import-Package.  The proposals can be created with the help of the JavaResolutionFactory
+		 */
+		abstract public void addResolutionModification(IProject project, ExportPackageDescription desc);
+
+		/*
+		 * Optimization for case where users is only interested in Import-Package and therefore can quit after first dependency is found
+		 */
+		public boolean isDone() {
+			return false;
+		}
+
+	}
+
+	/**
+	 * This class is used to try to find resolutions to unresolved java classes.  When either an Import-Package or Require-Bundle might 
+	 * resolve a class, the ExportPackageDescription which contains the package/bundle will be passed to the AbstractClassResoltuionCollector.
+	 * The collector is then responsible for creating an corresponding resolutions with the help of JavaResolutionFactory.
+	 * @param project the project which contains the unresolved class
+	 * @param className	the name of the class which is unresolved
+	 * @param collector a subclass of AbstractClassResolutionCollector to collect/handle possible resolutions 
+	 */
+	public FindClassResolutionsOperation(IProject project, String className, AbstractClassResolutionCollector collector) {
+		fProject = project;
+		fClassName = className;
+		fCollector = collector;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.jface.operation.IRunnableWithProgress#run(org.eclipse.core.runtime.IProgressMonitor)
+	 */
+	public void run(IProgressMonitor monitor) {
+		int idx = fClassName.lastIndexOf('.');
+		String packageName = idx != -1 ? fClassName.substring(0, idx) : null;
+		String typeName = fClassName.substring(idx + 1);
+		if (typeName.length() == 1 && typeName.charAt(0) == '*') {
+			typeName = null;
+		}
+
+		if (packageName != null && !isImportedPackage(packageName)) {
+			Set validPackages = getValidPackages(packageName);
+			Iterator validPackagesIter = validPackages.iterator();
+			Set visiblePkgs = null;
+
+			while (validPackagesIter.hasNext() && !fCollector.isDone()) {
+				// since getting visible packages is not very efficient, only do it once and cache result
+				if (visiblePkgs == null) {
+					visiblePkgs = getVisiblePackages();
+				}
+				ExportPackageDescription currentPackage = (ExportPackageDescription) validPackagesIter.next();
+				// if package is already visible, skip over
+				if (visiblePkgs.contains(currentPackage)) {
+					continue;
+				}
+				// if currentPackage will resolve class and is valid, pass it to collector
+				fCollector.addResolutionModification(fProject, currentPackage);
+			}
+		}
+	}
+
+	private boolean isImportedPackage(String packageName) {
+		BundleDescription desc = PluginRegistry.findModel(fProject.getProject()).getBundleDescription();
+		if (desc != null) {
+			ImportPackageSpecification[] importPkgs = desc.getImportPackages();
+			for (int i = 0; i < importPkgs.length; i++) {
+				if (importPkgs[i].getName().equals(packageName)) {
+					return true;
+				}
+			}
+			return false;
+		}
+		// if no BundleDescription, we return true so we don't create any proposals.  This is the safe way out if no BundleDescription is available.
+		return true;
+	}
+
+	private static Set getValidPackages(String pkgName) {
+		ExportPackageDescription[] knownPackages = PDECore.getDefault().getModelManager().getState().getState().getExportedPackages();
+		Set validPackages = new HashSet();
+		for (int i = 0; i < knownPackages.length; i++) {
+			if (knownPackages[i].getName().equals(pkgName)) {
+				validPackages.add(knownPackages[i]);
+			}
+		}
+		// remove system packages if they happen to be included. Adding a system package won't resolve anything, since package package already comes from JRE
+		if (!validPackages.isEmpty()) {
+			knownPackages = PDECore.getDefault().getModelManager().getState().getState().getSystemPackages();
+			for (int i = 0; i < knownPackages.length; i++) {
+				validPackages.remove(knownPackages[i]);
+			}
+		}
+		return validPackages;
+	}
+
+	private Set getVisiblePackages() {
+		IPluginModelBase base = PluginRegistry.findModel(fProject);
+		BundleDescription desc = base.getBundleDescription();
+
+		StateHelper helper = Platform.getPlatformAdmin().getStateHelper();
+		ExportPackageDescription[] visiblePkgs = helper.getVisiblePackages(desc);
+
+		HashSet set = new HashSet();
+		for (int i = 0; i < visiblePkgs.length; i++) {
+			set.add(visiblePkgs[i]);
+		}
+		return set;
+	}
+
+}
--- ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/correction/java/UnresolvedImportFixProcessor.java
+++ ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/correction/java/UnresolvedImportFixProcessor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007 IBM Corporation and others.
+ * Copyright (c) 2007, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,17 +10,16 @@
  *******************************************************************************/
 package org.eclipse.pde.internal.ui.correction.java;
 
-import com.ibm.icu.text.MessageFormat;
-import java.util.*;
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.runtime.*;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.ui.text.java.ClasspathFixProcessor;
-import org.eclipse.osgi.service.resolver.*;
-import org.eclipse.pde.core.plugin.*;
-import org.eclipse.pde.internal.core.PDECore;
-import org.eclipse.pde.internal.ui.*;
-import org.eclipse.swt.graphics.Image;
+import org.eclipse.jface.operation.IRunnableWithProgress;
+import org.eclipse.osgi.service.resolver.ExportPackageDescription;
+import org.eclipse.pde.internal.ui.correction.java.FindClassResolutionsOperation.AbstractClassResolutionCollector;
 
 /**
  * Offers a classpath fix proposal if the broken import statement can be
@@ -29,211 +28,43 @@ import org.eclipse.swt.graphics.Image;
  */
 public class UnresolvedImportFixProcessor extends ClasspathFixProcessor {
 
+	private class ClasspathFixCollector extends AbstractClassResolutionCollector {
+
+		private ArrayList fList = new ArrayList();
+
+		/*
+		 * (non-Javadoc)
+		 * @see org.eclipse.pde.internal.ui.correction.java.FindClassResolutionsOperation.AbstractClassResolutionCollector#addResolutionModification(org.eclipse.core.resources.IProject, org.eclipse.osgi.service.resolver.ExportPackageDescription)
+		 */
+		public void addResolutionModification(IProject project, ExportPackageDescription desc) {
+			if (desc.getSupplier() == null)
+				return;
+			Object proposal = JavaResolutionFactory.createRequireBundleProposal(project, desc, JavaResolutionFactory.TYPE_CLASSPATH_FIX, 16);
+			if (proposal != null)
+				fList.add(proposal);
+		}
+
+		/*
+		 * Returns all the ClasspathFixProposals which were found
+		 */
+		public ClasspathFixProposal[] getProposals() {
+			return (ClasspathFixProposal[]) fList.toArray(new ClasspathFixProposal[fList.size()]);
+		}
+
+	}
+
 	/* (non-Javadoc)
 	 * @see org.eclipse.jdt.ui.text.java.ClasspathFixProcessor#getFixImportProposals(org.eclipse.jdt.core.IJavaProject, java.lang.String)
 	 */
 	public ClasspathFixProposal[] getFixImportProposals(IJavaProject project, String name) throws CoreException {
-		int idx = name.lastIndexOf('.');
-		String packageName = idx != -1 ? name.substring(0, idx) : null;
-		String typeName = name.substring(idx + 1);
-		if (typeName.length() == 1 && typeName.charAt(0) == '*') {
-			typeName = null;
+		ClasspathFixCollector collector = new ClasspathFixCollector();
+		IRunnableWithProgress findOperation = new FindClassResolutionsOperation(project.getProject(), name, collector);
+		try {
+			findOperation.run(new NullProgressMonitor());
+		} catch (InvocationTargetException e) {
+		} catch (InterruptedException e) {
 		}
-
-		// if package is already referenced by Import-Package, exit since Import-Package overrides Require-Bundle during lookup
-		if (packageName != null && !isImportedPackage(project, packageName)) {
-			// Get the packages exported by all bundles, see if any can provide the required classes
-			Set validPackages = getValidPackages(project, packageName);
-			List proposals = new ArrayList();
-			Iterator validPackagesIter = validPackages.iterator();
-			Set visiblePkgs = null;
-			while (validPackagesIter.hasNext()) {
-				// since getting visible packages is not very efficient, only do it once and cache result
-				if (visiblePkgs == null) {
-					visiblePkgs = getVisiblePackages(project);
-				}
-				ExportPackageDescription currentPackage = (ExportPackageDescription) validPackagesIter.next();
-				// if package is already visible, skip over
-				if (visiblePkgs.contains(currentPackage)) {
-					continue;
-				}
-				addRequireBundleProposal(proposals, project.getProject(), currentPackage);
-			}
-
-//			if (!proposals.isEmpty() && PluginRegistry.findModel(project.getProject()) instanceof IBundlePluginModelBase) {
-//				ExportPackageDescription pkgDesc = ((UnresolvedImportFixProposal)proposals.get(0)).getDependency();
-//				addImportPackageProposal(proposals, project.getProject(), pkgDesc);
-//			}
-
-			return (ClasspathFixProposal[]) proposals.toArray(new ClasspathFixProposal[proposals.size()]);
-		}
-
-		return new ClasspathFixProposal[0];
-	}
-
-	/**
-	 * Helper method to create a proposal to add a require bundle dependency to the project
-	 */
-	private void addRequireBundleProposal(List proposalList, IProject project, ExportPackageDescription dependency) {
-		proposalList.add(new UnresolvedImportFixProposal(project, dependency) {
-
-			/* (non-Javadoc)
-			 * @see org.eclipse.pde.internal.ui.correction.java.UnresolvedImportFixProposal#handleDependencyChange(org.eclipse.core.runtime.IProgressMonitor, org.eclipse.pde.internal.core.ibundle.IBundlePluginModelBase, org.eclipse.osgi.service.resolver.ExportPackageDescription, boolean)
-			 */
-			public void handleDependencyChange(IProgressMonitor pm, IPluginModelBase model, ExportPackageDescription dependency, boolean isAdd) throws CoreException {
-				IPluginImport pluginImport = model.getPluginFactory().createImport();
-				pluginImport.setId(dependency.getExporter().getSymbolicName());
-				if (isAdd) {
-					model.getPluginBase().add(pluginImport);
-				} else {
-					model.getPluginBase().remove(pluginImport);
-				}
-			}
-
-			/* (non-Javadoc)
-			 * @see org.eclipse.pde.internal.ui.correction.java.UnresolvedImportFixProposal#getLabel(boolean)
-			 */
-			public String getLabel(boolean isAdd) {
-				if (isAdd) {
-					return MessageFormat.format(PDEUIMessages.UnresolvedImportFixProcessor_0, new Object[] {getDependency().getExporter().getName()});
-				}
-				return MessageFormat.format(PDEUIMessages.UnresolvedImportFixProcessor_1, new Object[] {getDependency().getExporter().getName()});
-			}
-
-			/* (non-Javadoc)
-			 * @see org.eclipse.pde.internal.ui.correction.java.UnresolvedImportFixProposal#getDescription()
-			 */
-			public String getDescription() {
-				return PDEUIMessages.UnresolvedImportFixProcessor_2;
-			}
-
-			/* (non-Javadoc)
-			 * @see org.eclipse.jdt.ui.text.java.ClasspathFixProcessor.ClasspathFixProposal#getImage()
-			 */
-			public Image getImage() {
-				return PDEPlugin.getDefault().getLabelProvider().get(PDEPluginImages.DESC_REQ_PLUGIN_OBJ);
-			}
-		});
-	}
-
-	/**
-	 * Helper method to create a proposal to add an import package dependency to the project
-	 */
-//	private void addImportPackageProposal(List proposalList, IProject project, ExportPackageDescription dependency){
-//
-//		proposalList.add(new UnresolvedImportFixProposal(project, dependency) {
-//
-//			/* (non-Javadoc)
-//			 * @see org.eclipse.pde.internal.ui.correction.java.UnresolvedImportFixProposal#handleDependencyChange(org.eclipse.core.runtime.IProgressMonitor, org.eclipse.pde.internal.core.ibundle.IBundlePluginModelBase, org.eclipse.osgi.service.resolver.ExportPackageDescription, boolean)
-//			 */
-//			public void handleDependencyChange(IProgressMonitor pm,	IPluginModelBase model, ExportPackageDescription dependency, boolean isAdd) throws CoreException {
-//				IBundle bundle = ((IBundlePluginModelBase)model).getBundleModel().getBundle();
-//				if (!(bundle instanceof Bundle)){
-//					return;
-//				}
-//				IManifestHeader mheader = bundle.getManifestHeader(Constants.IMPORT_PACKAGE);
-//				if (mheader == null) {
-//					bundle.setHeader(Constants.IMPORT_PACKAGE, new String());
-//					mheader = bundle.getManifestHeader(Constants.IMPORT_PACKAGE);
-//				}
-//				if (mheader instanceof ImportPackageHeader) {
-//					ImportPackageHeader header = (ImportPackageHeader) mheader;
-//					if (isAdd) {
-//						String versionAttr = (BundlePluginBase.getBundleManifestVersion(bundle) < 2) ? ICoreConstants.PACKAGE_SPECIFICATION_VERSION : Constants.VERSION_ATTRIBUTE;  
-//						ImportPackageObject obj = new ImportPackageObject(header, dependency, versionAttr);
-//						header.addPackage(obj);
-//					} else {
-//						header.removePackage(dependency.getName());
-//					}
-//				} else {
-//					StringBuffer buffer = new StringBuffer();
-//					String currentValue = (mheader != null) ? mheader.getValue() : null;
-//					if (currentValue != null){
-//						buffer.append(currentValue).append(PluginConverter.LIST_SEPARATOR);
-//					}
-//					if (dependency.getVersion().equals(Version.emptyVersion)){
-//						buffer.append(dependency.getName());
-//					} else {
-//						buffer.append(dependency.getName());
-//						buffer.append("; version=\""); //$NON-NLS-1$
-//						buffer.append(dependency.getVersion());
-//						buffer.append("\""); //$NON-NLS-1$
-//					}
-//					bundle.setHeader(Constants.IMPORT_PACKAGE, buffer.toString());
-//				}
-//			}
-//
-//			/* (non-Javadoc)
-//			 * @see org.eclipse.pde.internal.ui.correction.java.UnresolvedImportFixProposal#getLabel(boolean)
-//			 */
-//			public String getLabel(boolean isAdd) {
-//				if (isAdd) {
-//					return MessageFormat.format(PDEUIMessages.UnresolvedImportFixProcessor_3,new Object[]{getDependency().getName()});
-//				}
-//				return MessageFormat.format(PDEUIMessages.UnresolvedImportFixProcessor_4,new Object[]{getDependency().getName()});
-//			}
-//
-//			/* (non-Javadoc)
-//			 * @see org.eclipse.pde.internal.ui.correction.java.UnresolvedImportFixProposal#getDescription()
-//			 */
-//			public String getDescription() {
-//				return PDEUIMessages.UnresolvedImportFixProcessor_5;
-//			}
-//			
-//			/* (non-Javadoc)
-//			 * @see org.eclipse.jdt.ui.text.java.ClasspathFixProcessor.ClasspathFixProposal#getImage()
-//			 */
-//			public Image getImage() {
-//				return PDEPlugin.getDefault().getLabelProvider().get(PDEPluginImages.DESC_BUNDLE_OBJ);
-//			}
-//
-//		});
-//	}
-	private Set getVisiblePackages(IJavaProject project) {
-		IPluginModelBase base = PluginRegistry.findModel(project.getProject());
-		BundleDescription desc = base.getBundleDescription();
-
-		StateHelper helper = Platform.getPlatformAdmin().getStateHelper();
-		ExportPackageDescription[] visiblePkgs = helper.getVisiblePackages(desc);
-
-		HashSet set = new HashSet();
-		for (int i = 0; i < visiblePkgs.length; i++) {
-			set.add(visiblePkgs[i]);
-		}
-		return set;
-	}
-
-	private boolean isImportedPackage(IJavaProject project, String pkgName) {
-		BundleDescription desc = PluginRegistry.findModel(project.getProject()).getBundleDescription();
-		if (desc != null) {
-			ImportPackageSpecification[] importPkgs = desc.getImportPackages();
-			for (int i = 0; i < importPkgs.length; i++) {
-				if (importPkgs[i].getName().equals(pkgName)) {
-					return true;
-				}
-			}
-			return false;
-		}
-		// if no BundleDescription, we return true so we don't create any proposals.  This is the safe way out if no BundleDescription is available.
-		return true;
-	}
-
-	private Set getValidPackages(IJavaProject project, String pkgName) {
-		ExportPackageDescription[] knownPackages = PDECore.getDefault().getModelManager().getState().getState().getExportedPackages();
-		Set validPackages = new HashSet();
-		for (int i = 0; i < knownPackages.length; i++) {
-			if (knownPackages[i].getName().equals(pkgName)) {
-				validPackages.add(knownPackages[i]);
-			}
-		}
-		// remove system packages if they happen to be included. Adding a system package won't resolve anything, since package package already comes from JRE
-		if (!validPackages.isEmpty()) {
-			knownPackages = PDECore.getDefault().getModelManager().getState().getState().getSystemPackages();
-			for (int i = 0; i < knownPackages.length; i++) {
-				validPackages.remove(knownPackages[i]);
-			}
-		}
-		return validPackages;
+		return collector.getProposals();
 	}
 
 }
