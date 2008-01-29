c76a820b80e744dce25e80324bd49c34b37b62a5
Chris Aniszczyk <caniszczyk>
2008-01-29 15:35:32 +0000

    RESOLVED - bug 216511: Should exception be thrown when adding a bundle that already belongs to another state? https://bugs.eclipse.org/bugs/show_bug.cgi?id=216511

--- ui/org.eclipse.pde.core/src/org/eclipse/pde/internal/core/PDEState.java
+++ ui/org.eclipse.pde.core/src/org/eclipse/pde/internal/core/PDEState.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2007 IBM Corporation and others.
+ * Copyright (c) 2005, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -320,7 +320,7 @@ public class PDEState extends MinimalState {
 			for (int i = 0; i < models.length; i++) {
 				BundleDescription desc = models[i].getBundleDescription();
 				if (desc != null)
-					state.addBundle(desc);
+					state.addBundle(state.getFactory().createBundleDescription(desc));
 			}
 			saveState(state, dir);
 			PDEAuxiliaryState.writePluginInfo(models, dir);
