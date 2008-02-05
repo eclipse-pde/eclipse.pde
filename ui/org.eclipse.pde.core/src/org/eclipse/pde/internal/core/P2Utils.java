30768a8ca997a57e0851a2d50926281fd4a64253
Chris Aniszczyk <caniszczyk>
2008-02-05 03:24:45 +0000

    RESOLVED - bug 217304: [p2] Support source.bundles.txt in p2 installs https://bugs.eclipse.org/bugs/show_bug.cgi?id=217304

--- ui/org.eclipse.pde.core/src/org/eclipse/pde/internal/core/P2Utils.java
+++ ui/org.eclipse.pde.core/src/org/eclipse/pde/internal/core/P2Utils.java
@@ -18,6 +18,7 @@ import org.eclipse.core.runtime.*;
 import org.eclipse.osgi.service.resolver.BundleDescription;
 import org.eclipse.pde.core.plugin.IPluginBase;
 import org.eclipse.pde.core.plugin.IPluginModelBase;
+import org.eclipse.pde.internal.core.plugin.PluginBase;
 
 /**
  * Temporary utilities until P2 and FrameworkAdmin are graduated into the SDK.
@@ -26,10 +27,17 @@ import org.eclipse.pde.core.plugin.IPluginModelBase;
  */
 public class P2Utils {
 
+	private static final String SRC_BUNDLE_TXT_FOLDER = "org.eclipse.equinox.source"; //$NON-NLS-1$
+	private static final String BUNDLE_TXT_FOLDER = "org.eclipse.equinox.simpleconfigurator"; //$NON-NLS-1$
+	private static final String SRC_BUNDLE_TXT_PATH = SRC_BUNDLE_TXT_FOLDER + File.separator + "source.bundles.txt"; //$NON-NLS-1$
+	private static final String BUNDLE_TXT_PATH = BUNDLE_TXT_FOLDER + File.separator + "bundles.txt"; //$NON-NLS-1$
+
 	/**
 	 * Returns bundles defined by the 'bundles.txt' file in the
 	 * specified location, or <code>null</code> if none. The "bundles.txt" file
-	 * is assumed to be at a fixed relative location to the specified file.
+	 * is assumed to be at a fixed relative location to the specified file.  This 
+	 * method will also look for a "source.bundles.txt".  If available, any source
+	 * bundles found will also be added to the returned list.
 	 * 
 	 * @param platformHome absolute path in the local file system to an installation
 	 * @return URLs of all bundles in the installation or <code>null</code> if not able
@@ -38,20 +46,47 @@ public class P2Utils {
 	public static URL[] readBundlesTxt(String platformHome) {
 
 		File home = new File(platformHome);
-		File bundlesTxt = new File(home, "configuration" + File.separator + //$NON-NLS-1$
-				"org.eclipse.equinox.simpleconfigurator" + File.separator + //$NON-NLS-1$
-				"bundles.txt"); //$NON-NLS-1$
-		if (!bundlesTxt.exists()) {
+		Path basePath = new Path(platformHome);
+
+		File bundlesTxt = new File(home, "configuration" + File.separator + BUNDLE_TXT_PATH); //$NON-NLS-1$
+		List bundles = getBundlesFromFile(bundlesTxt, basePath);
+
+		if (bundles == null) {
+			return null;
+		}
+
+		File srcBundlesTxt = new File(home, "configuration" + File.separator + SRC_BUNDLE_TXT_PATH); //$NON-NLS-1$
+		List srcBundles = getBundlesFromFile(srcBundlesTxt, basePath);
+
+		if (srcBundles == null) {
+			return (URL[]) bundles.toArray(new URL[bundles.size()]);
+		}
+
+		bundles.addAll(srcBundles);
+		return (URL[]) bundles.toArray(new URL[bundles.size()]);
+
+	}
+
+	/**
+	 * Reads a file containing bundle entries of the following format:
+	 * <pre>symbolicName,version,location,start level,expectedState</pre>
+	 * Returns a list of URL locations, one for each bundle entry or <code>
+	 * null</code> if there is a problem reading the file.
+	 * 
+	 * @param file the file to read
+	 * @param basePath the path describing the base location of the platform install
+	 * @return list containing URL locations or <code>null</code>
+	 */
+	private static List getBundlesFromFile(File file, Path basePath) {
+		if (!file.exists()) {
 			return null;
 		}
 		URL url = null;
 		try {
-			url = bundlesTxt.toURL();
+			url = file.toURL();
 		} catch (MalformedURLException e) {
 			return null;
 		}
-
-		Path basePath = new Path(platformHome);
 		List bundles = new ArrayList();
 		try {
 			BufferedReader r = new BufferedReader(new InputStreamReader(url.openStream()));
@@ -62,7 +97,7 @@ public class P2Utils {
 				while ((line = r.readLine()) != null) {
 					if (line.startsWith("#")) //$NON-NLS-1$
 						continue;
-					line = line.trim();// symbolicName,version,location,start level,expectedState
+					line = line.trim();
 					if (line.length() == 0)
 						continue;
 
@@ -123,35 +158,62 @@ public class P2Utils {
 		} catch (IOException e) {
 			return null;
 		}
-		return (URL[]) bundles.toArray(new URL[bundles.size()]);
+		return bundles;
 	}
 
 	/**
 	 * Creates a bundles.txt file in the given directory containing the name,
 	 * version, location, start level and expected state of the bundles in the
-	 * launch.  The map of bundles must be of the form IModelPluginBase to a String
-	 * ("StartLevel:AutoStart").  Returns the URL location of the bundle.txt or <code>null</code>
-	 * if there was a problem creating it.
+	 * launch.  Will also create a source.bundles.txt containing all of the 
+	 * source bundles in the launch. The map of bundles must be of the form 
+	 * IModelPluginBase to a String ("StartLevel:AutoStart").  Returns the 
+	 * URL location of the bundle.txt or <code>null</code> if there was a 
+	 * problem creating it.
 	 * 
 	 * @param bundles map containing all bundles to write to the bundles.txt, maps IPluginModelBase to String ("StartLevel:AutoStart")
 	 * @param defaultStartLevel start level to use when "default" is the start level
 	 * @param defaultAutoStart auto start setting to use when "default" is the auto start setting
-	 * @param directory directory directory to create the bundles.txt file in
+	 * @param directory configuration directory to create the files in
 	 * @return URL location of the bundles.txt or <code>null</code>
 	 */
 	public static URL writeBundlesTxt(Map bundles, int defaultStartLevel, boolean defaultAutoStart, File directory) {
 		if (bundles.size() == 0) {
 			return null;
 		}
-
-		File bundlesTxt = new File(directory, "bundles.txt"); //$NON-NLS-1$
+		File bundlesTxt = new File(directory, BUNDLE_TXT_PATH);
+		File srcBundlesTxt = new File(directory, SRC_BUNDLE_TXT_PATH);
 		BufferedWriter out = null;
+		BufferedWriter srcOut = null;
 		try {
+			new File(directory, SRC_BUNDLE_TXT_FOLDER).mkdirs();
+			new File(directory, BUNDLE_TXT_FOLDER).mkdirs();
+			bundlesTxt.createNewFile();
+			srcBundlesTxt.createNewFile();
 			out = new BufferedWriter(new FileWriter(bundlesTxt));
+			srcOut = new BufferedWriter(new FileWriter(srcBundlesTxt));
 			for (Iterator iterator = bundles.keySet().iterator(); iterator.hasNext();) {
 				IPluginModelBase currentModel = (IPluginModelBase) iterator.next();
 				IPluginBase base = currentModel.getPluginBase();
-				if (base != null) {
+
+				if (base instanceof PluginBase && ((PluginBase) base).getBundleSourceEntry() != null) {
+					String modelName = base.getId();
+					srcOut.write(modelName);
+					srcOut.write(',');
+					srcOut.write(base.getVersion());
+					srcOut.write(',');
+					File location = new File(currentModel.getInstallLocation());
+					if (location != null) {
+						try {
+							srcOut.write(location.toURL().toString());
+						} catch (MalformedURLException e) {
+							PDECore.log(e);
+						}
+					} else {
+						PDECore.log(new Status(IStatus.ERROR, PDECore.PLUGIN_ID, "While creating source.bundles.txt, could not find the bundle location for bundle " + currentModel)); //$NON-NLS-1$
+					}
+					srcOut.write(",-1,false"); //$NON-NLS-1$
+					srcOut.newLine();
+				} else if (base != null) {
 					String modelName = base.getId();
 					out.write(modelName);
 					out.write(',');
@@ -184,6 +246,8 @@ public class P2Utils {
 			}
 			out.flush();
 			out.close();
+			srcOut.flush();
+			srcOut.close();
 		} catch (IOException e) {
 			PDECore.logException(e);
 			return null;
@@ -191,6 +255,12 @@ public class P2Utils {
 			if (out != null) {
 				try {
 					out.close();
+				} catch (IOException e) {
+				}
+			}
+			if (srcOut != null) {
+				try {
+					srcOut.close();
 				} catch (IOException e) {
 				}
 			}
@@ -209,12 +279,14 @@ public class P2Utils {
 	/**
 	 * Creates a bundles.txt file in the given directory containing the name,
 	 * version, location, start level and expected state of every bundle in the
-	 * given collection.  Uses special defaults for the start level and auto start
-	 * settings. Returns the URL location of the bundle.txt or <code>null</code>
-	 * if there was a problem creating it.
+	 * given collection.  Will also create a source.bundles.txt file containing
+	 * a lsit of all source bundles found in the given collection. Uses special 
+	 * defaults for the start level and auto start settings. Returns the URL 
+	 * location of the bundle.txt or <code>null</code> if there was a problem 
+	 * creating it.
 	 * 
-	 * @param bundles collection of IPluginModelBase objects to write into the bundles.txt
-	 * @param directory directory to create the bundles.txt file in
+	 * @param bundles collection of IPluginModelBase objects to write into the bundles.txt/source.bundles.txt
+	 * @param directory directory to create the bundles.txt and source.bundles.txt files in
 	 * @return URL location of the bundles.txt or <code>null</code>
 	 */
 	public static URL writeBundlesTxt(Collection bundles, File directory) {
