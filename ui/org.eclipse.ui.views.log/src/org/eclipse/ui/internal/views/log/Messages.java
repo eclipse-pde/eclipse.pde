387417f3461745d4dc0607695cc0641e85c89683
Chris Aniszczyk <caniszczyk>
2008-02-13 01:07:49 +0000

    remove unused entry and organize things a bit

--- ui/org.eclipse.ui.views.log/src/org/eclipse/ui/internal/views/log/Messages.java
+++ ui/org.eclipse.ui.views.log/src/org/eclipse/ui/internal/views/log/Messages.java
@@ -91,9 +91,11 @@ public class Messages extends NLS {
 	public static String FilterDialog_EnableFiltersCheckbox;
 	public static String FilterDialog_FilterShouldntContainSemicolon;
 	public static String FilterDialog_Remove;
+
 	public static String OpenLogDialog_title;
 	public static String OpenLogDialog_message;
 	public static String OpenLogDialog_cannotDisplay;
+
 	public static String ImportLogAction_noLaunchHistory;
 	public static String ImportLogAction_reloadWorkspaceLog;
 
