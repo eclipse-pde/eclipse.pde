d39417405e82cc583969217fd1222710c7218bf1
Chris Aniszczyk <caniszczyk>
2008-02-18 14:57:24 +0000

    RESOLVED - bug 219250: NPE opening non-feature.xml feature file https://bugs.eclipse.org/bugs/show_bug.cgi?id=219250

--- ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/editor/feature/FeatureEditor.java
+++ ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/editor/feature/FeatureEditor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2007 IBM Corporation and others.
+ * Copyright (c) 2000, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -90,7 +90,7 @@ public class FeatureEditor extends MultiSourceEditor implements IShowEditorInput
 			buildFile = file;
 			featureFile = createFeatureFile(project);
 		}
-		if (featureFile.exists()) {
+		if (featureFile != null && featureFile.exists()) {
 			FileEditorInput in = new FileEditorInput(featureFile);
 			manager.putContext(in, new FeatureInputContext(this, in, file == featureFile));
 		}
