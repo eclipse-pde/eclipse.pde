f4c9c3d6fe2191e78caa7feec3a52c14f51993ac
oliviert <>
2008-02-20 20:49:57 +0000

    HEAD - Support for test harness. To be confirm once the test build is completed

--- apitools/org.eclipse.pde.api.tools.tests/src/org/eclipse/pde/api/tools/model/tests/TestSuiteHelper.java
+++ apitools/org.eclipse.pde.api.tools.tests/src/org/eclipse/pde/api/tools/model/tests/TestSuiteHelper.java
@@ -12,10 +12,13 @@ package org.eclipse.pde.api.tools.model.tests;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.FilenameFilter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
@@ -25,6 +28,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Properties;
 import java.util.Set;
+import java.util.StringTokenizer;
 
 import junit.framework.Assert;
 
@@ -167,10 +171,46 @@ public class TestSuiteHelper {
 	 */
 	public static File getEEDescriptionFile() {
 		String eePath = System.getProperty("ee.file");
+		if (eePath == null) {
+			// generate a fake 1.5 ee file
+			File fakeEEFile = null;
+			PrintWriter writer = null;
+			try {
+				fakeEEFile = File.createTempFile("eefile", ".ee");
+				writer = new PrintWriter(new BufferedWriter(new FileWriter(fakeEEFile)));
+				writer.print("-Djava.home=");
+				writer.println(System.getProperty("java.home"));
+				writer.print("-Dee.bootclasspath=");
+				writer.println(getJavaClassLibsAsString());
+				writer.println("-Dee.language.level=1.5");
+				writer.println("-Dee.class.library.level=J2SE-1.5");
+				writer.flush();
+			} catch (IOException e) {
+				// ignore
+			} finally {
+				if (writer != null) {
+					writer.close();
+				}
+			}
+			fakeEEFile.deleteOnExit();
+			eePath = fakeEEFile.getAbsolutePath();
+		}
 		Assert.assertNotNull("-Dee.file not specified", eePath);
 		File eeFile = new File(eePath);
 		Assert.assertTrue("EE file does not exist: " + eePath, eeFile.exists());
 		return eeFile;
+	}
+
+	private static String getJavaClassLibsAsString() {
+		String[] libs = getJavaClassLibs();
+		StringBuffer buffer = new StringBuffer();
+		for (int i = 0, max = libs.length; i < max; i++) {
+			if (i > 0) {
+				buffer.append(File.pathSeparatorChar);
+			}
+			buffer.append(libs[i]);
+		}
+		return String.valueOf(buffer);
 	}
 
 	/**
@@ -209,14 +249,16 @@ public class TestSuiteHelper {
 	 */
 	public static void addAllRequired(IApiProfile baseline, Set done, IApiComponent component, List collection) throws CoreException {
 		IRequiredComponentDescription[] descriptions = component.getRequiredComponents();
+		boolean error = false;
+		StringBuffer buffer = new StringBuffer();
 		for (int i = 0; i < descriptions.length; i++) {
 			IRequiredComponentDescription description = descriptions[i];
 			if (!done.contains(description.getId())) {
 				File bundle = getBundle(description.getId());
 				if (bundle == null) {
 					if (!description.isOptional()) {
-						throw new CoreException(new Status(IStatus.ERROR,
-								"Missing required bundle: " + description.getId(), null));
+						buffer.append(description.getId()).append(',');
+						error = true;
 					}
 				} else {
 					IApiComponent apiComponent = baseline.newApiComponent(bundle.getAbsolutePath());
@@ -225,6 +267,10 @@ public class TestSuiteHelper {
 					addAllRequired(baseline, done, apiComponent, collection);
 				}
 			}
+		}
+		if (error) {
+			throw new CoreException(new Status(IStatus.ERROR,
+					"Missing required bundle: " + String.valueOf(buffer), null));
 		}
 	}
 
@@ -436,5 +482,111 @@ public class TestSuiteHelper {
 				}
 			}
 		}
+	}
+	/**
+	 * Search the user hard-drive for a Java class library.
+	 * Returns null if none could be found.
+	 *
+	 * Example of use: [org.eclipse.jdt.core.tests.util.Util.getJavaClassLib()]
+	*/
+	public static String[] getJavaClassLibs() {
+		// check bootclasspath properties for Sun, JRockit and Harmony VMs
+		String bootclasspathProperty = System.getProperty("sun.boot.class.path"); //$NON-NLS-1$
+		if ((bootclasspathProperty == null) || (bootclasspathProperty.length() == 0)) {
+			// IBM J9 VMs
+			bootclasspathProperty = System.getProperty("vm.boot.class.path"); //$NON-NLS-1$
+			if ((bootclasspathProperty == null) || (bootclasspathProperty.length() == 0)) {
+				// Harmony using IBM VME
+				bootclasspathProperty = System.getProperty("org.apache.harmony.boot.class.path"); //$NON-NLS-1$
+			}
+		}
+		String[] jars = null;
+		if ((bootclasspathProperty != null) && (bootclasspathProperty.length() != 0)) {
+			StringTokenizer tokenizer = new StringTokenizer(bootclasspathProperty, File.pathSeparator);
+			final int size = tokenizer.countTokens();
+			jars = new String[size];
+			int i = 0;
+			while (tokenizer.hasMoreTokens()) {
+				final String fileName = toNativePath(tokenizer.nextToken());
+				if (new File(fileName).exists()) {
+					jars[i] = fileName;
+					i++;
+				}
+			}
+			if (size != i) {
+				// resize
+				System.arraycopy(jars, 0, (jars = new String[i]), 0, i);
+			}
+		} else {
+			String jreDir = getJREDirectory();
+			final String osName = System.getProperty("os.name");
+			if (jreDir == null) {
+				return new String[] {};
+			}
+			if (osName.startsWith("Mac")) {
+				return new String[] {
+						toNativePath(jreDir + "/../Classes/classes.jar")
+				};
+			}
+			final String vmName = System.getProperty("java.vm.name");
+			if ("J9".equals(vmName)) {
+				return new String[] {
+						toNativePath(jreDir + "/lib/jclMax/classes.zip")
+				};
+			}
+			String[] jarsNames = null;
+			ArrayList paths = new ArrayList();
+			if ("DRLVM".equals(vmName)) {
+				FilenameFilter jarFilter = new FilenameFilter() {
+					public boolean accept(File dir, String name) {
+						return name.endsWith(".jar") & !name.endsWith("-src.jar");
+					}
+				};
+				jarsNames = new File(jreDir + "/lib/boot/").list(jarFilter);
+				addJarEntries(jreDir + "/lib/boot/", jarsNames, paths);
+			} else {
+				jarsNames = new String[] {
+						"/lib/vm.jar",
+						"/lib/rt.jar",
+						"/lib/core.jar",
+						"/lib/security.jar",
+						"/lib/xml.jar",
+						"/lib/graphics.jar"
+				};
+				addJarEntries(jreDir, jarsNames, paths);
+			}
+			jars = new String[paths.size()];
+			paths.toArray(jars);
+		}
+		return jars;
+	}
+	private static void addJarEntries(String jreDir, String[] jarNames, ArrayList paths) {
+		for (int i = 0, max = jarNames.length; i < max; i++) {
+			final String currentName = jreDir + jarNames[i];
+			File f = new File(currentName);
+			if (f.exists()) {
+				paths.add(toNativePath(currentName));
+			}
+		}
+	}
+	/**
+	 * Makes the given path a path using native path separators as returned by File.getPath()
+	 * and trimming any extra slash.
+	 */
+	private static String toNativePath(String path) {
+	    String nativePath = path.replace('\\', File.separatorChar).replace('/', File.separatorChar);
+	    return
+	        nativePath.endsWith("/") || nativePath.endsWith("\\") ?
+	            nativePath.substring(0, nativePath.length() - 1) :
+	            nativePath;
+	}
+	/**
+	 * Returns the JRE directory this tests are running on.
+	 * Returns null if none could be found.
+	 *
+	 * Example of use: [org.eclipse.jdt.core.tests.util.Util.getJREDirectory()]
+	 */
+	public static String getJREDirectory() {
+	    return System.getProperty("java.home");
 	}
 }
