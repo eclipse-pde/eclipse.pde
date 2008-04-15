2fe5f10410fcff53cb67da18b0e9833d14d8ef3d
Chris Aniszczyk <caniszczyk>
2008-04-15 14:57:22 +0000

    RESOLVED - bug 220179: [Manifest][Editors] XMLInputContext erase more characters when removing nodes https://bugs.eclipse.org/bugs/show_bug.cgi?id=220179

--- ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/editor/context/XMLInputContext.java
+++ ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/editor/context/XMLInputContext.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2003, 2007 IBM Corporation and others.
+ * Copyright (c) 2003, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -383,7 +383,7 @@ public abstract class XMLInputContext extends UTF8InputContext {
 
 			// if we reached start of line, remove newline
 			if (startOffset == startLineOffset)
-				startOffset -= doc.getLineDelimiter(startLine).length();
+				startOffset -= doc.getLineDelimiter(startLine - 1).length();
 
 			// add difference of new offset
 			length = extraLength + (offset - startOffset);
