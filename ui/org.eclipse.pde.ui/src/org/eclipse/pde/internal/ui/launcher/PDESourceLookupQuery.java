0b463b763b3810e461bed1b80da94d2409d96b5e
Chris Aniszczyk <caniszczyk>
2008-04-22 05:08:00 +0000

    RESOLVED - bug 228025: Possible NPE during source lookup https://bugs.eclipse.org/bugs/show_bug.cgi?id=228025

--- ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/launcher/PDESourceLookupQuery.java
+++ ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/launcher/PDESourceLookupQuery.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2007 IBM Corporation and others.
+ * Copyright (c) 2006, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -54,12 +54,14 @@ public class PDESourceLookupQuery implements ISafeRunnable {
 		} else if (fElement instanceof IJavaObject) {
 			IJavaObject object = (IJavaObject) fElement;
 			IJavaReferenceType type = (IJavaReferenceType) object.getJavaType();
-			classLoaderObject = type.getClassLoaderObject();
-			if (object.getJavaType() != null) {
-				declaringTypeName = object.getJavaType().getName();
-			}
-			if (declaringTypeName != null) {
-				sourcePath = generateSourceName(declaringTypeName);
+			if (type != null) {
+				classLoaderObject = type.getClassLoaderObject();
+				if (object.getJavaType() != null) {
+					declaringTypeName = object.getJavaType().getName();
+				}
+				if (declaringTypeName != null) {
+					sourcePath = generateSourceName(declaringTypeName);
+				}
 			}
 		} else if (fElement instanceof IJavaReferenceType) {
 			IJavaReferenceType type = (IJavaReferenceType) fElement;
