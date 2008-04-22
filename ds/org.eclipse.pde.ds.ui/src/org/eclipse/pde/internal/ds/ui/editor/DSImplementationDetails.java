d1541002ee6ecf929c6d2ebdc0d23730742589b5
Chris Aniszczyk <caniszczyk>
2008-04-22 12:04:39 +0000

    227904: [ds] editor polish https://bugs.eclipse.org/bugs/show_bug.cgi?id=227904

--- ds/org.eclipse.pde.ds.ui/src/org/eclipse/pde/internal/ds/ui/editor/DSImplementationDetails.java
+++ ds/org.eclipse.pde.ds.ui/src/org/eclipse/pde/internal/ds/ui/editor/DSImplementationDetails.java
@@ -12,30 +12,138 @@
  *******************************************************************************/
 package org.eclipse.pde.internal.ds.ui.editor;
 
+
+import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.pde.internal.ds.core.IDSImplementation;
+import org.eclipse.pde.internal.ui.editor.FormEntryAdapter;
+import org.eclipse.pde.internal.ui.editor.FormLayoutFactory;
+import org.eclipse.pde.internal.ui.parts.FormEntry;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.forms.IFormColors;
+import org.eclipse.ui.forms.IFormPart;
+import org.eclipse.ui.forms.widgets.ExpandableComposite;
+import org.eclipse.ui.forms.widgets.Section;
 
 public class DSImplementationDetails extends DSAbstractDetails {
 	
-	IDSImplementation fImplementation;
+	private IDSImplementation fImplementation;
+
+	private Section fMainSection;
+
+	private FormEntry fTitle;
+
 
 	public DSImplementationDetails(IDSMaster masterSection) {
 		super(masterSection, DSInputContext.CONTEXT_ID);
+		fImplementation = null;
+		fMainSection = null;
+		fTitle = null;
+
 	}
 
 	public void createDetails(Composite parent) {
-		// TODO Auto-generated method stub
+
+		Color foreground = getToolkit().getColors().getColor(IFormColors.TITLE);
+
+		// Create main section
+		fMainSection = getToolkit().createSection(parent,
+				Section.DESCRIPTION | ExpandableComposite.TITLE_BAR);
+		fMainSection.clientVerticalSpacing = FormLayoutFactory.SECTION_HEADER_VERTICAL_SPACING;
+		fMainSection.setText("Definition");
+		fMainSection
+				.setDescription("Specify the service's implementation class:");
+
+		fMainSection.setLayout(FormLayoutFactory
+				.createClearGridLayout(false, 1));
+
+		// Align the master and details section headers (misalignment caused
+		// by section toolbar icons)
+		getPage().alignSectionHeaders(getMasterSection().getSection(),
+				fMainSection);
+
+		// Create container for main section
+		 Composite mainSectionClient = getToolkit()
+				.createComposite(fMainSection);
+		mainSectionClient.setLayout(FormLayoutFactory
+				.createSectionClientGridLayout(false, 2));
+
+		// Attribute: title
+		fTitle = new FormEntry(mainSectionClient, getToolkit(), "Title*:",
+				SWT.NONE);
+		
+		// Bind widgets
+		 getToolkit().paintBordersFor(mainSectionClient);
+		fMainSection.setClient(mainSectionClient);
+		markDetailsPart(fMainSection);
+
+
 		
 	}
 
 	public void hookListeners() {
-		// TODO Auto-generated method stub
-		
+		// Attribute: title
+		fTitle.setFormEntryListener(new FormEntryAdapter(this) {
+			public void textValueChanged(FormEntry entry) {
+				// Ensure data object is defined
+				if (fImplementation == null) {
+					return;
+				}
+				fImplementation.setClassName(fTitle.getValue());
+			}
+		});
 	}
+
+	// }
 
 	public void updateFields() {
-		// TODO Auto-generated method stub
+
+		boolean editable = isEditableElement();
+		// Ensure data object is defined
+		if (fImplementation == null) {
+			return;
+		}
+
+		if (fImplementation.getClassName() == null) {
+			return;
+		}
+		// Attribute: title
+		fTitle.setValue(fImplementation.getClassName(), true);
+		fTitle.setEditable(editable);
+
+
 		
 	}
 
+	public void selectionChanged(IFormPart part, ISelection selection) {
+		// Get the first selected object
+		Object object = getFirstSelectedObject(selection);
+		// Ensure we have the right type
+		if ((object == null) || (object instanceof IDSImplementation) == false) {
+			return;
+		}
+		// Set data
+		setData((IDSImplementation) object);
+		// Update the UI given the new data
+		updateFields();
+	}
+
+	/**
+	 * @param object
+	 */
+	public void setData(IDSImplementation object) {
+		// Set data
+		fImplementation = object;
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.ui.forms.AbstractFormPart#commit(boolean)
+	 */
+	public void commit(boolean onSave) {
+		super.commit(onSave);
+		fTitle.commit();
+	}
 }
