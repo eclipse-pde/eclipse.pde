043ed013e478bdebbb421246a8cce3784f6229b8
Chris Aniszczyk <caniszczyk>
2008-05-05 17:46:00 +0000

    223741: [ds] create tests for the core models https://bugs.eclipse.org/bugs/show_bug.cgi?id=223741

--- ds/org.eclipse.pde.ds.tests/src/org/eclipse/pde/internal/ds/tests/DSServiceTestCase.java
+++ ds/org.eclipse.pde.ds.tests/src/org/eclipse/pde/internal/ds/tests/DSServiceTestCase.java
@@ -10,21 +10,131 @@
  *******************************************************************************/
 package org.eclipse.pde.internal.ds.tests;
 
+import org.eclipse.pde.internal.core.text.IDocumentElementNode;
+import org.eclipse.pde.internal.ds.core.IDSObject;
+import org.eclipse.pde.internal.ds.core.IDSProvide;
+import org.eclipse.pde.internal.ds.core.IDSService;
+
 public class DSServiceTestCase extends AbstractDSModelTestCase {
-	
-	public void testAddService() {
+
+	/**
+	 * Tests a service component with all Attributes and Child values
+	 */
+	public void testAddCompleteService() {
 		StringBuffer buffer = new StringBuffer();
-		buffer.append("<service>");
+		buffer.append("<service servicefactory=\"true\" >");
 		buffer.append(LF);
 		buffer.append("<provide interface=\"");
-		buffer.append("java.lang.Runnable");
+		buffer.append("java.lang.Runnable\">");
 		buffer.append("</provide>");
 		buffer.append(LF);
 		buffer.append("</service>");
+
 		setXMLContents(buffer, LF);
 		load();
-		
-		// TODO test stuff?
+
+		IDSObject root = fModel.getDSRoot();
+
+		assertTrue(root.getChildCount() == 1);
+
+		IDocumentElementNode child = root.getChildAt(0);
+
+		assertTrue(child instanceof IDSService);
+
+		IDSService service = (IDSService) child;
+		assertTrue(service.getServiceFactory() == true);
+
+		assertTrue(child.getChildCount() == 1);
+		IDocumentElementNode grandChild = child.getChildAt(0);
+
+		assertTrue(grandChild instanceof IDSProvide);
+		IDSProvide provide = (IDSProvide) grandChild;
+
+		String interface1 = provide.getInterface();
+
+		assertTrue(interface1.equals("java.lang.Runnable"));
 	}
 
+	/**
+	 * Tests a service component with multiple Childs 
+	 */
+	public void testMultipleProvideService() {
+		StringBuffer buffer = new StringBuffer();
+		buffer.append("<service>");
+
+		buffer.append(LF);
+		buffer.append("<provide interface=\"");
+		buffer.append("java.lang.Runnable1\"/>");
+
+		buffer.append(LF);
+		buffer.append("<provide interface=\"");
+		buffer.append("java.lang.Runnable2\"/>");
+
+		buffer.append(LF);
+		buffer.append("<provide interface=\"");
+		buffer.append("java.lang.Runnable3\"/>");
+
+		buffer.append(LF);
+		buffer.append("<provide interface=\"");
+		buffer.append("java.lang.Runnable4\"/>");
+
+		buffer.append(LF);
+		buffer.append("<provide interface=\"");
+		buffer.append("java.lang.Runnable5\"/>");
+
+		buffer.append(LF);
+		buffer.append("<provide interface=\"");
+		buffer.append("java.lang.Runnable6\"/> ");
+
+		buffer.append("</service>");
+
+//		System.out.println(buffer);
+		setXMLContents(buffer, LF);
+		load();
+
+		IDSObject root = fModel.getDSRoot();
+
+		assertTrue(root.getChildCount() == 1);
+
+		IDocumentElementNode child = root.getChildAt(0);
+
+		assertTrue(child instanceof IDSService);
+
+		IDSService service = (IDSService) child;
+
+		assertTrue(child.getChildCount() == 6);
+
+		for (int i = 0; i < 6; i++) {
+			IDocumentElementNode grandChild = child.getChildAt(i);
+
+			IDSProvide provide = (IDSProvide) grandChild;
+			String interface1 = provide.getInterface();
+			assertTrue(interface1.equals("java.lang.Runnable" + (i+1)));
+		}
+
+	}
+
+	/**
+	 * Tests a service component default values 
+	 */
+	public void testDefaultValuesService() {
+		StringBuffer buffer = new StringBuffer();
+		buffer.append("<service>");
+		buffer.append(LF);
+		buffer.append("</service>");
+
+		setXMLContents(buffer, LF);
+		load();
+
+		IDSObject root = fModel.getDSRoot();
+
+		assertTrue(root.getChildCount() == 1);
+
+		IDocumentElementNode child = root.getChildAt(0);
+
+		assertTrue(child instanceof IDSService);
+
+		IDSService service = (IDSService) child;
+		assertTrue(service.getServiceFactory() == false);
+	}
 }
