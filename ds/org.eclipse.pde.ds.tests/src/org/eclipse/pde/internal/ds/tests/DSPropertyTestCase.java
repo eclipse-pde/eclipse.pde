a88db20bfe3b16d12cdcf09d2035bca82f72e1eb
Chris Aniszczyk <caniszczyk>
2008-05-07 20:47:43 +0000

    stub out some more test cases

---
+++ ds/org.eclipse.pde.ds.tests/src/org/eclipse/pde/internal/ds/tests/DSPropertyTestCase.java
@@ -0,0 +1,30 @@
+/*******************************************************************************
+ * Copyright (c) 2008 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.pde.internal.ds.tests;
+
+import org.eclipse.pde.internal.ds.core.IDSComponent;
+
+public class DSPropertyTestCase extends AbstractDSModelTestCase {
+	
+	public void testProperty() {
+		StringBuffer buffer = new StringBuffer();
+		setXMLContents(buffer, LF);
+		load();
+
+		IDSComponent component = fModel.getDSComponent();
+		assertNotNull(component);
+		
+		// TODO fill in
+	}
+	
+	
+
+}
