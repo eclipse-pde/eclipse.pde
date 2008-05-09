3a8df195d8192bad3d1c09aaa90575e2838e4984
Chris Aniszczyk <caniszczyk>
2008-05-09 20:25:12 +0000

    227903: [ds] create a new wizard for DS files https://bugs.eclipse.org/bugs/show_bug.cgi?id=227903

---
+++ ds/org.eclipse.pde.ds.ui/src/org/eclipse/pde/internal/ds/ui/wizards/DSNewWizard.java
@@ -0,0 +1,23 @@
+/*******************************************************************************
+ * Copyright (c) 2008 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *     Chris Aniszczyk <caniszczyk@gmail.com>
+ *     Rafael Oliveira Nóbrega <rafael.oliveira@gmail.com> - bug 223738
+ *******************************************************************************/
+package org.eclipse.pde.internal.ds.ui.wizards;
+
+import org.eclipse.ui.wizards.newresource.BasicNewFileResourceWizard;
+
+public class DSNewWizard extends BasicNewFileResourceWizard {
+
+	public DSNewWizard() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
