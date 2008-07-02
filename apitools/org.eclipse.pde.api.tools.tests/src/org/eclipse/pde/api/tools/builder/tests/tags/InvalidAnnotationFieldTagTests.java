4e69e7401b663f950195309cf57fda7b5fe385bb
Michael Rennie <mrennie>
2008-07-02 15:42:41 +0000

    Bug 218571 test suite required for incremental builder

--- apitools/org.eclipse.pde.api.tools.tests/src/org/eclipse/pde/api/tools/builder/tests/tags/InvalidAnnotationFieldTagTests.java
+++ apitools/org.eclipse.pde.api.tools.tests/src/org/eclipse/pde/api/tools/builder/tests/tags/InvalidAnnotationFieldTagTests.java
@@ -13,6 +13,7 @@ package org.eclipse.pde.api.tools.builder.tests.tags;
 import junit.framework.Test;
 
 import org.eclipse.core.runtime.IPath;
+import org.eclipse.jdt.internal.compiler.impl.CompilerOptions;
 
 /**
  * Tests the use of invalid tags in annotation fields and constants
@@ -41,5 +42,480 @@ public class InvalidAnnotationFieldTagTests extends InvalidFieldTagTests {
 	 */
 	public static Test suite() {
 		return buildTestSuite(InvalidAnnotationFieldTagTests.class);
+	}
+	
+	/* (non-Javadoc)
+	 * @see org.eclipse.pde.api.tools.builder.tests.ApiBuilderTest#getTestCompliance()
+	 */
+	protected String getTestCompliance() {
+		return CompilerOptions.VERSION_1_5;
+	}
+	
+	/**
+	 * Tests the unsupported @noextend tag on an annotation field
+	 * using an incremental build
+	 */
+	public void test1I() {
+		setExpectedProblemIds(getDefaultProblemSet(1));
+		deployIncrementalBuildTest(TESTING_PACKAGE, "test1", true);
+	}
+	
+	/**
+	 * Tests the unsupported @noextend tag on an annotation field
+	 * using a full build
+	 */
+	public void test1F() {
+		setExpectedProblemIds(getDefaultProblemSet(1));
+		deployIncrementalBuildTest(TESTING_PACKAGE, "test1", true);
+	}
+	
+	/**
+	 * Tests the unsupported @noextend tag on an outer annotation field
+	 * using an incremental build
+	 */
+	public void test2I() {
+		setExpectedProblemIds(getDefaultProblemSet(1));
+		deployIncrementalBuildTest(TESTING_PACKAGE, "test2", true);
+	}
+	
+	/**
+	 * Tests the unsupported @noextend tag on an outer annotation field
+	 * using a full build
+	 */
+	public void test2F() {
+		setExpectedProblemIds(getDefaultProblemSet(1));
+		deployIncrementalBuildTest(TESTING_PACKAGE, "test2", true);
+	}
+	
+	/**
+	 * Tests the unsupported @noextend tag on an inner annotation field
+	 * using an incremental build
+	 */
+	public void test3I() {
+		setExpectedProblemIds(getDefaultProblemSet(1));
+		deployIncrementalBuildTest(TESTING_PACKAGE, "test3", true);
+	}
+	
+	/**
+	 * Tests the unsupported @noextend tag on an inner annotation field
+	 * using a full build
+	 */
+	public void test3F() {
+		setExpectedProblemIds(getDefaultProblemSet(1));
+		deployIncrementalBuildTest(TESTING_PACKAGE, "test3", true);
+	}
+	
+	/**
+	 * Tests the unsupported @noextend tag on a variety of inner / outer annotation fields
+	 * using an incremental build
+	 */
+	public void test4I() {
+		setExpectedProblemIds(getDefaultProblemSet(3));
+		deployIncrementalBuildTest(TESTING_PACKAGE, "test4", true);
+	}
+	
+	/**
+	 * Tests the unsupported @noextend tag on a variety of inner / outer annotation fields
+	 * using a full build
+	 */
+	public void test4F() {
+		setExpectedProblemIds(getDefaultProblemSet(3));
+		deployIncrementalBuildTest(TESTING_PACKAGE, "test4", true);
+	}
+	
+	/**
+	 * Tests the unsupported @noextend tag on an annotation field in the default package
+	 * using an incremental build
+	 */
+	public void test5I() {
+		setExpectedProblemIds(getDefaultProblemSet(1));
+		deployIncrementalBuildTest("", "test5", true);
+	}
+	
+	/**
+	 * Tests the unsupported @noextend tag on an annotation field in the default package
+	 * using a full build
+	 */
+	public void test5F() {
+		setExpectedProblemIds(getDefaultProblemSet(1));
+		deployIncrementalBuildTest("", "test5", true);
+	}
+	
+	/**
+	 * Tests the unsupported @noinstantiate tag on an annotation field
+	 * using an incremental build
+	 */
+	public void test6I() {
+		setExpectedProblemIds(getDefaultProblemSet(1));
+		deployIncrementalBuildTest(TESTING_PACKAGE, "test6", true);
+	}
+	
+	/**
+	 * Tests the unsupported @noinstantiate tag on an annotation field
+	 * using a full build
+	 */
+	public void test6F() {
+		setExpectedProblemIds(getDefaultProblemSet(1));
+		deployIncrementalBuildTest(TESTING_PACKAGE, "test6", true);
+	}
+	
+	/**
+	 * Tests the unsupported @noinstantiate tag on an outer annotation field
+	 * using an incremental build
+	 */
+	public void test7I() {
+		setExpectedProblemIds(getDefaultProblemSet(1));
+		deployIncrementalBuildTest(TESTING_PACKAGE, "test7", true);
+	}
+	
+	/**
+	 * Tests the unsupported @noinstantiate tag on an outer annotation field
+	 * using a full build
+	 */
+	public void test7F() {
+		setExpectedProblemIds(getDefaultProblemSet(1));
+		deployIncrementalBuildTest(TESTING_PACKAGE, "test7", true);
+	}
+	
+	/**
+	 * Tests the unsupported @noinstantiate tag on an inner annotation field
+	 * using an incremental build
+	 */
+	public void test8I() {
+		setExpectedProblemIds(getDefaultProblemSet(1));
+		deployIncrementalBuildTest(TESTING_PACKAGE, "test8", true);
+	}
+	
+	/**
+	 * Tests the unsupported @noinstantiate tag on an inner annotation field
+	 * using a full build
+	 */
+	public void test8F() {
+		setExpectedProblemIds(getDefaultProblemSet(1));
+		deployIncrementalBuildTest(TESTING_PACKAGE, "test8", true);
+	}
+	
+	/**
+	 * Tests the unsupported @noinstantiate tag on a variety of inner / outer annotation fields
+	 * using an incremental build
+	 */
+	public void test9I() {
+		setExpectedProblemIds(getDefaultProblemSet(3));
+		deployIncrementalBuildTest(TESTING_PACKAGE, "test9", true);
+	}
+	
+	/**
+	 * Tests the unsupported @noinstantiate tag on a variety of inner / outer annotation fields
+	 * using a full build
+	 */
+	public void test9F() {
+		setExpectedProblemIds(getDefaultProblemSet(3));
+		deployIncrementalBuildTest(TESTING_PACKAGE, "test9", true);
+	}
+	
+	/**
+	 * Tests the unsupported @noinstantiate tag on an annotation field in the default package
+	 * using an incremental build
+	 */
+	public void test10I() {
+		setExpectedProblemIds(getDefaultProblemSet(1));
+		deployIncrementalBuildTest("", "test10", true);
+	}
+	
+	/**
+	 * Tests the unsupported @noinstantiate tag on an annotation field in the default package
+	 * using a full build
+	 */
+	public void test10F() {
+		setExpectedProblemIds(getDefaultProblemSet(1));
+		deployIncrementalBuildTest("", "test10", true);
+	}
+	
+	/**
+	 * Tests the unsupported @noimplement tag on an annotation field
+	 * using an incremental build
+	 */
+	public void test11I() {
+		setExpectedProblemIds(getDefaultProblemSet(1));
+		deployIncrementalBuildTest(TESTING_PACKAGE, "test11", true);
+	}
+	
+	/**
+	 * Tests the unsupported @noimplement tag on an annotation field
+	 * using a full build
+	 */
+	public void test11F() {
+		setExpectedProblemIds(getDefaultProblemSet(1));
+		deployIncrementalBuildTest(TESTING_PACKAGE, "test11", true);
+	}
+	
+	/**
+	 * Tests the unsupported @noimplement tag on an outer annotation field
+	 * using an incremental build
+	 */
+	public void test12I() {
+		setExpectedProblemIds(getDefaultProblemSet(1));
+		deployIncrementalBuildTest(TESTING_PACKAGE, "test12", true);
+	}
+	
+	/**
+	 * Tests the unsupported @noimplement tag on an outer annotation field
+	 * using a full build
+	 */
+	public void test12F() {
+		setExpectedProblemIds(getDefaultProblemSet(1));
+		deployIncrementalBuildTest(TESTING_PACKAGE, "test12", true);
+	}
+	
+	/**
+	 * Tests the unsupported @noimplement tag on an inner annotation field
+	 * using an incremental build
+	 */
+	public void test13I() {
+		setExpectedProblemIds(getDefaultProblemSet(1));
+		deployIncrementalBuildTest(TESTING_PACKAGE, "test13", true);
+	}
+	
+	/**
+	 * Tests the unsupported @noimplement tag on an inner annotation field
+	 * using a full build
+	 */
+	public void test13F() {
+		setExpectedProblemIds(getDefaultProblemSet(1));
+		deployIncrementalBuildTest(TESTING_PACKAGE, "test13", true);
+	}
+	
+	/**
+	 * Tests the unsupported @noimplement tag on a variety of inner / outer annotation fields
+	 * using an incremental build
+	 */
+	public void test14I() {
+		setExpectedProblemIds(getDefaultProblemSet(3));
+		deployIncrementalBuildTest(TESTING_PACKAGE, "test14", true);
+	}
+	
+	/**
+	 * Tests the unsupported @noimplement tag on a variety of inner / outer annotation fields
+	 * using a full build
+	 */
+	public void test14F() {
+		setExpectedProblemIds(getDefaultProblemSet(3));
+		deployIncrementalBuildTest(TESTING_PACKAGE, "test14", true);
+	}
+	
+	/**
+	 * Tests the unsupported @noimplement tag on an annotation field in the default package
+	 * using an incremental build
+	 */
+	public void test15I() {
+		setExpectedProblemIds(getDefaultProblemSet(1));
+		deployIncrementalBuildTest("", "test15", true);
+	}
+	
+	/**
+	 * Tests the unsupported @noimplement tag on an annotation field in the default package
+	 * using a full build
+	 */
+	public void test15F() {
+		setExpectedProblemIds(getDefaultProblemSet(1));
+		deployIncrementalBuildTest("", "test15", true);
+	}
+	
+	/**
+	 * Tests the unsupported @nooverride tag on an annotation field
+	 * using an incremental build
+	 */
+	public void test16I() {
+		setExpectedProblemIds(getDefaultProblemSet(1));
+		deployIncrementalBuildTest(TESTING_PACKAGE, "test16", true);
+	}
+	
+	/**
+	 * Tests the unsupported @nooverride tag on an annotation field
+	 * using a full build
+	 */
+	public void test16F() {
+		setExpectedProblemIds(getDefaultProblemSet(1));
+		deployIncrementalBuildTest(TESTING_PACKAGE, "test16", true);
+	}
+	
+	/**
+	 * Tests the unsupported @nooverride tag on an outer annotation field
+	 * using an incremental build
+	 */
+	public void test17I() {
+		setExpectedProblemIds(getDefaultProblemSet(1));
+		deployIncrementalBuildTest(TESTING_PACKAGE, "test17", true);
+	}
+	
+	/**
+	 * Tests the unsupported @nooverride tag on an outer annotation field
+	 * using a full build
+	 */
+	public void test17F() {
+		setExpectedProblemIds(getDefaultProblemSet(1));
+		deployIncrementalBuildTest(TESTING_PACKAGE, "test17", true);
+	}
+	
+	/**
+	 * Tests the unsupported @nooverride tag on an inner annotation field
+	 * using an incremental build
+	 */
+	public void test18I() {
+		setExpectedProblemIds(getDefaultProblemSet(1));
+		deployIncrementalBuildTest(TESTING_PACKAGE, "test18", true);
+	}
+	
+	/**
+	 * Tests the unsupported @nooverride tag on an inner annotation field
+	 * using a full build
+	 */
+	public void test18F() {
+		setExpectedProblemIds(getDefaultProblemSet(1));
+		deployIncrementalBuildTest(TESTING_PACKAGE, "test18", true);
+	}
+	
+	/**
+	 * Tests the unsupported @nooverride tag on a variety of inner / outer annotation fields
+	 * using an incremental build
+	 */
+	public void test19I() {
+		setExpectedProblemIds(getDefaultProblemSet(3));
+		deployIncrementalBuildTest(TESTING_PACKAGE, "test19", true);
+	}
+	
+	/**
+	 * Tests the unsupported @nooverride tag on a variety of inner / outer annotation fields
+	 * using a full build
+	 */
+	public void test19F() {
+		setExpectedProblemIds(getDefaultProblemSet(3));
+		deployIncrementalBuildTest(TESTING_PACKAGE, "test19", true);
+	}
+	
+	/**
+	 * Tests the unsupported @nooverride tag on an annotation field in the default package
+	 * using an incremental build
+	 */
+	public void test20I() {
+		setExpectedProblemIds(getDefaultProblemSet(1));
+		deployIncrementalBuildTest("", "test20", true);
+	}
+	
+	/**
+	 * Tests the unsupported @nooverride tag on an annotation field in the default package
+	 * using a full build
+	 */
+	public void test20F() {
+		setExpectedProblemIds(getDefaultProblemSet(1));
+		deployIncrementalBuildTest("", "test20", true);
+	}
+	
+	/**
+	 * Tests all the unsupported tags on a variety of annotation fields
+	 * using an incremental build
+	 */
+	public void test21I() {
+		setExpectedProblemIds(getDefaultProblemSet(15));
+		deployIncrementalBuildTest(TESTING_PACKAGE, "test21", true);
+	}
+	
+	/**
+	 * Tests all the unsupported tags on a variety of annotation fields
+	 * using a full build
+	 */
+	public void test21F() {
+		setExpectedProblemIds(getDefaultProblemSet(15));
+		deployIncrementalBuildTest(TESTING_PACKAGE, "test21", true);
+	}
+	
+	/**
+	 * Tests the unsupported @noreference tag on a final annotation field
+	 * using an incremental build
+	 */
+	public void test22I() {
+		setExpectedProblemIds(getDefaultProblemSet(1));
+		deployIncrementalBuildTest(TESTING_PACKAGE, "test22", true);
+	}
+	
+	/**
+	 * Tests the unsupported @noreference tag on a final annotation field
+	 * using a full build
+	 */
+	public void test22F() {
+		setExpectedProblemIds(getDefaultProblemSet(1));
+		deployIncrementalBuildTest(TESTING_PACKAGE, "test22", true);
+	}
+	
+	/**
+	 * Tests the unsupported @noreference tag on a final outer annotation field
+	 * using an incremental build
+	 */
+	public void test23I() {
+		setExpectedProblemIds(getDefaultProblemSet(1));
+		deployIncrementalBuildTest(TESTING_PACKAGE, "test23", true);
+	}
+	
+	/**
+	 * Tests the unsupported @noreference tag on a final outer annotation field
+	 * using a full build
+	 */
+	public void test23F() {
+		setExpectedProblemIds(getDefaultProblemSet(1));
+		deployIncrementalBuildTest(TESTING_PACKAGE, "test23", true);
+	}
+	
+	/**
+	 * Tests the unsupported @noreference tag on a final inner annotation field
+	 * using an incremental build
+	 */
+	public void test24I() {
+		setExpectedProblemIds(getDefaultProblemSet(1));
+		deployIncrementalBuildTest(TESTING_PACKAGE, "test24", true);
+	}
+	
+	/**
+	 * Tests the unsupported @noreference tag on a final inner annotation field
+	 * using a full build
+	 */
+	public void test24F() {
+		setExpectedProblemIds(getDefaultProblemSet(1));
+		deployIncrementalBuildTest(TESTING_PACKAGE, "test24", true);
+	}
+	
+	/**
+	 * Tests the unsupported @noreference tag on a variety of final inner / outer annotation fields
+	 * using an incremental build
+	 */
+	public void test25I() {
+		setExpectedProblemIds(getDefaultProblemSet(3));
+		deployIncrementalBuildTest(TESTING_PACKAGE, "test25", true);
+	}
+	
+	/**
+	 * Tests the unsupported @noreference tag on a variety of final inner / outer annotation fields
+	 * using a full build
+	 */
+	public void test25F() {
+		setExpectedProblemIds(getDefaultProblemSet(3));
+		deployIncrementalBuildTest(TESTING_PACKAGE, "test25", true);
+	}
+	
+	/**
+	 * Tests the unsupported @noreference tag on a final annotation field in the default package
+	 * using an incremental build
+	 */
+	public void test26I() {
+		setExpectedProblemIds(getDefaultProblemSet(1));
+		deployIncrementalBuildTest("", "test26", true);
+	}
+	
+	/**
+	 * Tests the unsupported @noreference tag on a final annotation field in the default package
+	 * using a full build
+	 */
+	public void test26F() {
+		setExpectedProblemIds(getDefaultProblemSet(1));
+		deployIncrementalBuildTest("", "test26", true);
 	}
 }
---
+++ apitools/org.eclipse.pde.api.tools.tests/test-builder/tags/field/annotation/test19.java
@@ -0,0 +1,36 @@
+/*******************************************************************************
+ * Copyright (c) 2008 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package a.b.c;
+
+/**
+ * Test unsupported @nooverride tag on fields in outer / inner annotation
+ */
+public @interface test19 {
+	@interface inner {
+		/**
+		 * @nooverride
+		 */
+		public int f2 = 0;
+		@interface inner2 {
+			/**
+			 * @nooverride
+			 */
+			public char[] f3 = {};
+		}
+	}
+}
+
+@interface outer {
+	/**
+	 * @nooverride
+	 */
+	public static Object f1 = null;
+}
