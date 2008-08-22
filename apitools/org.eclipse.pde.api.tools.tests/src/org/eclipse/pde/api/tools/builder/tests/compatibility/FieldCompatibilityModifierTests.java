664d4deafe103067f35ade61feec610f63a09048
Darin Wright <darin>
2008-08-22 19:47:18 +0000

    field visibillity/modifier tests

---
+++ apitools/org.eclipse.pde.api.tools.tests/src/org/eclipse/pde/api/tools/builder/tests/compatibility/FieldCompatibilityModifierTests.java
@@ -0,0 +1,488 @@
+/*******************************************************************************
+ * Copyright (c) 2008 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.pde.api.tools.builder.tests.compatibility;
+
+import junit.framework.Test;
+
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.pde.api.tools.internal.problems.ApiProblemFactory;
+import org.eclipse.pde.api.tools.internal.provisional.comparator.IDelta;
+import org.eclipse.pde.api.tools.internal.provisional.problems.IApiProblem;
+
+/**
+ * Tests that the builder correctly reports compatibility problems
+ * related class hierarchies.
+ * 
+ * @since 3.4
+ */
+public class FieldCompatibilityModifierTests extends FieldCompatibilityTests {
+	
+	/**
+	 * Workspace relative path classes in bundle/project A
+	 */
+	protected static IPath WORKSPACE_CLASSES_PACKAGE_A = new Path("org.eclipse.api.tools.tests.compatability.a/src/a/fields/modifiers");
+
+	/**
+	 * Package prefix for test classes
+	 */
+	protected static String PACKAGE_PREFIX = "a.fields.modifiers.";
+	
+	/**
+	 * Constructor
+	 * @param name
+	 */
+	public FieldCompatibilityModifierTests(String name) {
+		super(name);
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.pde.api.tools.builder.tests.ApiBuilderTests#getTestSourcePath()
+	 */
+	protected IPath getTestSourcePath() {
+		return super.getTestSourcePath().append("modifiers");
+	}
+	
+	/**
+	 * @return the tests for this class
+	 */
+	public static Test suite() {
+		return buildTestSuite(FieldCompatibilityModifierTests.class);
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.pde.api.tools.builder.tests.ApiBuilderTests#getTestingProjectName()
+	 */
+	protected String getTestingProjectName() {
+		return "classcompat";
+	}
+
+	/**
+	 * Returns a problem id for a compatibility change to a class based on the
+	 * specified flags.
+	 * 
+	 * @param flags
+	 * @return problem id
+	 */
+	protected int getChangedProblemId(int flags) {
+		return ApiProblemFactory.createProblemId(
+				IApiProblem.CATEGORY_COMPATIBILITY,
+				IDelta.FIELD_ELEMENT_TYPE,
+				IDelta.CHANGED,
+				flags);
+	}	
+	
+	/**
+	 * Tests making a non-final field final
+	 */
+	private void xAddFinal(boolean incremental) throws Exception {
+		IPath filePath = WORKSPACE_CLASSES_PACKAGE_A.append("AddFinal.java");
+		int[] ids = new int[] {
+			getChangedProblemId(IDelta.NON_FINAL_TO_FINAL)
+		};
+		setExpectedProblemIds(ids);
+		String[][] args = new String[1][];
+		args[0] = new String[]{PACKAGE_PREFIX + "AddFinal", "ADD_FINAL"};
+		setExpectedMessageArgs(args);
+		performCompatibilityTest(filePath, incremental);
+	}
+	
+	public void testAddFinalI() throws Exception {
+		xAddFinal(true);
+	}	
+	
+	public void testAddFinalF() throws Exception {
+		xAddFinal(false);
+	}
+	
+	/**
+	 * Tests making a non-final no-reference field final
+	 */
+	private void xAddFinalNoReference(boolean incremental) throws Exception {
+		IPath filePath = WORKSPACE_CLASSES_PACKAGE_A.append("AddFinalNoReference.java");
+		// TODO: expect no problems since @noreference
+		int[] ids = new int[] {
+			getChangedProblemId(IDelta.NON_FINAL_TO_FINAL)
+		};
+		setExpectedProblemIds(ids);
+		String[][] args = new String[1][];
+		args[0] = new String[]{PACKAGE_PREFIX + "AddFinalNoReference", "ADD_FINAL"};
+		setExpectedMessageArgs(args);
+		performCompatibilityTest(filePath, incremental);
+	}
+	
+	public void testAddFinalNoReferenceI() throws Exception {
+		xAddFinalNoReference(true);
+	}	
+	
+	public void testAddFinalNoReferenceF() throws Exception {
+		xAddFinalNoReference(false);
+	}	
+	
+	/**
+	 * Tests making a non-final field final
+	 */
+	private void xAddFinalOnStatic(boolean incremental) throws Exception {
+		IPath filePath = WORKSPACE_CLASSES_PACKAGE_A.append("AddFinalOnStatic.java");
+		int[] ids = new int[] {
+			getChangedProblemId(IDelta.NON_FINAL_TO_FINAL)
+		};
+		setExpectedProblemIds(ids);
+		String[][] args = new String[1][];
+		args[0] = new String[]{PACKAGE_PREFIX + "AddFinalOnStatic", "ADD_FINAL"};
+		setExpectedMessageArgs(args);
+		performCompatibilityTest(filePath, incremental);
+	}
+	
+	public void testAddFinalOnStaticI() throws Exception {
+		xAddFinalOnStatic(true);
+	}	
+	
+	public void testAddFinalOnStaticF() throws Exception {
+		xAddFinalOnStatic(false);
+	}
+	
+	/**
+	 * Tests making a non-final no-reference field final
+	 */
+	private void xAddFinalOnStaticNoReference(boolean incremental) throws Exception {
+		IPath filePath = WORKSPACE_CLASSES_PACKAGE_A.append("AddFinalOnStaticNoReference.java");
+		// TODO: expect no problems since @noreference
+		int[] ids = new int[] {
+			getChangedProblemId(IDelta.NON_FINAL_TO_FINAL)
+		};
+		setExpectedProblemIds(ids);
+		String[][] args = new String[1][];
+		args[0] = new String[]{PACKAGE_PREFIX + "AddFinalOnStaticNoReference", "ADD_FINAL"};
+		setExpectedMessageArgs(args);
+		performCompatibilityTest(filePath, incremental);
+	}
+	
+	public void testAddFinalOnStaticNoReferenceI() throws Exception {
+		xAddFinalOnStaticNoReference(true);
+	}	
+	
+	public void testAddFinalOnStaticNoReferenceF() throws Exception {
+		xAddFinalOnStaticNoReference(false);
+	}		
+	
+	/**
+	 * Tests making a non-final field final
+	 */
+	private void xRemoveFinalOnConstant(boolean incremental) throws Exception {
+		IPath filePath = WORKSPACE_CLASSES_PACKAGE_A.append("RemoveFinalOnConstant.java");
+		// TODO: should only have the first error (removed final) - second error indicates
+		// the same problem (redundant)
+		int[] ids = new int[] {
+			getChangedProblemId(IDelta.FINAL_TO_NON_FINAL_STATIC_CONSTANT),
+			ApiProblemFactory.createProblemId(
+					IApiProblem.CATEGORY_COMPATIBILITY,
+					IDelta.FIELD_ELEMENT_TYPE,
+					IDelta.REMOVED,
+					IDelta.VALUE)
+		};
+		setExpectedProblemIds(ids);
+		String[][] args = new String[2][];
+		args[0] = new String[]{PACKAGE_PREFIX + "RemoveFinalOnConstant", "CONSTANT"};
+		args[1] = new String[]{PACKAGE_PREFIX + "RemoveFinalOnConstant", "CONSTANT", "CONSTANT"};
+		setExpectedMessageArgs(args);
+		performCompatibilityTest(filePath, incremental);
+	}
+	
+	public void testRemoveFinalOnConstantI() throws Exception {
+		xRemoveFinalOnConstant(true);
+	}	
+	
+	public void testRemoveFinalOnConstantF() throws Exception {
+		xRemoveFinalOnConstant(false);
+	}	
+	
+	/**
+	 * Tests making a non-static field static
+	 */
+	private void xAddStatic(boolean incremental) throws Exception {
+		IPath filePath = WORKSPACE_CLASSES_PACKAGE_A.append("AddStatic.java");
+		int[] ids = new int[] {
+			getChangedProblemId(IDelta.NON_STATIC_TO_STATIC)
+		};
+		setExpectedProblemIds(ids);
+		String[][] args = new String[1][];
+		args[0] = new String[]{PACKAGE_PREFIX + "AddStatic", "ADD_STATIC"};
+		setExpectedMessageArgs(args);
+		performCompatibilityTest(filePath, incremental);
+	}
+	
+	public void testAddStaticI() throws Exception {
+		xAddStatic(true);
+	}	
+	
+	public void testAddStaticF() throws Exception {
+		xAddStatic(false);
+	}
+	
+	/**
+	 * Tests making a non-static no-reference field static
+	 */
+	private void xAddStaticNoReference(boolean incremental) throws Exception {
+		IPath filePath = WORKSPACE_CLASSES_PACKAGE_A.append("AddStaticNoReference.java");
+		// TODO: should be no problems
+		int[] ids = new int[] {
+			getChangedProblemId(IDelta.NON_STATIC_TO_STATIC)
+		};
+		setExpectedProblemIds(ids);
+		String[][] args = new String[1][];
+		args[0] = new String[]{PACKAGE_PREFIX + "AddStaticNoReference", "ADD_STATIC"};
+		setExpectedMessageArgs(args);
+		performCompatibilityTest(filePath, incremental);
+	}
+	
+	public void testAddStaticNoReferenceI() throws Exception {
+		xAddStaticNoReference(true);
+	}	
+	
+	public void testAddStaticNoReferenceF() throws Exception {
+		xAddStaticNoReference(false);
+	}
+	
+	/**
+	 * Tests making a static field non-static
+	 */
+	private void xRemoveStatic(boolean incremental) throws Exception {
+		IPath filePath = WORKSPACE_CLASSES_PACKAGE_A.append("RemoveStatic.java");
+		int[] ids = new int[] {
+			getChangedProblemId(IDelta.STATIC_TO_NON_STATIC)
+		};
+		setExpectedProblemIds(ids);
+		String[][] args = new String[1][];
+		args[0] = new String[]{PACKAGE_PREFIX + "RemoveStatic", "REMOVE_STATIC"};
+		setExpectedMessageArgs(args);
+		performCompatibilityTest(filePath, incremental);
+	}
+	
+	public void testRemoveStaticI() throws Exception {
+		xRemoveStatic(true);
+	}	
+	
+	public void testRemoveStaticF() throws Exception {
+		xRemoveStatic(false);
+	}
+	
+	/**
+	 * Tests making a static no-reference field non-static
+	 */
+	private void xRemoveStaticNoReference(boolean incremental) throws Exception {
+		IPath filePath = WORKSPACE_CLASSES_PACKAGE_A.append("RemoveStaticNoReference.java");
+		// TODO: should be no problems
+		int[] ids = new int[] {
+			getChangedProblemId(IDelta.STATIC_TO_NON_STATIC)
+		};
+		setExpectedProblemIds(ids);
+		String[][] args = new String[1][];
+		args[0] = new String[]{PACKAGE_PREFIX + "RemoveStaticNoReference", "REMOVE_STATIC"};
+		setExpectedMessageArgs(args);
+		performCompatibilityTest(filePath, incremental);
+	}
+	
+	public void testRemoveStaticNoReferenceI() throws Exception {
+		xRemoveStaticNoReference(true);
+	}	
+	
+	public void testRemoveStaticNoReferenceF() throws Exception {
+		xRemoveStaticNoReference(false);
+	}	
+	
+	/**
+	 * Tests changing a protected field to package protected
+	 */
+	private void xProtectedToPackage(boolean incremental) throws Exception {
+		IPath filePath = WORKSPACE_CLASSES_PACKAGE_A.append("ProtectedToPackage.java");
+		int[] ids = new int[] {
+			getChangedProblemId(IDelta.DECREASE_ACCESS)
+		};
+		setExpectedProblemIds(ids);
+		String[][] args = new String[1][];
+		args[0] = new String[]{PACKAGE_PREFIX + "ProtectedToPackage", "FIELD"};
+		setExpectedMessageArgs(args);
+		performCompatibilityTest(filePath, incremental);
+	}
+	
+	public void testProtectedToPackageI() throws Exception {
+		xProtectedToPackage(true);
+	}	
+	
+	public void testProtectedToPackageF() throws Exception {
+		xProtectedToPackage(false);
+	}	
+	
+	/**
+	 * Tests changing a protected field to package protected when no-reference
+	 */
+	private void xProtectedToPackageNoReference(boolean incremental) throws Exception {
+		IPath filePath = WORKSPACE_CLASSES_PACKAGE_A.append("ProtectedToPackageNoReference.java");
+		// TODO: should be no problems
+		int[] ids = new int[] {
+			getChangedProblemId(IDelta.DECREASE_ACCESS)
+		};
+		setExpectedProblemIds(ids);
+		String[][] args = new String[1][];
+		args[0] = new String[]{PACKAGE_PREFIX + "ProtectedToPackageNoReference", "FIELD"};
+		setExpectedMessageArgs(args);
+		performCompatibilityTest(filePath, incremental);
+	}
+	
+	public void testProtectedToPackageNoReferenceI() throws Exception {
+		xProtectedToPackageNoReference(true);
+	}	
+	
+	public void testProtectedToPackageNoReferenceF() throws Exception {
+		xProtectedToPackageNoReference(false);
+	}
+	
+	/**
+	 * Tests changing a protected field to private
+	 */
+	private void xProtectedToPrivate(boolean incremental) throws Exception {
+		IPath filePath = WORKSPACE_CLASSES_PACKAGE_A.append("ProtectedToPrivate.java");
+		int[] ids = new int[] {
+			getChangedProblemId(IDelta.DECREASE_ACCESS)
+		};
+		setExpectedProblemIds(ids);
+		String[][] args = new String[1][];
+		args[0] = new String[]{PACKAGE_PREFIX + "ProtectedToPrivate", "FIELD"};
+		setExpectedMessageArgs(args);
+		performCompatibilityTest(filePath, incremental);
+	}
+	
+	public void testProtectedToPrivateI() throws Exception {
+		xProtectedToPrivate(true);
+	}	
+	
+	public void testProtectedToPrivateF() throws Exception {
+		xProtectedToPrivate(false);
+	}	
+	
+	/**
+	 * Tests changing a protected field to private field in a no-extend class
+	 */
+	private void xProtectedToPrivateNoExtend(boolean incremental) throws Exception {
+		IPath filePath = WORKSPACE_CLASSES_PACKAGE_A.append("ProtectedToPrivateNoExtend.java");
+		// TODO: should be no errors
+		int[] ids = new int[] {
+			getChangedProblemId(IDelta.DECREASE_ACCESS)
+		};
+		setExpectedProblemIds(ids);
+		String[][] args = new String[1][];
+		args[0] = new String[]{PACKAGE_PREFIX + "ProtectedToPrivateNoExtend", "FIELD"};
+		setExpectedMessageArgs(args);
+		performCompatibilityTest(filePath, incremental);
+	}
+	
+	public void testProtectedToPrivateNoExtendI() throws Exception {
+		xProtectedToPrivateNoExtend(true);
+	}	
+	
+	public void testProtectedToPrivateNoExtendF() throws Exception {
+		xProtectedToPrivateNoExtend(false);
+	}	
+	
+	/**
+	 * Tests changing a public field to package
+	 */
+	private void xPublicToPackage(boolean incremental) throws Exception {
+		IPath filePath = WORKSPACE_CLASSES_PACKAGE_A.append("PublicToPackage.java");
+		int[] ids = new int[] {
+			getChangedProblemId(IDelta.DECREASE_ACCESS)
+		};
+		setExpectedProblemIds(ids);
+		String[][] args = new String[1][];
+		args[0] = new String[]{PACKAGE_PREFIX + "PublicToPackage", "FIELD"};
+		setExpectedMessageArgs(args);
+		performCompatibilityTest(filePath, incremental);
+	}
+	
+	public void testPublicToPackageI() throws Exception {
+		xPublicToPackage(true);
+	}	
+	
+	public void testPublicToPackageF() throws Exception {
+		xPublicToPackage(false);
+	}	
+	
+	/**
+	 * Tests changing a public field to private
+	 */
+	private void xPublicToPrivate(boolean incremental) throws Exception {
+		IPath filePath = WORKSPACE_CLASSES_PACKAGE_A.append("PublicToPrivate.java");
+		int[] ids = new int[] {
+			getChangedProblemId(IDelta.DECREASE_ACCESS)
+		};
+		setExpectedProblemIds(ids);
+		String[][] args = new String[1][];
+		args[0] = new String[]{PACKAGE_PREFIX + "PublicToPrivate", "FIELD"};
+		setExpectedMessageArgs(args);
+		performCompatibilityTest(filePath, incremental);
+	}
+	
+	public void testPublicToPrivateI() throws Exception {
+		xPublicToPrivate(true);
+	}	
+	
+	public void testPublicToPrivateF() throws Exception {
+		xPublicToPrivate(false);
+	}	
+	
+	/**
+	 * Tests changing a public field to private
+	 */
+	private void xPublicToPrivateNoReference(boolean incremental) throws Exception {
+		IPath filePath = WORKSPACE_CLASSES_PACKAGE_A.append("PublicToPrivateNoReference.java");
+		// TODO: should be no problems
+		int[] ids = new int[] {
+			getChangedProblemId(IDelta.DECREASE_ACCESS)
+		};
+		setExpectedProblemIds(ids);
+		String[][] args = new String[1][];
+		args[0] = new String[]{PACKAGE_PREFIX + "PublicToPrivateNoReference", "FIELD"};
+		setExpectedMessageArgs(args);
+		performCompatibilityTest(filePath, incremental);
+	}
+	
+	public void testPublicToPrivateNoReferenceI() throws Exception {
+		xPublicToPrivateNoReference(true);
+	}	
+	
+	public void testPublicToPrivateNoReferenceF() throws Exception {
+		xPublicToPrivateNoReference(false);
+	}
+	
+	/**
+	 * Tests changing a public field to protected
+	 */
+	private void xPublicToProtected(boolean incremental) throws Exception {
+		IPath filePath = WORKSPACE_CLASSES_PACKAGE_A.append("PublicToProtected.java");
+		int[] ids = new int[] {
+			getChangedProblemId(IDelta.DECREASE_ACCESS)
+		};
+		setExpectedProblemIds(ids);
+		String[][] args = new String[1][];
+		args[0] = new String[]{PACKAGE_PREFIX + "PublicToProtected", "FIELD"};
+		setExpectedMessageArgs(args);
+		performCompatibilityTest(filePath, incremental);
+	}
+	
+	public void testPublicToProtectedI() throws Exception {
+		xPublicToProtected(true);
+	}	
+	
+	public void testPublicToProtectedF() throws Exception {
+		xPublicToProtected(false);
+	}	
+}
--- apitools/org.eclipse.pde.api.tools.tests/src/org/eclipse/pde/api/tools/builder/tests/compatibility/FieldCompatibilityTests.java
+++ apitools/org.eclipse.pde.api.tools.tests/src/org/eclipse/pde/api/tools/builder/tests/compatibility/FieldCompatibilityTests.java
@@ -10,9 +10,14 @@
  *******************************************************************************/
 package org.eclipse.pde.api.tools.builder.tests.compatibility;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 import junit.framework.Test;
+import junit.framework.TestSuite;
 
 import org.eclipse.core.runtime.IPath;
+import org.eclipse.jdt.core.tests.junit.extension.TestCase;
 
 /**
  * Tests that the builder correctly finds and reports field
@@ -22,6 +27,56 @@ import org.eclipse.core.runtime.IPath;
  */
 public class FieldCompatibilityTests extends CompatibilityTest {
 
+	/**
+	 * Collects tests from the getAllTestClasses() method into the given suite
+	 * @param suite
+	 */
+	private static void collectTests(TestSuite suite) {
+		// Hack to load all classes before computing their suite of test cases
+		// this allow to reset test cases subsets while running all Builder tests...
+		Class[] classes = getAllTestClasses();
+
+		// Reset forgotten subsets of tests
+		TestCase.TESTS_PREFIX = null;
+		TestCase.TESTS_NAMES = null;
+		TestCase.TESTS_NUMBERS = null;
+		TestCase.TESTS_RANGE = null;
+		TestCase.RUN_ONLY_ID = null;
+
+		/* tests */
+		for (int i = 0, length = classes.length; i < length; i++) {
+			Class clazz = classes[i];
+			Method suiteMethod;
+			try {
+				suiteMethod = clazz.getDeclaredMethod("suite", new Class[0]);
+			} catch (NoSuchMethodException e) {
+				e.printStackTrace();
+				continue;
+			}
+			Object test;
+			try {
+				test = suiteMethod.invoke(null, new Object[0]);
+			} catch (IllegalAccessException e) {
+				e.printStackTrace();
+				continue;
+			} catch (InvocationTargetException e) {
+				e.printStackTrace();
+				continue;
+			}
+			suite.addTest((Test) test);
+		}
+	}
+	
+	/**
+	 * @return all of the child test classes of this class
+	 */
+	private static Class[] getAllTestClasses() {
+		Class[] classes = new Class[] {
+			FieldCompatibilityModifierTests.class
+		};
+		return classes;
+	}	
+	
 	/**
 	 * Constructor
 	 * @param name
@@ -41,7 +96,9 @@ public class FieldCompatibilityTests extends CompatibilityTest {
 	 * @return the tests for this class
 	 */
 	public static Test suite() {
-		return buildTestSuite(FieldCompatibilityTests.class);
+		TestSuite suite = new TestSuite(FieldCompatibilityTests.class.getName());
+		collectTests(suite);
+		return suite;
 	}
 
 	/* (non-Javadoc)
