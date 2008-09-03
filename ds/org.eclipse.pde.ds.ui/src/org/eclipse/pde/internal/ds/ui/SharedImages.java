106d104c1b1c058792d5649db5c1685f0c21cae2
Chris Aniszczyk <caniszczyk>
2008-09-03 20:38:40 +0000

    242027: [ds] graduate the DS tooling https://bugs.eclipse.org/bugs/show_bug.cgi?id=242027

--- ds/org.eclipse.pde.ds.ui/src/org/eclipse/pde/internal/ds/ui/SharedImages.java
+++ ds/org.eclipse.pde.ds.ui/src/org/eclipse/pde/internal/ds/ui/SharedImages.java
@@ -1,12 +1,14 @@
 /*******************************************************************************
- * Copyright (c) 2008 IBM Corporation and others.
+ * Copyright (c) 2008 Code 9 Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *     IBM Corporation - initial API and implementation
+ *     Code 9 Corporation - initial API and implementation
+ *     Chris Aniszczyk <caniszczyk@gmail.com>
+ *     Rafael Oliveira Nobrega <rafael.oliveira@gmail.com> - bug 242028
  *******************************************************************************/
 package org.eclipse.pde.internal.ds.ui;
 
