3078b5615d23eab7b1492c29345d9e5c9ec67dca
Darin Wright <darin>
2008-09-10 20:54:25 +0000

    performance tests under construction

---
+++ apitools/org.eclipse.pde.api.tools.tests/src/org/eclipse/pde/api/tools/builder/tests/performance/FullSourceBuildTests.java
@@ -0,0 +1,148 @@
+/*******************************************************************************
+ * Copyright (c) 2008 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.pde.api.tools.builder.tests.performance;
+
+import junit.framework.Test;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.IncrementalProjectBuilder;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.pde.api.tools.internal.provisional.ApiPlugin;
+import org.eclipse.test.performance.Dimension;
+
+/**
+ * Performance tests for full source workspace build
+ * 
+ * @since 1.0
+ */
+public class FullSourceBuildTests extends PerformanceTest {
+	
+	/**
+	 * Workspace relative path classes in bundle/project A
+	 */
+	protected static IPath WORKSPACE_CLASSES_PACKAGE_A = new Path("bundle.a/src/a/enums");
+
+	/**
+	 * Package prefix for test classes
+	 */
+	protected static String PACKAGE_PREFIX = "a.enums.";
+	
+
+	/**
+	 * Constructor
+	 * @param name
+	 */
+	public FullSourceBuildTests(String name) {
+		super(name);
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.pde.api.tools.builder.tests.ApiBuilderTests#getTestSourcePath()
+	 */
+	protected IPath getTestSourcePath() {
+		return super.getTestSourcePath().append("enums");
+	}
+	
+	/**
+	 * @return the tests for this class
+	 */
+	public static Test suite() {
+		return buildTestSuite(FullSourceBuildTests.class);
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.pde.api.tools.builder.tests.ApiBuilderTest#getDefaultProblemId()
+	 */
+	protected int getDefaultProblemId() {
+		return 0;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.pde.api.tools.builder.tests.ApiBuilderTests#getTestingProjectName()
+	 */
+	protected String getTestingProjectName() {
+		return "enumcompat";
+	}
+	
+	/**
+	 * Tests a full build of a 3.4 workspace with source from debug.core and pre-reqs
+	 * against a baseline of 3.3 binary plug-ins.
+	 * 
+	 * @throws Exception
+	 */
+	public void testFullBuild() throws Exception {
+		tagAsGlobalSummary("Full Build", Dimension.ELAPSED_PROCESS);
+		
+		// get everything built
+		fullBuild();
+		IProject[] ps = getEnv().getWorkspace().getRoot().getProjects();
+		IJavaProject[] projects = new IJavaProject[ps.length];
+		for (int i = 0; i < ps.length; i++) {
+			projects[i] = JavaCore.create(ps[i]);
+		}
+		IWorkspace.ProjectOrder po = ResourcesPlugin.getWorkspace().computeProjectOrder(ps);
+		IProject[] order = po.projects;
+		
+		for (int j = 0; j < 10; j++) {
+			startMeasuring();
+			
+			// *** build each project ***
+			for (int i = 0; i < order.length; i++) {
+				order[i].build(IncrementalProjectBuilder.FULL_BUILD, ApiPlugin.BUILDER_ID, null, null);
+			}
+			
+			stopMeasuring();
+		}
+		
+		commitMeasurements();
+		assertPerformance();
+	}
+	
+	/**
+	 * Tests a clean and full build of a 3.4 workspace with source from debug.core and pre-reqs
+	 * against a baseline of 3.3 binary plug-ins.
+	 * 
+	 * @throws Exception
+	 */
+	public void testCleanFullBuild() throws Exception {
+		tagAsGlobalSummary("Clean & Full Build", Dimension.ELAPSED_PROCESS);
+		
+		// get everything built
+		fullBuild();
+		IProject[] ps = getEnv().getWorkspace().getRoot().getProjects();
+		IJavaProject[] projects = new IJavaProject[ps.length];
+		for (int i = 0; i < ps.length; i++) {
+			projects[i] = JavaCore.create(ps[i]);
+		}
+		IWorkspace.ProjectOrder po = ResourcesPlugin.getWorkspace().computeProjectOrder(ps);
+		IProject[] order = po.projects;
+		
+		for (int j = 0; j < 10; j++) {
+			startMeasuring();
+			
+			// *** build each project ***
+			for (int i = 0; i < order.length; i++) {
+				order[i].build(IncrementalProjectBuilder.CLEAN_BUILD, ApiPlugin.BUILDER_ID, null, null);
+				order[i].build(IncrementalProjectBuilder.FULL_BUILD, ApiPlugin.BUILDER_ID, null, null);
+			}
+			
+			stopMeasuring();
+		}
+		
+		commitMeasurements();
+		assertPerformance();
+	}	
+}
