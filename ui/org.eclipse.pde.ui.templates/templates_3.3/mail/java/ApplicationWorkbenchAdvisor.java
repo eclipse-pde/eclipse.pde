834f1b498a40f15608525abd72f20706694e0776
Chris Aniszczyk <caniszczyk>
2008-09-15 20:30:40 +0000

    RESOLVED - 243086: [Mail Template] move perspective ID from AWA to Perspective https://bugs.eclipse.org/bugs/show_bug.cgi?id=243086

--- ui/org.eclipse.pde.ui.templates/templates_3.3/mail/java/ApplicationWorkbenchAdvisor.java
+++ ui/org.eclipse.pde.ui.templates/templates_3.3/mail/java/ApplicationWorkbenchAdvisor.java
@@ -10,14 +10,12 @@ import org.eclipse.ui.application.WorkbenchWindowAdvisor;
  */
 public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
 	
-	private static final String PERSPECTIVE_ID = "$pluginId$.perspective";
-
     public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer) {
         return new ApplicationWorkbenchWindowAdvisor(configurer);
     }
 
 	public String getInitialWindowPerspectiveId() {
-		return PERSPECTIVE_ID;
+		return Perspective.ID;
 	} 
 	
 }
--- ui/org.eclipse.pde.ui.templates/templates_3.3/mail/java/Perspective.java
+++ ui/org.eclipse.pde.ui.templates/templates_3.3/mail/java/Perspective.java
@@ -6,6 +6,11 @@ import org.eclipse.ui.IPerspectiveFactory;
 
 public class Perspective implements IPerspectiveFactory {
 
+	/**
+	 * The ID of the perspective as specified in the extension.
+	 */
+	public static final String ID = "$pluginId$.perspective";
+
 	public void createInitialLayout(IPageLayout layout) {
 		String editorArea = layout.getEditorArea();
 		layout.setEditorAreaVisible(false);
