eb412f62c46514c0ffd17dfc7d9f623152d9b659
Chris Aniszczyk <caniszczyk>
2008-10-13 19:42:28 +0000

    javadoc cleanup

--- ui/org.eclipse.pde.core/src/org/eclipse/pde/internal/core/plugin/IWritableDelimiter.java
+++ ui/org.eclipse.pde.core/src/org/eclipse/pde/internal/core/plugin/IWritableDelimiter.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007 IBM Corporation and others.
+ * Copyright (c) 2007, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -20,12 +20,12 @@ import java.io.PrintWriter;
 public interface IWritableDelimiter {
 
 	/**
-	 * Write a customized delimeter to the serialized stream that delimits the
+	 * Write a customized delimiter to the serialized stream that delimits the
 	 * text representation of multiple model objects selected.
 	 * This is applicable when serializing multiple objects for copy, cut and
 	 * paste operations.
+	 * 
 	 * @param writer the print writer
-	 * @see org.eclipse.pde.internal.ui.editor.PDEFormEditor.copyToClipboard(ISelection)
 	 */
 	public void writeDelimeter(PrintWriter writer);
 
