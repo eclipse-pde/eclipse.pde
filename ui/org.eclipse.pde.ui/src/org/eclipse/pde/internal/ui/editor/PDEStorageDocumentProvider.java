ec3cdf93b2dd9e1fd18caad27a4a8e18f32d7885
Curtis Windatt <cwindatt>
2009-01-05 21:25:10 +0000

    Bug 259990 - CCE in PDEStorageDocumentProvider.createAnnotationModel https://bugs.eclipse.org/bugs/show_bug.cgi?id=259990

--- ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/editor/PDEStorageDocumentProvider.java
+++ ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/editor/PDEStorageDocumentProvider.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * Copyright (c) 2000, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -60,7 +60,7 @@ public class PDEStorageDocumentProvider extends StorageDocumentProvider {
 		if (element instanceof IAdaptable) {
 			IAdaptable input = (IAdaptable) element;
 			File file = (File) input.getAdapter(File.class);
-			if (file == null) {
+			if (file == null && (input instanceof IURIEditorInput)) {
 				URI uri = ((IURIEditorInput) input).getURI();
 				file = new File(uri);
 			}
