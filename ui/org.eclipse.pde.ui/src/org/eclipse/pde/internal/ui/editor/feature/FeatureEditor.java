7eda7f5caed3d00711755179ec1a8b7875aed582
Chris Aniszczyk <caniszczyk>
2009-01-12 20:34:05 +0000

    RESOLVED - bug 260753: NPE opening external feature.xml https://bugs.eclipse.org/bugs/show_bug.cgi?id=260753

--- ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/editor/feature/FeatureEditor.java
+++ ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/editor/feature/FeatureEditor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * Copyright (c) 2000, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
+ *     EclipseSource Corporation - ongoing enhancements
  *******************************************************************************/
 package org.eclipse.pde.internal.ui.editor.feature;
 
@@ -156,7 +157,7 @@ public class FeatureEditor extends MultiSourceEditor implements IShowEditorInput
 	}
 
 	protected void createSystemFileContexts(InputContextManager manager, FileStoreEditorInput input) {
-		File file = (File) input.getAdapter(File.class);
+		File file = new File(input.getURI());
 		File buildFile = null;
 		File featureFile = null;
 		String name = file.getName().toLowerCase(Locale.ENGLISH);
