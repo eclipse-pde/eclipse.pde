f11c9b7f5d4be4265a70b56a086bedd7cea11211
Chris Aniszczyk <caniszczyk>
2009-01-14 22:31:10 +0000

    RESOLVED - bug 261091: Use of Java 5 APIs https://bugs.eclipse.org/bugs/show_bug.cgi?id=261091

--- ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/editor/text/HTMLPrinter.java
+++ ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/editor/text/HTMLPrinter.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * Copyright (c) 2000, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -158,7 +158,7 @@ public class HTMLPrinter {
 		// a) within existing body tag with trailing space
 		int index = buffer.indexOf("<body "); //$NON-NLS-1$
 		if (index != -1) {
-			buffer.insert(index + 5, styleBuf);
+			buffer.insert(index + 5, styleBuf.toString());
 			return;
 		}
 
@@ -166,7 +166,7 @@ public class HTMLPrinter {
 		index = buffer.indexOf("<body>"); //$NON-NLS-1$
 		if (index != -1) {
 			buffer.insert(index + 5, ' ');
-			buffer.insert(index + 6, styleBuf);
+			buffer.insert(index + 6, styleBuf.toString());
 			return;
 		}
 	}
