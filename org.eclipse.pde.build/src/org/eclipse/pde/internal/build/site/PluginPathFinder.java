ab1e30dfcfc45eac05d628981a37cc8b70d8d72e
Andrew Niefer <aniefer>
2009-01-21 22:37:33 +0000

    bug 261890 - ArrayStoreException

--- org.eclipse.pde.build/src/org/eclipse/pde/internal/build/site/PluginPathFinder.java
+++ org.eclipse.pde.build/src/org/eclipse/pde/internal/build/site/PluginPathFinder.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2004, 2008 IBM Corporation and others. All rights reserved.
+ * Copyright (c) 2004, 2009 IBM Corporation and others. All rights reserved.
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
@@ -98,7 +98,7 @@ public class PluginPathFinder {
 		for (int i = 0; i < contents.length; i++) {
 			if (contents[i].isFile()) {
 				if (contents[i].getName().endsWith(LINK)) {
-					sites.add(getSitePath(platformHome, contents[i], features));
+					sites.add(new File(getSitePath(platformHome, contents[i], features)));
 				} else {
 					//bundle
 					results.add(contents[i]);
