1655efe0a7e8bd8428acccb6c32a9461421012c7
Chris Aniszczyk <caniszczyk>
2009-02-09 12:47:07 +0000

    let's remove some warnings ;D

--- ua/org.eclipse.pde.ua.ui/src/org/eclipse/pde/internal/ua/ui/editor/cheatsheet/comp/CompCSContentProvider.java
+++ ua/org.eclipse.pde.ua.ui/src/org/eclipse/pde/internal/ua/ui/editor/cheatsheet/comp/CompCSContentProvider.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2008 IBM Corporation and others.
+ * Copyright (c) 2006, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -14,29 +14,30 @@ package org.eclipse.pde.internal.ua.ui.editor.cheatsheet.comp;
 import java.util.List;
 
 import org.eclipse.jface.viewers.ITreeContentProvider;
+import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.pde.internal.ua.core.icheatsheet.comp.ICompCSModel;
 import org.eclipse.pde.internal.ua.core.icheatsheet.comp.ICompCSObject;
-import org.eclipse.pde.internal.ui.elements.DefaultContentProvider;
 
 /**
  * CompCSContentProvider
- *
+ * 
  */
-public class CompCSContentProvider extends DefaultContentProvider implements ITreeContentProvider {
+public class CompCSContentProvider implements ITreeContentProvider {
 
-	/**
-	 * 
-	 */
 	public CompCSContentProvider() {
 		// NO-OP
 	}
 
-	/* (non-Javadoc)
-	 * @see org.eclipse.jface.viewers.ITreeContentProvider#getChildren(java.lang.Object)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.jface.viewers.ITreeContentProvider#getChildren(java.lang.
+	 * Object)
 	 */
 	public Object[] getChildren(Object parentElement) {
 		if (parentElement instanceof ICompCSModel) {
-			return new Object[] {((ICompCSModel) parentElement).getCompCS()};
+			return new Object[] { ((ICompCSModel) parentElement).getCompCS() };
 		} else if (parentElement instanceof ICompCSObject) {
 			List list = ((ICompCSObject) parentElement).getChildren();
 			// List is never null
@@ -47,8 +48,12 @@ public class CompCSContentProvider extends DefaultContentProvider implements ITr
 		return new Object[0];
 	}
 
-	/* (non-Javadoc)
-	 * @see org.eclipse.jface.viewers.ITreeContentProvider#getParent(java.lang.Object)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.jface.viewers.ITreeContentProvider#getParent(java.lang.Object
+	 * )
 	 */
 	public Object getParent(Object element) {
 		if (element instanceof ICompCSObject) {
@@ -57,18 +62,34 @@ public class CompCSContentProvider extends DefaultContentProvider implements ITr
 		return null;
 	}
 
-	/* (non-Javadoc)
-	 * @see org.eclipse.jface.viewers.ITreeContentProvider#hasChildren(java.lang.Object)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.jface.viewers.ITreeContentProvider#hasChildren(java.lang.
+	 * Object)
 	 */
 	public boolean hasChildren(Object element) {
 		return (getChildren(element).length > 0);
 	}
 
-	/* (non-Javadoc)
-	 * @see org.eclipse.jface.viewers.IStructuredContentProvider#getElements(java.lang.Object)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.jface.viewers.IStructuredContentProvider#getElements(java
+	 * .lang.Object)
 	 */
 	public Object[] getElements(Object inputElement) {
 		return getChildren(inputElement);
+	}
+
+	public void dispose() {
+		// no op
+	}
+
+	public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
+		// no op
 	}
 
 }
