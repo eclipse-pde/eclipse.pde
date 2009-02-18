09e028fd39af6c3b82b794639a5b3c111b8caf41
Chris Aniszczyk <caniszczyk>
2009-02-18 15:23:17 +0000

    RESOLVED - bug 265094: Compiler errors when checking out compare from HEAD https://bugs.eclipse.org/bugs/show_bug.cgi?id=265094

--- ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/nls/NLSFragmentGenerator.java
+++ ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/nls/NLSFragmentGenerator.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008 IBM Corporation and others.
+ * Copyright (c) 2008, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -473,10 +473,6 @@ public class NLSFragmentGenerator {
 
 		public void add(Filter filter) {
 			filters.add(filter);
-		}
-
-		public void remove(Filter filter) {
-			filters.remove(filter);
 		}
 
 		public boolean include(Object object) {
