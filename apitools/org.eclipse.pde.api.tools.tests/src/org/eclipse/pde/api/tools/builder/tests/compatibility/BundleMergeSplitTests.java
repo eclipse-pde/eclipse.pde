405dea25667cdc67648d837536b9e1360cda5275
oliviert <>
2009-02-27 16:23:31 +0000

    HEAD - Reuse helper methods

--- apitools/org.eclipse.pde.api.tools.tests/src/org/eclipse/pde/api/tools/builder/tests/compatibility/BundleMergeSplitTests.java
+++ apitools/org.eclipse.pde.api.tools.tests/src/org/eclipse/pde/api/tools/builder/tests/compatibility/BundleMergeSplitTests.java
@@ -16,9 +16,7 @@ import java.util.Set;
 
 import junit.framework.Test;
 
-import org.eclipse.core.resources.IMarker;
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Path;
@@ -354,18 +352,7 @@ public class BundleMergeSplitTests extends ApiBuilderTest {
 	}
 	private void performMergeSplit() throws CoreException {
 		fullBuild();
-		IMarker[] jdtMarkers = getEnv().getAllJDTMarkers(ResourcesPlugin.getWorkspace().getRoot().getLocation());
-		int length = jdtMarkers.length;
-		if (length != 0) {
-			boolean condition = false;
-			for (int i = 0; i < length; i++) {
-				condition = condition || jdtMarkers[i].getAttribute(IMarker.SEVERITY, IMarker.SEVERITY_WARNING) == IMarker.SEVERITY_ERROR;
-				if (condition) {
-					System.err.println(jdtMarkers[i].getAttribute(IMarker.MESSAGE));
-				}
-			}
-			assertFalse("Should not be a JDT error", condition);
-		}
+		expectingNoJDTProblems();
 		IPath manifestPath = new Path("a.b.c").append("META-INF").append("MANIFEST.MF");
 		ApiProblem[] problems = getEnv().getProblemsFor(manifestPath, null);
 		assertProblems(problems);
