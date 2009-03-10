7fb76b47e75df994b19808091e206cf64f6223a6
Chris Aniszczyk <caniszczyk>
2009-03-10 15:33:49 +0000

    RESOLVED - bug 267878: PluginStructureViewer illegally extends StructureDiffViewer https://bugs.eclipse.org/bugs/show_bug.cgi?id=267878

--- ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/compare/PluginStructureViewerCreator.java
+++ ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/compare/PluginStructureViewerCreator.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007 IBM Corporation and others.
+ * Copyright (c) 2007, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,9 +10,10 @@
  *******************************************************************************/
 package org.eclipse.pde.internal.ui.compare;
 
-import org.eclipse.compare.CompareConfiguration;
-import org.eclipse.compare.IViewerCreator;
+import org.eclipse.compare.*;
+import org.eclipse.compare.structuremergeviewer.*;
 import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.jface.viewers.ViewerComparator;
 import org.eclipse.swt.widgets.Composite;
 
 public class PluginStructureViewerCreator implements IViewerCreator {
@@ -22,7 +23,39 @@ public class PluginStructureViewerCreator implements IViewerCreator {
 	}
 
 	public Viewer createViewer(Composite parent, CompareConfiguration config) {
-		return new PluginStructureViewer(parent, config);
+		StructureDiffViewer viewer = new StructureDiffViewer(parent, config);
+		viewer.setStructureCreator(new PluginStructureCreator());
+		viewer.setComparator(new ViewerComparator() {
+			public int compare(Viewer viewer, Object e1, Object e2) {
+				if (e1 instanceof DiffNode) {
+					if (e2 instanceof DiffNode) {
+						ITypedElement e1Element = ((DiffNode) e1).getAncestor();
+						ITypedElement e2Element = ((DiffNode) e2).getAncestor();
+						if (!(e1Element instanceof DocumentRangeNode))
+							e1Element = ((DiffNode) e1).getLeft();
+						if (!(e2Element instanceof DocumentRangeNode))
+							e2Element = ((DiffNode) e2).getLeft();
+						if (e1Element instanceof DocumentRangeNode && e2Element instanceof DocumentRangeNode) {
+							float e1off = getRelativeOffset(((DocumentRangeNode) e1Element));
+							float e2off = getRelativeOffset(((DocumentRangeNode) e2Element));
+							return e1off - e2off < 0 ? -1 : 1;
+						}
+						return 0;
+					}
+					return -1;
+				}
+				return 1;
+			}
+
+			// we may be comparing the ancestor to the left (local) document
+			// since the lengths may be different, base the comparison on the relative position in the doc
+			private float getRelativeOffset(DocumentRangeNode node) {
+				float absoluteOffset = node.getRange().getOffset();
+				float documentLength = node.getDocument().getLength();
+				return absoluteOffset / documentLength;
+			}
+		});
+		return viewer;
 	}
 
 }
