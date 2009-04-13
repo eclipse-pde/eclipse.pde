cc5e53d5603b6c520037680eceea906848874a47
Chris Aniszczyk <caniszczyk>
2009-04-13 22:21:10 +0000

    remove a deprecated call

--- ui/org.eclipse.pde.core/src/org/eclipse/pde/internal/core/MinimalState.java
+++ ui/org.eclipse.pde.core/src/org/eclipse/pde/internal/core/MinimalState.java
@@ -61,7 +61,7 @@ public class MinimalState {
 	protected MinimalState(MinimalState state) {
 		this.fState = stateObjectFactory.createState(state.fState);
 		this.fState.setPlatformProperties(state.fState.getPlatformProperties());
-		this.fState.setResolver(Platform.getPlatformAdmin().getResolver());
+		this.fState.setResolver(Platform.getPlatformAdmin().createResolver());
 		this.fId = state.fId;
 		this.fEEListChanged = state.fEEListChanged;
 		this.fExecutionEnvironments = state.fExecutionEnvironments;
--- ui/org.eclipse.pde.core/src/org/eclipse/pde/internal/core/PDEState.java
+++ ui/org.eclipse.pde.core/src/org/eclipse/pde/internal/core/PDEState.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2008 IBM Corporation and others.
+ * Copyright (c) 2005, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -127,7 +127,7 @@ public class PDEState extends MinimalState {
 			}
 
 			boolean propertiesChanged = initializePlatformProperties();
-			fState.setResolver(Platform.getPlatformAdmin().getResolver());
+			fState.setResolver(Platform.getPlatformAdmin().createResolver());
 			if (propertiesChanged)
 				fState.resolve(false);
 			fId = fState.getHighestBundleId();
