0c9ef1a8213b0f32506aad7d4404282d3286d9ad
Chris Aniszczyk <caniszczyk>
2009-04-21 21:30:28 +0000

    RESOLVED - bug 273049: [CheatSheet] Registering a cheat sheet fails with java.lang.NullPointerException https://bugs.eclipse.org/bugs/show_bug.cgi?id=273049

--- ua/org.eclipse.pde.ua.ui/src/org/eclipse/pde/internal/ua/ui/editor/cheatsheet/CSAbstractEditor.java
+++ ua/org.eclipse.pde.ua.ui/src/org/eclipse/pde/internal/ua/ui/editor/cheatsheet/CSAbstractEditor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2008 IBM Corporation and others.
+ * Copyright (c) 2007, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
+ *     EclipseSource Corporation - ongoing enhancements
  *******************************************************************************/
 package org.eclipse.pde.internal.ua.ui.editor.cheatsheet;
 
@@ -15,6 +16,7 @@ import org.eclipse.jface.action.IToolBarManager;
 import org.eclipse.jface.window.Window;
 import org.eclipse.jface.wizard.WizardDialog;
 import org.eclipse.pde.core.IModel;
+import org.eclipse.pde.internal.core.WorkspaceModelManager;
 import org.eclipse.pde.internal.ua.ui.PDEUserAssistanceUIPlugin;
 import org.eclipse.pde.internal.ua.ui.wizards.cheatsheet.RegisterCSWizard;
 import org.eclipse.pde.internal.ui.editor.MultiSourceEditor;
@@ -32,7 +34,8 @@ public abstract class CSAbstractEditor extends MultiSourceEditor {
 
 	public void contributeToToolbar(IToolBarManager manager) {
 		// Add the register cheat sheet link to the form title area
-		if (getAggregateModel().isEditable())
+		if (WorkspaceModelManager.isPluginProject(getCommonProject())
+				&& getAggregateModel().isEditable())
 			manager.add(createUIControlConRegisterCS());
 	}
 
@@ -58,26 +61,28 @@ public abstract class CSAbstractEditor extends MultiSourceEditor {
 		fImageHyperlinkRegisterCS = new ImageHyperlink(parent, SWT.NONE);
 		fImageHyperlinkRegisterCS.setText(Messages.CSAbstractEditor_register);
 		fImageHyperlinkRegisterCS.setUnderlined(true);
-		fImageHyperlinkRegisterCS.setForeground(getToolkit().getHyperlinkGroup().getForeground());
+		fImageHyperlinkRegisterCS.setForeground(getToolkit()
+				.getHyperlinkGroup().getForeground());
 	}
 
 	/**
 	 * 
 	 */
 	private void createUIListenerImageHyperlinkRegisterCS() {
-		fImageHyperlinkRegisterCS.addHyperlinkListener(new IHyperlinkListener() {
-			public void linkActivated(HyperlinkEvent e) {
-				handleLinkActivatedRegisterCS();
-			}
+		fImageHyperlinkRegisterCS
+				.addHyperlinkListener(new IHyperlinkListener() {
+					public void linkActivated(HyperlinkEvent e) {
+						handleLinkActivatedRegisterCS();
+					}
 
-			public void linkEntered(HyperlinkEvent e) {
-				handleLinkEnteredRegisterCS(e.getLabel());
-			}
+					public void linkEntered(HyperlinkEvent e) {
+						handleLinkEnteredRegisterCS(e.getLabel());
+					}
 
-			public void linkExited(HyperlinkEvent e) {
-				handleLinkExitedRegisterCS();
-			}
-		});
+					public void linkExited(HyperlinkEvent e) {
+						handleLinkExitedRegisterCS();
+					}
+				});
 	}
 
 	/**
@@ -85,9 +90,11 @@ public abstract class CSAbstractEditor extends MultiSourceEditor {
 	 */
 	private void handleLinkEnteredRegisterCS(String message) {
 		// Update colour
-		fImageHyperlinkRegisterCS.setForeground(getToolkit().getHyperlinkGroup().getActiveForeground());
+		fImageHyperlinkRegisterCS.setForeground(getToolkit()
+				.getHyperlinkGroup().getActiveForeground());
 		// Update IDE status line
-		getEditorSite().getActionBars().getStatusLineManager().setMessage(message);
+		getEditorSite().getActionBars().getStatusLineManager().setMessage(
+				message);
 	}
 
 	/**
@@ -95,7 +102,8 @@ public abstract class CSAbstractEditor extends MultiSourceEditor {
 	 */
 	private void handleLinkExitedRegisterCS() {
 		// Update colour
-		fImageHyperlinkRegisterCS.setForeground(getToolkit().getHyperlinkGroup().getForeground());
+		fImageHyperlinkRegisterCS.setForeground(getToolkit()
+				.getHyperlinkGroup().getForeground());
 		// Update IDE status line
 		getEditorSite().getActionBars().getStatusLineManager().setMessage(null);
 	}
@@ -104,17 +112,20 @@ public abstract class CSAbstractEditor extends MultiSourceEditor {
 	 * 
 	 */
 	private void handleLinkActivatedRegisterCS() {
-		RegisterCSWizard wizard = new RegisterCSWizard((IModel) getAggregateModel());
+		RegisterCSWizard wizard = new RegisterCSWizard(
+				(IModel) getAggregateModel());
 		// Initialize the wizard
 		wizard.init(PlatformUI.getWorkbench(), null);
 		// Create the dialog for the wizard
-		WizardDialog dialog = new WizardDialog(PDEUserAssistanceUIPlugin.getActiveWorkbenchShell(), wizard);
+		WizardDialog dialog = new WizardDialog(PDEUserAssistanceUIPlugin
+				.getActiveWorkbenchShell(), wizard);
 		dialog.create();
 		// Configure the dialogs size
 		dialog.getShell().setSize(400, 370);
 		// Check the result
 		if (dialog.open() == Window.OK) {
-			// TODO: MP: COMPCS: HIGH: Automatic save of editor after creating simple cheat sheet?
+			// TODO: MP: COMPCS: HIGH: Automatic save of editor after creating
+			// simple cheat sheet?
 		}
 	}
 
