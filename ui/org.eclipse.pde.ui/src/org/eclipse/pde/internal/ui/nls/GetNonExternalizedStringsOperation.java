32822606f8214526e3b92055e24acf9baeeb301e
Chris Aniszczyk <caniszczyk>
2009-05-07 18:47:01 +0000

    RESOLVED - bug 275240: Externalize strings wizard fails to detect strings that need externalizing https://bugs.eclipse.org/bugs/show_bug.cgi?id=275240

--- ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/nls/GetNonExternalizedStringsOperation.java
+++ ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/nls/GetNonExternalizedStringsOperation.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2008 IBM Corporation and others.
+ * Copyright (c) 2005, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -8,6 +8,7 @@
  * Contributors:
  *     IBM Corporation - initial API and implementation
  *     Benjamin Cabe <benjamin.cabe@anyware-tech.com> - bug 252329
+ *     David Green <dgreen99@gmail.com> - bug 275240
  *******************************************************************************/
 package org.eclipse.pde.internal.ui.nls;
 
@@ -167,12 +168,14 @@ public class GetNonExternalizedStringsOperation implements IRunnableWithProgress
 			PluginExtensionNode parentNode = (PluginExtensionNode) parent;
 			IDocumentAttributeNode[] attributes = parentNode.getNodeAttributes();
 			ISchemaElement schemaElement = schema.findElement(parentNode.getXMLTagName());
-			for (int j = 0; j < attributes.length; j++) {
-				IPluginAttribute attr = (IPluginAttribute) attributes[j];
-				ISchemaAttribute attInfo = schemaElement.getAttribute(attr.getName());
-				if (attInfo != null && attInfo.isTranslatable())
-					if (isNotTranslated(attr.getValue()))
-						fModelChangeTable.addToChangeTable(memModel, file, attr, selected(file));
+			if (schemaElement != null) {
+				for (int j = 0; j < attributes.length; j++) {
+					IPluginAttribute attr = (IPluginAttribute) attributes[j];
+					ISchemaAttribute attInfo = schemaElement.getAttribute(attr.getName());
+					if (attInfo != null && attInfo.isTranslatable())
+						if (isNotTranslated(attr.getValue()))
+							fModelChangeTable.addToChangeTable(memModel, file, attr, selected(file));
+				}
 			}
 		}
 
