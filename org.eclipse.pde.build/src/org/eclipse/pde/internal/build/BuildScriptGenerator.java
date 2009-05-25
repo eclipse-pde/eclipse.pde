00fb4fae6a54714304afa166d551c211c1caad7d
Andrew Niefer <aniefer>
2009-05-25 15:45:36 +0000

    update copyrights

--- org.eclipse.pde.build/src/org/eclipse/pde/internal/build/BuildScriptGenerator.java
+++ org.eclipse.pde.build/src/org/eclipse/pde/internal/build/BuildScriptGenerator.java
@@ -1,11 +1,14 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2008 IBM Corporation and others. All rights reserved.
- * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License v1.0 which accompanies this distribution,
- * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *  Copyright (c) 2000, 2009 IBM Corporation and others.
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
  * 
- * Contributors: IBM - Initial API and implementation Ben Pryor - Bug 148288
- ******************************************************************************/
+ *  Contributors:
+ *      IBM Corporation - initial API and implementation
+ *      Ben Pryor - Bug 148288
+ *******************************************************************************/
 package org.eclipse.pde.internal.build;
 
 import java.io.*;
