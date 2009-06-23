4376ff0502c2234484fc67336f4bc71fa4b8fcbc
Curtis Windatt <cwindatt>
2009-06-23 20:42:19 +0000

    Bug 276509: Eclipse samples review wizard has incorrect text margins https://bugs.eclipse.org/bugs/show_bug.cgi?id=276509

--- ui/org.eclipse.pde.ui/src_samples/org/eclipse/pde/internal/ui/samples/ReviewPage.java
+++ ui/org.eclipse.pde.ui/src_samples/org/eclipse/pde/internal/ui/samples/ReviewPage.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- *  Copyright (c) 2000, 2008 IBM Corporation and others.
+ *  Copyright (c) 2000, 2009 IBM Corporation and others.
  *  All rights reserved. This program and the accompanying materials
  *  are made available under the terms of the Eclipse Public License v1.0
  *  which accompanies this distribution, and is available at
@@ -70,6 +70,7 @@ public class ReviewPage extends WizardPage {
 		Composite container = new Composite(parent, SWT.NULL);
 		GridLayout layout = new GridLayout();
 		container.setLayout(layout);
+		container.setLayoutData(new GridData(GridData.FILL_BOTH));
 		formText = new ScrolledFormText(container, true);
 		formText.setBackground(parent.getBackground());
 		GridData gd = new GridData(GridData.FILL_BOTH);
