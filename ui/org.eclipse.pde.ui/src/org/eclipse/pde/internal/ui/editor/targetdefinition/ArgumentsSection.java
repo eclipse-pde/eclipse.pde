c32c60e8cea3d7cacffa427fe7951a7e355f6798
Curtis Windatt <cwindatt>
2009-07-07 15:15:48 +0000

    Bug 282622: [target] Edit Target dialog does not use sentence case https://bugs.eclipse.org/bugs/show_bug.cgi?id=282622

--- ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/editor/targetdefinition/ArgumentsSection.java
+++ ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/editor/targetdefinition/ArgumentsSection.java
@@ -26,6 +26,7 @@ import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.forms.IFormColors;
@@ -130,7 +131,12 @@ public class ArgumentsSection extends SectionPart {
 		});
 
 		Composite buttons = new Composite(vmComp, SWT.NONE);
-		buttons.setLayout(FormLayoutFactory.createSectionClientGridLayout(false, 2));
+		GridLayout layout = FormLayoutFactory.createSectionClientGridLayout(false, 2);
+		layout.marginTop = 0;
+		layout.marginBottom = 0;
+		layout.marginRight = 0;
+		layout.marginLeft = 0;
+		buttons.setLayout(layout);
 		buttons.setLayoutData(new GridData(GridData.HORIZONTAL_ALIGN_END));
 		Button vmArgs = toolkit.createButton(buttons, PDEUIMessages.ArgumentsSection_argumentsButtonTitle, SWT.NONE);
 		vmArgs.setLayoutData(new GridData(GridData.HORIZONTAL_ALIGN_END));
