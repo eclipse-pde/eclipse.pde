1905769c83c974a9e2bc0961b54f741cc09c7558
Benjamin Cabe <bcabe>
2009-07-07 18:43:43 +0000

    RESOLVED - bug 280678: [schema] SchemaTransformer doesn't generate help for "API Information" sections https://bugs.eclipse.org/bugs/show_bug.cgi?id=280678

--- ui/org.eclipse.pde.core/src/org/eclipse/pde/internal/core/builders/SchemaTransformer.java
+++ ui/org.eclipse.pde.core/src/org/eclipse/pde/internal/core/builders/SchemaTransformer.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * Copyright (c) 2000, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -160,7 +160,7 @@ public class SchemaTransformer {
 
 	private DocumentSection findSection(IDocumentSection[] sections, String sectionId) {
 		for (int i = 0; i < sections.length; i++) {
-			if (sections[i].getSectionId().equals(sectionId)) {
+			if (sections[i].getSectionId().equalsIgnoreCase(sectionId)) {
 				return (DocumentSection) sections[i];
 			}
 		}
