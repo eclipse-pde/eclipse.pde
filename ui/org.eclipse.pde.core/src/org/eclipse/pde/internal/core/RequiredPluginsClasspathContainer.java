7cfab319e7172fa81988583d63bdc1630530e309
Darin Wright <darin>
2009-10-02 15:22:29 +0000

    Bug 291207 - classpath container performs needless map insertion

--- ui/org.eclipse.pde.core/src/org/eclipse/pde/internal/core/RequiredPluginsClasspathContainer.java
+++ ui/org.eclipse.pde.core/src/org/eclipse/pde/internal/core/RequiredPluginsClasspathContainer.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- *  Copyright (c) 2000, 2008 IBM Corporation and others.
+ *  Copyright (c) 2000, 2009 IBM Corporation and others.
  *  All rights reserved. This program and the accompanying materials
  *  are made available under the terms of the Eclipse Public License v1.0
  *  which accompanies this distribution, and is available at
@@ -177,12 +177,13 @@ public class RequiredPluginsClasspathContainer extends PDEClasspathContainer imp
 			if (exporter == null)
 				continue;
 			ArrayList list = (ArrayList) visiblePackages.get(exporter);
-			if (list == null)
+			if (list == null) {
 				list = new ArrayList();
+				visiblePackages.put(exporter, list);
+			}
 			Rule rule = getRule(helper, desc, exports[i]);
 			if (!list.contains(rule))
 				list.add(rule);
-			visiblePackages.put(exporter, list);
 		}
 	}
 
