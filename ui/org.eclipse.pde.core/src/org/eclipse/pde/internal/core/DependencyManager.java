34718b7e1660280d4c994f96a9c3e79d851f0f47
Darin Wright <darin>
2009-11-27 21:19:48 +0000

    comments from patch in bug 296258

--- ui/org.eclipse.pde.core/src/org/eclipse/pde/internal/core/DependencyManager.java
+++ ui/org.eclipse.pde.core/src/org/eclipse/pde/internal/core/DependencyManager.java
@@ -16,43 +16,82 @@ import org.eclipse.pde.core.plugin.IPluginExtension;
 import org.eclipse.pde.core.plugin.IPluginModelBase;
 import org.osgi.framework.Constants;
 
+/**
+ * Utility class to return bundle id collections for a variety of dependency
+ * scenarios
+ * 
+ * @noextend This class is not intended to be subclassed by clients.
+ * @noinstantiate This class is not intended to be instantiated by clients.
+ */
 public class DependencyManager {
 
 	/** 
-	 * @return a set of plug-in IDs
+	 * Returns a {@link Set} of bundle ids for the dependents of the given
+	 * {@link IPluginModelBase}. The set includes the id of the given model base
+	 * as well as all computed implicit / optional dependencies. 
 	 * 
+	 * @param model the {@link IPluginModelBase} to compute dependencies for
+	 * @return a set of bundle IDs
 	 */
 	public static Set getSelfAndDependencies(IPluginModelBase model) {
 		return getDependencies(new Object[] {model}, getImplicitDependencies(), TargetPlatformHelper.getState(), false, true);
 	}
 
 	/** 
-	 * @return a set of plug-in IDs
+	 * Returns a {@link Set} of bundle ids for the dependents of the given
+	 * {@link IPluginModelBase}s. The set includes the ids of the given model bases
+	 * as well as all computed implicit / optional dependencies.
 	 * 
+	 * @param models the array of {@link IPluginModelBase}s to compute dependencies for
+	 * @return a set of bundle IDs
 	 */
 	public static Set getSelfandDependencies(IPluginModelBase[] models) {
 		return getDependencies(models, getImplicitDependencies(), TargetPlatformHelper.getState(), false, true);
 	}
 
 	/** 
-	 * @return a set of plug-in IDs
+	 * Returns a {@link Set} of bundle ids for the dependents of the given
+	 * objects from the given {@link State}. 
+	 * The set does not include the ids of the given objects
+	 * and only includes the given set of implicit dependencies.
 	 * 
+	 * @param selected the group of objects to compute dependencies for. Any items
+	 * in this array that are not {@link IPluginModelBase}s are ignored.
+	 * @param implicit the array of additional implicit dependencies to add to the {@link Set}
+	 * @param state the {@link State} to compute the dependencies in
+	 * @return a set of bundle IDs
 	 */
 	public static Set getDependencies(Object[] selected, String[] implicit, State state) {
 		return getDependencies(selected, implicit, state, true, true);
 	}
 
 	/** 
-	 * @return a set of plug-in IDs
+	 * Returns a {@link Set} of bundle ids for the dependents of the given
+	 * objects. The set does not include the ids of the given objects
+	 * but does include the computed set of implicit dependencies.
 	 * 
+	 * @param selected selected the group of objects to compute dependencies for. Any items
+	 * in this array that are not {@link IPluginModelBase}s are ignored.
+	 * @param includeOptional if optional bundle ids should be included
+	 * @return a set of bundle IDs
 	 */
 	public static Set getDependencies(Object[] selected, boolean includeOptional) {
 		return getDependencies(selected, getImplicitDependencies(), TargetPlatformHelper.getState(), true, includeOptional);
 	}
 
 	/** 
-	 * @return a set of plug-in IDs
+	 * Returns a {@link Set} of bundle ids for the dependents of the given
+	 * objects from the given {@link State}. 
+	 * The set additionally only includes the given set of implicit dependencies.
 	 * 
+	 * @param selected selected the group of objects to compute dependencies for. Any items
+	 * in this array that are not {@link IPluginModelBase}s are ignored.
+	 * @param implicit the array of additional implicit dependencies to add to the {@link Set}
+	 * @param state the {@link State} to compute the dependencies in
+	 * @param removeSelf if the id of one of the bundles were are computing dependencies for should be
+	 * included in the result {@link Set} or not
+	 * @param includeOptional if optional bundle ids should be included
+	 * @return a set of bundle IDs
 	 */
 	private static Set getDependencies(Object[] selected, String[] implicit, State state, boolean removeSelf, boolean includeOptional) {
 		Set set = new TreeSet();
@@ -80,8 +119,9 @@ public class DependencyManager {
 
 		if (removeSelf) {
 			for (int i = 0; i < selected.length; i++) {
-				if (!(selected[i] instanceof IPluginModelBase))
+				if (!(selected[i] instanceof IPluginModelBase)) {
 					continue;
+				}
 				IPluginModelBase model = (IPluginModelBase) selected[i];
 				set.remove(model.getPluginBase().getId());
 			}
@@ -89,44 +129,62 @@ public class DependencyManager {
 		return set;
 	}
 
+	/**
+	 * Computes the set of implicit dependencies from the {@link PDEPreferencesManager}
+	 * @return a set if bundle ids 
+	 */
 	private static String[] getImplicitDependencies() {
 		PDEPreferencesManager preferences = PDECore.getDefault().getPreferencesManager();
 		String dependencies = preferences.getString(ICoreConstants.IMPLICIT_DEPENDENCIES);
-		if (dependencies.length() == 0)
+		if (dependencies.length() == 0) {
 			return new String[0];
+		}
 		StringTokenizer tokenizer = new StringTokenizer(dependencies, ","); //$NON-NLS-1$
 		String[] implicitIds = new String[tokenizer.countTokens()];
-		for (int i = 0; i < implicitIds.length; i++)
+		for (int i = 0; i < implicitIds.length; i++) {
 			implicitIds[i] = tokenizer.nextToken();
+		}
 		return implicitIds;
 	}
 
+	/**
+	 * Recursively adds the given {@link BundleDescription} and its dependents to the given 
+	 * {@link Set}
+	 * @param desc the {@link BundleDescription} to compute dependencies for
+	 * @param set the {@link Set} to collect results in
+	 * @param includeOptional if optional dependencies should be included
+	 */
 	private static void addBundleAndDependencies(BundleDescription desc, Set set, boolean includeOptional) {
 		if (desc != null && set.add(desc.getSymbolicName())) {
 			BundleSpecification[] required = desc.getRequiredBundles();
 			for (int i = 0; i < required.length; i++) {
-				if (includeOptional || !required[i].isOptional())
+				if (includeOptional || !required[i].isOptional()) {
 					addBundleAndDependencies((BundleDescription) required[i].getSupplier(), set, includeOptional);
+				}
 			}
 			ImportPackageSpecification[] importedPkgs = desc.getImportPackages();
 			for (int i = 0; i < importedPkgs.length; i++) {
 				ExportPackageDescription exporter = (ExportPackageDescription) importedPkgs[i].getSupplier();
 				// Continue if the Imported Package is unresolved of the package is optional and don't want optional packages
-				if (exporter == null || (!includeOptional && Constants.RESOLUTION_OPTIONAL.equals(importedPkgs[i].getDirective(Constants.RESOLUTION_DIRECTIVE))))
+				if (exporter == null || (!includeOptional && Constants.RESOLUTION_OPTIONAL.equals(importedPkgs[i].getDirective(Constants.RESOLUTION_DIRECTIVE)))) {
 					continue;
+				}
 				addBundleAndDependencies(exporter.getExporter(), set, includeOptional);
 			}
 			BundleDescription[] fragments = desc.getFragments();
 			for (int i = 0; i < fragments.length; i++) {
-				if (!fragments[i].isResolved())
+				if (!fragments[i].isResolved()) {
 					continue;
+				}
 				String id = fragments[i].getSymbolicName();
-				if (!"org.eclipse.ui.workbench.compatibility".equals(id)) //$NON-NLS-1$
+				if (!"org.eclipse.ui.workbench.compatibility".equals(id)) { //$NON-NLS-1$
 					addBundleAndDependencies(fragments[i], set, includeOptional);
+				}
 			}
 			HostSpecification host = desc.getHost();
-			if (host != null)
+			if (host != null) {
 				addBundleAndDependencies((BundleDescription) host.getSupplier(), set, includeOptional);
+			}
 		}
 	}
 
