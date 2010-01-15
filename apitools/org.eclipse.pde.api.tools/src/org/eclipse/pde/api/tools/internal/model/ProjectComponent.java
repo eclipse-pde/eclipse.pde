8be07f1ef392bc0cd94a4fab9859f459190ea04b
Darin Wright <darin>
2010-01-15 22:18:22 +0000

    Bug 299829 - transient test failure

--- apitools/org.eclipse.pde.api.tools/src/org/eclipse/pde/api/tools/internal/model/ProjectComponent.java
+++ apitools/org.eclipse.pde.api.tools/src/org/eclipse/pde/api/tools/internal/model/ProjectComponent.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2009 IBM Corporation and others.
+ * Copyright (c) 2007, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -379,10 +379,8 @@ public class ProjectComponent extends BundleComponent {
 						} else {
 							container = fProject.getProject().getWorkspace().getRoot().getFolder(outputLocation);
 						}
-						if (container.exists()) {
-							cfc = new ProjectTypeContainer(component, container);
-							fOutputLocationToContainer.put(outputLocation, cfc);
-						}
+						cfc = new ProjectTypeContainer(component, container);
+						fOutputLocationToContainer.put(outputLocation, cfc);
 					}
 					return cfc;
 				}
