e8ebe88b1ff5d0df68105298c300b22f7172bb79
Chris Aniszczyk <caniszczyk>
2010-03-03 17:15:37 +0000

    RESOLVED - bug 304157: Avoid deprecated API from jdt.core (re hierarchy scope) https://bugs.eclipse.org/bugs/show_bug.cgi?id=304157

--- ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/util/PDEJavaHelperUI.java
+++ ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/util/PDEJavaHelperUI.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2009 IBM Corporation and others.
+ * Copyright (c) 2005, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -71,7 +71,7 @@ public class PDEJavaHelperUI {
 				IJavaProject javaProject = JavaCore.create(project);
 				IType superType = javaProject.findType(superTypeName);
 				if (superType != null)
-					searchScope = SearchEngine.createHierarchyScope(javaProject, superType, true, true, null);
+					searchScope = SearchEngine.createStrictHierarchyScope(javaProject, superType, true, false, null);
 			}
 			if (searchScope == null)
 				searchScope = PDEJavaHelper.getSearchScope(project);
