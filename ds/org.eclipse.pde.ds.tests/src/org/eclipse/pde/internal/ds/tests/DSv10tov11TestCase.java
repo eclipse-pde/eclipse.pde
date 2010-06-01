cb9be6032c14d24a6dba647eb7188bbad28c1758
Curtis Windatt <cwindatt>
2010-06-01 14:13:47 +0000

    copyright updates

--- ds/org.eclipse.pde.ds.tests/src/org/eclipse/pde/internal/ds/tests/DSv10tov11TestCase.java
+++ ds/org.eclipse.pde.ds.tests/src/org/eclipse/pde/internal/ds/tests/DSv10tov11TestCase.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009 EclipseSource IBM Corporation and others.
+ * Copyright (c) 2009 EclipseSource and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *     EclipseSource Corporation - initial API and implementation
+ *     IBM Corporation - continued improvements
  *******************************************************************************/
 package org.eclipse.pde.internal.ds.tests;
 
