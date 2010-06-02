f7d60cf957f0aa5bf4978de7131c192b512479f8
Andrew Niefer <aniefer>
2010-06-02 20:26:26 +0000

    fix copyrights

--- org.eclipse.pde.build/src/org/eclipse/pde/internal/build/AssembleConfigScriptGenerator.java
+++ org.eclipse.pde.build/src/org/eclipse/pde/internal/build/AssembleConfigScriptGenerator.java
@@ -1,13 +1,15 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2009 IBM Corporation and others. All rights reserved.
- * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License v1.0 which accompanies this distribution,
- * and is available at http://www.eclipse.org/legal/epl-v10.html
- * 
- * Contributors: IBM - Initial API and implementation G&H Softwareentwicklung
+ * Copyright (c) 2000, 2010 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * IBM - Initial API and implementation G&H Softwareentwicklung
  * GmbH - internationalization implementation (bug 150933) Prosyst - create
  * proper OSGi bundles (bug 174157)
- ******************************************************************************/
+ *******************************************************************************/
 package org.eclipse.pde.internal.build;
 
 import java.io.File;
--- org.eclipse.pde.build/src/org/eclipse/pde/internal/build/builder/BuildDirector.java
+++ org.eclipse.pde.build/src/org/eclipse/pde/internal/build/builder/BuildDirector.java
@@ -1,12 +1,13 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2009 IBM Corporation and others. All rights reserved.
- * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License v1.0 which accompanies this distribution,
- * and is available at http://www.eclipse.org/legal/epl-v10.html
- * 
- * Contributors: IBM - Initial API and implementation G&H Softwareentwicklung
+ * Copyright (c) 2000, 2010 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
  * GmbH - internationalization implementation (bug 150933)
- ******************************************************************************/
+ *******************************************************************************/
 package org.eclipse.pde.internal.build.builder;
 
 import java.util.*;
