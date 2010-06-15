2bb2f2329b64e4ee47b7837a55d625e0bbb5b533
Michael Rennie <mrennie>
2010-06-15 15:12:48 +0000

    Bug 316645 - API Use Report provides no Common tab

--- apitools/org.eclipse.pde.api.tools.ui/src/org/eclipse/pde/api/tools/ui/internal/use/ApiUseTabGroup.java
+++ apitools/org.eclipse.pde.api.tools.ui/src/org/eclipse/pde/api/tools/ui/internal/use/ApiUseTabGroup.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009 IBM Corporation and others.
+ * Copyright (c) 2009, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -11,11 +11,14 @@
 package org.eclipse.pde.api.tools.ui.internal.use;
 
 import org.eclipse.debug.ui.AbstractLaunchConfigurationTabGroup;
+import org.eclipse.debug.ui.CommonTab;
 import org.eclipse.debug.ui.ILaunchConfigurationDialog;
 import org.eclipse.debug.ui.ILaunchConfigurationTab;
 
 /**
+ * The tab group for API use scanning
  * 
+ * @since 1.0
  */
 public class ApiUseTabGroup extends AbstractLaunchConfigurationTabGroup {
 
@@ -25,7 +28,9 @@ public class ApiUseTabGroup extends AbstractLaunchConfigurationTabGroup {
 	public void createTabs(ILaunchConfigurationDialog dialog, String mode) {
 		setTabs(new ILaunchConfigurationTab[]{
 				new ApiUseScanTab(),
-				new ApiUsePatternTab()});
+				new ApiUsePatternTab(),
+				new CommonTab()
+				});
 	}
 
 }
