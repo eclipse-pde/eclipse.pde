ff27ab5f1d06aff509792c5c843ef113dc9d4056
Curtis Windatt <cwindatt>
2011-01-04 18:40:53 +0000

    Bug 331332: [target] Target editor should update its content, if the target file was edited with another eclipse editor https://bugs.eclipse.org/bugs/show_bug.cgi?id=331332

--- ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/PDEUIMessages.java
+++ ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/PDEUIMessages.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2010 IBM Corporation and others.
+ * Copyright (c) 2005, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -2793,6 +2793,12 @@ public class PDEUIMessages extends NLS {
 	public static String TargetEditor_1;
 
 	public static String TargetEditor_2;
+
+	public static String TargetEditor_3;
+
+	public static String TargetEditor_4;
+
+	public static String TargetEditor_5;
 
 	public static String FeatureImportWizardPage_reloadLocation;
 
--- ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/editor/targetdefinition/TargetEditor.java
+++ ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/editor/targetdefinition/TargetEditor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2009 IBM Corporation and others.
+ * Copyright (c) 2005, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,6 +10,7 @@
  *******************************************************************************/
 package org.eclipse.pde.internal.ui.editor.targetdefinition;
 
+import java.io.File;
 import java.util.*;
 import java.util.List;
 import org.eclipse.core.resources.*;
@@ -18,6 +19,7 @@ import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.core.runtime.jobs.JobChangeAdapter;
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.action.ControlContribution;
+import org.eclipse.jface.dialogs.ErrorDialog;
 import org.eclipse.jface.dialogs.IMessageProvider;
 import org.eclipse.jface.wizard.WizardDialog;
 import org.eclipse.pde.internal.core.PDECore;
@@ -49,9 +51,8 @@ import org.eclipse.ui.progress.UIJob;
  */
 public class TargetEditor extends FormEditor {
 
-	private ITargetDefinition fTarget;
 	private List fManagedFormPages = new ArrayList(2);
-	private FileInputListener fInputListener;
+	private InputHandler fInputHandler = new InputHandler();
 	private TargetChangedListener fTargetChangedListener;
 	private boolean fDirty;
 
@@ -68,7 +69,6 @@ public class TargetEditor extends FormEditor {
 	protected void addPages() {
 		try {
 			setActiveEditor(this);
-			setPartName(getEditorInput().getName());
 			addPage(new DefinitionPage(this));
 			addPage(new ContentPage(this));
 			addPage(new EnvironmentPage(this));
@@ -82,32 +82,28 @@ public class TargetEditor extends FormEditor {
 	 */
 	public void doSave(IProgressMonitor monitor) {
 		commitPages(true);
-		fDirty = false;
-		ITargetDefinition target = getTarget();
-		if (target != null) {
-			ITargetPlatformService service = (ITargetPlatformService) PDECore.getDefault().acquireService(ITargetPlatformService.class.getName());
-			if (service != null) {
-				try {
-					service.saveTargetDefinition(target);
-				} catch (CoreException e) {
-					PDEPlugin.log(e);
-				}
-			}
+		try {
+			fInputHandler.saveTargetDefinition();
+		} catch (CoreException e) {
+			PDEPlugin.log(e);
+			showError(PDEUIMessages.TargetEditor_3, e);
 		}
-		editorDirtyStateChanged();
 	}
 
 	/* (non-Javadoc)
 	 * @see org.eclipse.ui.part.EditorPart#doSaveAs()
 	 */
 	public void doSaveAs() {
+		commitPages(true);
 		ITargetDefinition target = getTarget();
-		WorkspaceFileTargetHandle currentTargetHandle = (WorkspaceFileTargetHandle) target.getHandle();
 
 		SaveAsDialog dialog = new SaveAsDialog(getSite().getShell());
 		dialog.create();
 		dialog.setMessage(PDEUIMessages.TargetEditor_0, IMessageProvider.NONE);
-		dialog.setOriginalFile(currentTargetHandle.getTargetFile());
+		if (target.getHandle() instanceof WorkspaceFileTargetHandle) {
+			WorkspaceFileTargetHandle currentTargetHandle = (WorkspaceFileTargetHandle) target.getHandle();
+			dialog.setOriginalFile(currentTargetHandle.getTargetFile());
+		}
 		dialog.open();
 
 		IPath path = dialog.getResult();
@@ -116,29 +112,24 @@ public class TargetEditor extends FormEditor {
 			return;
 		}
 		if (!"target".equalsIgnoreCase(path.getFileExtension())) { //$NON-NLS-1$
-			path.addFileExtension("target"); //$NON-NLS-1$
+			path = path.addFileExtension("target"); //$NON-NLS-1$
 		}
 
 		IWorkspace workspace = ResourcesPlugin.getWorkspace();
 		IFile file = workspace.getRoot().getFile(path);
 
 		if (target != null) {
-			try {
-				WorkspaceFileTargetHandle newFleTarget = new WorkspaceFileTargetHandle(file);
-				newFleTarget.save(target);
-			} catch (CoreException e) {
-				PDEPlugin.log(e);
+			if (workspace.validateEdit(new IFile[] {file}, getSite().getShell()).isOK()) {
+				try {
+					WorkspaceFileTargetHandle newFileTarget = new WorkspaceFileTargetHandle(file);
+					newFileTarget.save(target);
+					setInput(new FileEditorInput(file));
+				} catch (CoreException e) {
+					PDEPlugin.log(e);
+					showError(PDEUIMessages.TargetEditor_3, e);
+				}
 			}
 		}
-
-		PDEPlugin.getWorkspace().removeResourceChangeListener(fInputListener);
-		fInputListener = new FileInputListener(file);
-		PDEPlugin.getWorkspace().addResourceChangeListener(fInputListener);
-		setInput(new FileEditorInput(file));
-		setPartName(getEditorInput().getName());
-		commitPages(true);
-		fDirty = false;
-		editorDirtyStateChanged();
 	}
 
 	/* (non-Javadoc)
@@ -150,6 +141,7 @@ public class TargetEditor extends FormEditor {
 
 	protected void setDirty(boolean dirty) {
 		fDirty = fDirty || dirty;
+		editorDirtyStateChanged();
 	}
 
 	/* (non-Javadoc)
@@ -160,35 +152,12 @@ public class TargetEditor extends FormEditor {
 	}
 
 	/* (non-Javadoc)
-	 * @see org.eclipse.ui.forms.editor.FormEditor#init(org.eclipse.ui.IEditorSite, org.eclipse.ui.IEditorInput)
+	 * @see org.eclipse.ui.part.EditorPart#setInput(org.eclipse.ui.IEditorInput)
 	 */
-	public void init(IEditorSite site, IEditorInput input) throws PartInitException {
-		super.init(site, input);
-		IFile file = null;
-		boolean validFile = true;
-		if (input instanceof IFileEditorInput) {
-			file = ((IFileEditorInput) input).getFile();
-			validFile = file.getLocation().toFile().exists();
-		} else if (input instanceof IURIEditorInput) {
-			String part = ((IURIEditorInput) input).getURI().getSchemeSpecificPart();
-			Path path = new Path(part);
-			file = ResourcesPlugin.getWorkspace().getRoot().getFile(path);
-			validFile = path.toFile().exists();
-		}
-
-		//If the file is not available or invalid, close the target editor
-		if (file == null || !validFile) {
-			Display display = getSite().getShell().getDisplay();
-			display.asyncExec(new Runnable() {
-				public void run() {
-					getSite().getPage().closeEditor(TargetEditor.this, false);
-				}
-			});
-		}
-
-		fInputListener = new FileInputListener(file);
-		PDEPlugin.getWorkspace().addResourceChangeListener(fInputListener);
-		getTargetChangedListener().contentsChanged(getTarget(), null, true, false);
+	protected void setInput(IEditorInput input) {
+		super.setInput(input);
+		setPartName(getEditorInput().getName());
+		fInputHandler.setInput(input);
 	}
 
 	/* (non-Javadoc)
@@ -200,10 +169,7 @@ public class TargetEditor extends FormEditor {
 		getTargetChangedListener().setContentTree(null);
 		getTargetChangedListener().setLocationTree(null);
 
-		if (fInputListener != null) {
-			PDEPlugin.getWorkspace().removeResourceChangeListener(fInputListener);
-			fInputListener = null;
-		}
+		fInputHandler.dispose();
 		super.dispose();
 	}
 
@@ -212,30 +178,7 @@ public class TargetEditor extends FormEditor {
 	 * @return target model
 	 */
 	public ITargetDefinition getTarget() {
-		if (fTarget == null) {
-			ITargetPlatformService service = (ITargetPlatformService) PDECore.getDefault().acquireService(ITargetPlatformService.class.getName());
-			if (service != null) {
-				IEditorInput input = getEditorInput();
-				if (input instanceof IFileEditorInput) {
-					ITargetHandle fileHandle = service.getTarget(((IFileEditorInput) input).getFile());
-					try {
-						fTarget = fileHandle.getTargetDefinition();
-					} catch (CoreException e) {
-						PDEPlugin.log(e);
-					}
-				} else if (input instanceof IURIEditorInput) {
-					try {
-						ITargetHandle externalTarget = service.getTarget(((IURIEditorInput) input).getURI());
-						fTarget = externalTarget.getTargetDefinition();
-					} catch (CoreException e) {
-					}
-				}
-				if (fTarget == null) {
-					fTarget = service.newTarget();
-				}
-			}
-		}
-		return fTarget;
+		return fInputHandler.getTarget();
 	}
 
 	/**
@@ -252,7 +195,7 @@ public class TargetEditor extends FormEditor {
 	 * Handles the revert action
 	 */
 	public void doRevert() {
-		fTarget = null;
+		fInputHandler.reset();
 		for (Iterator iterator = fManagedFormPages.iterator(); iterator.hasNext();) {
 			IFormPart[] parts = ((IManagedForm) iterator.next()).getParts();
 			for (int i = 0; i < parts.length; i++) {
@@ -310,7 +253,7 @@ public class TargetEditor extends FormEditor {
 				dialog.open();
 			}
 		};
-		export.setToolTipText("Export");
+		export.setToolTipText("Export"); //$NON-NLS-1$
 		export.setImageDescriptor(PDEPluginImages.DESC_EXPORT_TARGET_TOOL);
 
 		form.getToolBarManager().add(setAsTarget);
@@ -330,43 +273,134 @@ public class TargetEditor extends FormEditor {
 	}
 
 	/**
-	 * Resource change listener for the file input to this editor in case it is deleted. 
+	 * Opens an error dialog using the editor's shell.
+	 * @param message error message to display
+	 * @param exception exception to display in details section
 	 */
-	class FileInputListener implements IResourceChangeListener, IResourceDeltaVisitor {
-		IFile fInput;
+	public void showError(final String message, final CoreException exception) {
+		Display display = getSite().getShell().getDisplay();
+		display.asyncExec(new Runnable() {
+			public void run() {
+				ErrorDialog.openError(getSite().getShell(), PDEUIMessages.TargetEditor_4, message, exception.getStatus());
+			}
+		});
+	}
 
-		public FileInputListener(IFile input) {
-			fInput = input;
+	/**
+	 * The InputHandler serves as a bridge between the input file and the TargetDefinition model.
+	 */
+	private class InputHandler implements IResourceChangeListener {
+		private IEditorInput fInput;
+		private ITargetDefinition fTarget;
+		private IFile fTargetFileInWorkspace;
+		private boolean fSaving = false;
+
+		public void dispose() {
+			PDEPlugin.getWorkspace().removeResourceChangeListener(this);
 		}
 
+		public void reset() {
+			setInput(fInput);
+		}
+
+		public void setInput(IEditorInput input) {
+			fInput = input;
+			fTargetFileInWorkspace = null;
+			fTarget = null;
+			File targetFile = null;
+			if (input instanceof IFileEditorInput) {
+				fTargetFileInWorkspace = ((IFileEditorInput) input).getFile();
+				targetFile = fTargetFileInWorkspace.getLocation().toFile();
+			} else if (input instanceof IURIEditorInput) {
+				String part = ((IURIEditorInput) input).getURI().getSchemeSpecificPart();
+				Path path = new Path(part);
+				fTargetFileInWorkspace = ResourcesPlugin.getWorkspace().getRoot().getFile(path);
+				targetFile = path.toFile();
+			}
+
+			//If the file is not available or invalid, close the target editor
+			if (fTargetFileInWorkspace == null || targetFile == null || !targetFile.exists()) {
+				TargetEditor.this.close(false);
+			}
+			PDEPlugin.getWorkspace().addResourceChangeListener(this);
+		}
+
+		/**
+		 * @return the target definition that is the input to the editor
+		 */
+		public ITargetDefinition getTarget() {
+			if (fTarget == null) {
+				try {
+					loadTargetDefinition();
+					TargetEditor.this.fDirty = false;
+					TargetEditor.this.editorDirtyStateChanged();
+				} catch (CoreException e) {
+					TargetEditor.this.showError(PDEUIMessages.TargetEditor_5, e);
+					TargetEditor.this.close(false);
+				}
+			}
+			return fTarget;
+		}
+
+		private ITargetDefinition loadTargetDefinition() throws CoreException {
+			ITargetPlatformService service = getTargetPlatformService();
+			try {
+				if (fInput instanceof IFileEditorInput) {
+					ITargetHandle fileHandle = service.getTarget(((IFileEditorInput) fInput).getFile());
+					fTarget = fileHandle.getTargetDefinition();
+				} else if (fInput instanceof IURIEditorInput) {
+					ITargetHandle externalTarget = service.getTarget(((IURIEditorInput) fInput).getURI());
+					fTarget = externalTarget.getTargetDefinition();
+				}
+			} catch (CoreException e) {
+				fTarget = service.newTarget();
+				throw e;
+			}
+			TargetEditor.this.getTargetChangedListener().contentsChanged(fTarget, this, true, false);
+			return fTarget;
+		}
+
+		public void saveTargetDefinition() throws CoreException {
+			ITargetPlatformService service = getTargetPlatformService();
+			fSaving = true;
+			try {
+				service.saveTargetDefinition(fTarget);
+				TargetEditor.this.fDirty = false;
+				TargetEditor.this.editorDirtyStateChanged();
+			} finally {
+				fSaving = false;
+			}
+		}
+
+		private ITargetPlatformService getTargetPlatformService() throws CoreException {
+			ITargetPlatformService service = (ITargetPlatformService) PDECore.getDefault().acquireService(ITargetPlatformService.class.getName());
+			if (service == null) {
+				throw new CoreException(new Status(IStatus.ERROR, PDECore.PLUGIN_ID, "ITargetPlatformService not available")); //$NON-NLS-1$
+			}
+			return service;
+		}
+
+		/* (non-Javadoc)
+		 * @see org.eclipse.core.resources.IResourceChangeListener#resourceChanged(org.eclipse.core.resources.IResourceChangeEvent)
+		 */
 		public void resourceChanged(IResourceChangeEvent event) {
 			if (event.getType() == IResourceChangeEvent.POST_CHANGE) {
-				IResourceDelta delta = event.getDelta();
-				try {
-					delta.accept(this);
-				} catch (CoreException e) {
-					PDEPlugin.logException(e);
-				}
-			}
-		}
-
-		public boolean visit(IResourceDelta delta) throws CoreException {
-			IResource resource = delta.getResource();
-			if (resource instanceof IFile) {
-				IFile file = (IFile) resource;
-				if (file.equals(fInput)) {
-					if (delta.getKind() == IResourceDelta.REMOVED || delta.getKind() == IResourceDelta.REPLACED) {
-						Display display = getSite().getShell().getDisplay();
-						display.asyncExec(new Runnable() {
-							public void run() {
-								getSite().getPage().closeEditor(TargetEditor.this, false);
-							}
-						});
+				IResourceDelta delta = event.getDelta().findMember(fTargetFileInWorkspace.getFullPath());
+				if (delta != null) {
+					if (delta.getKind() == IResourceDelta.REMOVED) {
+						TargetEditor.this.close(false);
+					} else if (delta.getKind() == IResourceDelta.CHANGED || delta.getKind() == IResourceDelta.REPLACED) {
+						if (!fSaving) {
+							Display display = getSite().getShell().getDisplay();
+							display.asyncExec(new Runnable() {
+								public void run() {
+									TargetEditor.this.doRevert();
+								}
+							});
+						}
 					}
-					return false;
 				}
 			}
-			return true;
 		}
 	}
 
