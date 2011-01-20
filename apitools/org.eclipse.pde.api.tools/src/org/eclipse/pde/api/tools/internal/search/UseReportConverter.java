81ae2859e9558fe1db20e2bd375247521d7b23f0
Michael Rennie <mrennie>
2011-01-20 19:23:42 +0000

    Bug 334843 - UseReportConverter prints out reference plus value(s) for "API patterns"

--- apitools/org.eclipse.pde.api.tools/src/org/eclipse/pde/api/tools/internal/search/UseReportConverter.java
+++ apitools/org.eclipse.pde.api.tools/src/org/eclipse/pde/api/tools/internal/search/UseReportConverter.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009, 2010 IBM Corporation and others.
+ * Copyright (c) 2009, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -1626,7 +1626,6 @@ public class UseReportConverter extends HTMLConvertor {
 			buffer.append(openTD(36)); 
 			String[] patterns = this.metadata.getApiPatterns();
 			if(patterns != null) {
-				buffer.append(this.metadata.getApiPatterns());
 				for (int i = 0; i < patterns.length; i++) {
 					buffer.append(patterns[i]).append(BR);
 				}
