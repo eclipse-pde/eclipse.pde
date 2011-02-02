9800e8fb64aa0b8c649b79455e0cec32c92effc4
Curtis Windatt <cwindatt>
2011-02-02 16:44:25 +0000

    Bug 319790: build.properties warning does not disappear after fixing BREE/buildpath https://bugs.eclipse.org/bugs/show_bug.cgi?id=319790

--- ui/org.eclipse.pde.core/src/org/eclipse/pde/internal/core/builders/ManifestConsistencyChecker.java
+++ ui/org.eclipse.pde.core/src/org/eclipse/pde/internal/core/builders/ManifestConsistencyChecker.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2010 IBM Corporation and others.
+ * Copyright (c) 2000, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -199,9 +199,9 @@ public class ManifestConsistencyChecker extends IncrementalProjectBuilder {
 			fClassFileVisitor.reset();
 			delta.accept(fClassFileVisitor);
 			if (fClassFileVisitor.hasChanged()) {
-				type |= MANIFEST | EXTENSIONS;
+				type |= MANIFEST | EXTENSIONS | BUILD;
 				if (DEBUG) {
-					System.out.println("Class files changed due to dependency changes: Project [" + getProject().getName() + "] - rebuild manifest files"); //$NON-NLS-1$ //$NON-NLS-2$
+					System.out.println("Class files changed due to dependency changes: Project [" + getProject().getName() + "] - rebuild manifest and properties files"); //$NON-NLS-1$ //$NON-NLS-2$
 				}
 			}
 		}
