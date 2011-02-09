3d32e26f456cae20e8875346d317f796d5e836f3
Andrew Niefer <aniefer>
2011-02-09 20:12:43 +0000

    split the test suite into two to avoid permgen errors

---
+++ org.eclipse.pde.build.tests/src/org/eclipse/pde/build/tests/P2TestSuite.java
@@ -0,0 +1,26 @@
+/*******************************************************************************
+ * Copyright (c) 2011 IBM Corporation and others. All rights reserved. This
+ * program and the accompanying materials are made available under the terms of
+ * the Eclipse Public License v1.0 which accompanies this distribution, and is
+ * available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: IBM Corporation - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.pde.build.tests;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import org.eclipse.pde.build.internal.tests.p2.P2Tests;
+import org.eclipse.pde.build.internal.tests.p2.PublishingTests;
+
+public class P2TestSuite {
+
+	public static Test suite() {
+		TestSuite suite = new TestSuite("Test Suite for p2 bits of org.eclipse.pde.build"); //$NON-NLS-1$
+		suite.addTestSuite(P2Tests.class);
+		suite.addTestSuite(PublishingTests.class);
+		return suite;
+	}
+
+}
--- org.eclipse.pde.build.tests/src/org/eclipse/pde/build/tests/PDEBuildTestSuite.java
+++ org.eclipse.pde.build.tests/src/org/eclipse/pde/build/tests/PDEBuildTestSuite.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2010 IBM Corporation and others. All rights reserved.
+ * Copyright (c) 2007, 2011 IBM Corporation and others. All rights reserved.
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
@@ -12,7 +12,7 @@ package org.eclipse.pde.build.tests;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 import org.eclipse.pde.build.internal.tests.*;
-import org.eclipse.pde.build.internal.tests.p2.*;
+import org.eclipse.pde.build.internal.tests.p2.LicenseTests;
 
 public class PDEBuildTestSuite {
 
@@ -21,11 +21,10 @@ public class PDEBuildTestSuite {
 		suite.addTestSuite(SourceTests.class);
 		suite.addTestSuite(ScriptGenerationTests.class);
 		suite.addTestSuite(ProductTests.class);
-		suite.addTestSuite(P2Tests.class);
-		suite.addTestSuite(PublishingTests.class);
 		suite.addTestSuite(LicenseTests.class);
 		suite.addTest(AssembleTests.suite());
-
+		if (Boolean.valueOf(System.getProperty("pde.build.includeP2", "true")).booleanValue())
+			suite.addTest(P2TestSuite.suite());
 		if (System.getProperty("pde.build.includeFetch") != null)
 			suite.addTestSuite(FetchTests.class);
 		return suite;
