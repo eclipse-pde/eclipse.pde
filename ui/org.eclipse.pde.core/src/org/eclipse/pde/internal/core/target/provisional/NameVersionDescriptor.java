5b02b2e28e5992ef7bd4ec78b5c6df88f3d1b9b6
Curtis Windatt <cwindatt>
2011-02-22 22:36:29 +0000

    Bug 337909: Only one package version visible in manifest editor https://bugs.eclipse.org/bugs/show_bug.cgi?id=337909

--- ui/org.eclipse.pde.core/src/org/eclipse/pde/internal/core/target/provisional/NameVersionDescriptor.java
+++ ui/org.eclipse.pde.core/src/org/eclipse/pde/internal/core/target/provisional/NameVersionDescriptor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009, 2010 IBM Corporation and others.
+ * Copyright (c) 2009, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -19,6 +19,7 @@ public class NameVersionDescriptor {
 
 	public static final String TYPE_PLUGIN = "plugin"; //$NON-NLS-1$
 	public static final String TYPE_FEATURE = "feature"; //$NON-NLS-1$
+	public static final String TYPE_PACKAGE = "package"; //$NON-NLS-1$
 
 	private String fId;
 	private String fVersion;
