ddec09cd3a3c78969fc0123f3d6e184ba7696bc1
Michael Rennie <mrennie>
2011-03-16 18:18:50 +0000

    [nobug] Archives file filter should be for both zip and jar, not two different file filters

--- apitools/org.eclipse.pde.api.tools.ui/src/org/eclipse/pde/api/tools/ui/internal/preferences/PreferenceMessages.java
+++ apitools/org.eclipse.pde.api.tools.ui/src/org/eclipse/pde/api/tools/ui/internal/preferences/PreferenceMessages.java
@@ -191,7 +191,6 @@ public class PreferenceMessages extends NLS {
 	public static String ApiUseScanPreferencePage_8;
 	public static String ApiUseScanPreferencePage_9;
 	public static String archives__zip;
-	public static String jars__jar;
 
 	static {
 		// initialize resource bundle
