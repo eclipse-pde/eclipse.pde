98a4739c0e943130c828f6147c05cac7f6c82a02
oliviert <>
2011-05-23 20:11:21 +0000

    HEAD - Fixed bug 346686: IllegalArgumentException inside exporting session from API compare view

--- apitools/org.eclipse.pde.api.tools.ui/src/org/eclipse/pde/api/tools/ui/internal/actions/ExportDialog.java
+++ apitools/org.eclipse.pde.api.tools.ui/src/org/eclipse/pde/api/tools/ui/internal/actions/ExportDialog.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009 IBM Corporation and others.
+ * Copyright (c) 2009, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -235,7 +235,9 @@ public class ExportDialog extends Dialog {
 					}
 				}
 				String newPath = dialog.open();
-				ExportDialog.this.getText().setText(newPath);
+				if (newPath != null) {
+					ExportDialog.this.getText().setText(newPath);
+				}
 			}
 		});
 		errorMessageText = new Text(comp, SWT.READ_ONLY | SWT.WRAP);
