497e9d66f660c7096515d84055b0981201bff835
Curtis Windatt <cwindatt@ca.ibm.com>
2012-01-20 17:30:17 -0600

    Bug 368753 - [target] Regression: Target resolution is fail fast and can not read non-osgi plug-ins

--- ui/org.eclipse.pde.core/src/org/eclipse/pde/internal/core/util/ManifestUtils.java
+++ ui/org.eclipse.pde.core/src/org/eclipse/pde/internal/core/util/ManifestUtils.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2011 IBM Corporation and others.
+ * Copyright (c) 2006, 2012 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -103,7 +103,8 @@ public class ManifestUtils {
 
 	/**
 	 * Utility method to parse a bundle's manifest into a dictionary. The bundle may be in 
-	 * a directory or an archive at the specified location.
+	 * a directory or an archive at the specified location.  If the manifest does not contain
+	 * the necessary entries, the plugin.xml and fragment.xml will be checked for a non-osgi plug-in.
 	 * 
 	 * TODO This method may be removed in favour of one that caches manifest contents
 	 * 
@@ -112,78 +113,102 @@ public class ManifestUtils {
 	 * @throws CoreException if manifest has invalid syntax or is missing
 	 */
 	public static Map loadManifest(File bundleLocation) throws CoreException {
-		ZipFile jarFile = null;
-		InputStream manifestStream = null;
+		// Check if the file is a archive or a directory
 		try {
 			if (bundleLocation.isFile()) {
-				jarFile = new ZipFile(bundleLocation, ZipFile.OPEN_READ);
-				ZipEntry manifestEntry = jarFile.getEntry(JarFile.MANIFEST_NAME);
-				if (manifestEntry != null) {
-					manifestStream = jarFile.getInputStream(manifestEntry);
+				ZipFile jarFile = null;
+				InputStream stream = null;
+				try {
+					jarFile = new ZipFile(bundleLocation, ZipFile.OPEN_READ);
+					// Check the manifest.MF
+					ZipEntry manifestEntry = jarFile.getEntry(JarFile.MANIFEST_NAME);
+					if (manifestEntry != null) {
+						stream = jarFile.getInputStream(manifestEntry);
+						if (stream != null) {
+							Map map = ManifestElement.parseBundleManifest(stream, new Hashtable(10));
+							// Symbolic name is the only required manifest entry, this is an ok bundle
+							if (map != null && map.containsKey(Constants.BUNDLE_SYMBOLICNAME)) {
+								return map;
+							}
+						}
+						// Manifest.MF wasn't good, check for plugin.xml or fragment.xml
+						ZipEntry pluginEntry = jarFile.getEntry(ICoreConstants.PLUGIN_FILENAME_DESCRIPTOR);
+						if (pluginEntry == null) {
+							pluginEntry = jarFile.getEntry(ICoreConstants.FRAGMENT_FILENAME_DESCRIPTOR);
+						}
+						if (pluginEntry != null) {
+							Map map = loadPluginXML(bundleLocation);
+							if (map != null && map.containsKey(Constants.BUNDLE_SYMBOLICNAME)) {
+								return map;
+							}
+						}
+					}
+				} finally {
+					closeZipFileAndStream(stream, jarFile);
 				}
 			} else {
+				// Check the manifest.MF
 				File file = new File(bundleLocation, JarFile.MANIFEST_NAME);
 				if (file.exists()) {
-					manifestStream = new FileInputStream(file);
-				} else {
+					InputStream stream = null;
+					try {
+						stream = new FileInputStream(file);
+						Map map = ManifestElement.parseBundleManifest(stream, new Hashtable(10));
+						if (map != null && map.containsKey(Constants.BUNDLE_SYMBOLICNAME)) {
+							return map;
+						}
+					} finally {
+						if (stream != null) {
+							stream.close();
+						}
+					}
+				}
+				// Manifest.MF wasn't good, check for plugin.xml or fragment.xml
+				File pxml = new File(bundleLocation, ICoreConstants.PLUGIN_FILENAME_DESCRIPTOR);
+				File fxml = new File(bundleLocation, ICoreConstants.FRAGMENT_FILENAME_DESCRIPTOR);
+				if (pxml.exists() || fxml.exists()) {
 					Map map = loadPluginXML(bundleLocation);
-					if (map != null) {
-						return map; // else fall through to invalid manifest
+					if (map != null && map.containsKey(Constants.BUNDLE_SYMBOLICNAME)) {
+						return map;
 					}
 				}
 			}
-			if (manifestStream == null) {
-				throw new CoreException(new Status(IStatus.ERROR, PDECore.PLUGIN_ID, 0, NLS.bind(Messages.DirectoryBundleContainer_3, bundleLocation.getAbsolutePath()), null));
-			}
-			Map map = ManifestElement.parseBundleManifest(manifestStream, new Hashtable(10));
-			// Validate manifest - BSN must be present.
-			// Else look for plugin.xml in case it's an old style plug-in
-			String bsn = (String) map.get(Constants.BUNDLE_SYMBOLICNAME);
-			if (bsn == null && bundleLocation.isDirectory()) {
-				map = loadPluginXML(bundleLocation); // not a bundle manifest, try plugin.xml
-			}
-			if (map == null || map.get(Constants.BUNDLE_SYMBOLICNAME) == null) {
-				throw new CoreException(new Status(IStatus.ERROR, PDECore.PLUGIN_ID, 0, NLS.bind(Messages.DirectoryBundleContainer_3, bundleLocation.getAbsolutePath()), null));
-			}
-			return map;
+
+			// The necessary bundle information has not been found in manifest.mf or plugin.xml
+			throw new CoreException(new Status(IStatus.ERROR, PDECore.PLUGIN_ID, 0, NLS.bind(Messages.DirectoryBundleContainer_3, bundleLocation.getAbsolutePath()), null));
+
 		} catch (BundleException e) {
 			throw new CoreException(new Status(IStatus.ERROR, PDECore.PLUGIN_ID, 0, NLS.bind(Messages.DirectoryBundleContainer_3, bundleLocation.getAbsolutePath()), e));
 		} catch (IOException e) {
 			throw new CoreException(new Status(IStatus.ERROR, PDECore.PLUGIN_ID, 0, NLS.bind(Messages.DirectoryBundleContainer_3, bundleLocation.getAbsolutePath()), e));
-		} finally {
-			closeZipFileAndStream(manifestStream, jarFile);
 		}
 	}
 
 	/**
 	 * Parses an old style plug-in's (or fragment's) XML definition file into a dictionary.
-	 * The plug-in must be in a directory at the specified location.
+	 * The provided file may be an zip archive or directory.  The existence of a plugin.xml
+	 * or fragment.xml is not checked in this method.
 	 * 
-	 * @param pluginDir root location of the plug-in
+	 * @param pluginLocation location of the bundle (archive file or directory)
 	 * @return bundle manifest dictionary or <code>null</code> if none
 	 * @throws CoreException if manifest has invalid syntax
 	 */
-	public static Map loadPluginXML(File pluginDir) throws CoreException {
-		File pxml = new File(pluginDir, ICoreConstants.PLUGIN_FILENAME_DESCRIPTOR);
-		File fxml = new File(pluginDir, ICoreConstants.FRAGMENT_FILENAME_DESCRIPTOR);
-		if (pxml.exists() || fxml.exists()) {
-			// support classic non-OSGi plug-in
-			PluginConverter converter = (PluginConverter) PDECore.getDefault().acquireService(PluginConverter.class.getName());
-			if (converter != null) {
-				try {
-					Dictionary convert = converter.convertManifest(pluginDir, false, null, false, null);
-					if (convert != null) {
-						Map map = new HashMap(convert.size(), 1.0f);
-						Enumeration keys = convert.keys();
-						while (keys.hasMoreElements()) {
-							Object key = keys.nextElement();
-							map.put(key, convert.get(key));
-						}
-						return map;
+	private static Map loadPluginXML(File pluginLocation) throws CoreException {
+		PluginConverter converter = (PluginConverter) PDECore.getDefault().acquireService(PluginConverter.class.getName());
+		if (converter != null) {
+			try {
+				Dictionary convert = converter.convertManifest(pluginLocation, false, null, false, null);
+				if (convert != null) {
+					Map map = new HashMap(convert.size(), 1.0f);
+					Enumeration keys = convert.keys();
+					while (keys.hasMoreElements()) {
+						Object key = keys.nextElement();
+						map.put(key, convert.get(key));
 					}
-				} catch (PluginConversionException e) {
-					throw new CoreException(new Status(IStatus.ERROR, PDECore.PLUGIN_ID, NLS.bind(Messages.DirectoryBundleContainer_2, pluginDir.getAbsolutePath()), e));
+					return map;
 				}
+			} catch (PluginConversionException e) {
+				throw new CoreException(new Status(IStatus.ERROR, PDECore.PLUGIN_ID, NLS.bind(Messages.DirectoryBundleContainer_2, pluginLocation.getAbsolutePath()), e));
 			}
 		}
 		return null;
