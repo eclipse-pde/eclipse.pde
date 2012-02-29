98d9dcddff65ef11e3ae33158007c44b448c46d9
Curtis Windatt <cwindatt@ca.ibm.com>
2012-02-29 16:34:03 -0600

    Bug 362398 - [patch] WorkspaceProductModel load and write error

--- ui/org.eclipse.pde.core/src/org/eclipse/pde/internal/core/product/ProductPlugin.java
+++ ui/org.eclipse.pde.core/src/org/eclipse/pde/internal/core/product/ProductPlugin.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2009 IBM Corporation and others.
+ * Copyright (c) 2005, 2012 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -55,7 +55,7 @@ public class ProductPlugin extends ProductObject implements IProductPlugin {
 			if (PluginRegistry.findModel(fId) instanceof IFragmentModel) {
 				writer.print(" fragment=\"true\""); //$NON-NLS-1$
 			}
-		} else if (fFragment != null) {
+		} else if (fFragment != null && fFragment.length() > 0) {
 			// save the cached value (bug 264462)
 			writer.print(" fragment=\"" + fFragment + "\""); //$NON-NLS-1$ //$NON-NLS-2$
 		}
