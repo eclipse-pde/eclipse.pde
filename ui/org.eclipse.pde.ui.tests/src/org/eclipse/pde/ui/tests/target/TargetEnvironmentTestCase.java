c3d55f6f4aeada63e600b476fdfc9db9a567a0a6
Curtis Windatt <cwindatt@ca.ibm.com>
2012-10-12 17:45:23 -0500

    Bug 391828 - TargetEnvironmentTestCase tests fail locally

--- ui/org.eclipse.pde.ui.tests/src/org/eclipse/pde/ui/tests/target/TargetEnvironmentTestCase.java
+++ ui/org.eclipse.pde.ui.tests/src/org/eclipse/pde/ui/tests/target/TargetEnvironmentTestCase.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- *  Copyright (c) 2006, 2008 IBM Corporation and others.
+ *  Copyright (c) 2006, 2012 IBM Corporation and others.
  *  All rights reserved. This program and the accompanying materials
  *  are made available under the terms of the Eclipse Public License v1.0
  *  which accompanies this distribution, and is available at
@@ -10,9 +10,11 @@
  *******************************************************************************/
 package org.eclipse.pde.ui.tests.target;
 
-import java.util.Dictionary;
+import java.util.*;
 import junit.framework.*;
 import org.eclipse.core.runtime.Platform;
+import org.eclipse.jdt.launching.JavaRuntime;
+import org.eclipse.jdt.launching.environments.IExecutionEnvironment;
 import org.eclipse.pde.core.plugin.TargetPlatform;
 import org.eclipse.pde.internal.core.TargetPlatformHelper;
 import org.osgi.framework.Constants;
@@ -68,30 +70,50 @@ public class TargetEnvironmentTestCase extends TestCase {
 		Dictionary dictionary = TargetPlatformHelper.getTargetEnvironment();
 		assertTrue("true".equals(dictionary.get("osgi.resolveOptional")));
 	}
+	
+	/**
+	 * Tests that the OSGi state for the PDE models has the correct properties set, based on known execution environments
+	 */
+	public void testStateEEProperties() {
+		Dictionary[] platformProps = TargetPlatformHelper.getState().getPlatformProperties();
 
-	public void testStateDictionaryNumber() {
-		Dictionary[] dictionaries = TargetPlatformHelper.getState().getPlatformProperties();
-		String[] envs = TargetPlatformHelper.getKnownExecutionEnvironments();
-		assertEquals(envs.length, dictionaries.length);
-	}
-
-	public void testStateDictionaryLength() {
-		Dictionary[] dictionaries = TargetPlatformHelper.getState().getPlatformProperties();
-		Dictionary dictionary = TargetPlatformHelper.getTargetEnvironment();
-		for (int i = 0; i < dictionaries.length; i++)
-			assertTrue(dictionary.size() + 2 <= dictionaries[i].size());
-	}
-
-	public void testSystemPackages() {
-		Dictionary[] dictionaries = TargetPlatformHelper.getState().getPlatformProperties();
-		for (int i = 0; i < dictionaries.length; i++)
-			assertNotNull(dictionaries[i].get(Constants.FRAMEWORK_SYSTEMPACKAGES));
-	}
-
-	public void testExecutionEnvironment() {
-		Dictionary[] dictionaries = TargetPlatformHelper.getState().getPlatformProperties();
-		for (int i = 0; i < dictionaries.length; i++)
-			assertNotNull(dictionaries[i].get(Constants.FRAMEWORK_EXECUTIONENVIRONMENT));
+		String[] profiles = TargetPlatformHelper.getKnownExecutionEnvironments();
+		ArrayList eeProps = new ArrayList(profiles.length);
+		for (int i = 0; i < profiles.length; i++) {
+			IExecutionEnvironment environment = JavaRuntime.getExecutionEnvironmentsManager().getEnvironment(profiles[i]);
+			if (environment != null) {
+				Properties profileProps = environment.getProfileProperties();
+				if (profileProps != null) {
+					// If we have profile properties for an execution environment, ensure that they were added to the state
+					String systemPackages = profileProps.getProperty(Constants.FRAMEWORK_SYSTEMPACKAGES);
+					if (systemPackages != null){
+						boolean foundSystemPackage = false;
+						for (int j = 0; j < platformProps.length; j++) {
+							if (systemPackages.equals(platformProps[j].get(Constants.FRAMEWORK_SYSTEMPACKAGES))){
+								foundSystemPackage = true;
+								break;
+							}
+						}
+						if (!foundSystemPackage){
+							fail("The system packages property for EE " + profiles[i] + " was not found in the state's propeties");
+						}
+					}
+					String ee = profileProps.getProperty(Constants.FRAMEWORK_EXECUTIONENVIRONMENT);
+					if (ee != null){
+						boolean foundEE = false;
+						for (int j = 0; j < platformProps.length; j++) {
+							if (ee.equals(platformProps[j].get(Constants.FRAMEWORK_EXECUTIONENVIRONMENT))){
+								foundEE = true;
+								break;
+							}
+						}
+						if (!foundEE){
+							fail("The framework EE property for EE " + profiles[i] + " was not found in the state's propeties");
+						}
+					}
+				}
+			}
+		}
 	}
 
 }
