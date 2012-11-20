4a8dfd4774b1ecc386b04a3b2e897bcd0ac8c185
Curtis Windatt <cwindatt@ca.ibm.com>
2012-11-20 10:33:21 -0600

    Bug 391305 - Host OSGi console warning does not show newline

--- ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/util/OSGiConsole.java
+++ ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/util/OSGiConsole.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010 Red Hat and others.
+ * Copyright (c) 2010, 2012 Red Hat and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -54,6 +54,7 @@ public class OSGiConsole extends IOConsole {
 		IOConsoleOutputStream info = newOutputStream(); // create a stream to write info message to
 		try {
 			info.write(PDEUIMessages.OSGiConsoleFactory_title);
+			info.write('\n');
 		} catch (IOException e) {
 		} finally {
 			try {
