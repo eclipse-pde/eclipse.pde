ce77981fcbe55c2f1f7f96429cecfdd18fa79c52
Curtis Windatt <cwindatt@ca.ibm.com>
2012-11-20 11:44:43 -0600

    Bug 394688 - NPEs in Log View

--- ui/org.eclipse.ui.views.log/src/org/eclipse/ui/internal/views/log/LogView.java
+++ ui/org.eclipse.ui.views.log/src/org/eclipse/ui/internal/views/log/LogView.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2011 IBM Corporation and others.
+ * Copyright (c) 2000, 2012 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -1061,9 +1061,13 @@ public class LogView extends ViewPart implements ILogListener {
 						fExportLogAction.setEnabled(fInputFile.exists());
 						fExportLogEntryAction.setEnabled(!viewer.getSelection().isEmpty());
 						if (activate && fActivateViewAction.isChecked()) {
-							IWorkbenchPage page = Activator.getDefault().getWorkbench().getActiveWorkbenchWindow().getActivePage();
-							if (page != null)
-								page.bringToTop(view);
+							IWorkbenchWindow window = Activator.getDefault().getWorkbench().getActiveWorkbenchWindow();
+							if (window != null) {
+								IWorkbenchPage page = window.getActivePage();
+								if (page != null) {
+									page.bringToTop(view);
+								}
+							}
 						}
 					}
 				}
