2cd88b7121445ec7b5ebca1a290f5278c9bc9ca1
Curtis Windatt <cwindatt@ca.ibm.com>
2013-01-09 11:34:49 -0600

    Bug 396429 - Samples cannot be loaded from the welcome page

--- ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/PDEUIMessages.java
+++ ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/PDEUIMessages.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2012 IBM Corporation and others.
+ * Copyright (c) 2005, 2013 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -1528,6 +1528,8 @@ public class PDEUIMessages extends NLS {
 	public static String ShowSampleAction_title;
 	public static String ShowSampleAction_msgDesc;
 	public static String ShowSampleAction_msgTitle;
+
+	public static String ShowSampleAction_NoSamplesFound;
 	public static String DependencyExtent_singular;
 	public static String DependencyExtent_plural;
 	public static String DetailsBlock_horizontal;
