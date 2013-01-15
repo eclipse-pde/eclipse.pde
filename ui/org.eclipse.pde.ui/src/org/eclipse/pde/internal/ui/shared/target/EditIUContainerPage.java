3670ffe8772549cc822d43aca22178b0d0d16902
Dani Megert <dmegert>
2013-01-15 11:13:55 +0100

    Fixed bug 398155: Compiler error in 'EditIUContainerPage' because internal code changed

--- ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/shared/target/EditIUContainerPage.java
+++ ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/shared/target/EditIUContainerPage.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009, 2012 IBM Corporation and others.
+ * Copyright (c) 2009, 2013 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -347,7 +347,7 @@ public class EditIUContainerPage extends WizardPage implements IEditBundleContai
 	private void createQueryContext() {
 		fQueryContext = ProvUI.getQueryContext(profileUI.getPolicy());
 		fQueryContext.setInstalledProfileId(P2TargetUtils.getProfileId(fTarget));
-		fQueryContext.showAlreadyInstalled();
+		fQueryContext.setHideAlreadyInstalled(false);
 	}
 
 	/**
