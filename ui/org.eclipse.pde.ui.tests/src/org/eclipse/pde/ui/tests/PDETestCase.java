05102e37332cb4b9fc3d5d5f6b399ca2475e61ae
Curtis Windatt <cwindatt@ca.ibm.com>
2013-02-12 15:13:07 -0600

    Bug 400002 - Reduce console output of the tests

--- ui/org.eclipse.pde.ui.tests/src/org/eclipse/pde/ui/tests/PDETestCase.java
+++ ui/org.eclipse.pde.ui.tests/src/org/eclipse/pde/ui/tests/PDETestCase.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- *  Copyright (c) 2005, 2011 IBM Corporation and others.
+ *  Copyright (c) 2005, 2013 IBM Corporation and others.
  *  All rights reserved. This program and the accompanying materials
  *  are made available under the terms of the Eclipse Public License v1.0
  *  which accompanies this distribution, and is available at
@@ -14,6 +14,7 @@ import junit.framework.TestCase;
 import org.eclipse.core.resources.*;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.ui.*;
 
 /**
  * Provides a default {@link #tearDown()} implementation to delete all
@@ -23,6 +24,16 @@ import org.eclipse.core.runtime.NullProgressMonitor;
 public abstract class PDETestCase extends TestCase {
 
 	protected void tearDown() {
+		// Close any editors we opened
+		IWorkbenchWindow[] workbenchPages = PlatformUI.getWorkbench().getWorkbenchWindows();
+		for (int i = 0; i < workbenchPages.length; i++) {
+			IWorkbenchPage page = workbenchPages[i].getActivePage();
+			if (page != null){
+				page.closeAllEditors(false);
+			}
+		}
+		
+		// Delete any projects that were created
 		IWorkspaceRoot workspaceRoot = ResourcesPlugin.getWorkspace().getRoot();
 		IProject[] projects = workspaceRoot.getProjects();
 		try {
