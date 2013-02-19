e85393d0c07f5a7c3c50c81f11dd1f7d70cd55ba
Curtis Windatt <cwindatt@ca.ibm.com>
2013-02-19 11:04:54 -0600

    Bug 400893 - The product includes native laucher artifacts - Click behavior

--- ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/editor/product/GeneralInfoSection.java
+++ ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/editor/product/GeneralInfoSection.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009, 2011 EclipseSource Corporation and others.
+ * Copyright (c) 2009, 2013 EclipseSource Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -133,7 +133,7 @@ public class GeneralInfoSection extends PDESection {
 
 	private void createLaunchersOption(Composite client, FormToolkit toolkit) {
 		fLaunchersButton = toolkit.createButton(client, PDEUIMessages.ProductInfoSection_launchers, SWT.CHECK);
-		GridData data = new GridData(GridData.FILL_HORIZONTAL);
+		GridData data = new GridData();
 		data.horizontalSpan = 2;
 		fLaunchersButton.setLayoutData(data);
 		fLaunchersButton.setEnabled(isEditable());
