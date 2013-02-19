0eb6d7d73b61b75b121aaeff0561e225f2b43bbe
Curtis Windatt <cwindatt@ca.ibm.com>
2013-02-19 11:14:47 -0600

    Bug 401081 - .ArrayIndexOutOfBoundsException: 1 at org.eclipse.pde.internal.core.util.PDESchemaHelper.gatherAttributes(PDESchemaHelper.java:85)

--- ui/org.eclipse.pde.core/src/org/eclipse/pde/internal/core/util/PDESchemaHelper.java
+++ ui/org.eclipse.pde.core/src/org/eclipse/pde/internal/core/util/PDESchemaHelper.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2012 IBM Corporation and others.
+ * Copyright (c) 2008, 2013 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -70,6 +70,9 @@ public class PDESchemaHelper {
 		if (basedOn == null) // check for null
 			return;
 		String[] path = basedOn.split("/"); //$NON-NLS-1$
+		if (path.length < 2) {
+			return; // No plug-in identifier
+		}
 		IExtension[] extensions = PDECore.getDefault().getExtensionsRegistry().findExtensions(path[0], true);
 
 		List<IConfigurationElement> members = new ArrayList<IConfigurationElement>();
