e6ef77ad11f05d9c75f9c318a8d15fd490918f73
Curtis Windatt <cwindatt@ca.ibm.com>
2013-05-06 16:36:56 -0500

    Bug 407339 - Find broken NLS strings in PDE Build

--- org.eclipse.pde.build/src/org/eclipse/pde/internal/build/Messages.java
+++ org.eclipse.pde.build/src/org/eclipse/pde/internal/build/Messages.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2011 IBM Corporation and others.
+ * Copyright (c) 2005, 2013 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -61,7 +61,6 @@ public class Messages extends NLS {
 	public static String exception_registryResolution;
 	public static String exception_errorConverting;
 	public static String exception_cannotAcquireService;
-	public static String exception_hostNotFound;
 	public static String exception_missing_pdebuild_folder;
 
 	// build.xml
