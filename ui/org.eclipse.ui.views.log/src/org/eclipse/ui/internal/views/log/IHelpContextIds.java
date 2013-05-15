b5c7f8cc76983f4bf3167f59dcd49bb1aada94fc
Dani Megert <dmegert>
2013-05-15 22:09:52 +0200

    Fixed compile warning (touching ui.views.log)

--- ui/org.eclipse.ui.views.log/src/org/eclipse/ui/internal/views/log/IHelpContextIds.java
+++ ui/org.eclipse.ui.views.log/src/org/eclipse/ui/internal/views/log/IHelpContextIds.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- *  Copyright (c) 2007, 2012 IBM Corporation and others.
+ *  Copyright (c) 2007, 2013 IBM Corporation and others.
  *  All rights reserved. This program and the accompanying materials
  *  are made available under the terms of the Eclipse Public License v1.0
  *  which accompanies this distribution, and is available at
@@ -12,7 +12,7 @@ package org.eclipse.ui.internal.views.log;
 
 public interface IHelpContextIds {
 
-	public static final String PREFIX = "org.eclipse.ui."; //$NON-NLS-1$ //$NON-NLS-2$
+	public static final String PREFIX = "org.eclipse.ui."; //$NON-NLS-1$
 	public static final String LOG_VIEW = PREFIX + "error_log"; //$NON-NLS-1$
 	public static final String LOG_EVENTDETAILS = PREFIX + "log_eventdetails"; //$NON-NLS-1$
 	public static final String LOG_FILTER = PREFIX + "log_filter"; //$NON-NLS-1$
