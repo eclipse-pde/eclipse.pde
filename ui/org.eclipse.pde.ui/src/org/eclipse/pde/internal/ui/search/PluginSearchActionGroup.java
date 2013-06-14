9d8beec2cb4ff378bd13235c9f9cc1c5e0e1cd8f
Curtis Windatt <cwindatt@ca.ibm.com>
2013-06-14 15:37:02 -0500

    Bug 409528 - Open extension point schema from context menu broken again
    Change-Id: Iceeb37de73eaed0f25de720a00d9259a4541a264

--- ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/search/PluginSearchActionGroup.java
+++ ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/search/PluginSearchActionGroup.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- *  Copyright (c) 2005, 2012 IBM Corporation and others.
+ *  Copyright (c) 2005, 2013 IBM Corporation and others.
  *  All rights reserved. This program and the accompanying materials
  *  are made available under the terms of the Eclipse Public License v1.0
  *  which accompanies this distribution, and is available at
@@ -72,7 +72,7 @@ public class PluginSearchActionGroup extends ActionGroup {
 				// Point ID is not fully qualified
 				action.setInput(fullyQualifyPointID(pointID));
 			} else {
-				action.setInput(point);
+				action.setInput(pointID);
 			}
 			action.setEnabled(true);
 			menu.add(action);
