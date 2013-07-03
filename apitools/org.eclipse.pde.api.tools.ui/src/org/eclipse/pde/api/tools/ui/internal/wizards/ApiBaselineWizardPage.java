bd4b2eafe3cbc582774106d0d8cd5d4804bdb0a9
Curtis Windatt <cwindatt@ca.ibm.com>
2013-07-03 11:20:03 -0500

    Bug 411926 - Can't change API baseline location
    Change-Id: I49f36983b993d989ba0c2518be165ed489b1610f

--- apitools/org.eclipse.pde.api.tools.ui/src/org/eclipse/pde/api/tools/ui/internal/wizards/ApiBaselineWizardPage.java
+++ apitools/org.eclipse.pde.api.tools.ui/src/org/eclipse/pde/api/tools/ui/internal/wizards/ApiBaselineWizardPage.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2010 IBM Corporation and others.
+ * Copyright (c) 2007, 2013 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -489,6 +489,10 @@ public class ApiBaselineWizardPage extends WizardPage {
 			IStatus status = fProfile.getExecutionEnvironmentStatus();
 			if (status.getSeverity() == IStatus.ERROR) {
 				setErrorMessage(status.getMessage());
+				return false;
+			}
+			if (!fProfile.getLocation().equals(locationcombo.getText())){
+				setErrorMessage(WizardMessages.ApiProfileWizardPage_location_needs_reset);
 				return false;
 			}
 		}
