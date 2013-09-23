8d9a2880eec7c20c32dee88134dde2e969728633
Michael Rennie <michael_rennie@ca.ibm.com>
2013-09-23 13:39:35 -0500

    Bug 415607 - Investigate supporting annotations for API Tools
    Change-Id: I0282e17705fc4b8f39abf81ce04383abaf809ccf

---
+++ apitools/org.eclipse.pde.api.tools.annotations/src/org/eclipse/pde/api/tools/annotations/NoExtend.java
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ * Copyright (c) 2013 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.pde.api.tools.annotations;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Classes and interfaces tagged with this annotation are declaring they are not
+ * to be extended by clients. If this annotation is used anywhere except classes
+ * or interfaces it will be ignored.
+ * 
+ * @since 1.0
+ */
+@Documented
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface NoExtend {
+
+}
---
+++ apitools/org.eclipse.pde.api.tools.annotations/src/org/eclipse/pde/api/tools/annotations/NoReference.java
@@ -0,0 +1,34 @@
+/*******************************************************************************
+ * Copyright (c) 2013 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.pde.api.tools.annotations;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Classes, interfaces, annotations, enums, methods and fields tagged with this
+ * annotation are declaring they are not to be referenced at all by clients. For
+ * example a method tagged with this annotation should not be called by clients.
+ * If this annotation appears anywhere other than classes, interfaces,
+ * annotations, enums, methods and fields it will be ignored.
+ * 
+ * @since 1.0
+ */
+@Documented
+@Target(value = {
+		ElementType.TYPE, ElementType.METHOD, ElementType.CONSTRUCTOR,
+		ElementType.FIELD })
+@Retention(RetentionPolicy.SOURCE)
+public @interface NoReference {
+}
