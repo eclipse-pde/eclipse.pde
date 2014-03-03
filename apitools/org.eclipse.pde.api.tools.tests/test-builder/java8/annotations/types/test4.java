604182f63f18d9f5c220be10f37acfb2dc41289d
Mike Rennie <Michael_Rennie@ca.ibm.com>
2014-03-03 15:18:02 -0600

    Bug 427499 - [1.8] Support type annotations - support tests
    Change-Id: Id82ee8ac269752663e6f2b574a18bcdf03015fcd

---
+++ apitools/org.eclipse.pde.api.tools.tests/test-builder/java8/annotations/types/test4.java
@@ -0,0 +1,22 @@
+/*******************************************************************************
+ * Copyright (c) 2014 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+import org.eclipse.pde.api.tools.annotations.NoReference;
+
+public class test4 {
+
+	void method() throws @NoReference Exception {
+		
+	}
+	
+	void method2(@NoReference String arg) {
+		
+	}
+}
