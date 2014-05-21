d015baf1e3fd85328e2b29c2695503859b9816cc
Markus Keller <markus_keller@ch.ibm.com>
2014-05-21 14:30:42 +0200

    Fixed bug 435205: Can't launch second runtime workbench when org.eclipse.equinox.launcher is in workspace (from Git)
    Signed-off-by: Markus Keller <markus_keller@ch.ibm.com>

--- ui/org.eclipse.pde.launching/src/org/eclipse/pde/internal/launching/launcher/LaunchArgumentsHelper.java
+++ ui/org.eclipse.pde.launching/src/org/eclipse/pde/internal/launching/launcher/LaunchArgumentsHelper.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2013 IBM Corporation and others.
+ * Copyright (c) 2005, 2014 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -266,6 +266,12 @@ public class LaunchArgumentsHelper {
 					if (files[i].isFile()) {
 						return files[i].getPath();
 					}
+				}
+
+				// Source bundle from git://git.eclipse.org/gitroot/equinox/rt.equinox.framework.git
+				File binFolder = new File(bundleFile, "bin"); //$NON-NLS-1$
+				if (binFolder.isDirectory()) {
+					return binFolder.getPath();
 				}
 				return null;
 			}
