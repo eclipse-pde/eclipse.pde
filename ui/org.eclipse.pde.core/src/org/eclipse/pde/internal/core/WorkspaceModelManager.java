6cbf3f84c3f291093ba21979a5e542752c2b7a7b
Vikas Chandra <Vikas.Chandra@in.ibm.com>
2014-07-02 13:21:47 -0500

    Bug 436562 - NPE with 'Open This version' of /org.eclipse.jdt.doc.user/topics_Tasks.xml from History view
    Change-Id: Id0bf9a582e8b776f112e58e1c1ed52e3d8cd2bde

--- ui/org.eclipse.pde.core/src/org/eclipse/pde/internal/core/WorkspaceModelManager.java
+++ ui/org.eclipse.pde.core/src/org/eclipse/pde/internal/core/WorkspaceModelManager.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- *  Copyright (c) 2003, 2012 IBM Corporation and others.
+ *  Copyright (c) 2003, 2014 IBM Corporation and others.
  *  All rights reserved. This program and the accompanying materials
  *  are made available under the terms of the Eclipse Public License v1.0
  *  which accompanies this distribution, and is available at
@@ -21,7 +21,9 @@ import org.eclipse.team.core.RepositoryProvider;
 
 public abstract class WorkspaceModelManager extends AbstractModelManager implements IResourceChangeListener, IResourceDeltaVisitor {
 
-	public static boolean isPluginProject(IProject project) {
+	public static boolean isPluginProject(IProject project) {	
+		if (project == null)
+			return false;
 		if (project.isOpen())
 			return PDEProject.getManifest(project).exists() || PDEProject.getPluginXml(project).exists() || PDEProject.getFragmentXml(project).exists();
 		return false;
