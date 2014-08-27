04f07ccfb2005c3494ca0961ed1584a5f7b83e5c
Vikas Chandra <Vikas.Chandra@in.ibm.com>
2014-08-27 12:02:07 +0530

    Bug 401741 - exception when creating new extension point

--- ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/wizards/extension/BaseExtensionPointMainPage.java
+++ ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/wizards/extension/BaseExtensionPointMainPage.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- *  Copyright (c) 2000, 2012 IBM Corporation and others.
+ *  Copyright (c) 2000, 2014 IBM Corporation and others.
  *  All rights reserved. This program and the accompanying materials
  *  are made available under the terms of the Eclipse Public License v1.0
  *  which accompanies this distribution, and is available at
@@ -282,7 +282,7 @@ public abstract class BaseExtensionPointMainPage extends WizardPage {
 		IRunnableWithProgress operation = new WorkspaceModifyOperation() {
 			public void execute(final IProgressMonitor monitor) {
 				try {
-					Display.getDefault().asyncExec(new Runnable() {
+					Display.getDefault().syncExec(new Runnable() {
 
 						public void run() {
 							String schemaName = schema;
