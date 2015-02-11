237cb9f07d1fb9b0db94dc71d95d559994d76c1f
Vikas Chandra <Vikas.Chandra@in.ibm.com>
2015-02-11 19:52:20 +0530

    Bug 459581 - NPE in Site.isValid

--- ui/org.eclipse.pde.core/src/org/eclipse/pde/internal/core/site/Site.java
+++ ui/org.eclipse.pde.core/src/org/eclipse/pde/internal/core/site/Site.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2013 IBM Corporation and others.
+ * Copyright (c) 2000, 2015 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -330,6 +330,7 @@ public class Site extends SiteObject implements ISite {
 	protected void reset() {
 		archives.clear();
 		categoryDefs.clear();
+		repositoryReferences.clear();
 		features.clear();
 		bundles.clear();
 		description = null;
@@ -338,7 +339,7 @@ public class Site extends SiteObject implements ISite {
 		mirrorsUrl = null;
 		digestUrl = null;
 		associateSitesUrl = null;
-		repositoryReferences = null;
+
 		statsInfo = null;
 	}
 
