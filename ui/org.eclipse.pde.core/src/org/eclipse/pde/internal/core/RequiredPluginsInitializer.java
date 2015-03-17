6ce443b4dafca3c1190db83edb8fd2c8c5b5f716
Vikas Chandra <Vikas.Chandra@in.ibm.com>
2015-03-17 08:53:40 +0530

    Bug 458072 - Resource Exception when closing a project before model init is complete

--- ui/org.eclipse.pde.core/src/org/eclipse/pde/internal/core/RequiredPluginsInitializer.java
+++ ui/org.eclipse.pde.core/src/org/eclipse/pde/internal/core/RequiredPluginsInitializer.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- *  Copyright (c) 2000, 2013 IBM Corporation and others.
+ *  Copyright (c) 2000, 2015 IBM Corporation and others.
  *  All rights reserved. This program and the accompanying materials
  *  are made available under the terms of the Eclipse Public License v1.0
  *  which accompanies this distribution, and is available at
@@ -45,8 +45,10 @@ public class RequiredPluginsInitializer extends ClasspathContainerInitializer {
 			} catch (InterruptedException e) {
 			}
 		}
-		IPluginModelBase model = manager.findModel(project);
-		JavaCore.setClasspathContainer(PDECore.REQUIRED_PLUGINS_CONTAINER_PATH, new IJavaProject[] {javaProject}, new IClasspathContainer[] {new RequiredPluginsClasspathContainer(model)}, null);
+		if (project.exists() && project.isOpen()) {
+			IPluginModelBase model = manager.findModel(project);
+			JavaCore.setClasspathContainer(PDECore.REQUIRED_PLUGINS_CONTAINER_PATH, new IJavaProject[] {javaProject}, new IClasspathContainer[] {new RequiredPluginsClasspathContainer(model)}, null);
+		}
 	}
 
 	/*
