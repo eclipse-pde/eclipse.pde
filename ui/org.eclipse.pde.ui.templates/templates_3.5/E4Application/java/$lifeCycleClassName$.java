ef0a526e18bda933e416d39f2a387adf12254997
Lars Vogel <Lars.Vogel@vogella.com>
2015-07-16 13:59:45 +0200

    [Bug 472810] Generated E4LifeCycle class contains copyright header
    Change-Id: I36d6ccce047f211fbe58648f1021af481a242d27
    Signed-off-by: Lars Vogel <Lars.Vogel@vogella.com>

--- ui/org.eclipse.pde.ui.templates/templates_3.5/E4Application/java/$lifeCycleClassName$.java
+++ ui/org.eclipse.pde.ui.templates/templates_3.5/E4Application/java/$lifeCycleClassName$.java
@@ -1,13 +1,3 @@
-/*******************************************************************************
- * Copyright (c) 2014 TwelveTone LLC and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *     Steven Spungin <steven@spungin.tv> - initial API and implementation
- *******************************************************************************/
 package $packageName$;
 
 import org.eclipse.e4.core.contexts.IEclipseContext;
