897edd40f9e53d5970294a3400dd82aeeb7479cf
Florian Holl <florian.holl@ts.fujitsu.com>
2015-10-29 17:03:41 +0100

    Bug 480747 - Use vararg MessageFormat.format method in PDE
    Change-Id: I236f9187e3ade6123e7b328d57ce0072a89942d1
    Signed-off-by: Florian Holl <florian.holl@ts.fujitsu.com>

--- ui/org.eclipse.pde.core/src/org/eclipse/pde/internal/core/target/TargetDefinitionPersistenceHelper.java
+++ ui/org.eclipse.pde.core/src/org/eclipse/pde/internal/core/target/TargetDefinitionPersistenceHelper.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009, 2013 IBM Corporation and others.
+ * Copyright (c) 2009, 2015 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -268,7 +268,7 @@ public class TargetDefinitionPersistenceHelper {
 		} else {
 			// Version doesn't match any known file structure, default to latest
 			String name = root.getAttribute(TargetDefinitionPersistenceHelper.ATTR_NAME);
-			PDECore.log(new Status(IStatus.WARNING, PDECore.PLUGIN_ID, MessageFormat.format(Messages.TargetDefinitionPersistenceHelper_2, new Object[] {version, name})));
+			PDECore.log(new Status(IStatus.WARNING, PDECore.PLUGIN_ID, MessageFormat.format(Messages.TargetDefinitionPersistenceHelper_2, version, name)));
 			TargetPersistence38Helper.initFromDoc(definition, root);
 		}
 	}
