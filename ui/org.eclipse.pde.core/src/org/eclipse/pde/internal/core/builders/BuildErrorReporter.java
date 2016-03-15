fd993a71c242be009964f44b8e7947aeb1fb7dcf
Alex Blewitt <alex.blewitt@gmail.com>
2016-03-15 00:32:41 +0000

    Bug 489606 - Only warn about missing Application.e4xmi
    A warning is shown when Application.e4xmi is missing from the
    plugin.xml file. Only show this warning if the Application.e4xmi
    file is present in the project.
    
    Signed-off-by: Alex Blewitt <alex.blewitt@gmail.com>
    Change-Id: I955dc0aa9a0df2fc52c094f071947c549b7e8106

--- ui/org.eclipse.pde.core/src/org/eclipse/pde/internal/core/builders/BuildErrorReporter.java
+++ ui/org.eclipse.pde.core/src/org/eclipse/pde/internal/core/builders/BuildErrorReporter.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2015 IBM Corporation and others.
+ * Copyright (c) 2005, 2016 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -659,8 +659,10 @@ public class BuildErrorReporter extends ErrorReporter implements IBuildPropertie
 						validateBinIncludes(binIncludes, attValue.getNodeValue());
 					}
 				} else {
-					// Default if not specified
-					validateBinIncludes(binIncludes, "Application.e4xmi"); //$NON-NLS-1$
+					if (fProject.exists(new Path("Application.e4xmi"))) { //$NON-NLS-1$
+						// Default if not specified
+						validateBinIncludes(binIncludes, "Application.e4xmi"); //$NON-NLS-1$
+					}
 				}
 			}
 
