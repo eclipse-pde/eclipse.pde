c449e7867786526cbf7019d92c07b90169be6afc
Olivier Thomann <Olivier_Thomann@ca.ibm.com>
2016-04-11 11:46:43 -0400

    Revert "Bug 490770 - Freeze report should mention compatible changes"
    This reverts commit 74441c2425e2c1c9ba9e78874081affb390cf497.

--- apitools/org.eclipse.pde.api.tools/src_ant/org/eclipse/pde/api/tools/internal/tasks/Messages.java
+++ apitools/org.eclipse.pde.api.tools/src_ant/org/eclipse/pde/api/tools/internal/tasks/Messages.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2016 IBM Corporation and others.
+ * Copyright (c) 2008, 2013 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -32,8 +32,6 @@ public class Messages extends NLS {
 	public static String deltaReportTask_missingXmlFile;
 	public static String deltaReportTask_xmlFileLocationMustBeAFile;
 	public static String deltaReportTask_hmlFileLocationMustBeAFile;
-	public static String deltaReportTask_compile_time_constant;
-	public static String deltaReportTask_entry_execution_environment;
 
 	public static String fullReportTask_bundlesheader;
 	public static String fullReportTask_bundlesentry_even;
