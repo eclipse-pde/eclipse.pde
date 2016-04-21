ece4ec1c5843e95c87993d909c37e5dd9c596ec6
Lars Vogel <Lars.Vogel@vogella.com>
2016-04-21 15:29:22 +0200

    Bug 488514 - Run copyright tooling for the source files in the new DS plug-in
    Change-Id: Ic7ceafbe97b2606371e4876732204ad64741b3f0
    Signed-off-by: Lars Vogel <Lars.Vogel@vogella.com>

--- ds/org.eclipse.pde.ds.annotations/src/org/eclipse/pde/ds/internal/annotations/ValidationErrorLevel.java
+++ ds/org.eclipse.pde.ds.annotations/src/org/eclipse/pde/ds/internal/annotations/ValidationErrorLevel.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Ecliptical Software Inc. and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Ecliptical Software Inc. - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.pde.ds.internal.annotations;
 
 public enum ValidationErrorLevel {
