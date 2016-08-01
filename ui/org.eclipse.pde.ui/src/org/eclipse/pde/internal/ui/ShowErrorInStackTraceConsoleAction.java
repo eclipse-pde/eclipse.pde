5f487585e6eeee2389e50b2f32887e004c2b7c3a
Vikas Chandra <Vikas.Chandra@in.ibm.com>
2016-08-01 14:22:41 +0530

    Bug 496237 - JavaStackTraceConsoleFactory as API
    Signed-off-by: Vikas Chandra <Vikas.Chandra@in.ibm.com>

--- ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/ShowErrorInStackTraceConsoleAction.java
+++ ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/ShowErrorInStackTraceConsoleAction.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2015 IBM Corporation and others.
+ * Copyright (c) 2008, 2016 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,7 +10,7 @@
  *******************************************************************************/
 package org.eclipse.pde.internal.ui;
 
-import org.eclipse.jdt.internal.debug.ui.console.JavaStackTraceConsoleFactory;
+import org.eclipse.jdt.debug.ui.console.JavaStackTraceConsoleFactory;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
