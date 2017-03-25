ddefb3adae32ec04a5037032165854067c5330d5
Vikas Chandra <Vikas.Chandra@in.ibm.com>
2017-03-25 23:59:19 +0530

    Bug 509034 - Further performance improvement in API tools build for large workspaces
    Signed-off-by: Vikas Chandra <Vikas.Chandra@in.ibm.com>

--- apitools/org.eclipse.pde.api.tools/src/org/eclipse/pde/api/tools/internal/ApiDescriptionProcessor.java
+++ apitools/org.eclipse.pde.api.tools/src/org/eclipse/pde/api/tools/internal/ApiDescriptionProcessor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2016 IBM Corporation and others.
+ * Copyright (c) 2007, 2017 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -756,6 +756,9 @@ public class ApiDescriptionProcessor {
 	 */
 	private static int getVisibility(Element element) {
 		String attribute = element.getAttribute(IApiXmlConstants.ATTR_VISIBILITY);
+		if (attribute != null && attribute.isEmpty()) {
+			return -1;
+		}
 		try {
 			return Integer.parseInt(attribute);
 		} catch (NumberFormatException nfe) {
