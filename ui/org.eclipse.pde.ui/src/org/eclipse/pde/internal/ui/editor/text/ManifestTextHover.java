25299970f32aabec4344ba19016706d6b813bafb
Vikas Chandra <Vikas.Chandra@in.ibm.com>
2017-05-09 17:03:22 +0530

    Bug 234611 - problems in manifest.mf editor don't get hover
    Change-Id: I38920ee095807adf8fe4d0d9f44efc5e3f66b835
    Signed-off-by: Vikas Chandra <Vikas.Chandra@in.ibm.com>

--- ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/editor/text/ManifestTextHover.java
+++ ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/editor/text/ManifestTextHover.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- *  Copyright (c) 2005, 2015 IBM Corporation and others.
+ *  Copyright (c) 2005, 2017 IBM Corporation and others.
  *  All rights reserved. This program and the accompanying materials
  *  are made available under the terms of the Eclipse Public License v1.0
  *  which accompanies this distribution, and is available at
@@ -10,11 +10,15 @@
  *******************************************************************************/
 package org.eclipse.pde.internal.ui.editor.text;
 
+import java.util.Iterator;
+import org.eclipse.core.resources.IMarker;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jface.text.IRegion;
 import org.eclipse.jface.text.ITextViewer;
+import org.eclipse.jface.text.source.Annotation;
+import org.eclipse.jface.text.source.IAnnotationModel;
 import org.eclipse.pde.core.IBaseModel;
 import org.eclipse.pde.core.IModel;
 import org.eclipse.pde.internal.core.ICoreConstants;
@@ -23,6 +27,7 @@ import org.eclipse.pde.internal.core.text.IDocumentRange;
 import org.eclipse.pde.internal.ui.editor.PDEFormEditor;
 import org.eclipse.pde.internal.ui.editor.PDESourcePage;
 import org.eclipse.pde.internal.ui.util.PDEJavaHelperUI;
+import org.eclipse.ui.texteditor.MarkerAnnotation;
 
 public class ManifestTextHover extends PDETextHover {
 
@@ -37,6 +42,29 @@ public class ManifestTextHover extends PDETextHover {
 
 	@Override
 	public String getHoverInfo(ITextViewer textViewer, IRegion hoverRegion) {
+		String hover = getHoverInfo2(textViewer, hoverRegion);
+		if (hover == null) {
+			IAnnotationModel annotationModel = fSourcePage.getViewer().getAnnotationModel();
+			if (annotationModel instanceof PDEMarkerAnnotationModel) {
+				int offset = hoverRegion.getOffset();
+				PDEMarkerAnnotationModel pdeAnnotationModel = (PDEMarkerAnnotationModel) annotationModel;
+				Iterator<Annotation> annotationIterator = pdeAnnotationModel.getAnnotationIterator(offset, 0, true,
+						true);
+				while (annotationIterator.hasNext()) {
+					Annotation object = annotationIterator.next();
+					if (object instanceof MarkerAnnotation) {
+						IMarker m = ((MarkerAnnotation) object).getMarker();
+						String message = m.getAttribute(IMarker.MESSAGE, (String) null);
+						return message;
+					}
+				}
+
+			}
+		}
+		return hover;
+	}
+
+	public String getHoverInfo2(ITextViewer textViewer, IRegion hoverRegion) {
 		int offset = hoverRegion.getOffset();
 		IDocumentRange range = fSourcePage.getRangeElement(offset, false);
 		if (range instanceof IManifestHeader) {
