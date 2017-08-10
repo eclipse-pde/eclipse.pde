c3de4702f292883aee0d8624ac84504e3feca044
Christian Dietrich <christian.dietrich@itemis.de>
2017-08-10 08:36:15 +0200

    Bug 520707: Show the "Configure Target Platform" Preference Page only.
    If others are shown this might lead to a ClassCastException cause other
    preference pages assume the pages container is a
    IWorkbenchPreferenceContainer
    
    Change-Id: Icbf3573fd19c06d1330db74f28a776274550a005
    Signed-off-by: Christian Dietrich <christian.dietrich@itemis.de>

--- ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/correction/ConfigureTargetPlatformResolution.java
+++ ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/correction/ConfigureTargetPlatformResolution.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- *  Copyright (c) 2015 Red Hat Inc.
+ *  Copyright (c) 2015, 2017 Red Hat Inc.
  *  All rights reserved. This program and the accompanying materials
  *  are made available under the terms of the Eclipse Public License v1.0
  *  which accompanies this distribution, and is available at
@@ -7,17 +7,18 @@
  *
  *  Contributors:
  *     Mickael Istria (Red Hat Inc.) - initial API and implementation
+ *     Christian Dietrich (itemis AG) - show target platform preference page correctly
  *******************************************************************************/
 package org.eclipse.pde.internal.ui.correction;
 
 import org.eclipse.core.resources.IMarker;
-import org.eclipse.jface.preference.PreferenceDialog;
 import org.eclipse.pde.internal.ui.*;
 import org.eclipse.pde.internal.ui.preferences.TargetPlatformPreferencePage;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IMarkerResolution2;
 import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.dialogs.PreferencesUtil;
 
 public class ConfigureTargetPlatformResolution implements IMarkerResolution2 {
 
@@ -40,9 +41,8 @@ public class ConfigureTargetPlatformResolution implements IMarkerResolution2 {
 	@Override
 	public void run(IMarker marker) {
 		Shell shell = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell();
-		PreferenceDialog preferenceDialog = new PreferenceDialog(shell, PlatformUI.getWorkbench().getPreferenceManager());
-		preferenceDialog.setSelectedNode(TargetPlatformPreferencePage.PAGE_ID);
-		preferenceDialog.open();
+		String pageId = TargetPlatformPreferencePage.PAGE_ID;
+		PreferencesUtil.createPreferenceDialogOn(shell, pageId, null, null).open();
 	}
 
 }
