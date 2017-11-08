22306acb6347cc3b33a2b62ed3bc097f65e5e49f
Alexander Kurtakov <akurtako@redhat.com>
2017-11-08 11:01:55 +0200

    Bug 525660 - Automatic-Module-Name header needed for Java 9
    Add the Automatic-Module-Name by default on new project creation. This
    fixes TestPDETemplates which are now always failing due to created
    projects containing warnings.
    
    Change-Id: I7c744dc979b04f9daafd70b0c762e8476f52c471
    Signed-off-by: Alexander Kurtakov <akurtako@redhat.com>

--- ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/wizards/plugin/NewProjectCreationOperation.java
+++ ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/wizards/plugin/NewProjectCreationOperation.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2015 IBM Corporation and others.
+ * Copyright (c) 2000, 2017 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -198,6 +198,7 @@ public class NewProjectCreationOperation extends WorkspaceModifyOperation {
 		// add Bundle Specific fields if applicable
 		if (pluginBase instanceof BundlePluginBase) {
 			IBundle bundle = ((BundlePluginBase) pluginBase).getBundle();
+			bundle.setHeader(ICoreConstants.AUTOMATIC_MODULE_NAME, bundle.getHeader(Constants.BUNDLE_SYMBOLICNAME));
 
 			String value = getCommaValuesFromPackagesSet(getImportPackagesSet(), fData.getVersion());
 			if (value.length() > 0)
