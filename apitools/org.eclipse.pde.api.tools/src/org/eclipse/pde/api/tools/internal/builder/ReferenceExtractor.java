8e1b978411d059b542616c60756c549f585b9072
Karsten Thoms <karsten.thoms@itemis.de>
2018-03-06 16:29:52 +0100

    Bug 532072 - Replace usage of String#replaceAll by #replace
    Change-Id: I49eb0d69bda7134c1031dfb04554f35bd8f86b35
    Signed-off-by: Karsten Thoms <karsten.thoms@itemis.de>

--- apitools/org.eclipse.pde.api.tools/src/org/eclipse/pde/api/tools/internal/builder/ReferenceExtractor.java
+++ apitools/org.eclipse.pde.api.tools/src/org/eclipse/pde/api/tools/internal/builder/ReferenceExtractor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2017 IBM Corporation and others.
+ * Copyright (c) 2007, 2018 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -1080,7 +1080,7 @@ public class ReferenceExtractor extends ClassVisitor {
 		if (type != null && type.getSort() == Type.OBJECT) {
 			newname = type.getInternalName();
 		}
-		return newname.replaceAll("/", "."); //$NON-NLS-1$ //$NON-NLS-2$
+		return newname.replace('/', '.');
 	}
 
 	/**
