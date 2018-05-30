84af4cf63636732980e8486923af86608590f755
Alexander Kurtakov <akurtako@redhat.com>
2018-05-30 20:44:44 +0300

    Stop using deprecated computeMD5 method.
    Change-Id: Ia1dc826c702f233cd7f2a6c77830a6182d3332e5
    Signed-off-by: Alexander Kurtakov <akurtako@redhat.com>

--- org.eclipse.pde.build.tests/src/org/eclipse/pde/build/internal/tests/p2/P2Tests.java
+++ org.eclipse.pde.build.tests/src/org/eclipse/pde/build/internal/tests/p2/P2Tests.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2017 IBM Corporation and others. All rights reserved.
+ * Copyright (c) 2008, 2018 IBM Corporation and others. All rights reserved.
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
@@ -604,7 +604,7 @@ public class P2Tests extends P2TestCase {
 			return;
 
 		IFile artifact = repository.getFile(getArtifactLocation(descriptor));
-		String actualMD5 = ChecksumProducer.computeMD5(artifact.getLocation().toFile());
+		String actualMD5 = ChecksumProducer.produce(artifact.getLocation().toFile(), "MD5");
 		assertEquals(md5, actualMD5);
 	}
 
