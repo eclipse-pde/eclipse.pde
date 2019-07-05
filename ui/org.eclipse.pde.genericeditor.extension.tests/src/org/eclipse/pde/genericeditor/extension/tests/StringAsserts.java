8baf9abf618c77ba4bb81578558d16a49321d994
Lars Vogel <Lars.Vogel@vogella.com>
2019-07-05 14:15:37 +0200

    Using StringBuilder in StringAsserts
    Change-Id: I25ba7fc0747d9cac8845d971370108edbd7f0a4d
    Signed-off-by: Lars Vogel <Lars.Vogel@vogella.com>

--- ui/org.eclipse.pde.genericeditor.extension.tests/src/org/eclipse/pde/genericeditor/extension/tests/StringAsserts.java
+++ ui/org.eclipse.pde.genericeditor.extension.tests/src/org/eclipse/pde/genericeditor/extension/tests/StringAsserts.java
@@ -132,7 +132,7 @@ public class StringAsserts {
 				assertEqualString(list1.get(0), list2.get(0));
 			}
 
-			StringBuffer buf = new StringBuffer();
+			StringBuilder buf = new StringBuilder();
 			for (int i = 0; i < n1; i++) {
 				String s1 = list1.get(i);
 				if (s1 != null) {
@@ -142,7 +142,7 @@ public class StringAsserts {
 			}
 			String actual = buf.toString();
 
-			buf = new StringBuffer();
+			buf = new StringBuilder();
 			for (int i = 0; i < n2; i++) {
 				String s2 = list2.get(i);
 				if (s2 != null) {
@@ -175,7 +175,7 @@ public class StringAsserts {
 				assertEqualString(list1.get(0), list2.get(0));
 			}
 
-			StringBuffer buf = new StringBuffer();
+			StringBuilder buf = new StringBuilder();
 			for (int i = 0; i < n1; i++) {
 				String s1 = list1.get(i);
 				if (s1 != null) {
@@ -185,7 +185,7 @@ public class StringAsserts {
 			}
 			String actual = buf.toString();
 
-			buf = new StringBuffer();
+			buf = new StringBuilder();
 			for (int i = 0; i < n2; i++) {
 				String s2 = list2.get(i);
 				if (s2 != null) {
