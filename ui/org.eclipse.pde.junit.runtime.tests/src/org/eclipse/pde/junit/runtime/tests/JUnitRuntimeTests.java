53cdedae95960575b06e88f908a68f18f3cd06d0
Julian Honnen <julian.honnen@vector.com>
2019-07-25 13:22:49 +0200

    Bug 535531 - added test.xml for junit.runtime.tests
    Change-Id: I38a1014167e1fae5abf2cbe7c594305e54807a10
    Signed-off-by: Julian Honnen <julian.honnen@vector.com>

---
+++ ui/org.eclipse.pde.junit.runtime.tests/src/org/eclipse/pde/junit/runtime/tests/JUnitRuntimeTests.java
@@ -0,0 +1,24 @@
+/*******************************************************************************
+ *  Copyright (c) 2019 Julian Honnen
+ *
+ *  This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License 2.0
+ *  which accompanies this distribution, and is available at
+ *  https://www.eclipse.org/legal/epl-2.0/
+ *
+ *  SPDX-License-Identifier: EPL-2.0
+ *
+ *  Contributors:
+ *     Julian Honnen <julian.honnen@vector.com> - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.pde.junit.runtime.tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ JUnitExecutionTest.class })
+public class JUnitRuntimeTests {
+
+}
