eclipse-pde/eclipse.pde

commit 345c700637dea3a8f7b1029f2fdb393e842802e3

Bug 558239 - Replace usage of VersionRange#getMaximum by #getRight
Includes version update for 4.15

Change-Id: Ib973da58114bff9d62c75d64359c6218202a0391
Signed-off-by: Karsten Thoms <karsten.thoms@itemis.de>

org.eclipse.pde.build/src/org/eclipse/pde/internal/build/Utils.java
@@ -139,7 +139,7 @@ public final class Utils implements IPDEBuildConstants, IBuildPropertiesConstant
 			return new VersionRange(version, true, upper, false);
 		}
 		if (match.equals("greaterOrEqual")) //$NON-NLS-1$
-			return new VersionRange(version, true, new VersionRange(null).getMaximum(), true);
+			return new VersionRange(version, true, new VersionRange(null).getRight(), true);
 
 		return VersionRange.emptyRange;
 	}

org.eclipse.pde.build/src/org/eclipse/pde/internal/build/site/ReachablePlugin.java
@@ -55,7 +55,7 @@ public class ReachablePlugin implements Comparable<Object> {
 			if (result != 0)
 				return result;
 			//We want the object with the widest version range to sort first
-			result = substract(toCompare.range.getMaximum(), toCompare.range.getMinimum()).compareTo(substract(range.getMaximum(), range.getMinimum()));
+			result = substract(toCompare.range.getRight(), toCompare.range.getMinimum()).compareTo(substract(range.getRight(), range.getMinimum()));
 			if (result != 0)
 				return result;
 			if (range.getIncludeMaximum() && !toCompare.range.getIncludeMaximum())
@@ -64,7 +64,7 @@ public class ReachablePlugin implements Comparable<Object> {
 				return 1;
 			if (this.equals(o))
 				return 0;
-			result = range.getMinimum().compareTo(toCompare.range.getMaximum());
+			result = range.getMinimum().compareTo(toCompare.range.getRight());
 			if (result != 0)
 				return result;
 			//Give up
@@ -110,7 +110,7 @@ public class ReachablePlugin implements Comparable<Object> {
 				return false;
 			if (range.getIncludeMaximum() != toCompare.range.getIncludeMaximum())
 				return false;
-			return range.getMinimum().equals(toCompare.range.getMinimum()) && range.getMaximum().equals(toCompare.range.getMaximum());
+			return range.getMinimum().equals(toCompare.range.getMinimum()) && range.getRight().equals(toCompare.range.getRight());
 		}
 		return false;
 	}

org.eclipse.pde.build.tests/META-INF/MANIFEST.MF
@@ -2,7 +2,7 @@ Manifest-Version: 1.0
 Bundle-ManifestVersion: 2
 Bundle-Name: Tests Plug-in
 Bundle-SymbolicName: org.eclipse.pde.build.tests;singleton:=true
-Bundle-Version: 1.2.500.qualifier
+Bundle-Version: 1.2.600.qualifier
 Bundle-Activator: org.eclipse.pde.build.tests.Activator
 Export-Package: org.eclipse.pde.build.internal.tests;x-internal:=true,
  org.eclipse.pde.build.internal.tests.ant;x-internal:=true,

org.eclipse.pde.build.tests/pom.xml
@@ -18,7 +18,7 @@
   </parent>
   <groupId>org.eclipse.pde</groupId>
   <artifactId>org.eclipse.pde.build.tests</artifactId>
-  <version>1.2.500-SNAPSHOT</version>
+  <version>1.2.600-SNAPSHOT</version>
   <packaging>eclipse-test-plugin</packaging>
   <properties>
     <skipTests>true</skipTests>

org.eclipse.pde.build/META-INF/MANIFEST.MF
@@ -2,7 +2,7 @@ Manifest-Version: 1.0
 Bundle-ManifestVersion: 2
 Bundle-Name: %pluginName
 Bundle-SymbolicName: org.eclipse.pde.build;singleton:=true
-Bundle-Version: 3.10.600.qualifier
+Bundle-Version: 3.10.700.qualifier
 Bundle-ClassPath: pdebuild.jar
 Bundle-Activator: org.eclipse.pde.internal.build.BuildActivator
 Bundle-Vendor: %providerName

org.eclipse.pde.build/pom.xml
@@ -18,6 +18,6 @@
   </parent>
   <groupId>org.eclipse.pde</groupId>
   <artifactId>org.eclipse.pde.build</artifactId>
-  <version>3.10.600-SNAPSHOT</version>
+  <version>3.10.700-SNAPSHOT</version>
   <packaging>eclipse-plugin</packaging>
 </project>

commit fd4a96a9d063ba324a809e77ccca9dd894ae2d81

Bug 558239 - reverted change caused NPE in ReachablePlugin.compareTo
Original change from commit 345c700637dea3a8f7b1029f2fdb393e842802e3
caused 15 test fails in PDE build.

Change-Id: I4e7be178215e8ac1f37c736e1f3c074025b0a4ee
Signed-off-by: Andrey Loskutov <loskutov@gmx.de>

org.eclipse.pde.build/src/org/eclipse/pde/internal/build/Utils.java
@@ -139,7 +139,7 @@ public final class Utils implements IPDEBuildConstants, IBuildPropertiesConstant
 			return new VersionRange(version, true, upper, false);
 		}
 		if (match.equals("greaterOrEqual")) //$NON-NLS-1$
-			return new VersionRange(version, true, new VersionRange(null).getRight(), true);
+			return new VersionRange(version, true, new VersionRange(null).getMaximum(), true);
 
 		return VersionRange.emptyRange;
 	}

org.eclipse.pde.build/src/org/eclipse/pde/internal/build/site/ReachablePlugin.java
@@ -55,7 +55,7 @@ public class ReachablePlugin implements Comparable<Object> {
 			if (result != 0)
 				return result;
 			//We want the object with the widest version range to sort first
-			result = substract(toCompare.range.getRight(), toCompare.range.getMinimum()).compareTo(substract(range.getRight(), range.getMinimum()));
+			result = substract(toCompare.range.getMaximum(), toCompare.range.getMinimum()).compareTo(substract(range.getMaximum(), range.getMinimum()));
 			if (result != 0)
 				return result;
 			if (range.getIncludeMaximum() && !toCompare.range.getIncludeMaximum())
@@ -64,7 +64,7 @@ public class ReachablePlugin implements Comparable<Object> {
 				return 1;
 			if (this.equals(o))
 				return 0;
-			result = range.getMinimum().compareTo(toCompare.range.getRight());
+			result = range.getMinimum().compareTo(toCompare.range.getMaximum());
 			if (result != 0)
 				return result;
 			//Give up
@@ -110,7 +110,7 @@ public class ReachablePlugin implements Comparable<Object> {
 				return false;
 			if (range.getIncludeMaximum() != toCompare.range.getIncludeMaximum())
 				return false;
-			return range.getMinimum().equals(toCompare.range.getMinimum()) && range.getRight().equals(toCompare.range.getRight());
+			return range.getMinimum().equals(toCompare.range.getMinimum()) && range.getMaximum().equals(toCompare.range.getMaximum());
 		}
 		return false;
 	}