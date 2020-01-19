be32740f1ed36c70cee5a7a3a54d3f10323bf756
Vikas Chandra <Vikas.Chandra@in.ibm.com>
2020-01-19 17:24:08 +0530

    Bug 559235 - Overenthusiastic "One of the plug-in's dependencies requires an execution environment of JavaSE-1.8, hence this plug-in's execution environment should be at least JavaSE-1.8.
    Change-Id: I6459e822af61008e4a2c606f219e439de20cc729
    Signed-off-by: Vikas Chandra <Vikas.Chandra@in.ibm.com>

--- ui/org.eclipse.pde.core/src/org/eclipse/pde/internal/core/builders/BundleErrorReporter.java
+++ ui/org.eclipse.pde.core/src/org/eclipse/pde/internal/core/builders/BundleErrorReporter.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2019 IBM Corporation and others.
+ * Copyright (c) 2000, 2020 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -809,9 +809,14 @@ public class BundleErrorReporter extends JarManifestErrorReporter {
 		while (!bundleDescriptions.isEmpty()) {
 			BundleDescription dependencyDesc = bundleDescriptions.pop();
 			visitedBundles.add(dependencyDesc);
-			for (BundleDescription transitiveDependencyDesc : dependencyDesc.getResolvedRequires()) {
-				if (!visitedBundles.contains(transitiveDependencyDesc)) {
-				bundleDescriptions.push(transitiveDependencyDesc);
+			for (BundleSpecification transitiveDependencyDesc : dependencyDesc.getRequiredBundles()) {
+				if (transitiveDependencyDesc.isOptional()) {
+					continue;
+				}
+				if (!visitedBundles.contains(transitiveDependencyDesc.getSupplier())) {
+					if (transitiveDependencyDesc.getSupplier() instanceof BundleDescription) {
+						bundleDescriptions.push((BundleDescription) transitiveDependencyDesc.getSupplier());
+					}
 				}
 			}
 			try {
