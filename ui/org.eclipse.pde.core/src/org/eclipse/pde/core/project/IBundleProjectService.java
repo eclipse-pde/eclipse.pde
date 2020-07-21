6562448b8a25790ff37aeb5ae9c7697aaa266e8d
Alexander Kurtakov <akurtako@redhat.com>
2020-07-21 15:25:41 +0300

    Bug 565340 - Use ServiceCaller for org.eclipse.pde.ui
    Fix javadoc error:
    ../../../eclipse.pde.ui/ui/org.eclipse.pde.core/src/org/eclipse/pde/core/project/IBundleProjectService.java:31:
    error: bad use of '>'
     * ServiceCaller.callOnce(getClass(), IBundleContextService.class,
    (service) -> ...);
    
    
    Change-Id: Ib0a03c55f01c09352d9bbee5a04e550566b259eb
    Signed-off-by: Alexander Kurtakov <akurtako@redhat.com>

--- ui/org.eclipse.pde.core/src/org/eclipse/pde/core/project/IBundleProjectService.java
+++ ui/org.eclipse.pde.core/src/org/eclipse/pde/core/project/IBundleProjectService.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010, 2017 IBM Corporation and others.
+ * Copyright (c) 2010, 2020 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -28,7 +28,7 @@ import org.osgi.framework.Version;
  * </p>
  *
  * <pre>
- * ServiceCaller.callOnce(getClass(), IBundleContextService.class, (service) -> ...);
+ * ServiceCaller.callOnce(getClass(), IBundleContextService.class, (service) -&gt; ...);
  * </pre>
  *
  * @since 3.6
