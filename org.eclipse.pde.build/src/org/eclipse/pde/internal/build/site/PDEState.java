c9b3ca28512f89b64b99cce536b207698dc422b9
Vikas Chandra <Vikas.Chandra@in.ibm.com>
2021-09-13 13:00:28 +0530

    Bug 575940 - [17] Remove JCP disclaimers and BETA from code in PDE
    Change-Id: Iaa8396c4248b2fc906584445936a36c2015b44d5
    Signed-off-by: Vikas Chandra <Vikas.Chandra@in.ibm.com>
    Reviewed-on: https://git.eclipse.org/r/c/pde/eclipse.pde.build/+/185352

--- org.eclipse.pde.build/src/org/eclipse/pde/internal/build/site/PDEState.java
+++ org.eclipse.pde.build/src/org/eclipse/pde/internal/build/site/PDEState.java
@@ -1,20 +1,15 @@
 /*******************************************************************************
  *  Copyright (c) 2004, 2021 IBM Corporation and others.
  *
- *  This program and the accompanying materials
- *  are made available under the terms of the Eclipse Public License 2.0
- *  which accompanies this distribution, and is available at
- *  https://www.eclipse.org/legal/epl-2.0/
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
  *
- *  SPDX-License-Identifier: EPL-2.0
- * 
- *  Contributors:
- *      IBM Corporation - initial API and implementation
- *      
- * This is an implementation of an early-draft specification developed under the Java
- * Community Process (JCP) and is made available for testing and evaluation purposes
- * only. The code is not compatible with any specification of the JCP.
+ * SPDX-License-Identifier: EPL-2.0
  *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
  *******************************************************************************/
 package org.eclipse.pde.internal.build.site;
 
