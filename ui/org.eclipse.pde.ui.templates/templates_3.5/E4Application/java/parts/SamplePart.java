9286b26d273a8159f92816caa0e9f3159f5ee80b
Lars Vogel <Lars.Vogel@vogella.com>
2021-09-30 18:12:45 +0200

    Bug 576231 - Update e4 RCP templates
    Usage of TextFactory to create a Text widget
    
    Change-Id: Ibaf20d5d4bba76e0534e659b6ba276fcf073e9f2
    Signed-off-by: Lars Vogel <Lars.Vogel@vogella.com>
    Reviewed-on: https://git.eclipse.org/r/c/pde/eclipse.pde.ui/+/186002
    Tested-by: PDE Bot <pde-bot@eclipse.org>

--- ui/org.eclipse.pde.ui.templates/templates_3.5/E4Application/java/parts/SamplePart.java
+++ ui/org.eclipse.pde.ui.templates/templates_3.5/E4Application/java/parts/SamplePart.java
@@ -10,11 +10,11 @@ import org.eclipse.e4.ui.di.Persist;
 import org.eclipse.e4.ui.model.application.ui.basic.MPart;
 import org.eclipse.jface.viewers.ArrayContentProvider;
 import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.jface.widgets.TextFactory;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Text;
 
 public class SamplePart {
 
@@ -27,10 +27,11 @@ public class SamplePart {
 	public void createComposite(Composite parent) {
 		parent.setLayout(new GridLayout(1, false));
 
-		Text txtInput = new Text(parent, SWT.BORDER);
-		txtInput.setMessage("Enter text to mark part as dirty");
-		txtInput.addModifyListener(e -> part.setDirty(true));
-		txtInput.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		TextFactory.newText(SWT.BORDER) //
+				.message("Enter text to mark part as dirty") //
+				.onModify(e -> part.setDirty(true)) //
+				.layoutData(new GridData(GridData.FILL_HORIZONTAL))//
+				.create(parent);
 
 		tableViewer = new TableViewer(parent);
 
@@ -48,7 +49,7 @@ public class SamplePart {
 	public void save() {
 		part.setDirty(false);
 	}
-	
+
 	private List<String> createInitialDataModel() {
 		return Arrays.asList("Sample item 1", "Sample item 2", "Sample item 3", "Sample item 4", "Sample item 5");
 	}
