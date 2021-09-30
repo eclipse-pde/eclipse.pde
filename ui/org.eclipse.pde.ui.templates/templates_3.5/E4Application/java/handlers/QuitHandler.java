b9f14497133cdc27252fa9990b4766f1b0263835
Lars Vogel <Lars.Vogel@vogella.com>
2021-09-30 18:15:36 +0200

    Bug 576231 - Update e4 RCP templates
    Uses PlainMessageDialog for the quit handler.
    
    Change-Id: Iab604bab060574893fa900ac52fab190f1172cae
    Signed-off-by: Lars Vogel <Lars.Vogel@vogella.com>
    Reviewed-on: https://git.eclipse.org/r/c/pde/eclipse.pde.ui/+/186003
    Tested-by: PDE Bot <pde-bot@eclipse.org>

--- ui/org.eclipse.pde.ui.templates/templates_3.5/E4Application/java/handlers/QuitHandler.java
+++ ui/org.eclipse.pde.ui.templates/templates_3.5/E4Application/java/handlers/QuitHandler.java
@@ -1,16 +1,20 @@
 package $packageName$.handlers;
 
+import java.util.List;
+
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.ui.workbench.IWorkbench;
-import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.dialogs.PlainMessageDialog;
 import org.eclipse.swt.widgets.Shell;
-
 
 public class QuitHandler {
 	@Execute
-	public void execute(IWorkbench workbench, Shell shell){
-		if (MessageDialog.openConfirm(shell, "Confirmation",
-				"Do you want to exit?")) {
+	public void execute(IWorkbench workbench, Shell shell) {
+
+		PlainMessageDialog dialog = PlainMessageDialog.getBuilder(shell, "Confirmation")
+				.message("Do you want to exit?").buttonLabels(List.of("Exit", "Cancel")).build();
+
+		if (dialog.open() == 0) {
 			workbench.close();
 		}
 	}
