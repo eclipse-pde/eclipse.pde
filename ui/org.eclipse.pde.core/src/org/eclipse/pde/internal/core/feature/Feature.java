aac67bb3999ec2ce28742e10b2d9152521d7e141
Christoph Läubrich <laeubi@laeubi-soft.de>
2021-10-13 15:13:52 +0200

    Bug 576606 - Feature.hasRequiredAttributes() should be protected
    make Feature.hasRequiredAttributes protected to allow reuse by
    extensions
    
    Change-Id: I39d6cdddc3d3c0e233f3a705264f862949930da8
    Signed-off-by: Christoph Läubrich <laeubi@laeubi-soft.de>
    Reviewed-on: https://git.eclipse.org/r/c/pde/eclipse.pde.ui/+/186434
    Tested-by: PDE Bot <pde-bot@eclipse.org>
    Tested-by: Vikas Chandra <Vikas.Chandra@in.ibm.com>
    Reviewed-by: Vikas Chandra <Vikas.Chandra@in.ibm.com>
    Reviewed-by: Alexander Fedorov <alexander.fedorov@arsysop.ru>

--- ui/org.eclipse.pde.core/src/org/eclipse/pde/internal/core/feature/Feature.java
+++ ui/org.eclipse.pde.core/src/org/eclipse/pde/internal/core/feature/Feature.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2014 IBM Corporation and others.
+ * Copyright (c) 2000, 2021 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -766,7 +766,7 @@ public class Feature extends VersionableObject implements IFeature {
 		return fValid;
 	}
 
-	private boolean hasRequiredAttributes() {
+	protected boolean hasRequiredAttributes() {
 		// Verify that all the required attributes are
 		// defined.
 		if (id == null) {
