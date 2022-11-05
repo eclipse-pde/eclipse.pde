e55a4ccecdfc33e438a01ae4a8630305c1fd9b8a
Christoph Läubrich <laeubi@laeubi-soft.de>
2022-11-05 14:11:43 +0100

    Test adding an interface and an impl

---
+++ ui/org.eclipse.pde.core/src/org/eclipse/pde/core/ANewInterface.java
@@ -0,0 +1,8 @@
+package org.eclipse.pde.core;
+
+/**
+ * @since 3.16
+ */
+public interface ANewInterface {
+
+}
---
+++ ui/org.eclipse.pde.core/src/org/eclipse/pde/internal/core/ANewClass.java
@@ -0,0 +1,7 @@
+package org.eclipse.pde.internal.core;
+
+import org.eclipse.pde.core.ANewInterface;
+
+public class ANewClass implements ANewInterface {
+
+}
