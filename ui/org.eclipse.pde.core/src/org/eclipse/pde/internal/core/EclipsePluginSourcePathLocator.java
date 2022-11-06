1e6c073e12af1ee4b18e08aaf3b7ffe4ea2bbcad
Christoph Läubrich <laeubi@laeubi-soft.de>
2022-11-06 14:47:45 +0100

    Add support for dynamically contribute bundle source locations (#379)
    * Add support for dynamically contribute bundle source locations
    
    Currently a plugin can contribute source locations inside the plugin
    itself through the extension point
    
    org.eclipse.pde.core.source
    
    this has the limitation that the source must be embedded and known in
    advance. In some situations it would be required to provide external
    locations dynamically e.g. maven has the concept of source artifacts
    that could be queried, if a bundle contains a Bundle-Source-Location one
    could checkout one could have a "source-repository" and so on.
    
    This adds the necessary parts for a new extension point
    
    org.eclipse.pde.core.dynamicSource
    
    and a extracts the implementation that search for Eclipse/Maven
    co-located source bundles.
    
    Part of #202
    
    * [FIXUP] adjustments for "support for dynamically contribute bundle
    source locations"
    
    Co-authored-by: Hannes Wellmann <wellmann.hannes1@gmx.net>

---
+++ ui/org.eclipse.pde.core/src/org/eclipse/pde/internal/core/EclipsePluginSourcePathLocator.java
@@ -0,0 +1,43 @@
+/*******************************************************************************
+ * Copyright (c) 2022 Christoph Läubrich and others.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Christoph Läubrich - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.pde.internal.core;
+
+import java.io.File;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.pde.core.IPluginSourcePathLocator;
+import org.eclipse.pde.core.plugin.IPluginBase;
+
+/**
+ * A plugin source path locator that uses for a co-located source bundle in the
+ * same directory. This could be e.g the case for bundle pools or products with
+ * a plugin folder.
+ */
+public class EclipsePluginSourcePathLocator implements IPluginSourcePathLocator {
+
+	@Override
+	public IPath locateSource(IPluginBase plugin) {
+		String installLocation = plugin.getModel().getInstallLocation();
+		if (installLocation != null) {
+			// For example in a bundle pool foo_123.jar => foo.source_123.jar...
+			File container = new File(installLocation).getParentFile();
+			File sourceFile = new File(container, plugin.getId() + ".source_" + plugin.getVersion() + ".jar"); //$NON-NLS-1$ //$NON-NLS-2$
+			if (sourceFile.isFile()) {
+				return new Path(sourceFile.getAbsolutePath());
+			}
+		}
+		return null;
+	}
+
+}
