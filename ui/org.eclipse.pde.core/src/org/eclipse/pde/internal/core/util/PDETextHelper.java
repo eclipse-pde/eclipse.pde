d94ede3187b964f7c9b7df18d6c78fa7259a3bd1
Александър Куртаков <akurtakov@gmail.com>
2022-11-08 13:11:08 +0200

    Simplify PDETextHelper
    * if (boolean == false) and if(boolean ==true) are weird at least
    * Use String.isEmpty

--- ui/org.eclipse.pde.core/src/org/eclipse/pde/internal/core/util/PDETextHelper.java
+++ ui/org.eclipse.pde.core/src/org/eclipse/pde/internal/core/util/PDETextHelper.java
@@ -43,7 +43,7 @@ public class PDETextHelper {
 	}
 
 	public static boolean isDefined(String text) {
-		if ((text == null) || (text.length() == 0)) {
+		if ((text == null) || (text.isEmpty())) {
 			return false;
 		}
 		return true;
@@ -54,7 +54,7 @@ public class PDETextHelper {
 			return false;
 		}
 		String trimmedText = text.trim();
-		if (trimmedText.length() == 0) {
+		if (trimmedText.isEmpty()) {
 			return false;
 		}
 		return true;
@@ -96,7 +96,7 @@ public class PDETextHelper {
 				previousChar = currentChar;
 			}
 		}
-		String result = buffer.toString(); //$NON-NLS-1$
+		String result = buffer.toString();
 		if (PDEHTMLHelper.isAllWhitespace(result)) {
 			return ""; //$NON-NLS-1$
 		}
@@ -115,14 +115,14 @@ public class PDETextHelper {
 		// Process tag exceptions if provided
 		boolean processTagExceptions = false;
 		int scanLimit = 0;
-		if ((tagExceptions != null) && (tagExceptions.isEmpty() == false)) {
+		if ((tagExceptions != null) && (!tagExceptions.isEmpty())) {
 			processTagExceptions = true;
 			// Use the biggest entry in the set as the limit
 			scanLimit = determineMaxLength(tagExceptions);
 		}
 		// Process substitute characters if provided
 		boolean processSubstituteChars = false;
-		if ((substituteChars != null) && (substituteChars.isEmpty() == false)) {
+		if ((substituteChars != null) && (!substituteChars.isEmpty())) {
 			processSubstituteChars = true;
 		}
 		// Translated buffer
@@ -135,19 +135,19 @@ public class PDETextHelper {
 			// If we are processing tag exceptions, check to see if this
 			// character is part of a tag exception and process it accordingly
 			// if it is
-			if ((processed == false) && (processTagExceptions == true)) {
-				processed = processTagExceptions(currentChar, substituteChars, tagExceptions, buffer, scanLimit, text, index);
+			if (!processed && processTagExceptions) {
+				processed = processTagExceptions(currentChar, substituteChars, tagExceptions, buffer, text, index);
 			}
 			// If the character was not part of a tag exception and we are
 			// processing substitution characters, check to see if this
 			// character needs to be translated and process it accordingly if
 			// it is
-			if ((processed == false) && (processSubstituteChars == true)) {
+			if (!processed && processSubstituteChars) {
 				processed = processSubstituteChars(currentChar, substituteChars, buffer);
 			}
 			// If the character did not need to be translated, just append it
 			// as is to the buffer
-			if (processed == false) {
+			if (!processed) {
 				buffer.append(currentChar);
 			}
 		}
@@ -171,7 +171,7 @@ public class PDETextHelper {
 	}
 
 	private static boolean processTagExceptions(char currentChar, Map<Character, String> substituteChars,
-			Set<String> tagExceptions, StringBuilder buffer, int scanLimit, String text, IntegerPointer index) {
+			Set<String> tagExceptions, StringBuilder buffer, String text, IntegerPointer index) {
 		// If the current character is an open angle bracket, then it may be
 		// part of a valid tag exception
 		if (currentChar == '<') {
@@ -218,7 +218,7 @@ public class PDETextHelper {
 		// It may contain trailing spaces or a trailing '/'
 		String attributeList = text.substring(tagName.length());
 		// If the attribute list is not valid, discard the attribute list
-		if ((isValidTagAttributeList(attributeList) == false)) {
+		if (!isValidTagAttributeList(attributeList)) {
 			buffer.append('<');
 			buffer.append(tagName);
 			// Since, this tag has an attribute list and we are discarding it,
@@ -228,7 +228,7 @@ public class PDETextHelper {
 			}
 			buffer.append('>');
 			return;
-		} else if (attributeList.length() == 0) {
+		} else if (attributeList.isEmpty()) {
 			// If the tag has no attribute list then just return the tag
 			// as is (trailing slash is already including in the tag name)
 			buffer.append('<');
@@ -263,7 +263,7 @@ public class PDETextHelper {
 			}
 			// If the character did not require translation, just append it
 			// as-is
-			if (processed == false) {
+			if (!processed) {
 				buffer.append(currentChar);
 			}
 		}
