021a4d7d902c72a273b13ced3909d0f539f60f2e
Hannes Wellmann <wellmann.hannes1@gmx.net>
2023-04-30 00:07:40 +0200

    Simplify E4-project wizards and templates

--- ui/org.eclipse.pde.ui.templates/src/org/eclipse/pde/internal/ui/templates/e4/E4ViewTemplate.java
+++ ui/org.eclipse.pde.ui.templates/src/org/eclipse/pde/internal/ui/templates/e4/E4ViewTemplate.java
@@ -14,7 +14,7 @@
 
 package org.eclipse.pde.internal.ui.templates.e4;
 
-import java.util.ArrayList;
+import java.util.List;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.jface.wizard.Wizard;
@@ -22,11 +22,19 @@ import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.pde.core.plugin.*;
 import org.eclipse.pde.internal.ui.templates.*;
 import org.eclipse.pde.ui.IFieldData;
-import org.eclipse.pde.ui.templates.PluginReference;
 
 public class E4ViewTemplate extends PDETemplateSection {
 
 	static final String E4_FRAGMENT_FILE = "fragment.e4xmi"; //$NON-NLS-1$
+
+	private static final List<String> REQUIRED_BUNDLES = List.of(//
+			"javax.inject", //$NON-NLS-1$
+			"org.eclipse.osgi", //$NON-NLS-1$
+			"org.eclipse.jface", //$NON-NLS-1$
+			"org.eclipse.e4.ui.model.workbench", //$NON-NLS-1$
+			"org.eclipse.e4.ui.di", //$NON-NLS-1$
+			"org.eclipse.e4.ui.services", //$NON-NLS-1$
+			"org.eclipse.e4.core.di.annotations"); //$NON-NLS-1$
 
 	/**
 	 * Constructor for HelloWorldTemplate.
@@ -53,7 +61,6 @@ public class E4ViewTemplate extends PDETemplateSection {
 		addOption("className", PDETemplateMessages.E4ViewTemplate_className, "SampleView", 0); //$NON-NLS-1$ //$NON-NLS-2$
 		addOption("viewName", PDETemplateMessages.E4ViewTemplate_name, PDETemplateMessages.E4ViewTemplate_defaultName, 0); //$NON-NLS-1$
 		addOption("viewCategoryName", PDETemplateMessages.E4ViewTemplate_categoryName, PDETemplateMessages.E4ViewTemplate_defaultCategoryName, 0); //$NON-NLS-1$
-
 	}
 
 	@Override
@@ -61,7 +68,6 @@ public class E4ViewTemplate extends PDETemplateSection {
 		// In a new project wizard, we don't know this yet - the
 		// model has not been created
 		initializeFields(data.getId());
-
 	}
 
 	@Override
@@ -90,8 +96,6 @@ public class E4ViewTemplate extends PDETemplateSection {
 		markPagesAdded();
 	}
 
-
-
 	@Override
 	public String getUsedExtensionPoint() {
 		return "org.eclipse.e4.workbench.model"; //$NON-NLS-1$
@@ -99,9 +103,7 @@ public class E4ViewTemplate extends PDETemplateSection {
 
 	@Override
 	protected void updateModel(IProgressMonitor monitor) throws CoreException {
-
 		createE4ModelExtension();
-
 	}
 
 	private void createE4ModelExtension() throws CoreException {
@@ -121,7 +123,6 @@ public class E4ViewTemplate extends PDETemplateSection {
 			plugin.add(extension);
 	}
 
-
 	@Override
 	public String[] getNewFiles() {
 		return new String[] { "icons/", E4_FRAGMENT_FILE }; //$NON-NLS-1$
@@ -129,23 +130,7 @@ public class E4ViewTemplate extends PDETemplateSection {
 
 	@Override
 	public IPluginReference[] getDependencies(String schemaVersion) {
-
-
-		ArrayList<PluginReference> result = new ArrayList<>();
-		final int matchRule = IMatchRules.GREATER_OR_EQUAL;
-
-		result.add(new PluginReference("javax.inject", null, matchRule)); //$NON-NLS-1$
-		result.add(new PluginReference("org.eclipse.osgi", null, matchRule)); //$NON-NLS-1$
-		result.add(new PluginReference("org.eclipse.jface", null, matchRule)); //$NON-NLS-1$
-		result.add(new PluginReference("org.eclipse.e4.ui.model.workbench", null, matchRule)); //$NON-NLS-1$
-		result.add(new PluginReference("org.eclipse.e4.ui.di", null, matchRule)); //$NON-NLS-1$
-		result.add(new PluginReference("org.eclipse.e4.ui.services", null, matchRule)); //$NON-NLS-1$
-		result.add(new PluginReference("org.eclipse.e4.core.di.annotations", null, matchRule)); //$NON-NLS-1$
-
-		return result.toArray(new IPluginReference[result.size()]);
-
+		return AbstractE4NewPluginTemplateWizard.createPluginReferences(REQUIRED_BUNDLES);
 	}
-
-
 
 }
