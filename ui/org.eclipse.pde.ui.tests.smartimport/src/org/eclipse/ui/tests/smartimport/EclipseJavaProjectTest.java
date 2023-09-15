e6df1b5d1234d453535051281e987c52f1b94468
Александър Куртаков <akurtakov@gmail.com>
2023-09-15 19:24:13 +0300

    Rewrite smartimport test as plain JUnit test
    This removes RedDeer dependency which has been announced to drop from
    simrel.

--- ui/org.eclipse.pde.ui.tests.smartimport/src/org/eclipse/ui/tests/smartimport/EclipseJavaProjectTest.java
+++ ui/org.eclipse.pde.ui.tests.smartimport/src/org/eclipse/ui/tests/smartimport/EclipseJavaProjectTest.java
@@ -14,19 +14,16 @@
 package org.eclipse.ui.tests.smartimport;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.ui.tests.smartimport.plugins.ImportedProject;
-import org.eclipse.ui.tests.smartimport.plugins.ProjectProposal;
 
 public class EclipseJavaProjectTest extends ProjectTestTemplate {
+
+	private static final String JAVA_ECLIPSE_PROJECT = "JavaEclipseProject";
 
 	@Override
 	File getProjectPath() {
@@ -34,35 +31,15 @@ public class EclipseJavaProjectTest extends ProjectTestTemplate {
 	}
 
 	@Override
-	List<ProjectProposal> getExpectedProposals() {
-		ArrayList<ProjectProposal> returnList = new ArrayList<>();
-		ProjectProposal projectProposal = new ProjectProposal("JavaEclipseProject");
-		projectProposal.addImportAs("Eclipse project");
-		returnList.add(projectProposal);
-		return returnList;
-	}
-
-	@Override
-	List<ImportedProject> getExpectedImportedProjects() {
-		ArrayList<ImportedProject> returnList = new ArrayList<>();
-		ImportedProject project = new ImportedProject("JavaEclipseProject", "");
-		project.addImportedAs("Eclipse project");
-		returnList.add(project);
-		return returnList;
-	}
-
-
-	@Override
-	void checkImportedProject() {
-		IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject("JavaEclipseProject");
-		String[] natureIds = {};
-		try {
-			natureIds = project.getDescription().getNatureIds();
-		} catch (CoreException e) {
-			e.printStackTrace();
-			fail();
-		}
+	void checkImportedProject() throws CoreException {
+		IProject project = getProject();
+		String[] natureIds = project.getDescription().getNatureIds();
 		assertEquals("Project should have exactly 1 nature", 1, natureIds.length);
 		assertEquals("Project should have java nature", "org.eclipse.jdt.core.javanature", natureIds[0]);
+	}
+
+	@Override
+	IProject getProject() {
+		return ResourcesPlugin.getWorkspace().getRoot().getProject(JAVA_ECLIPSE_PROJECT);
 	}
 }
