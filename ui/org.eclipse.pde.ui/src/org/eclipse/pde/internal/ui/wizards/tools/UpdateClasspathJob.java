ed2f2c93fb07076b0e35bdf5ef5e224a25f07986
Hannes Wellmann <wellmann.hannes1@gmx.net>
2023-09-21 23:16:59 +0200

    Simplify UpdateClasspathJob
    Leverage the implicit cancellation checks and progress reporting of
    SubMonitor.split()

--- ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/wizards/tools/UpdateClasspathJob.java
+++ ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/wizards/tools/UpdateClasspathJob.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- *  Copyright (c) 2000, 2015 IBM Corporation and others.
+ *  Copyright (c) 2000, 2023 IBM Corporation and others.
  *
  *  This program and the accompanying materials
  *  are made available under the terms of the Eclipse Public License 2.0
@@ -10,95 +10,73 @@
  *
  *  Contributors:
  *     IBM Corporation - initial API and implementation
+ *     Hannes Wellmann - Simplify UpdateClasspathJob and leverage the implicit cancellation checks and progress reporting of SubMonitor.split()
  *******************************************************************************/
 package org.eclipse.pde.internal.ui.wizards.tools;
+
+import java.util.List;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IProjectDescription;
 import org.eclipse.core.resources.IResource;
-import org.eclipse.core.resources.IWorkspaceRunnable;
+import org.eclipse.core.resources.WorkspaceJob;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.SubMonitor;
 import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.pde.core.plugin.IPluginModelBase;
 import org.eclipse.pde.internal.core.ClasspathComputer;
 import org.eclipse.pde.internal.core.builders.PDEMarkerFactory;
-import org.eclipse.pde.internal.ui.IPDEUIConstants;
 import org.eclipse.pde.internal.ui.PDEPlugin;
 import org.eclipse.pde.internal.ui.PDEUIMessages;
 
-public class UpdateClasspathJob extends Job {
-	IPluginModelBase[] fModels;
-
-	public UpdateClasspathJob(IPluginModelBase[] models) {
-		super(PDEUIMessages.UpdateClasspathJob_title);
-		setPriority(Job.LONG);
-		fModels = models;
+public class UpdateClasspathJob {
+	private UpdateClasspathJob() {
 	}
 
-	public boolean doUpdateClasspath(IProgressMonitor monitor, IPluginModelBase[] models) throws CoreException {
-		monitor.beginTask(PDEUIMessages.UpdateClasspathJob_task, models.length);
-		try {
-			for (IPluginModelBase model : models) {
-				monitor.subTask(model.getPluginBase().getId());
-				// no reason to compile classpath for a non-Java model
-				IProject project = model.getUnderlyingResource().getProject();
-				if (!project.hasNature(JavaCore.NATURE_ID)) {
-					monitor.worked(1);
-					continue;
+	public static Job scheduleFor(List<IPluginModelBase> models, boolean user) {
+		WorkspaceJob job = new WorkspaceJob(PDEUIMessages.UpdateClasspathJob_title) {
+			@Override
+			public IStatus runInWorkspace(IProgressMonitor monitor) throws CoreException {
+				SubMonitor mon = SubMonitor.convert(monitor, PDEUIMessages.UpdateClasspathJob_task, models.size());
+				for (IPluginModelBase model : models) {
+					updateClasspath(model, mon.split(1));
 				}
-				IProjectDescription projDesc = project.getDescription();
-				if (projDesc == null)
-					continue;
-				projDesc.setReferencedProjects(new IProject[0]);
-				project.setDescription(projDesc, null);
-				IFile file = project.getFile(".project"); //$NON-NLS-1$
-				if (file.exists())
-					file.deleteMarkers(PDEMarkerFactory.MARKER_ID, true, IResource.DEPTH_ZERO);
-				ClasspathComputer.setClasspath(project, model);
-				monitor.worked(1);
-				if (monitor.isCanceled())
-					return false;
+				return Status.OK_STATUS;
 			}
-		} finally {
-			monitor.done();
-		}
-		return true;
+		};
+		job.setUser(user);
+		job.setPriority(Job.LONG);
+		job.schedule();
+		return job;
 	}
 
-	class UpdateClasspathWorkspaceRunnable implements IWorkspaceRunnable {
-		boolean fCanceled = false;
-
-		@Override
-		public void run(IProgressMonitor monitor) throws CoreException {
-			fCanceled = !doUpdateClasspath(monitor, fModels);
-		}
-
-		public boolean isCanceled() {
-			return fCanceled;
-		}
-	}
-
-	@Override
-	protected IStatus run(IProgressMonitor monitor) {
+	private static void updateClasspath(IPluginModelBase model, SubMonitor monitor) throws CoreException {
 		try {
-			UpdateClasspathWorkspaceRunnable runnable = new UpdateClasspathWorkspaceRunnable();
-			PDEPlugin.getWorkspace().run(runnable, monitor);
-			if (runnable.isCanceled()) {
-				return new Status(IStatus.CANCEL, IPDEUIConstants.PLUGIN_ID, IStatus.CANCEL, "", null); //$NON-NLS-1$
+			monitor.subTask(model.getPluginBase().getId());
+			// no reason to compile classpath for a non-Java model
+			IProject project = model.getUnderlyingResource().getProject();
+			if (project.hasNature(JavaCore.NATURE_ID)) {
+				IProjectDescription projDesc = project.getDescription();
+				if (projDesc != null) {
+					projDesc.setReferencedProjects(new IProject[0]);
+					project.setDescription(projDesc, null);
+					IFile file = project.getFile(".project"); //$NON-NLS-1$
+					if (file.exists()) {
+						file.deleteMarkers(PDEMarkerFactory.MARKER_ID, true, IResource.DEPTH_ZERO);
+					}
+					ClasspathComputer.setClasspath(project, model);
+				}
 			}
-
 		} catch (CoreException e) {
-			String title = PDEUIMessages.UpdateClasspathJob_error_title;
 			String message = PDEUIMessages.UpdateClasspathJob_error_message;
-			PDEPlugin.logException(e, title, message);
-			return Status.error(message, e);
+			PDEPlugin.logException(e, PDEUIMessages.UpdateClasspathJob_error_title, message);
+			throw new CoreException(Status.error(message, e));
 		}
-		return Status.OK_STATUS;
 	}
 
 }
