93c55c663be5643c6da3449489d7c9dc03dbe4b6
Christoph Läubrich <laeubi@laeubi-soft.de>
2023-10-28 13:27:08 +0200

    Use ExtensionBasedTextEditor as the base instead of TextEditor
    Actually all PDE source pages can benefit from Generic Editor also some
    functionality relies on the PDESourcePage as a base.
    
    This makes the PDESourcePage extend the ExtensionBasedTextEditor to
    account for this.

--- ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/editor/bnd/BndSourcePage.java
+++ ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/editor/bnd/BndSourcePage.java
@@ -13,101 +13,12 @@
  *******************************************************************************/
 package org.eclipse.pde.internal.ui.editor.bnd;
 
-import org.eclipse.core.resources.IMarker;
-import org.eclipse.pde.internal.ui.IHelpContextIds;
+import org.eclipse.pde.internal.ui.editor.GenericSourcePage;
 import org.eclipse.pde.internal.ui.editor.PDEFormEditor;
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Control;
-import org.eclipse.ui.PlatformUI;
-import org.eclipse.ui.forms.IManagedForm;
-import org.eclipse.ui.forms.editor.FormEditor;
-import org.eclipse.ui.forms.editor.IFormPage;
-import org.eclipse.ui.ide.IDE;
 
-@SuppressWarnings("restriction")
-public class BndSourcePage extends org.eclipse.ui.internal.genericeditor.ExtensionBasedTextEditor implements IFormPage {
-
-	private FormEditor editor;
-	private boolean active;
-	private int index;
-	private String id;
-	private Control fControl;
+public class BndSourcePage extends GenericSourcePage {
 
 	public BndSourcePage(PDEFormEditor editor, String id, String title) {
-		this.id = id;
+		super(editor, id, title);
 	}
-
-	@Override
-	public void initialize(FormEditor editor) {
-		this.editor = editor;
-	}
-
-	@Override
-	public FormEditor getEditor() {
-		return editor;
-	}
-
-	@Override
-	public IManagedForm getManagedForm() {
-		return null;
-	}
-
-	@Override
-	public void setActive(boolean active) {
-		this.active = active;
-	}
-
-	@Override
-	public boolean isActive() {
-		return active;
-	}
-
-	@Override
-	public boolean canLeaveThePage() {
-		return true;
-	}
-
-	@Override
-	public Control getPartControl() {
-		return fControl;
-	}
-
-	@Override
-	public void createPartControl(Composite parent) {
-		super.createPartControl(parent);
-		Control[] children = parent.getChildren();
-		fControl = children[children.length - 1];
-		PlatformUI.getWorkbench().getHelpSystem().setHelp(fControl, IHelpContextIds.MANIFEST_SOURCE_PAGE);
-	}
-
-	@Override
-	public String getId() {
-		return id;
-	}
-
-	@Override
-	public int getIndex() {
-		return index;
-	}
-
-	@Override
-	public void setIndex(int index) {
-		this.index = index;
-	}
-
-	@Override
-	public boolean isEditor() {
-		return true;
-	}
-
-	@Override
-	public boolean selectReveal(Object object) {
-		if (object instanceof IMarker) {
-			IDE.gotoMarker(this, (IMarker) object);
-			return true;
-		}
-		return false;
-	}
-
-
 }
