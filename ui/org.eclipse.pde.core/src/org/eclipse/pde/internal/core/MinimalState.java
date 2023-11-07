46b606440ed6467e155b89f0a60d42eb891622fb
Dinesh0723 <palanisamy.dinesh@in.bosch.com>
2023-11-07 10:27:57 +0530

    Revert "Extract IPluginModelBase.getInstallLocation() expression in MinimalState"
    This reverts commit 443b6e828c5eba2c429ef2c0abd09b640b3af0ba.

--- ui/org.eclipse.pde.core/src/org/eclipse/pde/internal/core/MinimalState.java
+++ ui/org.eclipse.pde.core/src/org/eclipse/pde/internal/core/MinimalState.java
@@ -105,8 +105,7 @@ public class MinimalState {
 		BundleDescription desc = model.getBundleDescription();
 		long bundleId = desc == null || !update ? -1 : desc.getBundleId();
 		try {
-			String installLocation = model.getInstallLocation();
-			if (installLocation == null) {
+			if (model.getInstallLocation() == null) {
 				// This exception should help diagnosing the problem. If this
 				// exception wasn't thrown then the code would throw a NPE in
 				// the next line anyway.
@@ -114,7 +113,7 @@ public class MinimalState {
 						"The plugin '" + model + "' was not created from a resource in the file system"); //$NON-NLS-1$ //$NON-NLS-2$
 			}
 
-			File bundleLocation = new File(installLocation);
+			File bundleLocation = new File(model.getInstallLocation());
 			BundleDescription newDesc = addBundle(bundleLocation, bundleId,
 					loadWorkspaceBundleManifest(bundleLocation, model.getUnderlyingResource()));
 			model.setBundleDescription(newDesc);
