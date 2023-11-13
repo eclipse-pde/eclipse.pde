c7a72b44afe25b5c99ad62d24040a84a3b0cbf3d
Christoph Läubrich <laeubi@laeubi-soft.de>
2023-11-13 18:17:40 +0100

    Import classes from bndtools and add headers restored from git history

---
+++ ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/bndtools/RepositoryTargetLocationWizard.java
@@ -0,0 +1,59 @@
+/*******************************************************************************
+ * Copyright (c) 2017, 2019 bndtools project and others.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Elias N Vasylenko <eliasvasylenko@gmail.com> - initial API and implementation
+ *     BJ Hargrave <bj@bjhargrave.com> - ongoing enhancements
+ *******************************************************************************/
+package org.eclipse.pde.internal.ui.bndtools;
+
+import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.pde.core.target.ITargetDefinition;
+import org.eclipse.pde.core.target.ITargetLocation;
+import org.eclipse.pde.ui.target.ITargetLocationWizard;
+
+public class RepositoryTargetLocationWizard extends Wizard implements ITargetLocationWizard {
+	private ITargetDefinition				targetDefinition;
+	private RepositoryTargetLocation		targetLocation;
+
+	private RepositoryTargetLocationPage	targetLocationPage;
+
+	public RepositoryTargetLocationWizard() {
+		setWindowTitle("Bnd Repository Target Location");
+	}
+
+	@Override
+	public void setTarget(ITargetDefinition targetDefinition) {
+		this.targetDefinition = targetDefinition;
+	}
+
+	public void setTargetLocation(RepositoryTargetLocation targetLocation) {
+		this.targetLocation = targetLocation;
+	}
+
+	@Override
+	public void addPages() {
+		targetLocationPage = new RepositoryTargetLocationPage(targetDefinition, targetLocation);
+		addPage(targetLocationPage);
+	}
+
+	@Override
+	public boolean performFinish() {
+		targetLocation = targetLocationPage.getBundleContainer();
+		return true;
+	}
+
+	@Override
+	public ITargetLocation[] getLocations() {
+		return new ITargetLocation[] {
+			targetLocation
+		};
+	}
+}
