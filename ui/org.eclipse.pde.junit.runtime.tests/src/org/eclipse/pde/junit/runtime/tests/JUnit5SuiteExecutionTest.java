d43c014b5a6810f69dce627e9bce35e481ffe277
Hannes Wellmann <wellmann.hannes1@gmx.net>
2024-05-19 16:10:24 +0200

    Clean-up and simplify PDE JUnit Runtime tests

--- ui/org.eclipse.pde.junit.runtime.tests/src/org/eclipse/pde/junit/runtime/tests/JUnit5SuiteExecutionTest.java
+++ ui/org.eclipse.pde.junit.runtime.tests/src/org/eclipse/pde/junit/runtime/tests/JUnit5SuiteExecutionTest.java
@@ -14,76 +14,74 @@
 package org.eclipse.pde.junit.runtime.tests;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.eclipse.pde.junit.runtime.tests.JUnitExecutionTest.findType;
+
+import java.util.StringJoiner;
 
 import org.eclipse.core.runtime.Platform;
+import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.internal.junit.model.TestElement;
 import org.eclipse.jdt.junit.model.ITestElement;
 import org.eclipse.jdt.junit.model.ITestElementContainer;
 import org.eclipse.jdt.junit.model.ITestRunSession;
-import org.eclipse.pde.junit.runtime.tests.JUnitExecutionTest.TestInput;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class JUnit5SuiteExecutionTest {
 
-	private static TestInput input;
+	private static IJavaProject project;
 
 	@BeforeClass
 	public static void setupProjects() throws Exception {
 		Assert.assertNotNull("junit-platform-suite-engine bundle missing", Platform.getBundle("junit-platform-suite-engine"));
 		Assert.assertNotNull("org.eclipse.jdt.junit5.runtime bundle missing", Platform.getBundle("org.eclipse.jdt.junit5.runtime"));
-			
+
 		JUnitExecutionTest.setupProjects();
-		input = new TestInput("JUnit5 Suite", "verification.tests.junit5.suite");
+		project = JUnitExecutionTest.getJProject("verification.tests.junit5.suite");
 	}
 
 	@Test
 	public void executeSuite() throws Exception {
-		ITestRunSession session = TestExecutionUtil.runTest(input.findType("TestSuite"));
-
+		ITestRunSession session = TestExecutionUtil.runTest(findType(project, "TestSuite"));
 		JUnitExecutionTest.assertSuccessful(session);
-		Assert.assertEquals(
-				"verification.tests.junit5.suite.TestSuite\n"
-				+ "  JUnit Jupiter\n"
-				+ "    verification.tests.junit5.Test1\n"
-				+ "      test1(verification.tests.junit5.Test1)\n"
-				+ "      test2(verification.tests.junit5.Test1)\n"
-				+ "    verification.tests.junit5.Test2\n"
-				+ "      test(verification.tests.junit5.Test2)\n",
-				toString(session));
+		Assert.assertEquals("""
+				verification.tests.junit5.suite.TestSuite
+				  JUnit Jupiter
+				    verification.tests.junit5.Test1
+				      test1(verification.tests.junit5.Test1)
+				      test2(verification.tests.junit5.Test1)
+				    verification.tests.junit5.Test2
+				      test(verification.tests.junit5.Test2)
+				""".strip(), toString(session).strip());
 	}
 
 	@Test
 	public void executePackage() throws Exception {
-		ITestRunSession session = TestExecutionUtil.runTest(input.findType("TestSuite").getPackageFragment());
+		ITestRunSession session = TestExecutionUtil.runTest(findType(project, "TestSuite").getPackageFragment());
 		JUnitExecutionTest.assertSuccessful(session);
 		assertThat(session.getChildren()).isNotEmpty();
 	}
 
 	@Test
 	public void executeProject() throws Exception {
-		ITestRunSession session = TestExecutionUtil.runTest(input.project);
+		ITestRunSession session = TestExecutionUtil.runTest(project);
 		JUnitExecutionTest.assertSuccessful(session);
 		assertThat(session.getChildren()).isNotEmpty();
 	}
 
 	private static String toString(ITestRunSession session) {
-		StringBuilder sb = new StringBuilder();
+		StringJoiner sb = new StringJoiner("\n");
 		for (ITestElement element : session.getChildren()) {
 			append(sb, element, 0);
 		}
-
 		return sb.toString();
 	}
 
-	private static void append(StringBuilder sb, ITestElement element, int indent) {
-		sb.append("  ".repeat(indent));
-		sb.append(((TestElement) element).getTestName());
-		sb.append('\n');
-
-		if (element instanceof ITestElementContainer) {
-			for (ITestElement child : ((ITestElementContainer) element).getChildren()) {
+	private static void append(StringJoiner sb, ITestElement element, int indent) {
+		sb.add("  ".repeat(indent) + ((TestElement) element).getTestName());
+		if (element instanceof ITestElementContainer container) {
+			for (ITestElement child : container.getChildren()) {
 				append(sb, child, indent + 1);
 			}
 		}
