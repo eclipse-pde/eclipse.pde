0d51f20ab3b227291da8390936c01fb17c1688c1
Александър Куртаков <akurtakov@gmail.com>
2024-12-18 15:23:19 +0200

    Stop using deprecated Thread.getId
    Use Thread.threadId instead.

--- ui/org.eclipse.pde.ui.tests/src/org/eclipse/pde/ui/tests/runtime/TestUtils.java
+++ ui/org.eclipse.pde.ui.tests/src/org/eclipse/pde/ui/tests/runtime/TestUtils.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2021 IBM Corporation and others.
+ * Copyright (c) 2008, 2024 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -201,7 +201,7 @@ public class TestUtils {
 			Thread thread = job.getThread();
 			if (thread != null) {
 				ThreadInfo[] threadInfos = ManagementFactory.getThreadMXBean()
-						.getThreadInfo(new long[] { thread.getId() }, true, true);
+						.getThreadInfo(new long[] { thread.threadId() }, true, true);
 				if (threadInfos[0] != null) {
 					sb.append("\nthread info: ").append(threadInfos[0]);
 				}
