eclipse-pde/eclipse.pde

commit 34b8e38ef25242a4992f61c8676569a8dcffc7f7

Bug 578946 - Avoid generic-wildcards if possible
Change-Id: I08ac337985055aeacbb38c1b5f4713dbc990190d
Signed-off-by: Hannes Wellmann <wellmann.hannes1@gmx.net>
Reviewed-on: https://git.eclipse.org/r/c/pde/eclipse.pde.ui/+/191166
Tested-by: PDE Bot <pde-bot@eclipse.org>

ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/editor/plugin/rows/IdAttributeRow.java
@@ -46,30 +46,30 @@ public class IdAttributeRow extends ButtonAttributeRow {
 		@Override
 		public String getText(Object element) {
 			if (element instanceof Map.Entry) {
-				Map.Entry<?, ?> entry = (Entry<?, ?>) element;
-				String text = (String) entry.getKey();
-				if (entry.getValue() instanceof IConfigurationElement) {
-					IConfigurationElement value = (IConfigurationElement) entry.getValue();
-					String name = value.getAttribute("name"); //$NON-NLS-1$
+				@SuppressWarnings("unchecked") // filled from corresponding map
+				Entry<String, IConfigurationElement> entry = (Entry<String, IConfigurationElement>) element;
+				String text = entry.getKey();
+				IConfigurationElement value = entry.getValue();
+				String name = value.getAttribute("name"); //$NON-NLS-1$
+				if (name == null) {
+					name = value.getAttribute("label"); //$NON-NLS-1$
 					if (name == null) {
-						name = value.getAttribute("label"); //$NON-NLS-1$
-						if (name == null) {
-							name = value.getAttribute("description"); //$NON-NLS-1$
-						}
+						name = value.getAttribute("description"); //$NON-NLS-1$
 					}
+				}
 
-					String contributor = value.getContributor().getName();
+				String contributor = value.getContributor().getName();
 
-					if (input != null && name != null && name.startsWith("%") && contributor != null) { //$NON-NLS-1$
-						IPluginModelBase model = PluginRegistry.findModel(contributor);
-						name = model.getResourceString(name);
-					}
+				if (input != null && name != null && name.startsWith("%") && contributor != null) { //$NON-NLS-1$
+					IPluginModelBase model = PluginRegistry.findModel(contributor);
+					name = model.getResourceString(name);
+				}
 
-					if (name != null) {
-						text += " - " + name; //$NON-NLS-1$
-					}
-					if (contributor != null)
-						text += " [" + contributor + "]"; //$NON-NLS-1$ //$NON-NLS-2$
+				if (name != null) {
+					text += " - " + name; //$NON-NLS-1$
+				}
+				if (contributor != null) {
+					text += " [" + contributor + "]"; //$NON-NLS-1$ //$NON-NLS-2$
 				}
 				return text;
 			}
@@ -97,8 +97,9 @@ public class IdAttributeRow extends ButtonAttributeRow {
 		dialog.setElements(attributeMap.entrySet().toArray());
 		dialog.setFilter("*"); //$NON-NLS-1$
 		if (dialog.open() == Window.OK) {
-			Map.Entry<?, ?> entry = (Entry<?, ?>) dialog.getFirstResult();
-			text.setText(entry.getKey().toString());
+			@SuppressWarnings({ "unchecked", "rawtypes" })
+			Map.Entry<String, IConfigurationElement> entry = (Entry) dialog.getFirstResult();
+			text.setText(entry.getKey());
 		}
 	}
 

commit 37821b0e0af5484b292d5e7eb1c83af9c5f8b8cc

Resolve NPE when Ids don't have associated configuration elements
This fixes a regression introduced in 34b8e38ef25242a4992f61c8676569a8dcffc7f7
When that commit was done some instanceof checks were removed.
The result of removing the instanceof checks had the side effect
of removing the implicit null check that instanceof essentially
does.

This change restores the null check for when IConfigurationElement
is null.

Fixes https://github.com/eclipse-pde/eclipse.pde/issues/1608

ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/editor/plugin/rows/IdAttributeRow.java
@@ -53,26 +53,28 @@ public class IdAttributeRow extends ButtonAttributeRow {
 				Entry<String, IConfigurationElement> entry = (Entry<String, IConfigurationElement>) element;
 				String text = entry.getKey();
 				IConfigurationElement value = entry.getValue();
-				String name = value.getAttribute("name"); //$NON-NLS-1$
-				if (name == null) {
-					name = value.getAttribute("label"); //$NON-NLS-1$
+				if (value != null) {
+					String name = value.getAttribute("name"); //$NON-NLS-1$
 					if (name == null) {
-						name = value.getAttribute("description"); //$NON-NLS-1$
+						name = value.getAttribute("label"); //$NON-NLS-1$
+						if (name == null) {
+							name = value.getAttribute("description"); //$NON-NLS-1$
+						}
 					}
-				}
 
-				String contributor = value.getContributor().getName();
+					String contributor = value.getContributor().getName();
 
-				if (input != null && name != null && name.startsWith("%") && contributor != null) { //$NON-NLS-1$
-					IPluginModelBase model = PluginRegistry.findModel(contributor);
-					name = model.getResourceString(name);
-				}
+					if (input != null && name != null && name.startsWith("%") && contributor != null) { //$NON-NLS-1$
+						IPluginModelBase model = PluginRegistry.findModel(contributor);
+						name = model.getResourceString(name);
+					}
 
-				if (name != null) {
-					text += " - " + name; //$NON-NLS-1$
-				}
-				if (contributor != null) {
-					text += " [" + contributor + "]"; //$NON-NLS-1$ //$NON-NLS-2$
+					if (name != null) {
+						text += " - " + name; //$NON-NLS-1$
+					}
+					if (contributor != null) {
+						text += " [" + contributor + "]"; //$NON-NLS-1$ //$NON-NLS-2$
+					}
 				}
 				return text;
 			}