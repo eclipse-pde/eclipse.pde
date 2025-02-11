eclipse-pde/eclipse.pde

commit 34b8e38ef25242a4992f61c8676569a8dcffc7f7

Bug 578946 - Avoid generic-wildcards if possible
Change-Id: I08ac337985055aeacbb38c1b5f4713dbc990190d
Signed-off-by: Hannes Wellmann <wellmann.hannes1@gmx.net>
Reviewed-on: https://git.eclipse.org/r/c/pde/eclipse.pde.ui/+/191166
Tested-by: PDE Bot <pde-bot@eclipse.org>

apitools/org.eclipse.pde.api.tools/src/org/eclipse/pde/api/tools/internal/util/Signatures.java
@@ -610,7 +610,7 @@ public final class Signatures {
 					ASTNode parent = type.getParent();
 					while (parent != null) {
 						try {
-							List<?> typeParameters = null;
+							List<TypeParameter> typeParameters = null;
 							if (parent.getNodeType() == ASTNode.METHOD_DECLARATION) {
 								MethodDeclaration md = (MethodDeclaration) parent;
 								typeParameters = md.typeParameters();
@@ -619,19 +619,16 @@ public final class Signatures {
 								typeParameters = td.typeParameters();
 							}
 							if (typeParameters != null) {
-								for (Object o : typeParameters) {
-									if (o instanceof TypeParameter) {
-										TypeParameter typeParameter = (TypeParameter) o;
-										if (fullyQualifiedName.equals(typeParameter.getName().getFullyQualifiedName())) {
-											if (typeParameter.typeBounds().isEmpty()) {
-												return Signature.createTypeSignature("Object", false); //$NON-NLS-1$
-											} else {
-												// the erasure of a type
-												// variable is the erasure of
-												// its leftmost bound
-												Type bound = (Type) typeParameter.typeBounds().get(0);
-												return getTypeSignature(bound, erased);
-											}
+								for (TypeParameter typeParameter : typeParameters) {
+									if (fullyQualifiedName.equals(typeParameter.getName().getFullyQualifiedName())) {
+										if (typeParameter.typeBounds().isEmpty()) {
+											return Signature.createTypeSignature("Object", false); //$NON-NLS-1$
+										} else {
+											// the erasure of a type
+											// variable is the erasure of
+											// its leftmost bound
+											Type bound = (Type) typeParameter.typeBounds().get(0);
+											return getTypeSignature(bound, erased);
 										}
 									}
 								}

ui/org.eclipse.pde.core/src/org/eclipse/pde/internal/core/target/P2TargetUtils.java
@@ -369,18 +369,20 @@ public class P2TargetUtils {
 	public static IProvisioningAgent getAgent() throws CoreException {
 		//Is there already an agent for this location?
 		String filter = "(locationURI=" + AGENT_LOCATION + ")"; //$NON-NLS-1$//$NON-NLS-2$
-		ServiceReference<?>[] serviceReferences = null;
+		ServiceReference<IProvisioningAgent> reference = null;
 		BundleContext context = PDECore.getDefault().getBundleContext();
 		try {
-			serviceReferences = context.getServiceReferences(IProvisioningAgent.SERVICE_NAME, filter);
-			if (serviceReferences != null) {
-				return (IProvisioningAgent) context.getService(serviceReferences[0]);
+			Collection<ServiceReference<IProvisioningAgent>> serviceReferences = context
+					.getServiceReferences(IProvisioningAgent.class, filter);
+			if (!serviceReferences.isEmpty()) {
+				reference = serviceReferences.iterator().next();
+				return context.getService(reference);
 			}
 		} catch (InvalidSyntaxException e) {
 			// ignore
 		} finally {
-			if (serviceReferences != null) {
-				context.ungetService(serviceReferences[0]);
+			if (reference != null) {
+				context.ungetService(reference);
 			}
 		}
 
@@ -580,11 +582,11 @@ public class P2TargetUtils {
 		// check top level IU's. If any have been removed from the containers that are
 		// still in the profile, we need to recreate (rather than uninstall)
 		IUProfilePropertyQuery propertyQuery = new IUProfilePropertyQuery(PROP_INSTALLED_IU, Boolean.toString(true));
-		IQueryResult<?> queryResult = fProfile.query(propertyQuery, null);
-		Iterator<?> iterator = queryResult.iterator();
+		IQueryResult<IInstallableUnit> queryResult = fProfile.query(propertyQuery, null);
+		Iterator<IInstallableUnit> iterator = queryResult.iterator();
 		Set<NameVersionDescriptor> installedIUs = new HashSet<>();
 		while (iterator.hasNext()) {
-			IInstallableUnit unit = (IInstallableUnit) iterator.next();
+			IInstallableUnit unit = iterator.next();
 			installedIUs.add(new NameVersionDescriptor(unit.getId(), unit.getVersion().toString()));
 		}
 		ITargetLocation[] containers = target.getTargetLocations();
@@ -784,7 +786,8 @@ public class P2TargetUtils {
 	 * @return the set of associated IUs
 	 * @throws CoreException if there is a problem discovering the IUs
 	 */
-	public static IQueryResult<?> getIUs(ITargetDefinition target, IProgressMonitor monitor) throws CoreException {
+	public static IQueryResult<IInstallableUnit> getIUs(ITargetDefinition target, IProgressMonitor monitor)
+			throws CoreException {
 		P2TargetUtils synchronizer = getSynchronizer(target);
 		if (synchronizer == null) {
 			return null;
@@ -1262,10 +1265,10 @@ public class P2TargetUtils {
 		}
 
 		// remove all units that are in the current profile but not in the new slice
-		Set<?> toRemove = fProfile.query(QueryUtil.ALL_UNITS, null).toSet();
+		Set<IInstallableUnit> toRemove = fProfile.query(QueryUtil.ALL_UNITS, null).toSet();
 		toRemove.removeAll(newSet);
-		for (Object name : toRemove) {
-			plan.removeInstallableUnit((IInstallableUnit) name);
+		for (IInstallableUnit name : toRemove) {
+			plan.removeInstallableUnit(name);
 		}
 
 		subMonitor.split(5);
@@ -1418,13 +1421,10 @@ public class P2TargetUtils {
 			URI dataArea = location.getDataArea("org.eclipse.equinox.p2.engine"); //$NON-NLS-1$
 			dataArea = URIUtil.append(dataArea, "profileRegistry/" + self.getProfileId() + ".profile"); //$NON-NLS-1$//$NON-NLS-2$
 			ProfileMetadataRepository profileRepo = new ProfileMetadataRepository(getGlobalAgent(), dataArea, null);
-			Collection<?> repos = profileRepo.getReferences();
-			for (Object element : repos) {
-				if (element instanceof IRepositoryReference) {
-					IRepositoryReference reference = (IRepositoryReference) element;
-					if (reference.getType() == IRepository.TYPE_ARTIFACT && reference.getLocation() != null) {
-						additionalRepos.add(reference.getLocation());
-					}
+			Collection<IRepositoryReference> repos = profileRepo.getReferences();
+			for (IRepositoryReference reference : repos) {
+				if (reference.getType() == IRepository.TYPE_ARTIFACT && reference.getLocation() != null) {
+					additionalRepos.add(reference.getLocation());
 				}
 			}
 		} catch (CoreException e) {
@@ -1507,7 +1507,7 @@ public class P2TargetUtils {
 
 			IArtifactRepositoryManager manager;
 			try {
-				Collection<?> toDownload = installableUnit.getArtifacts();
+				Collection<IArtifactKey> toDownload = installableUnit.getArtifacts();
 				if (toDownload == null) {
 					return Status.OK_STATUS;
 				}
@@ -1516,8 +1516,7 @@ public class P2TargetUtils {
 				List<IArtifactRequest> artifactRequests = (List<IArtifactRequest>) parameters.get(NATIVE_ARTIFACTS);
 				IArtifactRepository destinationArtifactRepository = getBundlePool();
 				manager = getArtifactRepositoryManager();
-				for (Object name : toDownload) {
-					IArtifactKey keyToDownload = (IArtifactKey) name;
+				for (IArtifactKey keyToDownload : toDownload) {
 					IArtifactRequest request = manager.createMirrorRequest(keyToDownload, destinationArtifactRepository, null, null);
 					artifactRequests.add(request);
 				}

commit 61884b8a4b99f57fb6fe668615b1df474adab564

Add potentially missing null checks
This fixes a potential regression introduced in
34b8e38ef25242a4992f61c8676569a8dcffc7f7 When that commit was
done some instanceof checks were removed.
The result of removing the instanceof checks had the side effect
of removing the implicit null check that instanceof essentially
does.

This change restores the null checks for all the instanceofs that
were removed in 34b8e38ef25242a4992f61c8676569a8dcffc7f7

apitools/org.eclipse.pde.api.tools/src/org/eclipse/pde/api/tools/internal/util/Signatures.java
@@ -584,15 +584,18 @@ public final class Signatures {
 							}
 							if (typeParameters != null) {
 								for (TypeParameter typeParameter : typeParameters) {
-									if (fullyQualifiedName.equals(typeParameter.getName().getFullyQualifiedName())) {
-										if (typeParameter.typeBounds().isEmpty()) {
-											yield Signature.createTypeSignature("Object", false); //$NON-NLS-1$
-										} else {
-											// the erasure of a type
-											// variable is the erasure of
-											// its leftmost bound
-											Type bound = (Type) typeParameter.typeBounds().get(0);
-											yield getTypeSignature(bound, erased);
+									if (typeParameter != null) {
+										if (fullyQualifiedName
+												.equals(typeParameter.getName().getFullyQualifiedName())) {
+											if (typeParameter.typeBounds().isEmpty()) {
+												yield Signature.createTypeSignature("Object", false); //$NON-NLS-1$
+											} else {
+												// the erasure of a type
+												// variable is the erasure of
+												// its leftmost bound
+												Type bound = (Type) typeParameter.typeBounds().get(0);
+												yield getTypeSignature(bound, erased);
+											}
 										}
 									}
 								}

ui/org.eclipse.pde.core/src/org/eclipse/pde/internal/core/target/P2TargetUtils.java
@@ -1554,8 +1554,10 @@ public class P2TargetUtils {
 			Collection<IRepositoryReference> repos = new org.eclipse.equinox.internal.p2.engine.ProfileMetadataRepository(
 					getGlobalAgent(), dataArea, null).getReferences();
 			for (IRepositoryReference reference : repos) {
-				if (reference.getType() == IRepository.TYPE_ARTIFACT && reference.getLocation() != null) {
-					additionalRepos.add(reference.getLocation());
+				if (reference != null) {
+					if (reference.getType() == IRepository.TYPE_ARTIFACT && reference.getLocation() != null) {
+						additionalRepos.add(reference.getLocation());
+					}
 				}
 			}
 		} catch (CoreException e) {