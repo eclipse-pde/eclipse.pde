14f66ae9e57d5f7a03973a41f01ef77a6f77c474
Patrick Ziegler <ziegler.patrick@hotmail.de>
2025-04-01 21:48:48 +0200

    Remove listeners in MinimalState if bundle is stopped
    This change adapts the listeners that have been added with
    24e9bb548b0de8c8ea79ee70d28096e28202da31 so that they are removed again,
    once the bundle is shut down and thus makes the classloader eligible for
    garbage collection.

--- ui/org.eclipse.pde.core/src/org/eclipse/pde/internal/core/PDECore.java
+++ ui/org.eclipse.pde.core/src/org/eclipse/pde/internal/core/PDECore.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2018 IBM Corporation and others.
+ * Copyright (c) 2000, 2025 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -424,6 +424,8 @@ public class PDECore extends Plugin implements DebugOptionsListener {
 		IWorkspace workspace = ResourcesPlugin.getWorkspace();
 		workspace.removeSaveParticipant(PLUGIN_ID);
 		workspace.removeResourceChangeListener(bndResourceChangeListener);
+
+		MinimalState.shutdown();
 	}
 
 	/**
