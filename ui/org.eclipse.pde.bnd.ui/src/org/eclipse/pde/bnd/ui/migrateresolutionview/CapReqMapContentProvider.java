6e6aa53c9ef0097c10b9de3150174cffaefa2e4b
Christoph Rueger <chrisrueger@gmail.com>
2025-05-07 09:51:00 +0200

    Initial bnd->PDE migration + License headers
    - This commit does not compile
    - it only serves the purpose of having all migrated files in one place +
    the license headers
    - later commits will put those files in different folders, do cleanup
    etc.

---
+++ ui/org.eclipse.pde.bnd.ui/src/org/eclipse/pde/bnd/ui/migrateresolutionview/CapReqMapContentProvider.java
@@ -0,0 +1,190 @@
+/*******************************************************************************
+ * Copyright (c) 2014, 2023 bndtools project and others.
+ *
+* This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Neil Bartlett <njbartlett@gmail.com> - initial API and implementation
+ *     BJ Hargrave <bj@hargrave.dev> - ongoing enhancements
+ *     Christoph Rueger <chrisrueger@gmail.com> - ongoing enhancements
+*******************************************************************************/
+package bndtools.model.resolution;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+import org.eclipse.jface.viewers.ITreeContentProvider;
+import org.eclipse.jface.viewers.Viewer;
+import org.osgi.framework.namespace.BundleNamespace;
+import org.osgi.framework.namespace.HostNamespace;
+import org.osgi.framework.namespace.IdentityNamespace;
+import org.osgi.framework.namespace.PackageNamespace;
+import org.osgi.resource.Capability;
+
+import aQute.bnd.unmodifiable.Sets;
+import aQute.libg.glob.Glob;
+
+public class CapReqMapContentProvider implements ITreeContentProvider {
+
+	private static final Object[]		EMPTY		= new Object[0];
+
+	private static final Set<String>	NAMESPACES	= Sets.of(BundleNamespace.BUNDLE_NAMESPACE,
+		IdentityNamespace.IDENTITY_NAMESPACE, HostNamespace.HOST_NAMESPACE, PackageNamespace.PACKAGE_NAMESPACE);
+
+	private final Comparator<Object>	comparator	= new CapReqComparator();
+
+	private String						wildcardFilter	= null;
+
+	@Override
+	public void dispose() {}
+
+	@Override
+	public void inputChanged(Viewer viewer, Object oldValue, Object newValue) {}
+
+	@Override
+	public Object[] getElements(Object input) {
+		List<Object[]> arrays = new LinkedList<>();
+
+		@SuppressWarnings("unchecked")
+		Map<String, List<Object>> map = (Map<String, List<Object>>) input;
+
+		// Add entries for our preferred ordering of namespaces
+		for (String namespace : NAMESPACES) {
+			List<Object> listForNs = map.get(namespace);
+			if (listForNs != null) {
+				Object[] array = listForNs.toArray();
+				Arrays.sort(array, comparator);
+				arrays.add(array);
+			}
+		}
+
+		// Now the rest in any order
+		for (Entry<String, List<Object>> entry : map.entrySet()) {
+			// Skip if namespace is a member of the namespaces we have already
+			// added.
+			if (NAMESPACES.contains(entry.getKey()))
+				continue;
+
+			List<Object> listForNs = entry.getValue();
+			Object[] array = listForNs.toArray();
+			Arrays.sort(array, comparator);
+			arrays.add(array);
+		}
+
+		return filter(flatten(arrays));
+	}
+
+	private Object[] flatten(List<Object[]> arrays) {
+		// Iterate over once to count the lengths
+		int length = 0;
+		for (Object[] array : arrays) {
+			length += array.length;
+		}
+		Object[] result = new Object[length];
+
+		// Iterate again to flatten out the arrays
+		int position = 0;
+		for (Object[] array : arrays) {
+			System.arraycopy(array, 0, result, position, array.length);
+			position += array.length;
+		}
+		return result;
+	}
+
+	@Override
+	public Object getParent(Object object) {
+		return null;
+	}
+
+	@Override
+	public boolean hasChildren(Object object) {
+		boolean children = false;
+
+		if (object instanceof RequirementWrapper) {
+			RequirementWrapper rw = (RequirementWrapper) object;
+			children = rw.requirers != null && !rw.requirers.isEmpty();
+		}
+
+		return children;
+	}
+
+	@Override
+	public Object[] getChildren(Object parent) {
+		Object[] result = EMPTY;
+		if (parent instanceof RequirementWrapper) {
+			Collection<? extends Object> requirers = ((RequirementWrapper) parent).requirers;
+			if (requirers != null)
+				result = requirers.toArray();
+		}
+		return result;
+	}
+
+	public void setFilter(String filterString) {
+		if (filterString == null || filterString.length() == 0 || filterString.trim()
+			.equals("*"))
+			wildcardFilter = null;
+		else
+			wildcardFilter = "*" + filterString.trim() + "*";
+
+	}
+
+	private Object[] filter(Object[] array) {
+		List<Object> filteredResults = new ArrayList<>();
+		if (wildcardFilter == null || wildcardFilter.equals("*") || wildcardFilter.equals("")) {
+			return array;
+		} else {
+			String[] split = wildcardFilter.split("\\s+");
+			Glob globs[] = new Glob[split.length];
+			for (int i = 0; i < split.length; i++) {
+				globs[i] = new Glob(split[i].toLowerCase());
+			}
+
+			// parallel search
+			Arrays.stream(array).parallel().forEach(obj -> {
+
+				if (obj instanceof RequirementWrapper rw) {
+
+					for (Glob g : globs) {
+							if (g.matcher(RequirementWrapperLabelProvider.tooltipText(rw)
+								.toLowerCase())
+							.find()) {
+							filteredResults.add(obj);
+							return;
+						}
+					}
+				}
+				else if (obj instanceof Capability cap) {
+
+					for (Glob g : globs) {
+							if (g.matcher(CapabilityLabelProvider.tooltipText(cap)
+								.toLowerCase())
+							.find()) {
+							filteredResults.add(obj);
+							return;
+						}
+					}
+				}
+
+			});
+
+		}
+
+		// sort because parallel search causes random ordering
+		filteredResults.sort(comparator);
+
+		return filteredResults.toArray();
+	}
+
+}
---
+++ ui/org.eclipse.pde.bnd.ui/src/org/eclipse/pde/bnd/ui/migrateresolutionview/SelectionUtils.java
@@ -0,0 +1,77 @@
+/*******************************************************************************
+ * Copyright (c) 2010, 2019 bndtools project and others.
+ *
+* This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Neil Bartlett <njbartlett@gmail.com> - initial API and implementation
+ *     BJ Hargrave <bj@hargrave.dev> - ongoing enhancements
+*******************************************************************************/
+package bndtools.utils;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.osgi.util.function.Predicate;
+
+public class SelectionUtils {
+
+	public static <T> Collection<T> getSelectionMembers(ISelection selection, Class<T> clazz) throws Exception {
+		return getSelectionMembers(selection, clazz, null);
+	}
+
+	public static <T> T adaptObject(Object obj, Class<T> clazz) {
+		if (clazz.isInstance(obj)) {
+			@SuppressWarnings("unchecked")
+			T result = (T) obj;
+			return result;
+		}
+
+		if (obj instanceof IAdaptable) {
+			T result = ((IAdaptable) obj).getAdapter(clazz);
+			return result;
+		}
+
+		return null;
+	}
+
+	public static <T> Collection<T> getSelectionMembers(ISelection selection, Class<T> clazz,
+		Predicate<? super T> filter) throws Exception {
+		if (selection.isEmpty() || !(selection instanceof IStructuredSelection)) {
+			return Collections.emptyList();
+		}
+
+		IStructuredSelection structSel = (IStructuredSelection) selection;
+		List<T> result = new ArrayList<>(structSel.size());
+		Iterator<?> iter = structSel.iterator();
+		while (iter.hasNext()) {
+			Object element = iter.next();
+			if (clazz.isInstance(element)) {
+				@SuppressWarnings("unchecked")
+				T casted = (T) element;
+				if (filter == null || filter.test(casted)) {
+					result.add(casted);
+				}
+			} else if (element instanceof IAdaptable) {
+				T adapted = ((IAdaptable) element).getAdapter(clazz);
+				if (adapted != null) {
+					if (filter == null || filter.test(adapted)) {
+						result.add(adapted);
+					}
+				}
+			}
+		}
+		return result;
+	}
+}
