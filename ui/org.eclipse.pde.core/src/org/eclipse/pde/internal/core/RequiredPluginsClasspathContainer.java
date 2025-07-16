b0b103f7bcb5de29568d61351d056f28d01c8352
Mehmet Emin Karaman <mehmet.karaman@advantest.com>
2025-07-16 17:20:10 +0200

    Problem during restore of ClassFileEditor
    - Changed the RequiredPluginsClasspathContainer interface from
    IClasspathContainer to IAsyncClasspathContainer
    
    See https://github.com/eclipse-jdt/eclipse.jdt.ui/issues/2245

--- ui/org.eclipse.pde.core/src/org/eclipse/pde/internal/core/RequiredPluginsClasspathContainer.java
+++ ui/org.eclipse.pde.core/src/org/eclipse/pde/internal/core/RequiredPluginsClasspathContainer.java
@@ -42,8 +42,8 @@ import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.core.runtime.IExtensionRegistry;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Platform;
+import org.eclipse.jdt.core.IAsyncClasspathContainer;
 import org.eclipse.jdt.core.IClasspathAttribute;
-import org.eclipse.jdt.core.IClasspathContainer;
 import org.eclipse.jdt.core.IClasspathEntry;
 import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.osgi.service.resolver.BaseDescription;
@@ -71,7 +71,8 @@ import aQute.bnd.build.Project;
 import aQute.bnd.build.Workspace;
 import aQute.bnd.osgi.Constants;
 
-public class RequiredPluginsClasspathContainer extends PDEClasspathContainer implements IClasspathContainer {
+public class RequiredPluginsClasspathContainer extends PDEClasspathContainer
+		implements IAsyncClasspathContainer {
 
 	@SuppressWarnings("nls")
 	private static final Set<String> JUNIT5_RUNTIME_PLUGINS = Set.of("org.junit", //
@@ -747,5 +748,15 @@ public class RequiredPluginsClasspathContainer extends PDEClasspathContainer imp
 		} finally {
 			addImportedPackages = false;
 		}
+	}
+
+	@Override
+	public boolean isInitialized() {
+		return fEntries != null;
+	}
+
+	@Override
+	public void initialize() {
+		getClasspathEntries();
 	}
 }
