eclipse-pde/eclipse.pde

commit 4d596adf4a3fa9c723ce833dfec2c92731bc4bd9

Use JFace's DecorationOverlayIcon
And remove the internal slightly more complicated copies providing same
functionality.

ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/util/SharedLabelProvider.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- *  Copyright (c) 2000, 2019 IBM Corporation and others.
+ *  Copyright (c) 2000, 2025 IBM Corporation and others.
  *
  *  This program and the accompanying materials
  *  are made available under the terms of the Eclipse Public License 2.0
@@ -26,6 +26,7 @@ import java.util.zip.ZipFile;
 
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jface.resource.ResourceLocator;
+import org.eclipse.jface.viewers.DecorationOverlayIcon;
 import org.eclipse.jface.viewers.ITableLabelProvider;
 import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.pde.core.plugin.IPluginModelBase;
@@ -121,11 +122,12 @@ public class SharedLabelProvider extends LabelProvider implements ITableLabelPro
 		if (flags == 0) {
 			return baseDesc.createImage();
 		}
-		ImageDescriptor[] lowerLeft = getLowerLeftOverlays(flags);
-		ImageDescriptor[] upperRight = getUpperRightOverlays(flags);
-		ImageDescriptor[] lowerRight = getLowerRightOverlays(flags);
-		ImageDescriptor[] upperLeft = getUpperLeftOverlays(flags);
-		OverlayIcon compDesc = new OverlayIcon(baseDesc, new ImageDescriptor[][] {upperRight, lowerRight, lowerLeft, upperLeft});
+		ImageDescriptor lowerLeft = getLowerLeftOverlays(flags);
+		ImageDescriptor upperRight = getUpperRightOverlays(flags);
+		ImageDescriptor lowerRight = getLowerRightOverlays(flags);
+		ImageDescriptor upperLeft = getUpperLeftOverlays(flags);
+		DecorationOverlayIcon compDesc = new DecorationOverlayIcon(baseDesc.createImage(),
+				new ImageDescriptor[] { upperRight, lowerRight, lowerLeft, upperLeft });
 		return compDesc.createImage();
 	}
 
@@ -133,62 +135,63 @@ public class SharedLabelProvider extends LabelProvider implements ITableLabelPro
 		if (flags == 0) {
 			return baseImage;
 		}
-		ImageDescriptor[] lowerLeft = getLowerLeftOverlays(flags);
-		ImageDescriptor[] upperRight = getUpperRightOverlays(flags);
-		ImageDescriptor[] lowerRight = getLowerRightOverlays(flags);
-		ImageDescriptor[] upperLeft = getUpperLeftOverlays(flags);
-		ImageOverlayIcon compDesc = new ImageOverlayIcon(baseImage, new ImageDescriptor[][] {upperRight, lowerRight, lowerLeft, upperLeft});
+		ImageDescriptor lowerLeft = getLowerLeftOverlays(flags);
+		ImageDescriptor upperRight = getUpperRightOverlays(flags);
+		ImageDescriptor lowerRight = getLowerRightOverlays(flags);
+		ImageDescriptor upperLeft = getUpperLeftOverlays(flags);
+		DecorationOverlayIcon compDesc = new DecorationOverlayIcon(baseImage,
+				new ImageDescriptor[] { upperRight, lowerRight, lowerLeft, upperLeft });
 		return compDesc.createImage();
 	}
 
-	private ImageDescriptor[] getLowerLeftOverlays(int flags) {
+	private ImageDescriptor getLowerLeftOverlays(int flags) {
 		if ((flags & F_ERROR) != 0) {
-			return new ImageDescriptor[] {PDEPluginImages.DESC_ERROR_CO};
+			return PDEPluginImages.DESC_ERROR_CO;
 		}
 		if ((flags & F_WARNING) != 0) {
-			return new ImageDescriptor[] {PDEPluginImages.DESC_WARNING_CO};
+			return PDEPluginImages.DESC_WARNING_CO;
 		}
 		return null;
 	}
 
-	private ImageDescriptor[] getUpperRightOverlays(int flags) {
+	private ImageDescriptor getUpperRightOverlays(int flags) {
 		if ((flags & F_EXPORT) != 0) {
-			return new ImageDescriptor[] {PDEPluginImages.DESC_EXPORT_CO};
+			return PDEPluginImages.DESC_EXPORT_CO;
 		}
 		if ((flags & F_EDIT) != 0) {
-			return new ImageDescriptor[] {PDEPluginImages.DESC_DOC_CO};
+			return PDEPluginImages.DESC_DOC_CO;
 		}
 		if ((flags & F_JAVA) != 0) {
-			return new ImageDescriptor[] {PDEPluginImages.DESC_JAVA_CO};
+			return PDEPluginImages.DESC_JAVA_CO;
 		}
 		return null;
 	}
 
-	private ImageDescriptor[] getLowerRightOverlays(int flags) {
+	private ImageDescriptor getLowerRightOverlays(int flags) {
 		if ((flags & F_JAR) != 0) {
-			return new ImageDescriptor[] {PDEPluginImages.DESC_JAR_CO};
+			return PDEPluginImages.DESC_JAR_CO;
 		}
 		if ((flags & F_PROJECT) != 0) {
-			return new ImageDescriptor[] {PDEPluginImages.DESC_PROJECT_CO};
+			return PDEPluginImages.DESC_PROJECT_CO;
 		}
 		if ((flags & F_OPTIONAL) != 0) {
-			return new ImageDescriptor[] {PDEPluginImages.DESC_OPTIONAL_CO};
+			return PDEPluginImages.DESC_OPTIONAL_CO;
 		}
 		if ((flags & F_INTERNAL) != 0) {
-			return new ImageDescriptor[] {PDEPluginImages.DESC_INTERNAL_CO};
+			return PDEPluginImages.DESC_INTERNAL_CO;
 		}
 		if ((flags & F_FRIEND) != 0) {
-			return new ImageDescriptor[] {PDEPluginImages.DESC_FRIEND_CO};
+			return PDEPluginImages.DESC_FRIEND_CO;
 		}
 		return null;
 	}
 
-	private ImageDescriptor[] getUpperLeftOverlays(int flags) {
+	private ImageDescriptor getUpperLeftOverlays(int flags) {
 		if ((flags & F_EXTERNAL) != 0) {
-			return new ImageDescriptor[] {PDEPluginImages.DESC_EXTERNAL_CO};
+			return PDEPluginImages.DESC_EXTERNAL_CO;
 		}
 		if ((flags & F_BINARY) != 0) {
-			return new ImageDescriptor[] {PDEPluginImages.DESC_BINARY_CO};
+			return PDEPluginImages.DESC_BINARY_CO;
 		}
 		return null;
 	}

ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/editor/contentassist/ManifestContentAssistProcessor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2021 IBM Corporation and others.
+ * Copyright (c) 2006, 2025 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -34,7 +34,6 @@ import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.core.search.IJavaSearchConstants;
 import org.eclipse.jdt.launching.JavaRuntime;
 import org.eclipse.jdt.launching.environments.IExecutionEnvironment;
-import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.IRegion;
@@ -42,6 +41,8 @@ import org.eclipse.jface.text.ITextViewer;
 import org.eclipse.jface.text.contentassist.ContentAssistEvent;
 import org.eclipse.jface.text.contentassist.ICompletionListener;
 import org.eclipse.jface.text.contentassist.ICompletionProposal;
+import org.eclipse.jface.viewers.DecorationOverlayIcon;
+import org.eclipse.jface.viewers.IDecoration;
 import org.eclipse.osgi.service.resolver.BundleDescription;
 import org.eclipse.osgi.service.resolver.ExportPackageDescription;
 import org.eclipse.osgi.util.ManifestElement;
@@ -54,7 +55,6 @@ import org.eclipse.pde.internal.ui.PDEPluginImages;
 import org.eclipse.pde.internal.ui.editor.PDEFormEditor;
 import org.eclipse.pde.internal.ui.editor.PDESourcePage;
 import org.eclipse.pde.internal.ui.editor.plugin.ManifestEditor;
-import org.eclipse.pde.internal.ui.util.ImageOverlayIcon;
 import org.eclipse.pde.internal.ui.util.PDEJavaHelperUI;
 import org.eclipse.swt.graphics.Image;
 import org.osgi.framework.BundleException;
@@ -885,7 +885,8 @@ public class ManifestContentAssistProcessor extends TypePackageCompletionProcess
 						return fImages[type] = PDEPluginImages.DESC_ATT_URI_OBJ.createImage();
 					case F_TYPE_DIRECTIVE :
 						fImages[F_TYPE_ATTRIBUTE] = PDEPluginImages.DESC_ATT_URI_OBJ.createImage();
-						ImageOverlayIcon icon = new ImageOverlayIcon(fImages[F_TYPE_ATTRIBUTE], new ImageDescriptor[][] {new ImageDescriptor[] {PDEPluginImages.DESC_DOC_CO}, null, null, null});
+						DecorationOverlayIcon icon = new DecorationOverlayIcon(fImages[F_TYPE_ATTRIBUTE],
+								PDEPluginImages.DESC_DOC_CO, IDecoration.TOP_RIGHT);
 						return fImages[type] = icon.createImage();
 					case F_TYPE_EXEC_ENV :
 						return fImages[type] = PDEPluginImages.DESC_JAVA_LIB_OBJ.createImage();

ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/util/AbstractOverlayIcon.java
@@ -1,135 +1,0 @@
-/*******************************************************************************
- *  Copyright (c) 2000, 2015 IBM Corporation and others.
- *
- *  This program and the accompanying materials
- *  are made available under the terms of the Eclipse Public License 2.0
- *  which accompanies this distribution, and is available at
- *  https://www.eclipse.org/legal/epl-2.0/
- *
- *  SPDX-License-Identifier: EPL-2.0
- *
- *  Contributors:
- *     IBM Corporation - initial API and implementation
- *******************************************************************************/
-package org.eclipse.pde.internal.ui.util;
-
-import org.eclipse.jface.resource.CompositeImageDescriptor;
-import org.eclipse.jface.resource.ImageDescriptor;
-import org.eclipse.swt.graphics.ImageData;
-import org.eclipse.swt.graphics.Point;
-
-/**
- * An OverlayIcon consists of a main icon and several adornments.
- */
-public abstract class AbstractOverlayIcon extends CompositeImageDescriptor {
-
-	static final int DEFAULT_WIDTH = 16;
-	static final int DEFAULT_HEIGHT = 16;
-
-	private Point fSize = null;
-
-	private final ImageDescriptor fOverlays[][];
-
-	public AbstractOverlayIcon(ImageDescriptor[][] overlays) {
-		this(overlays, null);
-	}
-
-	public AbstractOverlayIcon(ImageDescriptor[][] overlays, Point size) {
-		fOverlays = overlays;
-		if (size != null) {
-			fSize = size;
-		} else {
-			fSize = new Point(DEFAULT_WIDTH, DEFAULT_HEIGHT);
-		}
-	}
-
-	protected void drawBottomLeft(ImageDescriptor[] overlays) {
-		if (overlays == null) {
-			return;
-		}
-		int length = overlays.length;
-		int x = 0;
-		for (int i = 0; i < 3; i++) {
-			if (i < length && overlays[i] != null) {
-				ImageData id = overlays[i].getImageData();
-				drawImage(id, x, getSize().y - id.height);
-				x += id.width;
-			}
-		}
-	}
-
-	protected void drawBottomRight(ImageDescriptor[] overlays) {
-		if (overlays == null) {
-			return;
-		}
-		int length = overlays.length;
-		int x = getSize().x;
-		for (int i = 2; i >= 0; i--) {
-			if (i < length && overlays[i] != null) {
-				ImageData id = overlays[i].getImageData();
-				x -= id.width;
-				drawImage(id, x, getSize().y - id.height);
-			}
-		}
-	}
-
-	protected abstract ImageData getBaseImageData();
-
-	@Override
-	protected void drawCompositeImage(int width, int height) {
-		ImageData base = getBaseImageData();
-		drawImage(base, 0, 0);
-		if (fOverlays != null) {
-			if (fOverlays.length > 0) {
-				drawTopRight(fOverlays[0]);
-			}
-
-			if (fOverlays.length > 1) {
-				drawBottomRight(fOverlays[1]);
-			}
-
-			if (fOverlays.length > 2) {
-				drawBottomLeft(fOverlays[2]);
-			}
-
-			if (fOverlays.length > 3) {
-				drawTopLeft(fOverlays[3]);
-			}
-		}
-	}
-
-	protected void drawTopLeft(ImageDescriptor[] overlays) {
-		if (overlays == null) {
-			return;
-		}
-		int length = overlays.length;
-		int x = 0;
-		for (int i = 0; i < 3; i++) {
-			if (i < length && overlays[i] != null) {
-				ImageData id = overlays[i].getImageData();
-				drawImage(id, x, 0);
-				x += id.width;
-			}
-		}
-	}
-
-	protected void drawTopRight(ImageDescriptor[] overlays) {
-		if (overlays == null) {
-			return;
-		}
-		int length = overlays.length;
-		int x = getSize().x;
-		for (int i = 2; i >= 0; i--) {
-			if (i < length && overlays[i] != null) {
-				ImageData id = overlays[i].getImageData();
-				x -= id.width;
-				drawImage(id, x, 0);
-			}
-		}
-	}
-
-	@Override
-	protected Point getSize() {
-		return fSize;
-	}
-}

ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/util/ImageOverlayIcon.java
@@ -1,40 +1,0 @@
-/*******************************************************************************
- *  Copyright (c) 2000, 2015 IBM Corporation and others.
- *
- *  This program and the accompanying materials
- *  are made available under the terms of the Eclipse Public License 2.0
- *  which accompanies this distribution, and is available at
- *  https://www.eclipse.org/legal/epl-2.0/
- *
- *  SPDX-License-Identifier: EPL-2.0
- *
- *  Contributors:
- *     IBM Corporation - initial API and implementation
- *******************************************************************************/
-package org.eclipse.pde.internal.ui.util;
-
-import org.eclipse.jface.resource.ImageDescriptor;
-import org.eclipse.swt.graphics.Image;
-import org.eclipse.swt.graphics.ImageData;
-import org.eclipse.swt.graphics.Point;
-
-/**
- * An OverlayIcon consists of a main icon and several adornments.
- */
-public class ImageOverlayIcon extends AbstractOverlayIcon {
-	private final Image base;
-
-	public ImageOverlayIcon(Image base, ImageDescriptor[][] overlays) {
-		this(base, overlays, null);
-	}
-
-	public ImageOverlayIcon(Image base, ImageDescriptor[][] overlays, Point size) {
-		super(overlays, size);
-		this.base = base;
-	}
-
-	@Override
-	protected ImageData getBaseImageData() {
-		return base.getImageData();
-	}
-}

ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/util/OverlayIcon.java
@@ -1,42 +1,0 @@
-/*******************************************************************************
- *  Copyright (c) 2000, 2015 IBM Corporation and others.
- *
- *  This program and the accompanying materials
- *  are made available under the terms of the Eclipse Public License 2.0
- *  which accompanies this distribution, and is available at
- *  https://www.eclipse.org/legal/epl-2.0/
- *
- *  SPDX-License-Identifier: EPL-2.0
- *
- *  Contributors:
- *     IBM Corporation - initial API and implementation
- *******************************************************************************/
-package org.eclipse.pde.internal.ui.util;
-
-import org.eclipse.jface.resource.ImageDescriptor;
-import org.eclipse.swt.graphics.ImageData;
-import org.eclipse.swt.graphics.Point;
-
-/**
- * An OverlayIcon consists of a main icon and several adornments.
- */
-public class OverlayIcon extends AbstractOverlayIcon {
-	private ImageDescriptor fBase;
-
-	public OverlayIcon(ImageDescriptor base, ImageDescriptor[][] overlays) {
-		this(base, overlays, null);
-	}
-
-	public OverlayIcon(ImageDescriptor base, ImageDescriptor[][] overlays, Point size) {
-		super(overlays, size);
-		fBase = base;
-		if (fBase == null) {
-			fBase = ImageDescriptor.getMissingImageDescriptor();
-		}
-	}
-
-	@Override
-	protected ImageData getBaseImageData() {
-		return fBase.getImageData();
-	}
-}

commit b28b3ed47f8e5586915f17c0f61f966f70d4e446

Fix SharedLabelProvider leaking base images
This is the fix for the regression introduced in 4d596ad while changing
from PDE own `OverlayIcon` to `DecorationOverlayIcon` from JFace.

The `SharedLabelProvider.createImage(ImageDescriptor baseDesc, int
flags)` which originally created only one `Image` instance for image
with decorations (which was properly stored & disposed if
`SharedLabelProvider` was disposed), now creates **two** `Image`
instances, one from which (for the base image) always leaks because
`DecorationOverlayIcon` never disposes base image.

Moved the responsibility to "remember" created images down to the code
that creates images, and added extra code to remember base image too.

Fixes https://github.com/eclipse-pde/eclipse.pde/issues/2211

ui/org.eclipse.pde.ui/src/org/eclipse/pde/internal/ui/util/SharedLabelProvider.java
@@ -53,7 +53,7 @@ public class SharedLabelProvider extends LabelProvider implements ITableLabelPro
 	private Image fBlankImage;
 
 	public SharedLabelProvider() {
-
+		super();
 	}
 
 	public void connect(Object consumer) {
@@ -95,8 +95,7 @@ public class SharedLabelProvider extends LabelProvider implements ITableLabelPro
 		}
 		Image image = images.get(key);
 		if (image == null) {
-			image = createImage(desc, flags);
-			images.put(key, image);
+			image = createImage(desc, flags, key);
 		}
 		return image;
 	}
@@ -108,8 +107,7 @@ public class SharedLabelProvider extends LabelProvider implements ITableLabelPro
 		String key = getKey(image.hashCode(), flags);
 		Image resultImage = images.get(key);
 		if (resultImage == null) {
-			resultImage = createImage(image, flags);
-			images.put(key, resultImage);
+			resultImage = createImage(image, flags, key);
 		}
 		return resultImage;
 	}
@@ -118,20 +116,32 @@ public class SharedLabelProvider extends LabelProvider implements ITableLabelPro
 		return ("" + hashCode) + ":" + flags; //$NON-NLS-1$ //$NON-NLS-2$
 	}
 
-	private Image createImage(ImageDescriptor baseDesc, int flags) {
+	private Image createImage(ImageDescriptor baseDesc, int flags, Object key) {
 		if (flags == 0) {
-			return baseDesc.createImage();
+			Image baseImage = baseDesc.createImage();
+			remember(baseImage, key);
+			return baseImage;
 		}
+		// Remember base image, so it can be disposed if SharedLabelProvider is
+		// disposed
+		Image baseImage = images.get(baseDesc);
+		if (baseImage == null) {
+			baseImage = baseDesc.createImage();
+			remember(baseImage, baseDesc);
+		}
+
 		ImageDescriptor lowerLeft = getLowerLeftOverlays(flags);
 		ImageDescriptor upperRight = getUpperRightOverlays(flags);
 		ImageDescriptor lowerRight = getLowerRightOverlays(flags);
 		ImageDescriptor upperLeft = getUpperLeftOverlays(flags);
-		DecorationOverlayIcon compDesc = new DecorationOverlayIcon(baseDesc.createImage(),
+		DecorationOverlayIcon compDesc = new DecorationOverlayIcon(baseImage,
 				new ImageDescriptor[] { upperRight, lowerRight, lowerLeft, upperLeft });
-		return compDesc.createImage();
+		Image image = compDesc.createImage();
+		remember(image, key);
+		return image;
 	}
 
-	private Image createImage(Image baseImage, int flags) {
+	private Image createImage(Image baseImage, int flags, Object key) {
 		if (flags == 0) {
 			return baseImage;
 		}
@@ -141,7 +151,18 @@ public class SharedLabelProvider extends LabelProvider implements ITableLabelPro
 		ImageDescriptor upperLeft = getUpperLeftOverlays(flags);
 		DecorationOverlayIcon compDesc = new DecorationOverlayIcon(baseImage,
 				new ImageDescriptor[] { upperRight, lowerRight, lowerLeft, upperLeft });
-		return compDesc.createImage();
+		Image image = compDesc.createImage();
+		remember(image, key);
+		return image;
+	}
+
+	private void remember(Image image, Object key) {
+		Image old = images.put(key, image);
+		if (old != null) {
+			// Should not happen, but if it does, dispose old image to prevent
+			// leaks
+			old.dispose();
+		}
 	}
 
 	private ImageDescriptor getLowerLeftOverlays(int flags) {