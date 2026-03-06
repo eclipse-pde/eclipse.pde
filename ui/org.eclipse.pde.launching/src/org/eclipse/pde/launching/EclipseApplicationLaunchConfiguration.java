d9e9bc87bd469f670814f690987eaa0fc51ae966
Elsa Zacharia <elsa.zacharia@ibm.com>
2026-03-06 19:31:37 +0530

    Option to add console argument in the launch configuration
    This commit adds an option in launch configuration to add -console
    argument in program arguments.
    
    Fixes : https://github.com/eclipse-pde/eclipse.pde/issues/1851

--- ui/org.eclipse.pde.launching/src/org/eclipse/pde/launching/EclipseApplicationLaunchConfiguration.java
+++ ui/org.eclipse.pde.launching/src/org/eclipse/pde/launching/EclipseApplicationLaunchConfiguration.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2024 IBM Corporation and others.
+ * Copyright (c) 2005, 2026 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -157,6 +157,9 @@ public class EclipseApplicationLaunchConfiguration extends AbstractPDELaunchConf
 				programArgs.add(0, "-showsplash"); //$NON-NLS-1$
 				programArgs.add(1, computeShowsplashArgument());
 			}
+		}
+		if (configuration.getAttribute(IPDELauncherConstants.ADD_CONSOLE, false)) {
+			programArgs.add("-console"); //$NON-NLS-1$
 		}
 		return programArgs.toArray(new String[programArgs.size()]);
 	}
