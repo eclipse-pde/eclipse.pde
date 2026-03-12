eclipse-pde/eclipse.pde

commit 7c51a07d69d79aace7b1b23b59132c8e0362d350

Version bump(s) for 4.40 stream

ui/org.eclipse.pde.ui.tests/META-INF/MANIFEST.MF
@@ -2,7 +2,7 @@ Manifest-Version: 1.0
 Bundle-ManifestVersion: 2
 Bundle-Name: PDE JUnit Tests
 Bundle-SymbolicName: org.eclipse.pde.ui.tests; singleton:=true
-Bundle-Version: 3.13.400.qualifier
+Bundle-Version: 3.13.500.qualifier
 Bundle-ClassPath: tests.jar
 Bundle-Vendor: Eclipse.org
 Require-Bundle: org.eclipse.pde.ui,

ui/org.eclipse.pde.ui.tests/pom.xml
@@ -18,7 +18,7 @@
     <relativePath>../../</relativePath>
   </parent>
   <artifactId>org.eclipse.pde.ui.tests</artifactId>
-  <version>3.13.400-SNAPSHOT</version>
+  <version>3.13.500-SNAPSHOT</version>
   <packaging>eclipse-test-plugin</packaging>
 
   <properties>

commit a8384dd0b0f67804c51bdb41c3f0e9c4a4d76c8b

Add reproducer for cyclic bundle dependencies caused by #2218

ui/org.eclipse.pde.ui.tests/src/org/eclipse/pde/core/tests/internal/classpath/ClasspathResolutionTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2020, 2023 Red Hat Inc. and others.
+ * Copyright (c) 2020, 2026 Red Hat Inc. and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -19,6 +19,7 @@ import static org.junit.Assert.fail;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
 import java.util.function.Predicate;
@@ -40,6 +41,7 @@ import org.eclipse.pde.internal.core.ClasspathComputer;
 import org.eclipse.pde.internal.core.MinimalState;
 import org.eclipse.pde.internal.core.PDECore;
 import org.eclipse.pde.internal.core.TargetPlatformHelper;
+import org.eclipse.pde.ui.tests.runtime.TestUtils;
 import org.eclipse.pde.ui.tests.util.ProjectUtils;
 import org.eclipse.pde.ui.tests.util.TargetPlatformUtil;
 import org.junit.BeforeClass;
@@ -49,6 +51,7 @@ import org.junit.Test;
 import org.junit.rules.TestRule;
 import org.mockito.Mockito;
 import org.osgi.framework.Bundle;
+import org.osgi.framework.Constants;
 import org.osgi.framework.FrameworkUtil;
 
 public class ClasspathResolutionTest {
@@ -144,6 +147,21 @@ public class ClasspathResolutionTest {
 			List<String> classpathEntries = getRequiredPluginContainerEntries(project);
 			assertThat(classpathEntries).noneMatch(filename -> filename.contains(jakartaAnnotationProviderBSN));
 		}
+	}
+
+	@Test
+	public void testRequiredPluginsViaCapabilityForFragment() throws CoreException {
+		ProjectUtils.createPluginProject("A", "1.0.0");
+		ProjectUtils.createPluginProject("capabilities.provider", "1.0.0",
+				Map.of(Constants.PROVIDE_CAPABILITY, "some.test.capability", Constants.REQUIRE_BUNDLE, "A"));
+		IProject projectConsumer = ProjectUtils.createPluginProject("capabilities.consumer", "1.0.0",
+				Map.of(Constants.REQUIRE_CAPABILITY, "some.test.capability", Constants.FRAGMENT_HOST, "A"));
+
+		TestUtils.waitForJobs("ClasspathResolutionTest.testRequiredPluginsViaCapabilityForFragment", 200, 30_000);
+
+		List<String> requiredPluginContainers = getRequiredPluginContainerEntries(projectConsumer);
+		assertThat(requiredPluginContainers).contains("A1_0_0");
+		assertThat(requiredPluginContainers).noneMatch(containerName -> containerName.contains("capabilities.provider"));
 	}
 
 	// --- utilitiy methods ---

ui/org.eclipse.pde.ui.tests/src/org/eclipse/pde/ui/tests/util/ProjectUtils.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2022 IBM Corporation and others.
+ * Copyright (c) 2008, 2026 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -217,6 +217,9 @@ public class ProjectUtils {
 						case Constants.EXPORT_PACKAGE -> setPackageExports(description, projectService, value);
 						case Constants.IMPORT_PACKAGE -> setPackageImports(description, projectService, value);
 						case Constants.REQUIRE_BUNDLE -> setRequiredBundles(description, projectService, value);
+						case Constants.FRAGMENT_HOST -> setFragmentHost(description, projectService, value);
+						case Constants.REQUIRE_CAPABILITY -> setRequiredCapability(description, projectService, value);
+						case Constants.PROVIDE_CAPABILITY -> setProvidedCapability(description, projectService, value);
 						default -> throw new IllegalArgumentException("Unsupported header: " + header);
 						}
 					});
@@ -254,6 +257,26 @@ public class ProjectUtils {
 			return projectService.newRequiredBundle(h.getValue(), bundleVersion, optional, reexport);
 		}).toArray(IRequiredBundleDescription[]::new);
 		project.setRequiredBundles(imports);
+	}
+
+	private static void setFragmentHost(IBundleProjectDescription project, IBundleProjectService projectService,
+			String value) {
+		var host = parseHeader(Constants.REQUIRE_BUNDLE, value, h -> {
+			VersionRange bundleVersion = Optional.ofNullable(h.getAttribute(Constants.BUNDLE_VERSION))
+					.map(VersionRange::valueOf).orElse(null);
+			return projectService.newHost(h.getValue(), bundleVersion);
+		}).findFirst().orElse(null);
+		project.setHost(host);
+	}
+
+	private static void setRequiredCapability(IBundleProjectDescription project, IBundleProjectService projectService,
+			String value) {
+		project.setHeader(Constants.REQUIRE_CAPABILITY, value);
+	}
+
+	private static void setProvidedCapability(IBundleProjectDescription project, IBundleProjectService projectService,
+			String value) {
+		project.setHeader(Constants.PROVIDE_CAPABILITY, value);
 	}
 
 	private static <T> Stream<T> parseHeader(String header, String value, Function<ManifestElement, T> parser) {